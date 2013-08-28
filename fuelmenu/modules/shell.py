b09b42e445755f5ba91e7498a92a5dbb71175096
Matthew Mosesohn <mmosesohn@mirantis.com>
2013-08-28 21:42:47 +0400

    added welcome logo and shell

---
+++ fuelmenu/modules/shell.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import urwid
+import urwid.raw_display
+import urwid.web_display
+from urwidwrapper import *
+import subprocess
+
+blank = urwid.Divider()
+
+
+class shell():
+  def __init__(self, parent):
+    self.name="Shell Login"
+    self.priority=99
+    self.visible=True
+    self.parent=parent
+    self.screen = self.screenUI()
+  def check(self):
+    #TODO: Ensure all params are filled out and sensible
+    return True
+
+  def start_shell(self, args):
+    self.parent.mainloop.screen.stop()
+    message="Type exit to return to the main UI."
+    subprocess.call("clear ; echo '%s';echo; bash" % message, shell=True)
+    self.parent.mainloop.screen.start()
+
+  def screenUI(self):
+    #Define your text labels, text fields, and buttons first
+    text1 = urwid.Text("Press the button below to enter a shell login.")
+    login_button = Button("Shell Login",self.start_shell)
+    #Build all of these into a list
+    listbox_content = [ text1, blank, login_button ]
+   
+    #Add everything into a ListBox and return it
+    screen = urwid.ListBox(urwid.SimpleListWalker(listbox_content))
+    return screen
+    

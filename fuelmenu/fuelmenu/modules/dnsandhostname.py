d6fd835325b5045bf5bc6fa08f23793f0c062547
Matthew Mosesohn <mmosesohn@mirantis.com>
2013-09-26 12:34:22 +0400

    dnsandhostname rebase fix

--- fuelmenu/fuelmenu/modules/dnsandhostname.py
+++ fuelmenu/fuelmenu/modules/dnsandhostname.py
@@ -238,3 +238,234 @@ class dnsandhostname(urwid.WidgetWrap):
         #if self.deployment == "post":
         #  self.updateCobbler(responses)
         #  services.restart("cobbler")
+
+#  def updateCobbler(self, params):
+#    patterns={
+#      'cblr_server'      : '^server: .*',
+#      'cblr_next_server' : '^next_server: .*',
+#      'mgmt_if'     : '^interface=.*',
+#      'domain'      : '^domain=.*',
+#      'server'      : '^server=.*',
+#      'dhcp-range'  : '^dhcp-range=',
+#      'dhcp-option' : '^dhcp-option=',
+#      'pxe-service' : '^pxe-service=(^,)',
+#      'dhcp-boot'   : '^dhcp-boot=([^,],{3}),'
+#      }
+    def cancel(self, button):
+        for index, fieldname in enumerate(fields):
+            if fieldname == "blank":
+                pass
+            else:
+                self.edits[index].set_edit_text(DEFAULTS[fieldname]['value'])
+
+    def load(self):
+        #Read in yaml
+        defaultsettings = Settings().read(self.parent.defaultsettingsfile)
+        oldsettings = defaultsettings
+        oldsettings.update(Settings().read(self.parent.settingsfile))
+
+        oldsettings = Settings().read(self.parent.settingsfile)
+        for setting in DEFAULTS.keys():
+            try:
+                if "/" in setting:
+                    part1, part2 = setting.split("/")
+                    DEFAULTS[setting]["value"] = oldsettings[part1][part2]
+                else:
+                    DEFAULTS[setting]["value"] = oldsettings[setting]
+            except:
+                log.warning("No setting named %s found." % setting)
+                continue
+        #Read hostname if it's already set
+        try:
+            import os
+            oldsettings["HOSTNAME"] = os.uname()[1]
+        except:
+            log.warning("Unable to look up system hostname")
+        return oldsettings
+
+    def save(self, responses):
+        ## Generic settings start ##
+        newsettings = dict()
+        for setting in responses.keys():
+            if "/" in setting:
+                part1, part2 = setting.split("/")
+                if part1 not in newsettings:
+                #We may not touch all settings, so copy oldsettings first
+                    newsettings[part1] = self.oldsettings[part1]
+                newsettings[part1][part2] = responses[setting]
+            else:
+                newsettings[setting] = responses[setting]
+        ## Generic settings end ##
+
+        #log.debug(str(newsettings))
+        Settings().write(newsettings, defaultsfile=self.parent.settingsfile,
+                         outfn="newsettings.yaml")
+        #Write naily.facts
+        factsettings = dict()
+        #log.debug(newsettings)
+        for key in newsettings.keys():
+            if key != "blank":
+                factsettings[key] = newsettings[key]
+        n = nailyfactersettings.NailyFacterSettings()
+        n.write(factsettings)
+
+        #Set oldsettings to reflect new settings
+        self.oldsettings = newsettings
+        #Update DEFAULTS
+        for index, fieldname in enumerate(fields):
+            if fieldname != "blank":
+                DEFAULTS[fieldname]['value'] = newsettings[fieldname]
+
+    def checkDNS(self, server):
+        #Note: Python's internal resolver caches negative answers.
+        #Therefore, we should call dig externally to be sure.
+
+        noout = open('/dev/null', 'w')
+        dns_works = subprocess.call(["dig", "+short", "+time=3",
+                                     "+retries=1",
+                                     DEFAULTS["TEST_DNS"]['value'],
+                                     "@%s" % server], stdout=noout,
+                                    stderr=noout)
+        if dns_works != 0:
+            return False
+        else:
+            return True
+
+    def getNetwork(self):
+        """Uses netifaces module to get addr, broadcast, netmask about
+           network interfaces"""
+        import netifaces
+        for iface in netifaces.interfaces():
+            if 'lo' in iface or 'vir' in iface:
+            #if 'lo' in iface or 'vir' in iface or 'vbox' in iface:
+                if iface != "virbr2-nic":
+                    continue
+            try:
+                self.netsettings.update({iface: netifaces.ifaddresses(
+                    iface)[netifaces.AF_INET][0]})
+                self.netsettings[iface]["onboot"] = "Yes"
+            except:
+                self.netsettings.update({iface: {"addr": "", "netmask": "",
+                                                 "onboot": "no"}})
+            self.netsettings[iface]['mac'] = netifaces.ifaddresses(
+                iface)[netifaces.AF_LINK][0]['addr']
+
+            #Set link state
+            try:
+                with open("/sys/class/net/%s/operstate" % iface) as f:
+                    content = f.readlines()
+                    self.netsettings[iface]["link"] = content[0].strip()
+            except:
+                self.netsettings[iface]["link"] = "unknown"
+            #Change unknown link state to up if interface has an IP
+            if self.netsettings[iface]["link"] == "unknown":
+                if self.netsettings[iface]["addr"] != "":
+                    self.netsettings[iface]["link"] = "up"
+
+            #Read bootproto from /etc/sysconfig/network-scripts/ifcfg-DEV
+            try:
+                with open("/etc/sysconfig/network-scripts/ifcfg-%s" % iface) \
+                        as fh:
+                    for line in fh:
+                        if re.match("^BOOTPROTO=", line):
+                            self.netsettings[
+                                iface]['bootproto'] = line.split('=').strip()
+                            break
+            except:
+            #Let's try checking for dhclient process running for this interface
+                if self.getDHCP(iface):
+                    self.netsettings[iface]['bootproto'] = "dhcp"
+                else:
+                    self.netsettings[iface]['bootproto'] = "none"
+
+    def getDHCP(self, iface):
+        """Returns True if the interface has a dhclient process running"""
+        import subprocess
+        noout = open('/dev/null', 'w')
+        dhclient_running = subprocess.call(
+            ["pgrep", "-f", "dhclient.*%s" % (iface)],
+            stdout=noout, stderr=noout)
+
+    def get_default_gateway_linux(self):
+        """Read the default gateway directly from /proc."""
+        with open("/proc/net/route") as fh:
+            for line in fh:
+                fields = line.strip().split()
+                if fields[1] != '00000000' or not int(fields[3], 16) & 2:
+                    continue
+
+                return socket.inet_ntoa(struct.pack("<L", int(fields[2], 16)))
+
+    def radioSelectIface(self, current, state, user_data=None):
+        """Update network details and display information"""
+        ### This makes no sense, but urwid returns the previous object.
+        ### The previous object has True state, which is wrong..
+        ### Somewhere in current.group a RadioButton is set to True.
+        ### Our quest is to find it.
+        for rb in current.group:
+            if rb.get_label() == current.get_label():
+                continue
+            if rb.base_widget.state is True:
+                self.activeiface = rb.base_widget.get_label()
+                break
+        self.gateway = self.get_default_gateway_linux()
+        self.getNetwork()
+        self.setNetworkDetails()
+        return
+
+    def refresh(self):
+        pass
+
+    def screenUI(self):
+        #Define your text labels, text fields, and buttons first
+        text1 = urwid.Text("DNS and hostname setup")
+        text2 = urwid.Text("Note: Leave External DNS blank if you do not have"
+                           " Internet access.")
+
+        self.edits = []
+        toolbar = self.parent.footer
+        for key in fields:
+        #for key, values in DEFAULTS.items():
+            #Example: key = hostname, label = Hostname, value = fuel-pm
+            if key == "blank":
+                self.edits.append(blank)
+            elif DEFAULTS[key]["value"] == "radio":
+                label = TextLabel(DEFAULTS[key]["label"])
+                choices = ChoicesGroup(self, ["Yes", "No"],
+                                       default_value="Yes",
+                                       fn=self.radioSelectExtIf)
+                self.edits.append(Columns([label, choices]))
+            else:
+                caption = DEFAULTS[key]["label"]
+                default = DEFAULTS[key]["value"]
+                tooltip = DEFAULTS[key]["tooltip"]
+                self.edits.append(
+                    TextField(key, caption, 23, default, tooltip, toolbar))
+
+        #Button to check
+        button_check = Button("Check", self.check)
+        #Button to revert to previously saved settings
+        button_cancel = Button("Cancel", self.cancel)
+        #Button to apply (and check again)
+        button_apply = Button("Apply", self.apply)
+
+        #Wrap buttons into Columns so it doesn't expand and look ugly
+        check_col = Columns([button_check, button_cancel,
+                             button_apply, ('weight', 2, blank)])
+
+        self.listbox_content = [text1, blank, text2, blank]
+        self.listbox_content.extend(self.edits)
+        self.listbox_content.append(blank)
+        self.listbox_content.append(check_col)
+
+        #Add listeners
+
+        #Build all of these into a list
+        #self.listbox_content = [ text1, blank, blank, edit1, edit2, \
+        #                    edit3, edit4, edit5, edit6, button_check ]
+
+        #Add everything into a ListBox and return it
+        self.listwalker = urwid.SimpleListWalker(self.listbox_content)
+        screen = urwid.ListBox(self.listwalker)
+        return screen
+

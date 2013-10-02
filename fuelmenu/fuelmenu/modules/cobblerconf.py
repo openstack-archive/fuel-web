a83576d97144ada7af675d3e8dd2aff0b62d0d6f
Matthew Mosesohn <mmosesohn@mirantis.com>
2013-10-02 16:38:20 +0400

    added copyright headers

--- fuelmenu/fuelmenu/modules/cobblerconf.py
+++ fuelmenu/fuelmenu/modules/cobblerconf.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
+# Copyright 2013 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
 import urwid
 import urwid.raw_display
@@ -10,7 +23,7 @@ import copy
 import socket
 import struct
 import netaddr
-import dhcp_checker.api
+import dhcp_checker.api, dhcp_checker.utils
 import netifaces
 import subprocess
 from fuelmenu.settings import *
@@ -123,8 +136,9 @@ Please wait...")
             #                '52:54:00:12:35:02', 'server_ip': '192.168.200.2',
             #                   'dport': 67, 'message': 'offer',
             #                   'gateway': '0.0.0.0'}]
-            dhcp_server_data = dhcp_checker.api.check_dhcp_on_eth(
-                self.activeiface, timeout=5)
+            with dhcp_checker.utils.IfaceState(self.activeiface) as iface:
+                dhcp_server_data = dhcp_checker.api.check_dhcp_on_eth(
+                    iface, timeout=5)
 
             num_dhcp = len(dhcp_server_data)
             if num_dhcp == 0:

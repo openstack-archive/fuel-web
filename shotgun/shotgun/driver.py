9cccfeab0afb5cea6e3a41a68f7b6462091b90f0
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2013-10-11 17:04:59 +0400

    Hot fix for shotgun
    Change-Id: Id4990dfc7cccb909801e5b3d277d3f2002e1d6f2

--- shotgun/shotgun/driver.py
+++ shotgun/shotgun/driver.py
@@ -202,8 +202,8 @@ class Command(Driver):
         execute("mkdir -p {0}".format(os.path.dirname(self.target_path)))
         with open(self.target_path, "w") as f:
             f.write("===== COMMAND =====: {0}\n".format(self.cmdname))
-            f.write("===== RETURN CODE =====: {0}\n".format(out.return_code))
+            f.write("===== RETURN CODE =====: {0}\n".format(str(out.return_code)))
             f.write("===== STDOUT =====:\n")
-            f.write(out.stdout)
+            f.write(str(out.stdout))
             f.write("\n===== STDERR =====:\n")
-            f.write(out.stderr)
+            f.write(str(out.stderr))

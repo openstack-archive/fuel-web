7d418d8319f32ac35f9a50db214672b73ab13746
Vitaly Kramskikh <vkramskikh@mirantis.com>
2013-10-29 15:21:29 +0400

    Fix UI build
    Change-Id: I5f7dc2c38c39e53d5a9eb2ccddf945d036f52deb

--- nailgun/static/js/models.js
+++ nailgun/static/js/models.js
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
 **/
-define(['utils'], function(utils) {
+define(['utils', 'deepModel'], function(utils) {
     'use strict';
 
     var models = {};

5a1341e20331415a90b4e2606589f9ae50c6e9f9
Aleksey Kasatkin <akasatkin@mirantis.com>
2013-11-07 11:41:24 +0200

    Deployment orchestrator refactoring 2nd step
    This PR is based on comments to the "1st step" mostly.
    
    Implements: blueprint nailgun-refactor-network-searilization-for-neutron
    
    Change-Id: I8023af7c8a6ae55bb5c3b92b04d0c295a6c7b832

---
+++ nailgun/nailgun/test/unit/test_utils.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2013 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nailgun.utils import dict_merge
+from nailgun.test.base import BaseIntegrationTest
+
+
+class TestUtils(BaseIntegrationTest):
+
+    def test_dict_merge(self):
+        custom = {"coord": [10, 10],
+                  "pass": "qwerty",
+                  "dict": {"body": "solid",
+                           "color": "black",
+                           "dict": {"stuff": "hz"}}}
+        common = {"parent": None,
+                  "dict": {"transparency": 100,
+                           "dict": {"another_stuff": "hz"}}}
+        result = dict_merge(custom, common)
+        self.assertEqual(result, {"coord": [10, 10],
+                                  "parent": None,
+                                  "pass": "qwerty",
+                                  "dict": {"body": "solid",
+                                           "color": "black",
+                                           "transparency": 100,
+                                           "dict": {"stuff": "hz",
+                                                    "another_stuff": "hz"}}})

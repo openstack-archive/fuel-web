b448dcdc84653916db91037ce2f625f3c2213dee
astepanchuk <astepanchuk@mirantis.com>
2013-11-28 11:58:00 +0200

    Grunt with Bower package manager
    Change-Id: I4ee64dcf625e20bca1f8dd51ba542d93f3cbd85c

--- nailgun/Gruntfile.js
+++ nailgun/Gruntfile.js
@@ -12,45 +12,64 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations
  * under the License.
-**/
+ **/
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    requirejs: {
-      compile: {
-        options: {
-          baseUrl: '.',
-          appDir: 'static',
-          dir: grunt.option('static-dir') || '/tmp/static_compressed',
-          mainConfigFile: 'static/js/main.js',
-          modules: [{name: 'js/main'}],
-          waitSeconds: 60,
-          optimize: 'uglify2',
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: ".",
+                    appDir: "static",
+                    dir: grunt.option('static-dir') || "/tmp/static_compressed",
+                    mainConfigFile: "static/js/main.js",
+                    modules: [{name: "js/main"}],
+                    waitSeconds: 60,
+                    optimize: "uglify2",
+                }
+            }
+        },
+        jslint: {
+            client: {
+                src: [
+                    'static/js/*.js',
+                    'static/js/views/*.js',
+                ],
+                directives: {
+                    predef: ['requirejs', 'require', 'define', 'app', 'Backbone', '$', '_'],
+                    ass: true,
+                    browser: true,
+                    unparam: true,
+                    nomen: true,
+                    eqeq: true,
+                    vars: true,
+                    white: true,
+                    es5: false
+                }
+            }
+        },
+        bower: {
+            install: {
+                options: {
+                    //set install to true to load Bower packages from inet
+                    install: true,
+                    targetDir: 'static/js/libs/bower',
+                    verbose: true,
+                    cleanTargetDir: false,
+                    cleanBowerDir: true,
+                    layout: "byComponent",
+                    bowerOptions: {
+                        production: true,
+                        install: '--offline'
+                    }
+                }
+            }
         }
-      }
-    },
-    jslint: {
-      client: {
-        src: [
-          'static/js/*.js',
-          'static/js/views/*.js',
-        ],
-        directives: {
-          predef: ['requirejs', 'require', 'define', 'app', 'Backbone', '$', '_'],
-          ass: true,
-          browser: true,
-          unparam: true,
-          nomen: true,
-          eqeq: true,
-          vars: true,
-          white: true,
-          es5: false
-        }
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
-  grunt.loadNpmTasks('grunt-jslint');
-  grunt.registerTask('build', ['requirejs']);
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-jslint');
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.registerTask('build', ['bower', 'requirejs']);
+    grunt.registerTask('default', ['build']);
 };

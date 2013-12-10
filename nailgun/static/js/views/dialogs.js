526565db47920a0c335ff3ad323f5f71a253a549
Oleg Balakirev <obalakirev@mirantis.com>
2013-12-10 16:46:13 +0400

    Ceilometer install checkbox returned to cluster installation wizard

--- nailgun/static/js/views/dialogs.js
+++ nailgun/static/js/views/dialogs.js
@@ -460,6 +460,7 @@ function(require, utils, models, simpleMessageTemplate, createClusterWizardTempl
                     additionalServices.savanna.value = this.$('input[name=savanna]').is(':checked');
                     additionalServices.murano.value = this.$('input[name=murano]').is(':checked');
                     additionalServices.heat.value = this.$('input[name=murano]').is(':checked');
+                    additionalServices.ceilometer.value = this.$('input[name=ceilometer]').is(':checked');
                 }
             } catch (e) {
                 return (new $.Deferred()).reject();

a4a5b7521d40b69b5aabb532dc655d56aad918e6
Kate Pimenova <kpimenova@mirantis.com>
2014-01-20 17:39:04 +0400

    Fencing tab layout
    Change-Id: Ie047129f4d916432546e62ad013eeb1cc81f652b

--- nailgun/static/js/views/cluster_page.js
+++ nailgun/static/js/views/cluster_page.js
@@ -25,12 +25,13 @@ define(
     'views/cluster_page_tabs/logs_tab',
     'views/cluster_page_tabs/actions_tab',
     'views/cluster_page_tabs/healthcheck_tab',
+    'views/cluster_page_tabs/fencing_tab',
     'text!templates/cluster/page.html',
     'text!templates/cluster/customization_message.html',
     'text!templates/cluster/deployment_result.html',
     'text!templates/cluster/deployment_control.html'
 ],
-function(utils, models, commonViews, dialogViews, NodesTab, NetworkTab, SettingsTab, LogsTab, ActionsTab, HealthCheckTab, clusterPageTemplate, clusterCustomizationMessageTemplate, deploymentResultTemplate, deploymentControlTemplate) {
+function(utils, models, commonViews, dialogViews, NodesTab, NetworkTab, SettingsTab, LogsTab, ActionsTab, HealthCheckTab, FencingTab, clusterPageTemplate, clusterCustomizationMessageTemplate, deploymentResultTemplate, deploymentControlTemplate) {
     'use strict';
     var ClusterPage, ClusterCustomizationMessage, DeploymentResult, DeploymentControl;
 
@@ -42,7 +43,7 @@ function(utils, models, commonViews, dialogViews, NodesTab, NetworkTab, Settings
         title: function() {
             return this.model.get('name');
         },
-        tabs: ['nodes', 'network', 'settings', 'logs', 'healthcheck', 'actions'],
+        tabs: ['nodes', 'network', 'settings', 'logs', 'healthcheck', 'actions', 'fencing'],
         updateInterval: 5000,
         template: _.template(clusterPageTemplate),
         events: {
@@ -225,7 +226,8 @@ function(utils, models, commonViews, dialogViews, NodesTab, NetworkTab, Settings
                 'settings': SettingsTab,
                 'actions': ActionsTab,
                 'logs': LogsTab,
-                'healthcheck': HealthCheckTab
+                'healthcheck': HealthCheckTab,
+                'fencing': FencingTab
             };
             if (_.has(tabs, this.activeTab)) {
                 this.tab = new tabs[this.activeTab]({model: this.model, tabOptions: this.tabOptions, page: this});

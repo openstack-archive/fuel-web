48a2c8c2c5a613c709b54a2cf1704186004ee9a4
Aleksey Kasatkin <akasatkin@mirantis.com>
2014-01-29 17:59:09 +0200

    Introduce fencing in Nailgun
    Change-Id: I67b33416918635b784593cc586421051a7a9c2de

---
+++ nailgun/nailgun/fixtures/fencing.py
@@ -0,0 +1,232 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+from nailgun.db.sqlalchemy.models import fencing
+
+
+fencing_metadata = {
+    "primitives_ui_parameters": {
+        "common": {
+            "parameters": ["ipaddr", "login", "passwd"]
+        },
+        "virsh": {
+            "type": "power",
+            "per_node_relation": 1,
+            "parameters": []
+        },
+        "ipmilan": {
+            "type": "remote",
+            "per_node_relation": 1,
+            "parameters": []
+        },
+        "ilo": {
+            "type": "remote",
+            "per_node_relation": 1,
+            "parameters": []
+        },
+        "apc": {
+            "type": "power",
+            "per_node_relation": 2,
+            "parameters": ["port", "secure"]
+        },
+        "apc_snmp": {
+            "type": "power",
+            "per_node_relation": 2,
+            "parameters": ["port", "snmp_version", "community",
+                           "snmp_sec_level", "snmp_auth_prot",
+                           "snmp_priv_prot", "snmp_priv_passwd"]
+        },
+        "drac5": {
+            "type": "remote",
+            "per_node_relation": 1,
+            "parameters": ["cmd_prompt", "drac_version", "module_name",
+                           "secure"]
+        }
+    },
+    "primitives_all_parameters": {
+        "common": {
+            "operations": {
+                "monitor": {
+                    "interval": "3600s",
+                    "timeout": "120s"
+                },
+                "start": {
+                    "interval": "0",
+                    "timeout": "120s",
+                    "on-fail": "restart"
+                },
+                "stop": {
+                    "interval": "0",
+                    "timeout": "1800s",
+                    "on-fail": "restart"
+                }
+            },
+            "meta": {
+                "migration-threshold": "5",
+                "failure-timeout": "180"
+            },
+            "parameters": {
+                "ipaddr": "",
+                "login": "",
+                "passwd": "",
+                "delay": "10",
+                "action": "off",
+                "pcmk_reboot_action": "off",
+                "pcmk_off_action": "off"
+            }
+        },
+        "virsh": {
+            "agent_type": "fence_virsh",
+            "parameters": {
+                "power_timeout": "30",
+                "shell_timeout": "10",
+                "login_timeout": "10",
+                "power_wait": "15",
+                "pcmk_host_argument": "",
+                "pcmk_host_map": ""
+            }
+        },
+        "ipmilan": {
+            "agent_type": "fence_ipmilan",
+            "parameters": {
+                "privlvl": "operator",
+                "auth": "password",
+                "power_wait": "15",
+                "pcmk_host_list": ""
+            }
+        },
+        "ilo": {
+            "agent_type": "fence_ipmilan",
+            "parameters": {
+                "privlvl": "operator",
+                "auth": "password",
+                "lanplus": "true",
+                "power_wait": "15",
+                "pcmk_host_list": ""
+            }
+        },
+        "apc": {
+            "agent_type": "fence_apc",
+            "parameters": {
+                "port": "",
+                "power_timeout": "30",
+                "shell_timeout": "20",
+                "login_timeout": "20",
+                "power_wait": "15",
+                "secure": "true",
+                "pcmk_host_list": ""
+            }
+        },
+        "apc_snmp": {
+            "agent_type": "fence_apc_snmp",
+            "parameters": {
+                "port": "",
+                "snmp_version": "3",
+                "community": "",
+                "snmp_sec_level": "noAuthNoPriv",
+                "snmp_auth_prot": "",
+                "snmp_priv_prot": "",
+                "snmp_priv_passwd": "",
+                "power_timeout": "30",
+                "shell_timeout": "20",
+                "login_timeout": "20",
+                "power_wait": "15",
+                "pcmk_host_list": ""
+            }
+        },
+        "drac5": {
+            "agent_type": "fence_drac5",
+            "parameters": {
+                "module_name": "",
+                "drac_version": "2",
+                "secure": "true",
+                "cmd_prompt": "\\$",
+                "power_timeout": "30",
+                "shell_timeout": "10",
+                "login_timeout": "10",
+                "power_wait": "15",
+                "pcmk_host_list": ""
+            }
+        }
+    },
+    "request_data_format": {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "title": "FencingConfiguration",
+        "description": "Fencing API request data format",
+        "type": "object",
+        "required": [
+            "policy",
+            "primitive_configuration"
+        ],
+        "properties": {
+            "policy": {
+                "type": "string",
+                "enum": list(fencing.FENCING_POLICIES)
+            },
+            "primitive_configuration": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "required": ["name", "node_primitive_configuration"],
+                    "properties": {
+                        "name": {
+                            "type": "string",
+                            "enum": list(fencing.FENCING_PRIM_NAMES)
+                        },
+                        "node_primitive_configuration": {
+                            "description": "",
+                            "type": "array",
+                            "items": {
+                                "type": "object",
+                                "required": ["node_id", "index", "parameters"],
+                                "properties": {
+                                    "node_id": {"type": "integer"},
+                                    "index": {"type": "integer"},
+                                    "parameters": {
+                                        "type": "object",
+                                        "required": ["ipaddr", "login",
+                                                     "passwd"],
+                                        "properties": {
+                                            "ipaddr": {"type": "string"},
+                                            "login": {"type": "string"},
+                                            "passwd": {"type": "string"},
+                                            "port": {"type": "string"},
+                                            "secure": {"type": "string"},
+                                            "snmp_version": {"type": "string"},
+                                            "community": {"type": "string"},
+                                            "snmp_sec_level": {
+                                                "type": "string"},
+                                            "snmp_auth_prot": {
+                                                "type": "string"},
+                                            "snmp_priv_prot": {
+                                                "type": "string"},
+                                            "snmp_priv_passwd": {
+                                                "type": "string"},
+                                            "cmd_prompt": {"type": "string"},
+                                            "drac_version": {"type": "string"},
+                                            "module_name": {"type": "string"}
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

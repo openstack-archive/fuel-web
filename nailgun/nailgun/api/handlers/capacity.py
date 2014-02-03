2240a0b61841449f22caeae47eca94e994442d50
Ivan Kolodyazhny <ikolodyazhny@mirantis.com>
2014-02-03 16:06:03 +0200

    Added unicode support for Capacity CSV Log
    Change-Id: If39748395db0c9aa8c3f0229c467d7470aaa5a08
    Closes: bug #1275066

--- nailgun/nailgun/api/handlers/capacity.py
+++ nailgun/nailgun/api/handlers/capacity.py
@@ -14,7 +14,9 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import cStringIO
 import csv
+import codecs
 from hashlib import md5
 import tempfile
 
@@ -31,6 +33,40 @@ from nailgun.task.manager import GenerateCapacityLogTaskManager
 """
 Capacity audit handlers
 """
+
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    Source: http://docs.python.org/2/library/csv.html#examples
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        # We have only string and int types in capacity log now.
+        # Don't need to convert int values to string for writhing it to file.
+        self.writer.writerow(
+            [s.encode("utf-8") if type(s) != int else s for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
 
 
 class CapacityLogHandler(BaseHandler):
@@ -75,8 +111,8 @@ class CapacityLogCsvHandler(BaseHandler):
 
         report = capacity_log.report
         f = tempfile.TemporaryFile(mode='r+b')
-        csv_file = csv.writer(f, delimiter=',',
-                              quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_file = UnicodeWriter(f, delimiter=',',
+                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
         csv_file.writerow(['Fuel version', report['fuel_data']['release']])
         csv_file.writerow(['Fuel UUID', report['fuel_data']['uuid']])

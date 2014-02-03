7014b1d329336f23b7ea6c63498d6dd5fd5136dc
Nikolay Markov <enchantner@gmail.com>
2014-02-03 14:16:59 +0400

    Implementation of stopping/resetting env in Nailgun
    Added implementation of environment resetting
    
    Implements: blueprint nailgun-reset-env
    
    Change-Id: Ib2c8d2f7f570ba4293f7930cf4837a7ec09e222b

---
+++ nailgun/nailgun/test/integration/test_stop_deployment.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2013 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nailgun.db import db
+from nailgun.db.sqlalchemy.models.task import Task
+
+from nailgun.test.base import BaseIntegrationTest
+from nailgun.test.base import fake_tasks
+
+
+class TestStopDeployment(BaseIntegrationTest):
+
+    def setUp(self):
+        super(TestStopDeployment, self).setUp()
+        self.env.create(
+            cluster_kwargs={},
+            nodes_kwargs=[
+                {"name": "First",
+                 "pending_addition": True},
+                {"name": "Second",
+                 "roles": ["compute"],
+                 "pending_addition": True}
+            ]
+        )
+        self.cluster = self.env.clusters[0]
+        self.controller = self.env.nodes[0]
+        self.compute = self.env.nodes[1]
+        self.node_uids = [n.uid for n in self.cluster.nodes][:3]
+
+    def tearDown(self):
+        self._wait_for_threads()
+        super(TestStopDeployment, self).tearDown()
+
+    @fake_tasks()
+    def test_stop_deployment(self):
+        supertask = self.env.launch_deployment()
+        deploy_task_uuid = supertask.uuid
+        stop_task = self.env.stop_deployment()
+        self.env.wait_ready(stop_task, 60)
+        self.assertIsNone(
+            db().query(Task).filter_by(
+                uuid=deploy_task_uuid
+            ).first()
+        )
+        self.assertEquals(self.cluster.status, "stopped")
+        self.assertEquals(stop_task.progress, 100)
+
+        for n in self.cluster.nodes:
+            self.assertEquals(n.online, False)
+            self.assertEquals(n.roles, [])
+            self.assertNotEquals(n.pending_roles, [])
+
+    @fake_tasks(tick_interval=3)
+    def test_stop_duplicate(self):
+        self.env.launch_deployment()
+        self.env.stop_deployment()
+        stop_task2_resp = self.env.stop_deployment(expect_http=409)
+        self.assertEquals(
+            stop_task2_resp,
+            "Stopping deployment task "
+            "is already launched"
+        )
+
+    @fake_tasks(tick_interval=3)
+    def test_stop_provisioning(self):
+        provisioning_task = self.env.launch_provisioning_selected(
+            self.node_uids
+        )
+        stop_task_resp = self.env.stop_deployment(
+            expect_http=400
+        )
+        self.db.refresh(provisioning_task)
+        self.assertEquals(
+            stop_task_resp,
+            u"Provisioning interruption for environment "
+            u"'{0}' is not implemented right now".format(
+                self.cluster.id
+            )
+        )

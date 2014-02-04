d50867b21c0bb4941bf471a5c32d3c64b3e674e2
Alexandra Stepanchuk <astepanchuk@mirantis.com>
2014-02-04 12:39:40 +0200

    Declarative wizard implementation
    Change-Id: Ie8a4b02fdc57d675d8934c15170637a2d5d88cd8
    Relates-to-bp: https://blueprints.launchpad.net/fuel/+spec/ui-declarative-wizard

--- nailgun/static/js/models.js
+++ nailgun/static/js/models.js
@@ -728,5 +728,24 @@ define(['utils', 'deepModel'], function(utils) {
         urlRoot: '/api/capacity'
     });
 
+    models.WizardPaneModel = Backbone.DeepModel.extend({
+        constructorName: 'WizardPanes',
+        initialize: function() {
+            this.attributesToTrack = [];
+            this.valuesToTrack = [];
+        },
+        stringifyKeys: function(config) {
+            function processPiece(base, piece) {
+                return _.map(piece, function(value, key) {
+                    var localBase = base ? base + '.' + key : key;
+                    return (_.isPlainObject(value)) ? processPiece(localBase, value) : [localBase, value];
+                });
+            }
+            var unifiedKeys = _.uniq(_.flatten(processPiece(null, config)));
+            this.attributesToTrack.push(unifiedKeys[0]);
+            this.valuesToTrack.push(unifiedKeys[1]);
+        }
+    });
+
     return models;
 });

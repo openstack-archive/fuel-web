7c9f919ba9dadb9155269ea2def0a15bb1280f8e
Andrew Woodward <awoodward@mirantis.com>
2014-02-07 11:03:08 -0800

    Reuse current DHCP address for admin interface
    Change admin address allocator to re-use node.ip as address for admin
     interface.
    
    This allows for substantially more usage of the admin network range sice it
     no longer needs to be divided in half to provde dedicated address range for
     DHCP and another range for admin addresses.
    
    Duplicate addresses are un-likely to occur as DHCP assigned the address in
     the first place so we can assume that we can re-use this address for the
     life of the Node
    
    Another change is needed in fuel-library and will be tracked shortly.
    
    Closes-bug: #1274595
    Closes-bug: #1271571
    Implments: blueprint get-gid-of-splitted-fueladmin-network
    
    Change-Id: I1353f46b0cb0e6285fd03bd43ff26a4cba52a71e

--- nailgun/nailgun/task/helpers.py
+++ nailgun/nailgun/task/helpers.py
@@ -466,7 +466,7 @@ class TaskHelper(object):
         """
         cls.update_slave_nodes_fqdn(nodes)
         for node in nodes:
-            NetworkManager.assign_admin_ips(node.id)
+            NetworkManager.assign_admin_ip(node.id)
 
     @classmethod
     def prepare_for_deployment(cls, nodes):
@@ -483,4 +483,4 @@ class TaskHelper(object):
             netmanager.assign_ips(nodes_ids, 'storage')
 
             for node in nodes:
-                netmanager.assign_admin_ips(node.id)
+                netmanager.assign_admin_ip(node.id)

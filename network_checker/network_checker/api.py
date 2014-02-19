065d6b1157e2fd9523b5b9286c921945ac1513a7
Dima Shulyak <dshulyak@mirantis.com>
2014-02-19 14:40:02 +0200

    Implement multicast group verification
    Provide api for assignment, receiving and sending multicast
    packages
    
    Change-Id: Ia4470d10ddc280b836fafd0490958d9ed7c1fce5
    Implements: blueprint l23-net-checker

---
+++ network_checker/network_checker/api.py
@@ -0,0 +1,100 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""Fuel network checker
+<check> should be multicast | dhcp | l2
+<command> should be listen | send | get_info
+<config> arbitrary set of jsonized parameters specific for each check
+
+Usage:
+  fuel-netcheck <check> <command> <config>
+  fuel-netcheck (-h | --help)
+  fuel-netcheck --version
+
+Examples:
+  fuel-netcheck multicast listen '{"node_id": "111", "group": "225.0.0.250",
+  "port": 8890}'
+  fuel-netcheck multicast send '{"node_id": "111", "group": "225.0.0.250",
+  "port": 8890}'
+  fuel-netcheck multicast get_info '{"node_id": "111", "group": "225.0.0.250",
+  "port": 8890}'
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+
+"""
+import json
+import multiprocessing
+import socket
+import sys
+import time
+
+
+from stevedore import driver
+
+from network_checker import server
+from network_checker import mcollective_action
+
+
+class Api(object):
+
+    def __init__(self, check, config):
+        self.manager = driver.DriverManager(
+            'network_checker', check,
+            invoke_on_load=True, invoke_kwds=config)
+        self.config = config
+        self.checker = self.manager.driver
+
+    def listen(self):
+        comm_server = server.CommunicationServer(self.checker)
+        proc = multiprocessing.Process(target=comm_server.daemonize_serve)
+        proc.start()
+        client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        client.connect('/tmp/{name}'.format(name=self.checker.name))
+        client.send('listen')
+        client.settimeout(5)
+        return client.recv(1024)
+
+    def send(self):
+        return self.checker.send()
+
+    def get_info(self):
+        client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        client.connect('/tmp/{name}'.format(name=self.checker.name))
+        client.send('get_info')
+        data = client.recv(1024)
+        return data
+
+
+def mcollective():
+
+    with mcollective_action.MCollectiveAction() as mc:
+        config = json.loads(mc.request['data']['config'])
+        command = mc.request['data']['command']
+        check = mc.request['data']['check']
+        api = Api(check, config)
+        result = getattr(api, command)()
+        mc.reply['message'] = result
+        mc.reply['timestamp'] = time.strftime("%c")
+
+
+def cli():
+    # cli is very usefull for debugging purposes
+    from docopt import docopt
+    arguments = docopt(__doc__, version='Fuel Network Checker 0.1')
+    config = json.loads(arguments['<config>'])
+    api = Api(arguments['<check>'], config)
+    result = getattr(api, arguments['<command>'])()
+    print(result)
+    sys.exit(0)

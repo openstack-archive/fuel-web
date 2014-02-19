bbf438bb906a976ced8e67c0bade1cd88e8db95d
Dima Shulyak <dshulyak@mirantis.com>
2014-02-19 14:40:02 +0200

    Implement multicast group verification
    Provide api for assignment, receiving and sending multicast
    packages
    
    Change-Id: Ia4470d10ddc280b836fafd0490958d9ed7c1fce5
    Implements: blueprint l23-net-checker

---
+++ network_checker/network_checker/tests/base.py
@@ -0,0 +1,37 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+import unittest
+
+
+class BaseMcollectActionTest(unittest.TestCase):
+
+    def setUp(self):
+        self.nailgun_uid_file = '/etc/nailgun_uid'
+        self.node_uid = '101'
+        with open(self.nailgun_uid_file, 'w') as f:
+            f.write(self.node_uid)
+        self.request_file = '/tmp/mco_request'
+        self.reply_file = '/tmp/mco_reply'
+        os.environ['MCOLLECTIVE_REPLY_FILE'] = self.reply_file
+        os.environ['MCOLLECTIVE_REQUEST_FILE'] = self.request_file
+
+    def tearDown(self):
+        if os.path.exists(self.request_file):
+            os.unlink(self.request_file)
+        if os.path.exists(self.reply_file):
+            os.unlink(self.reply_file)
+        if os.path.exists(self.nailgun_uid_file):
+            os.unlink(self.nailgun_uid_file)

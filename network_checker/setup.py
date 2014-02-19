2b7f37e058e3123224dbea3838e642261b1990a0
Dima Shulyak <dshulyak@mirantis.com>
2014-02-19 14:40:02 +0200

    Implement multicast group verification
    Provide api for assignment, receiving and sending multicast
    packages
    
    Change-Id: Ia4470d10ddc280b836fafd0490958d9ed7c1fce5
    Implements: blueprint l23-net-checker

--- network_checker/setup.py
+++ network_checker/setup.py
@@ -29,13 +29,21 @@ setuptools.setup(
         "Topic :: Software Development :: Testing"
     ],
     install_requires=[
-        'argparse'
+        'argparse',
+        'stevedore',
+        'daemonize'
     ],
     include_package_data=True,
-    packages=['net_check'],
+    packages=setuptools.find_packages(exclude=['tests']),
     entry_points={
         'console_scripts': [
-            'net_probe.py = net_check.api:main'
+            'net_probe.py = network_checker.net_check.api:main',
+            'fuel-netcheck = network_checker.api:cli',
+            'fuel-mco = network_checker.api:mcollective'
         ],
+        'network_checker': [
+            'multicast = network_checker.multicast.api:MulticastChecker',
+            'simple = network_checker.tests.simple:SimpleChecker'
+        ]
     },
 )

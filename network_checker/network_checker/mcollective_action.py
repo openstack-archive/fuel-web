3ca70afb1e4071ce582f49d5363e08c358eecb83
Dima Shulyak <dshulyak@mirantis.com>
2014-02-19 14:40:02 +0200

    Implement multicast group verification
    Provide api for assignment, receiving and sending multicast
    packages
    
    Change-Id: Ia4470d10ddc280b836fafd0490958d9ed7c1fce5
    Implements: blueprint l23-net-checker

---
+++ network_checker/network_checker/mcollective_action.py
@@ -0,0 +1,97 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import json
+import os
+import sys
+
+
+class MCollectiveActionNoEnv(Exception):
+    pass
+
+
+class MCollectiveActionFileError(Exception):
+    pass
+
+
+class MCollectiveAction(object):
+
+    def __init__(self, *args, **kwargs):
+        try:
+            self.infile = os.environ['MCOLLECTIVE_REQUEST_FILE']
+        except KeyError:
+            raise MCollectiveActionNoEnv('No MCOLLECTIVE_REQUEST_FILE'
+                                         ' environment variable')
+        try:
+            self.outfile = os.environ['MCOLLECTIVE_REPLY_FILE']
+        except KeyError:
+            raise MCollectiveActionNoEnv('No MCOLLECTIVE_REPLY_FILE'
+                                         ' environment variable')
+
+        self.request = {}
+        self.reply = {}
+
+        self.load()
+
+    def load(self):
+        if not self.infile:
+            return False
+        with open(self.infile, 'r') as infile:
+            try:
+                self.request = json.load(infile)
+            except IOError as e:
+                raise MCollectiveActionFileError(
+                    'Could not read request '
+                    'file `%s`: %s' % (self.infile, e))
+            except json.JSONDecodeError as e:
+                raise MCollectiveActionFileError(
+                    'Could not parse JSON data'
+                    ' in file `%s`: %s', (self.infile, e))
+
+    def send(self):
+        if not getattr(self, 'outfile', None):
+            return False
+        with open(self.outfile, 'w') as outfile:
+            try:
+                json.dump(self.reply, outfile)
+            except IOError as e:
+                raise MCollectiveActionFileError(
+                    "Could not write reply file `%s`: %s" % (self.outfile, e))
+
+    def error(self, msg):
+        """Prints line to STDERR that will be logged
+            at error level in the mcollectived log file
+        """
+        sys.stderr.write("%s\n" % msg)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_value is not None:
+            self.fail(str(exc_value))
+
+    def fail(self, msg):
+        """Logs error message and exitst with RPCAborted."""
+        self.error(msg)
+        sys.exit(1)
+
+    def info(self, msg):
+        """Prints line to STDOUT that will be logged
+            at info level in the mcollectived log file
+        """
+        sys.stdout.write("%s\n" % msg)
+
+    def __del__(self):
+        self.send()
---
+++ network_checker/network_checker/tests/test_mcollective_action.py
@@ -0,0 +1,68 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import json
+
+from network_checker import api
+from network_checker.tests import base
+
+
+class TestMcollectiveAction(base.BaseMcollectActionTest):
+
+    def setUp(self):
+        super(TestMcollectiveAction, self).setUp()
+        self.request = {'data': {
+            'config': {}, 'check': 'simple', 'command': None}}
+
+    def test_perform_start_action(self):
+        self.request['data']['command'] = 'listen'
+
+        with open(self.request_file, 'w') as f:
+            f.write(json.dumps(self.request))
+
+        api.mcollective()
+
+        with open(self.reply_file) as f:
+            data = json.loads(f.read())
+
+        self.assertIsInstance(data, dict)
+        self.assertEqual(data['status'], 'inprogress')
+
+    def test_perform_send_action(self):
+        self.request['data']['command'] = 'send'
+
+        with open(self.request_file, 'w') as f:
+            f.write(json.dumps(self.request))
+
+        api.mcollective()
+
+        with open(self.reply_file) as f:
+            data = json.loads(f.read())
+
+        self.assertIsInstance(data, dict)
+        self.assertEqual(data['status'], 'inprogress')
+
+    def test_perform_get_info_action(self):
+        self.request['data']['command'] = 'get_info'
+
+        with open(self.request_file, 'w') as f:
+            f.write(json.dumps(self.request))
+
+        api.mcollective()
+
+        with open(self.reply_file) as f:
+            data = json.loads(f.read())
+
+        self.assertIsInstance(data, dict)
+        self.assertEqual(data['status'], 'success')

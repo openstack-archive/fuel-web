d97a401771750571b2089c63476fbd32d1644f05
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2014-02-21 13:24:37 +0400

    Added uuid field into Node model
    It is necessary for integration with Ironic
    
    Change-Id: Ia2af75aaa2c92094dc4144e9059df6c7612f6cc8

--- nailgun/nailgun/api/handlers/node.py
+++ nailgun/nailgun/api/handlers/node.py
@@ -47,7 +47,7 @@ class NodeHandler(BaseHandler):
     fields = ('id', 'name', 'meta', 'progress', 'roles', 'pending_roles',
               'status', 'mac', 'fqdn', 'ip', 'manufacturer', 'platform_name',
               'pending_addition', 'pending_deletion', 'os_platform',
-              'error_type', 'online', 'cluster')
+              'error_type', 'online', 'cluster', 'uuid')
     model = Node
     validator = NodeValidator
 
@@ -157,7 +157,7 @@ class NodeCollectionHandler(BaseHandler):
     fields = ('id', 'name', 'meta', 'progress', 'roles', 'pending_roles',
               'status', 'mac', 'fqdn', 'ip', 'manufacturer', 'platform_name',
               'pending_addition', 'pending_deletion', 'os_platform',
-              'error_type', 'online', 'cluster')
+              'error_type', 'online', 'cluster', 'uuid')
 
     validator = NodeValidator
 

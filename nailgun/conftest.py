3c7e38dbf96cbf0bfdd625a58156843a8f7822a9
Dmitry Shulyak <dshulyak@mirantis.com>
2014-03-22 00:31:28 +0200

    Add parallel execution for nailgun tests:
    --dbname option can be specified
    
    py.test --dbname nailgun123 nailgun/test
    by default settings.DATABASE['name'] will be used
    
    from fuel-web/nailgun you should be able to run:
    py.test -n 4 --boxed nailgun/test
    this command will run tests in 4 subprocess
    with 4 database for each subprocess, e.g nailgun_gw0, nailgun_gw1
    
    requirements:
    1. conftest.py should be in current working directory
    2. nailgun db user should be able to create db
    3. py.test should be always started with --boxed option
    to run slave workers by subprocess, not multiprocessing
    
    Change-Id: Ie5a24d970f73ef35b1c4e61bbc4b2a738d96b34f
    Closes-Bug: #1282565

---
+++ nailgun/conftest.py
@@ -0,0 +1,39 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+from nailgun.settings import settings
+
+
+def pytest_addoption(parser):
+    parser.addoption("--dbname", default=settings.DATABASE['name'],
+                     help="Overwrite database name")
+
+
+def pytest_configure(config):
+    db_name = config.getoption('dbname')
+    if hasattr(config, 'slaveinput'):
+        slaveid = config.slaveinput['slaveid']
+        db_name = '{0}_{1}'.format(db_name, slaveid)
+        os.environ['PGUSER'] = settings.DATABASE['user']
+        os.environ['PGHOST'] = settings.DATABASE['host']
+        os.environ['PGPASSWORD'] = settings.DATABASE['passwd']
+        if not_present(db_name):
+            os.system("psql -c 'create database {0}'".format(db_name))
+    settings.DATABASE['name'] = db_name
+
+
+def not_present(name):
+    return os.system("psql -l | grep -o '^ {0}'".format(name))

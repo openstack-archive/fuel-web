40622debbccb7f2cdb94605b3bfeb2ff3b5802ee
Evgeniy L <eli@mirantis.com>
2014-03-21 15:24:58 +0400

    Created command line interface 'fuel_upgrade' for master node upgrading
    This script will be delivired with update file.
    
    Responsibility of the script:
    * run backup
    * run upgrade
    * run rollback in case of errors
    * check that update deployed correctly
    
    Related to blueprint fuel-upgrade
    
    Change-Id: Id4daf45a32d5f3fd3e2efb4c5767d85d9c03c58c

---
+++ fuel_upgrade_system/fuel_upgrade/fuel_upgrade/cli.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from fuel_upgrade.logger import configure_logger
+# TODO (eli): move to config
+logger = configure_logger('/tmp/file.log')
+
+
+import argparse
+import sys
+import traceback
+
+from fuel_upgrade import errors
+from fuel_upgrade.upgrade import PuppetUpgrader
+from fuel_upgrade.upgrade import Upgrade
+
+
+def handle_exception(exc):
+    if isinstance(exc, errors.FuelUpgradeException):
+        err_msg = str(exc)
+        logger.error(err_msg)
+        sys.exit(-1)
+    else:
+        traceback.print_exc(exc)
+        sys.exit(-1)
+
+
+def parse_args():
+    """Parse arguments and return them
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--src',
+        help='path to update file',
+        required=True)
+
+    parser.add_argument(
+        '--dst',
+        help='working directory',
+        required=True)
+
+    parser.add_argument(
+        '--disable_rollback',
+        help='disable rollabck in case of errors',
+        action='store_false')
+
+    return parser.parse_args()
+
+
+def run_upgrade(args):
+    """Run upgrade on master node
+    """
+    upgrader = Upgrade(
+        args.src,
+        args.dst,
+        PuppetUpgrader(args.src),
+        disable_rollback=args.disable_rollback)
+
+    upgrader.run()
+
+
+def main():
+    """Entry point
+    """
+    try:
+        run_upgrade(parse_args())
+    except Exception as exc:
+        handle_exception(exc)
---
+++ fuel_upgrade_system/fuel_upgrade/setup.py
@@ -0,0 +1,48 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+import os.path
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def find_requires():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    requirements = []
+    with open('{0}/requirements.txt'.format(dir_path), 'r') as reqs:
+        requirements = reqs.readlines()
+    return requirements
+
+
+if __name__ == "__main__":
+    setup(name='fuel_upgrade',
+          version='0.1.0',
+          description='Upgrade system for Fuel-master node',
+          long_description="""Upgrade system for Fuel-master node""",
+          classifiers=[
+              "Programming Language :: Python",
+              "Topic :: System :: Software Distribution"],
+          author='Mirantis Inc.',
+          author_email='product@mirantis.com',
+          url='http://mirantis.com',
+          keywords='fuel upgrade mirantis',
+          packages=find_packages(),
+          zip_safe=False,
+          install_requires=find_requires(),
+          include_package_data=True,
+          entry_points={
+              'console_scripts': [
+                  'fuel-upgrade = fuel_upgrade.cli:main']})

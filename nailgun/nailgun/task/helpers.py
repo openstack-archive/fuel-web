42658056847ec49eebc4d93fdaf6da1a81a44dc9
Dmitry Nikishov <dnikishov@mirantis.com>
2014-04-07 10:12:17 +0400

    Automatic nodes removal from zabbix
    Change-Id: Ieb41279713940a28a5aaed7295500ffd7882237a
    Implements: blueprint monitoring-system

--- nailgun/nailgun/task/helpers.py
+++ nailgun/nailgun/task/helpers.py
@@ -14,14 +14,17 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import json
 import os
 import shutil
+import urllib2
 
 from sqlalchemy import or_
 
 from nailgun import objects
 
 from nailgun.db import db
+from nailgun.db.sqlalchemy.models import Attributes
 from nailgun.db.sqlalchemy.models import IPAddr
 from nailgun.db.sqlalchemy.models import Node
 from nailgun.db.sqlalchemy.models import Task
@@ -484,3 +487,100 @@ class TaskHelper(object):
 
             for node in nodes:
                 netmanager.assign_admin_ips(node.id)
+
+
+class ZabbixHelper(TaskHelper):
+
+    @classmethod
+    def _make_zabbix_request(cls, url, method, params, auth=None):
+        header = {'Content-type': 'application/json'}
+        data = {'jsonrpc': '2.0',
+                'id': '1',
+                'method': method,
+                'params': params}
+        if auth:
+            data['auth'] = auth
+
+        logger.debug("Zabbix request: %s", data)
+        request = urllib2.Request(url, json.dumps(data), header)
+
+        try:
+            response = urllib2.urlopen(request)
+        except urllib2.URLError as e:
+            raise errors.CannotMakeZabbixRequest(
+                "Can't make a request to Zabbix: {0}".format(e)
+            )
+            return None
+
+        result = json.loads(response.read())
+        logger.debug("Zabbix response: %s", result)
+
+        if 'error' in result:
+            code = result['error']['code']
+            msg = result['error']['message']
+            data = result['error']['data']
+            raise errors.ZabbixRequestError(
+                "Zabbix returned error code {0}, {1}: {2}".format(
+                    code, msg, data
+                )
+            )
+            return None
+
+        return result['result']
+
+    @classmethod
+    def _zabbix_auth(cls, url, user, password):
+        method = 'user.authenticate'
+        params = {'user': user,
+                  'password': password}
+        auth_hash = cls._make_zabbix_request(url, method, params)
+        return auth_hash
+
+    @classmethod
+    def _get_zabbix_hostid(cls, url, auth, name):
+        method = 'host.get'
+        params = {'filter': {'host': name}}
+        result = cls._make_zabbix_request(url, method, params, auth=auth)
+        if isinstance(result, list) and not result:
+            logger.info("Host %s does not exist in zabbix, skipping", name)
+            return None
+
+        return result[0]['hostid']
+
+    @classmethod
+    def remove_from_zabbix(cls, zabbix, nodes):
+        url = zabbix['url']
+        user = zabbix['user']
+        password = zabbix['password']
+        auth = cls._zabbix_auth(url, user, password)
+
+        if not auth:
+            return None
+
+        hostids = []
+        method = "host.delete"
+
+        for node in nodes:
+            name = cls.make_slave_fqdn(node['id'])
+            hostid = cls._get_zabbix_hostid(url, auth, name)
+            if hostid:
+                hostids.append(hostid)
+
+        if hostids:
+            cls._make_zabbix_request(url, method, hostids, auth=auth)
+
+    @classmethod
+    def parse_cluster(cls, cluster):
+        zabbix = {}
+        role_filter = lambda role: role.name == 'zabbix-server'
+        node_filter = lambda node: filter(role_filter, node.role_list)
+        zabbix_node = filter(node_filter, cluster.nodes)
+        if not zabbix_node:
+            return None
+        zabbix_node = zabbix_node[0]
+        attributes = db().query(Attributes).get(cluster.id)
+        zabbix_attrs = attributes.editable['zabbix']
+        zabbix['user'] = zabbix_attrs['username']['value']
+        zabbix['password'] = zabbix_attrs['password']['value']
+        zabbix['url'] = "http://%s/zabbix/api_jsonrpc.php" % zabbix_node.ip
+        return zabbix

04bd50447a1fba63b625556e7e9115b04dbeccc0
Nikolay Markov <nmarkov@mirantis.com>
2014-04-18 18:43:13 +0400

    [DON'T MERGE]Support for merging of migration scripts
    Change-Id: Iec7e7adcb7c64808e2a7cf51d2f13490c1ffaa0d

---
+++ nailgun/nailgun/db/migration/helpers.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import sqlalchemy as sa
+
+from alembic import op
+
+
+def upgrade_enum(table, column_name, enum_name, old_options, new_options):
+    old_type = sa.Enum(*old_options, name=enum_name)
+    new_type = sa.Enum(*new_options, name=enum_name)
+    tmp_type = sa.Enum(*new_options, name="_" + enum_name)
+    # Create a tempoary type, convert and drop the "old" type
+    tmp_type.create(op.get_bind(), checkfirst=False)
+    op.execute(
+        u'ALTER TABLE {0} ALTER COLUMN {1} TYPE _{2}'
+        u' USING {1}::text::_{2}'.format(
+            table,
+            column_name,
+            enum_name
+        )
+    )
+    old_type.drop(op.get_bind(), checkfirst=False)
+    # Create and convert to the "new" type
+    new_type.create(op.get_bind(), checkfirst=False)
+    op.execute(
+        u'ALTER TABLE {0} ALTER COLUMN {1} TYPE {2}'
+        u' USING {1}::text::{2}'.format(
+            table,
+            column_name,
+            enum_name
+        )
+    )
+    tmp_type.drop(op.get_bind(), checkfirst=False)
+    ### end Alembic commands ###
+
+
+def drop_enum(name):
+    op.execute(
+        u'DROP TYPE {0}'.format(name)
+    )

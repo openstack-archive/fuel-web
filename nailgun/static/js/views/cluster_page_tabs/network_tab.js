7cc3127ee1f5d34480cb823e433bbe4ab3cfef84
Julia Aranovich <jkirnosova@mirantis.com>
2014-04-22 12:40:59 +0400

    Fixes network configuration saving
    Closes-Bug:#1310955
    
    Change-Id: I6bd5467196838eac9b98255c4dd6a8536658b30b

--- nailgun/static/js/views/cluster_page_tabs/network_tab.js
+++ nailgun/static/js/views/cluster_page_tabs/network_tab.js
@@ -106,7 +106,8 @@ function(utils, models, commonViews, dialogViews, networkTabTemplate, networkTem
                             }, this));
                         } else {
                             this.hasChanges = false;
-                            this.model.set({networkConfiguration: new models.NetworkConfiguration(this.networkConfiguration.toJSON(), {parse: true})});
+                            var fakeModel = new models.NetworkConfiguration(this.networkConfiguration.toJSON(), {parse: true});
+                            this.model.get('networkConfiguration').set(fakeModel.attributes);
                             this.model.fetch();
                             this.model.fetchRelated('tasks');
                         }

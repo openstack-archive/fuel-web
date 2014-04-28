c90bbd713c040d6819a758a959d81a5ffc538648
Vitaly Kramskikh <vkramskikh@mirantis.com>
2014-04-28 16:36:43 +0400

    Parser for expressions for config (python part)
    Change-Id: If2d4154d8edfec6ac30518f696917da13d84f0b6

---
+++ nailgun/nailgun/test/unit/test_expression_parser.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import inspect
+
+from nailgun.errors import errors
+from nailgun.test.base import BaseTestCase
+from nailgun.utils import evaluate_expression
+
+
+class TestExpressionParser(BaseTestCase):
+
+    def test_expression_parser(self):
+        cluster = self.env.create_cluster(api=False, mode='ha_compact')
+        models = {
+            'cluster': cluster,
+            'settings': cluster.attributes.editable,
+        }
+        hypervisor = models['settings']['common']['libvirt_type']['value']
+
+        test_cases = (
+            # test scalars
+            ('true', True),
+            ('false', False),
+            ('123', 123),
+            ('"123"', '123'),
+            ("'123'", '123'),
+            # test boolean operators
+            ('true or false', True),
+            ('true and false', False),
+            ('not true', False),
+            # test precedence
+            ('true or true and false or false', True),
+            ('true == true and false == false', True),
+            # test comparison
+            ('123 == 123', True),
+            ('123 == 321', False),
+            ('123 != 321', True),
+            ('123 != "123"', True),
+            # test grouping
+            ('(true or true) and not (false or false)', True),
+            # test errors
+            ('(true', errors.ParseError),
+            ('false and', errors.ParseError),
+            ('== 123', errors.ParseError),
+            ('#^@$*()#@!', errors.ParseError),
+            # test modelpaths
+            ('cluster:mode', 'ha_compact'),
+            ('cluster:mode == "ha_compact"', True),
+            ('cluster:mode != "multinode"', True),
+            ('settings:common.libvirt_type.value', hypervisor),
+            ('settings:common.libvirt_type.value == "%s"' % hypervisor, True),
+            ('cluster:mode == "ha_compact" and not ('
+                'settings:common.libvirt_type.value != "%s")' % hypervisor,
+                True),
+        )
+
+        for test_case in test_cases:
+            expression, result = test_case
+            if inspect.isclass(result) and issubclass(result, Exception):
+                self.assertRaises(result, evaluate_expression,
+                                  expression, models)
+            else:
+                self.assertEqual(evaluate_expression(expression, models),
+                                 result)

728e09006545b197cab96e6896f5a3f62a60e049
Alexander Kislitsky <akislitsky@mirantis.com>
2014-05-20 17:53:22 +0400

    Fix for deadlocks in stop_deployment task
    Nailgun base object:
    - order_by method added into NailgunCollection
    - get_by_id_lists renamed to filter_by_id_list
    - filtering by param logic extracted to method
    - filter_by_name method added
    
    Nailgun Node object:
    - move_roles_to_pending_roles method added
    
    Rpc receiver:
    - stop_deployment task refactored to nailgun objects
    - queries with locking and ordering used in stop_deployment
    - ordering added into locking queries in deploy_resp
    - fetched from db nodes used in deploy_resp
    
    Change-Id: Idff49dd7e01a159f4d5c4f4208aadf1894346b8e
    Closes-Bug: #1321302

--- nailgun/nailgun/objects/node.py
+++ nailgun/nailgun/objects/node.py
@@ -609,6 +609,14 @@ class Node(NailgunObject):
                (instance.status == consts.NODE_STATUSES.error
                 and instance.error_type == consts.NODE_ERRORS.deploy)
 
+    @classmethod
+    def move_roles_to_pending_roles(cls, instance):
+        """Move roles to pending_roles
+        """
+        instance.pending_roles += instance.roles
+        instance.roles = []
+        db().flush()
+
 
 class NodeCollection(NailgunCollection):
     """Node collection

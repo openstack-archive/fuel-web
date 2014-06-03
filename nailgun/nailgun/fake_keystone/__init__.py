3d2e3376f1e3bbe768325333154720339fc0d50d
Vitaly Kramskikh <vkramskikh@mirantis.com>
2014-06-03 20:33:14 +0400

    Keystone Authorization for UI
    Change-Id: Ib8627701c30c74cd8c5afb3dde1f12e2aa033c91
    Blueprint: bp/access-control-master-node

--- nailgun/nailgun/fake_keystone/__init__.py
+++ nailgun/nailgun/fake_keystone/__init__.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-
-#    Copyright 2013 Mirantis, Inc.
+#    Copyright 2014 Mirantis, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -13,19 +11,3 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-
-from nailgun.api.v1 import urls as api_urls
-from nailgun.fake_keystone import urls as fake_keystone_urls
-from nailgun.settings import settings
-from nailgun.webui import urls as webui_urls
-
-
-def urls():
-    urls = [
-        "/api/v1", api_urls.app(),
-        "/api", api_urls.app(),
-        "", webui_urls.app()
-    ]
-    if settings.AUTH['AUTHENTICATION_METHOD'] == 'fake':
-        urls = ["/keystone", fake_keystone_urls.app()] + urls
-    return urls

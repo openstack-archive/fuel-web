d24a664110b0efdf6a4ad2c874f38f6952019954
Evgeniy L <eli@mirantis.com>
2014-06-17 20:47:39 +0400

    Fuel upgrades, add yaml files to egg
    Even if include_package_data is True
    when we run pip install fuel_upgrade
    it doesn't include *.yaml files.
    We need explicitly specify this files.
    
    blueprint upgrade-to-5-1
    
    Change-Id: I6a0508ab917e1c3adeb842ddc11a78d48936ff44

--- fuel_upgrade_system/fuel_upgrade/setup.py
+++ fuel_upgrade_system/fuel_upgrade/setup.py
@@ -63,6 +63,7 @@ setup(
     install_requires=REQUIREMENTS,
     dependency_links=DEPENDENCIES,
     include_package_data=True,
+    package_data={'': ['*.yaml']},
     entry_points={
         'console_scripts': [
             'fuel-upgrade = fuel_upgrade.cli:main'

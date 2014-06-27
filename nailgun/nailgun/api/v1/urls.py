bf98d18f1474a0d8ddad2c8b92457dd0585069ce
Łukasz Oleś <loles@mirantis.com>
2014-06-27 08:30:33 +0200

    Access control support
    Change-Id: Ic842885c7bcef0e9dc7728837b6cd396ccf52fbe
    Implements: blueprint access-control-master-node

--- nailgun/nailgun/api/v1/urls.py
+++ nailgun/nailgun/api/v1/urls.py
@@ -196,3 +196,9 @@ _locals = locals()
 
 def app():
     return web.application(urls, _locals)
+
+def public_urls():
+    return { r'/nodes/?$' : ['POST'],
+             r'/nodes/agent/?$' : ['PUT'],
+             r'/version/?$' : ['GET'],
+           }

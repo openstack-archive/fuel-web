51ab86738658e542cbf39357a92dfef8741bf025
Kamil Sambor <ksambor@mirantis.com>
2014-06-30 14:52:02 +0200

    Add authentication in fuel-cli
    * modified APIClient to get token
    * modified base test class for fuel-cli to mock get_token method
    * modified fuel-cli to use user and password
    
    Change-Id: I4b0a2dc485c086949b103c11689ea892069f02e5
    Implements: blueprint access-control-master-node

--- fuelclient/fuelclient/cli/parser.py
+++ fuelclient/fuelclient/cli/parser.py
@@ -44,6 +44,7 @@ class Parser:
         self.generate_actions()
         self.add_version_args()
         self.add_debug_arg()
+        self.add_keystone_credentials_args()
         self.add_serializers_args()
 
     def generate_actions(self):
@@ -101,6 +102,14 @@ class Parser:
             default=False
         )
 
+    def add_keystone_credentials_args(self):
+        self.parser.add_argument(
+            "--user",
+            dest="user",
+            help="credentials for keystone authentication user:password",
+            default=None
+        )
+
     def add_version_args(self):
         for args in (get_version_arg(), get_fuel_version_arg()):
             self.parser.add_argument(*args["args"], **args["params"])
@@ -118,6 +127,7 @@ class Parser:
                 self.args.insert(1, flag)
 
         self.move_argument_before_action("--env", )
+        self.move_argument_before_action("--user", )
 
     def move_argument_before_action(self, argument):
         for arg in self.args:

24a443188f81c521b913d455fbfd8829051ad102
Nikolay Markov <enchantner@gmail.com>
2014-07-03 20:17:02 +0400

    Ceph & REST API sample plugins
    Related to blueprint nailgun-plugins
    
    Added Ceph plugin which reimplements Ceph support moved out of Nailgun
    
    Added sample REST API plugin
    
    DB migration is merged into 5.1
    
    Change-Id: Ibb2c50c3865bf179af14bc48f33048810ad89e39

---
+++ plugins/nailgun-api-plugin/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import setuptools
+
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # flake8: noqa
+except ImportError:
+    pass
+
+
+major_version = '0.1'
+minor_version = '0'
+name = 'nailgun-api-plugin'
+
+version = "{0}.{1}".format(major_version, minor_version)
+
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author='Mirantis Inc.',
+    author_email='product@mirantis.com',
+    url='http://mirantis.com',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License"
+        "Programming Language :: Python"
+        "Programming Language :: Python :: 2"
+        "Programming Language :: Python :: 2.6"
+        "Language :: Python :: 2.7"
+    ],
+    entry_points={
+        'nailgun.plugin': [
+            'plugin = api_plugin.plugin:SampleAPIPlugin'
+        ],
+        'nailgun.rest_api': [
+            'application = api_plugin.plugin:SampleAPIPlugin'
+        ]
+    }
+)

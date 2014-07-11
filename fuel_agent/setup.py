a7eab25c086bdcffe156970c688f510693d11471
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2014-07-12 00:52:29 +0400

    Removed pbr from fuel_agent setup.py
    To make it possible to build fuel-agent
    package on a system without pbr installed.
    
    Change-Id: I718e1b32c9b5fe840fbaf684f0fdf8df8dce97c2

--- fuel_agent/setup.py
+++ fuel_agent/setup.py
@@ -12,16 +12,46 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import setuptools
+import os
+import os.path
 
-import pbr
-import pbr.packaging
+from setuptools import find_packages
+from setuptools import setup
+
+name = 'fuel-agent'
+version = '0.1.0'
 
 
-# this monkey patch is to avoid appending git version to version
-pbr.packaging._get_version_from_git = lambda pre_version: pre_version
+def find_requires():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    requirements = []
+    with open('{0}/requirements.txt'.format(dir_path), 'r') as reqs:
+        requirements = reqs.readlines()
+    print requirements
+    return requirements
 
 
-setuptools.setup(
-    setup_requires=['pbr'],
-    pbr=True)
+if __name__ == "__main__":
+    setup(
+        name=name,
+        version=version,
+        description='Fuel agent',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Programming Language :: Python'
+        ],
+        author='Mirantis',
+        author_email='fuel-dev@lists.launchpad.net',
+        packages=find_packages(),
+        zip_safe=False,
+        install_requires=find_requires(),
+        entry_points={
+            'console_scripts': [
+                'agent_new = fuel_agent.cmd.agent:main',
+                'provision = fuel_agent.cmd.provision:main'
+            ],
+            'fuel_agent.drivers': [
+                'nailgun = fuel_agent.drivers.nailgun:Nailgun'
+            ]
+        }
+    )

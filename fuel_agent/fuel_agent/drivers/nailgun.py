c130be7c0b005b63b6dea872cb9a43b27c3f8bcd
Alexander Gordeev <agordeev@mirantis.com>
2014-07-14 20:40:39 +0400

    Improve test coverage for fuel_agent
    * should be 100%
    * also comes with fixups
    Partially implements: blueprint image-based-provisioning
    
    Change-Id: I73ff92f65c0872ad82917d22900c3f410986629d

--- fuel_agent/fuel_agent/drivers/nailgun.py
+++ fuel_agent/fuel_agent/drivers/nailgun.py
@@ -67,7 +67,7 @@ class Nailgun(object):
     @property
     def ks_vgs(self):
         vg_filter = lambda x: x['type'] == 'vg'
-        self.ks_vgs = filter(vg_filter, self.partition_data())
+        return filter(vg_filter, self.partition_data())
 
     @property
     def hu_disks(self):
@@ -107,7 +107,7 @@ class Nailgun(object):
         ks_spaces_validator.validate(data)
         partition_scheme = objects.PartitionScheme()
 
-        for disk in enumerate(self.ks_disks):
+        for disk in self.ks_disks:
             parted = partition_scheme.add_parted(
                 name=self._disk_dev(disk), label='gpt')
             # legacy boot partition
@@ -147,7 +147,7 @@ class Nailgun(object):
             if partition_scheme.configdrive_device() is None:
                 parted.add_partition(size=20, configdrive=True)
 
-        for vg in enumerate(self.ks_vgs):
+        for vg in self.ks_vgs:
             for volume in vg['volumes']:
                 if volume['size'] <= 0:
                     continue
@@ -172,7 +172,7 @@ class Nailgun(object):
         admin_interface = filter(
             lambda x: (x['mac_address'] ==
                        data['kernel_options']['netcfg/choose_interface']),
-            [spec.update(name=name) for name, spec
+            [dict(name=name, **spec) for name, spec
              in data['interfaces'].iteritems()])[0]
         configdrive_scheme.set_common(
             ssh_auth_key=data['ks_meta']['auth_key'],
@@ -181,7 +181,7 @@ class Nailgun(object):
             name_servers=data['name_servers'],
             search_domain=data['name_servers_search'],
             master_ip=data['ks_meta']['master_ip'],
-            master_url='http:/%s:8000/api' % self.data['master_ip'],
+            master_url='http://%s:8000/api' % data['ks_meta']['master_ip'],
             udevrules=data['kernel_options']['udevrules'],
             admin_mac=data['kernel_options']['netcfg/choose_interface'],
             admin_ip=admin_interface['ip_address'],
@@ -198,7 +198,7 @@ class Nailgun(object):
             pskey=data['ks_meta']['mco_pskey'],
             vhost=data['ks_meta']['mco_vhost'],
             host=data['ks_meta']['mco_host'],
-            user=data['ks_meta']['mco_host'],
+            user=data['ks_meta']['mco_user'],
             password=data['ks_meta']['mco_password'],
             connector=data['ks_meta']['mco_connector']
         )
---
+++ fuel_agent/fuel_agent/tests/test_fs_utils.py
@@ -0,0 +1,34 @@
+# Copyright 2014 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import mock
+from oslotest import base as test_base
+
+from fuel_agent.utils import fs_utils as fu
+from fuel_agent.utils import utils
+
+
+class TestFSUtils(test_base.BaseTestCase):
+
+    @mock.patch.object(utils, 'execute')
+    def test_make_fs(self, mock_exec):
+        fu.make_fs('ext4', ' -F ', ' -L fake_label ', '/dev/fake')
+        mock_exec.assert_called_once_with('mkfs.ext4', '-F', '-L',
+                                          'fake_label', '/dev/fake')
+
+    @mock.patch.object(utils, 'execute')
+    def test_make_fs_swap(self, mock_exec):
+        fu.make_fs('swap', ' -f ', ' -L fake_label ', '/dev/fake')
+        mock_exec.assert_called_once_with('mkswap', '-f', '-L', 'fake_label',
+                                          '/dev/fake')

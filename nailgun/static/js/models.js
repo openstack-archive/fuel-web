fdd81ef62695835c7e72aaf2f377714b83bd26a7
Alexandra Morozova <astepanchuk@mirantis.com>
2014-07-15 12:42:20 +0300

    vCenter credentials validation in wizard
    added client-side validation
    - changes to wizard.json with regexes
    - model change listener added
    
    Change-Id: I2d37821b95b00954b10809afd9c9c1aa7d6115ef
    Closes-bug: 1314613

--- nailgun/static/js/models.js
+++ nailgun/static/js/models.js
@@ -807,6 +807,27 @@ define(['utils', 'deepModel'], function(utils) {
             }, this));
             this.set(result);
         },
+        validate: function(attrs, options) {
+            var errors = [];
+            _.each(options.config, function(attributeConfig, attribute) {
+                if (!(attributeConfig.regex && attributeConfig.regex.source)) { return; }
+                var shouldValidate = false;
+                _.each(attributeConfig.restrictions, function(restriction) {
+                    // this probably will be changed when other controls need validation
+                    shouldValidate = !utils.evaluateExpression(restriction.condition, {default: this}).value;
+                }, this);
+                if (shouldValidate) {
+                    var regExp = new RegExp(attributeConfig.regex.source);
+                    if (!this.get(options.paneName + '.' + attribute).match(regExp)) {
+                        errors.push({
+                            field: attribute,
+                            message: $.t(attributeConfig.regex.error)
+                        });
+                    }
+                }
+            }, this);
+            return errors.length ? errors : null;
+        },
         initialize: function(config) {
             this.defaults = this.parseConfig(config);
         }

0bbee3ad7f71776a19cdd2f9d39259ee2cce4927
Vitaly Kramskikh <vkramskikh@mirantis.com>
2014-07-16 20:15:58 +0400

    [React] Healthcheck tab
    Related to blueprint backbone-to-react
    
    Change-Id: I4c54b22ef210d84613f85cc61481f629166f7f9a

--- nailgun/static/js/views/controls.jsx
+++ nailgun/static/js/views/controls.jsx
@@ -40,6 +40,7 @@ define(['jquery', 'underscore', 'react'], function($, _, React) {
             type = type || this.props.type;
             var radioButton = this.isRadioButton();
             return (<input
+                id={this.props.id}
                 className={this.getError() && 'error'}
                 type={type}
                 name={this.props.name}
@@ -54,7 +55,7 @@ define(['jquery', 'underscore', 'react'], function($, _, React) {
             return (
                 <div className={labelClass + ' enable-selection'}>
                     {radioButton ? radioButton.label : this.props.label}
-                    {!!this.props.warnings.length &&
+                    {(this.props.warnings && this.props.warnings.length) &&
                         <controls.TooltipIcon warnings={this.props.warnings} />
                     }
                 </div>
@@ -77,19 +78,30 @@ define(['jquery', 'underscore', 'react'], function($, _, React) {
         getDefaultProps: function() {
             return {type: 'checkbox'};
         },
+        renderControl: function() {
+            return (
+                <div className='custom-tumbler'>
+                    {this.renderInput()}
+                    <span>&nbsp;</span>
+                </div>
+            );
+        },
         render: function() {
             return (
-                <div className={this.props.cs.common}>
-                    <label className='parameter-box'>
-                        <div className='parameter-control'>
-                            <div className='custom-tumbler'>
-                                {this.renderInput()}
-                                <span>&nbsp;</span>
-                            </div>
-                        </div>
-                        {this.renderLabel()}
-                        {this.renderDescription()}
-                    </label>
+                <div>
+                    {this.props.controlOnly ?
+                        this.renderControl()
+                    :
+                        (<div className={this.props.cs.common}>
+                            <label className='parameter-box'>
+                                <div className='parameter-control'>
+                                    {this.renderControl()}
+                                </div>
+                                {this.renderLabel()}
+                                {this.renderDescription()}
+                            </label>
+                        </div>)
+                    }
                 </div>
             );
         }
@@ -102,7 +114,7 @@ define(['jquery', 'underscore', 'react'], function($, _, React) {
         },
         render: function() {
             return (
-                <div className={this.props.cs.common}>
+                <div className={'select-all ' + this.props.cs.common}>
                     <label className={this.props.cs.label}>
                         {this.renderInput()}
                         <span>&nbsp;</span>

4540068cec791790cbc35113825c8eed9364855e
Vitaly Kramskikh <vkramskikh@mirantis.com>
2014-07-16 20:15:58 +0400

    React.js integration
    Change-Id: Idd801b607fad77c130d528b7fa9c90a5cb19edcd

--- nailgun/static/js/app.js
+++ nailgun/static/js/app.js
@@ -15,6 +15,9 @@
 **/
 define(
 [
+    'react',
+    'utils',
+    'jsx!views/layout',
     'coccyx',
     'js/coccyx_mixins',
     'models',
@@ -29,7 +32,7 @@ define(
     'views/support_page',
     'views/capacity_page'
 ],
-function(Coccyx, coccyxMixins, models, KeystoneClient, commonViews, LoginPage, ClusterPage, NodesTab, ClustersPage, ReleasesPage, NotificationsPage, SupportPage, CapacityPage) {
+function(React, utils, layoutComponents, Coccyx, coccyxMixins, models, KeystoneClient, commonViews, LoginPage, ClusterPage, NodesTab, ClustersPage, ReleasesPage, NotificationsPage, SupportPage, CapacityPage) {
     'use strict';
 
     var AppRouter = Backbone.Router.extend({
@@ -147,28 +150,30 @@ function(Coccyx, coccyxMixins, models, KeystoneClient, commonViews, LoginPage, C
         },
         renderLayout: function() {
             this.content = $('#content');
-            this.navbar = new commonViews.Navbar({elements: [
-                {label: 'environments', url: '#clusters'},
-                {label: 'releases', url:'#releases'},
-                {label: 'support', url:'#support'}
-            ]});
-            this.content.before(this.navbar.render().el);
-            this.breadcrumbs = new commonViews.Breadcrumbs();
-            this.content.before(this.breadcrumbs.render().el);
-            this.footer = new commonViews.Footer();
-            $('#footer').html(this.footer.render().el);
+            this.navbar = React.renderComponent(new layoutComponents.Navbar({
+                elements: [
+                    {label: 'environments', url: '#clusters'},
+                    {label: 'releases', url:'#releases'},
+                    {label: 'support', url:'#support'}
+                ],
+                user: this.user,
+                version: this.version,
+                statistics: new models.NodesStatistics(),
+                notifications: new models.Notifications()
+            }), $('#navbar')[0]);
+            this.breadcrumbs = React.renderComponent(new layoutComponents.Breadcrumbs(), $('#breadcrumbs')[0]);
+            this.footer = React.renderComponent(new layoutComponents.Footer({version: this.version}), $('#footer')[0]);
             this.content.find('.loading').addClass('layout-loaded');
         },
         setPage: function(NewPage, options) {
             if (this.page) {
-                this.page.tearDown();
+                utils.universalUnmount(this.page);
             }
             this.page = new NewPage(options);
             this.page.updateNavbar();
             this.page.updateBreadcrumbs();
             this.page.updateTitle();
-            this.content.html(this.page.render().el);
-
+            utils.universalMount(this.page, this.content);
         },
         // routes
         login: function() {
@@ -276,7 +281,7 @@ function(Coccyx, coccyxMixins, models, KeystoneClient, commonViews, LoginPage, C
             }, this));
         },
         showNotifications: function() {
-            this.setPage(NotificationsPage, {notifications: app.navbar.notifications});
+            this.setPage(NotificationsPage, {notifications: app.navbar.props.notifications});
         },
         showSupportPage: function() {
             this.setPage(SupportPage);

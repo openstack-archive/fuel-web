cd21c3388a3b8039818ba102c3c99830b8d6b4a3
Julia Aranovich <jkirnosova@mirantis.com>
2014-07-17 15:57:17 +0400

    Settings initial data reorganisation
    Change-Id: I910d059504f96561102a90b8542f68a0a2ec9296

--- nailgun/static/js/views/cluster_page_tabs/settings_tab.js
+++ nailgun/static/js/views/cluster_page_tabs/settings_tab.js
@@ -31,7 +31,7 @@ function(utils, models, viewMixins, commonViews, dialogViews, settingsTabTemplat
         template: _.template(settingsTabTemplate),
         mixins: [viewMixins.toggleablePassword],
         hasChanges: function() {
-            return !_.isEqual(this.settings.toJSON(), this.initialSettings.toJSON());
+            return !_.isEqual(this.settings.toJSON().editable, this.settings.initialAttributes);
         },
         events: {
             'click .btn-apply-changes:not([disabled])': 'applyChanges',
@@ -83,11 +83,12 @@ function(utils, models, viewMixins, commonViews, dialogViews, settingsTabTemplat
                 })
                 .always(_.bind(this.render, this));
         },
-        updateInitialSettings: function() {
-            this.initialSettings.set(this.settings.attributes);
+        updateInitialSettings: function(settings) {
+            this.settings.initialAttributes = settings ? settings.editable : this.settings.toJSON().editable;
         },
         loadInitialSettings: function() {
-            this.settings.set(this.initialSettings.attributes);
+            var initialSettingsModel = new models.Settings(this.settings.initialAttributes);
+            this.settings.set(initialSettingsModel.processRestrictions(this.configModels));
         },
         onSettingChange: function() {
             this.$('input.error').removeClass('error');
@@ -299,7 +300,6 @@ function(utils, models, viewMixins, commonViews, dialogViews, settingsTabTemplat
             this.model.get('tasks').bindToView(this, [{group: 'deployment'}], function(task) {
                 task.on('change:status', this.render, this);
             });
-            this.initialSettings = new models.Settings();
             this.settings = this.model.get('settings');
             this.settings.on('invalid', function(model, errors) {
                 _.each(errors, function(error) {
@@ -309,8 +309,8 @@ function(utils, models, viewMixins, commonViews, dialogViews, settingsTabTemplat
                     input.parent().siblings('.validation-error').text(error.message).removeClass('hide');
                 }, this);
             }, this);
-            (this.loading = $.when(this.settings.fetch({cache: true}), this.model.get('networkConfiguration').fetch({cache: true}))).done(_.bind(function() {
-                this.updateInitialSettings();
+            (this.loading = $.when(this.settings.fetch({cache: true}), this.model.get('networkConfiguration').fetch({cache: true}))).done(_.bind(function(settings) {
+                this.updateInitialSettings(settings);
                 this.configModels = {
                     cluster: this.model,
                     settings: this.settings,

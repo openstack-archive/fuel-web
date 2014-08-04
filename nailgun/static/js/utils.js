5417f0efc95267c12c60fa53b19976ccb073d0c9
Julia Aranovich <jkirnosova@mirantis.com>
2014-08-04 14:34:37 +0400

    [React] Settings tab
    Change-Id: Ieca96f8969b9b25eb74983b1fdf2c2ce9d8ab0e5

--- nailgun/static/js/utils.js
+++ nailgun/static/js/utils.js
@@ -78,6 +78,19 @@ define(['require', 'expression', 'expression/objects', 'react'], function(requir
             }
             return result;
         },
+        checkRestrictions: function(restrictions, models, action) {
+            var result = false,
+                warnings = _.map(restrictions, function(restriction) {
+                    if (!_.isPlainObject(restriction) || !_.has(restriction, 'condition')) {
+                        restriction = utils.expandRestriction(restriction);
+                    }
+                    if ((!action || restriction.action == action) && utils.evaluateExpression(restriction.condition, models).value) {
+                        result = true;
+                        return restriction.message;
+                    }
+                });
+            return {result: result, warnings: _.compact(warnings)};
+        },
         universalMount: function(view, el, parentView) {
             if (view instanceof Backbone.View) {
                 view.render();

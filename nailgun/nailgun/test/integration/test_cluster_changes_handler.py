6ee7c4473543a1896eb5b84f60bb4a461b3df2da
Aleksey Kasatkin <akasatkin@mirantis.com>
2014-08-13 12:56:25 +0300

    Added selective public network serialization
    Added hepler method objects.Node.should_have_public() to determine
    is it required to assign Public network IPs to particular node.
    It is checked on IPs assignment and deployment data serialization.
    
    Closes-Bug: #1272349
    
    Change-Id: I80d3ccdcc8e4b440f40f5b6e089532adaa5d02df

--- nailgun/nailgun/test/integration/test_cluster_changes_handler.py
+++ nailgun/nailgun/test/integration/test_cluster_changes_handler.py
@@ -405,11 +405,7 @@ class TestHandlers(BaseIntegrationTest):
         L2 = {
             "base_mac": "fa:16:3e:00:00:00",
             "segmentation_type": "gre",
-            "phys_nets": {
-                "physnet1": {
-                    "bridge": "br-ex",
-                    "vlan_range": None}
-            },
+            "phys_nets": {},
             "tunnel_id_ranges": "2:65535"
         }
         L3 = {
@@ -420,8 +416,8 @@ class TestHandlers(BaseIntegrationTest):
                 'shared': False,
                 'L2': {
                     'router_ext': True,
-                    'network_type': 'flat',
-                    'physnet': 'physnet1',
+                    'network_type': 'local',
+                    'physnet': None,
                     'segment_id': None},
                 'L3': {
                     'subnet': u'172.16.0.0/24',
@@ -601,9 +597,6 @@ class TestHandlers(BaseIntegrationTest):
                                 "name": u"eth1"},
                             {
                                 "action": "add-br",
-                                "name": "br-ex"},
-                            {
-                                "action": "add-br",
                                 "name": "br-mgmt"},
                             {
                                 "action": "add-br",
@@ -612,13 +605,12 @@ class TestHandlers(BaseIntegrationTest):
                                 "action": "add-br",
                                 "name": "br-fw-admin"},
                             {
+                                "action": "add-br",
+                                "name": "br-ex"},
+                            {
                                 "action": "add-patch",
                                 "bridges": [u"br-eth0", "br-storage"],
                                 "tags": [102, 0]},
-                            {
-                                "action": "add-patch",
-                                "bridges": [u"br-eth0", "br-ex"],
-                                "trunks": [0]},
                             {
                                 "action": "add-patch",
                                 "bridges": [u"br-eth0", "br-mgmt"],
@@ -626,6 +618,10 @@ class TestHandlers(BaseIntegrationTest):
                             {
                                 "action": "add-patch",
                                 "bridges": [u"br-eth1", "br-fw-admin"],
+                                "trunks": [0]},
+                            {
+                                "action": "add-patch",
+                                "bridges": [u"br-eth0", "br-ex"],
                                 "trunks": [0]},
                         ]
                     }

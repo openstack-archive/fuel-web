1189a5f103050a62950c01fc3047ce5677f1213b
Dima Shulyak <dshulyak@mirantis.com>
2014-08-14 14:45:52 +0300

    Save astute keys and port them back to astute
    Closes-Bug: 1353497
    Change-Id: I5df75fc71fc7ac5d0fb6221caf6ab8097052eb71

--- fuel_upgrade_system/fuel_upgrade/fuel_upgrade/engines/docker_engine.py
+++ fuel_upgrade_system/fuel_upgrade/fuel_upgrade/engines/docker_engine.py
@@ -68,6 +68,7 @@ class DockerUpgrader(UpgradeEngine):
         # Preapre env for upgarde
         self.save_db()
         self.save_cobbler_configs()
+        self.save_astute_keys()
 
         # NOTE(akislitsky): fix for bug
         # https://bugs.launchpad.net/fuel/+bug/1354465
@@ -186,6 +187,20 @@ class DockerUpgrader(UpgradeEngine):
                 u'Failed to make database dump, there '
                 'are no valid database backup '
                 'files, {0}'.format(pg_dump_path))
+
+    def save_astute_keys(self):
+        """Copy any astute generated keys."""
+        container_name = self.make_container_name(
+            'astute', self.from_version)
+        try:
+            utils.exec_cmd('docker cp {0}:{1} {2}'.format(
+                container_name,
+                '/var/lib/astute/',
+                '/tmp/upgrade/'
+            ))
+        except errors.ExecutedErrorNonZeroExitCode:
+            if os.path.exists('/tmp/upgrade/astute/'):
+                utils.rmtree('/tmp/upgrade/astute/')
 
     def save_cobbler_configs(self):
         """Copy config files from container

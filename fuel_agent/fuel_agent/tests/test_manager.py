51e9e166efc8f3e06f840f1ea0156fe8b45fb683
Alexander Gordeev <agordeev@mirantis.com>
2014-09-02 20:49:16 +0400

    Fix fuel_agent, add ceph things
    Partially implements: blueprint image-based-provisioning
    Change-Id: Id23033a537b150875e452f81bd52d63f6286fa1d

--- fuel_agent/fuel_agent/tests/test_manager.py
+++ fuel_agent/fuel_agent/tests/test_manager.py
@@ -52,13 +52,14 @@ class TestManager(test_base.BaseTestCase):
     @mock.patch.object(lu, 'vgcreate')
     @mock.patch.object(lu, 'pvcreate')
     @mock.patch.object(mu, 'mdcreate')
+    @mock.patch.object(pu, 'set_gpt_type')
     @mock.patch.object(pu, 'set_partition_flag')
     @mock.patch.object(pu, 'make_partition')
     @mock.patch.object(pu, 'make_label')
     @mock.patch.object(hu, 'list_block_devices')
     def test_do_partitioning(self, mock_hu_lbd, mock_pu_ml, mock_pu_mp,
-                             mock_pu_spf, mock_mu_m, mock_lu_p, mock_lu_v,
-                             mock_lu_l, mock_fu_mf):
+                             mock_pu_spf, mock_pu_sgt, mock_mu_m, mock_lu_p,
+                             mock_lu_v, mock_lu_l, mock_fu_mf):
         mock_hu_lbd.return_value = test_nailgun.LIST_BLOCK_DEVICES_SAMPLE
         self.mgr.do_parsing()
         self.mgr.do_partitioning()

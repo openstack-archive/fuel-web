e4b98e33a859e911918f719e42f23349787d7441
Dima Shulyak <dshulyak@mirantis.com>
2014-09-02 14:40:51 +0300

    Tasklib implementation in python
    Medium between configuration providers and fuel orchestration
    solution.
    
    check README.md and functional tests for better understanding
    how it works
    
    HOW TO RUN:
    python setup.py develop
    
    taskcmd -c tasklib/tests/functional/conf.yaml run puppet/sleep
    
    taskcmd -c tasklib/tests/functional/conf.yaml run exec/fail
    
    taskcmd -c tasklib/tests/functional/conf.yaml daemon exec/long
    taskcmd -c tasklib/tests/functional/conf.yaml status exec/long
    
    Related to blueprint granular-deployment-based-on-tasks
    
    Change-Id: Ifed1a9b90042bbbc93215a30dfb34e29dbe8fba2

---
+++ tasklib/tasklib/config.py
@@ -0,0 +1,61 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+import yaml
+
+
+class Config(object):
+
+    def __init__(self, config_file=None):
+        self.config_file = config_file
+        self.curdir = os.getcwd()
+        self._config = {}
+
+    @property
+    def default_config(self):
+        return {
+            'library_dir': '/etc/puppet',
+            'puppet_modules': '/etc/puppet/modules',
+            'puppet_options': '--logdest syslog --trace --no-report',
+            'report_dir': '/var/tmp/task_report',
+            'pid_dir': '/var/tmp/task_run',
+            'puppet_manifest': 'site.pp',
+            'status_file': 'status',
+            'debug': False,
+            'task_file': 'task.yaml',
+            'config_file': self.config_file,
+            'log_file': '/var/log/tasklib.log'}
+
+    @property
+    def config(self):
+        if self._config:
+            return self._config
+        default_config = self.default_config
+        if self.config_file and os.path.exists(default_config['config_file']):
+            with open(default_config['config_file']) as f:
+                loaded = yaml.load(f.read())
+            default_config.update(loaded)
+        self._config = default_config
+        return self._config
+
+    def __getitem__(self, key):
+        return self.config.get(key, None)
+
+    def __setitem__(self, key, value):
+        self.config[key] = value
+
+    def __repr__(self):
+        return yaml.dump(self.config, default_flow_style=False)

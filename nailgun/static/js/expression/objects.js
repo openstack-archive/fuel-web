47275134ee3a97442937c4862037dfba04b8cc69
Vitaly Kramskikh <vkramskikh@mirantis.com>
2014-09-24 11:06:50 -0700

    Fix random UI loading failure
    Change-Id: Ia7fc3a7f2b45fb1fd4648062ca06553f42132f61

--- nailgun/static/js/expression/objects.js
+++ nailgun/static/js/expression/objects.js
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
 **/
-define(function() {
+define(['underscore'], function(_) {
     'use strict';
 
     function ModelPath(path) {

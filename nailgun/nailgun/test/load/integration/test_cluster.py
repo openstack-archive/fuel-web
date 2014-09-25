fc23da5ca4a6ab7fe50bf92e5c9f039681fa4e72
Kamil Sambor <ksambor@mirantis.com>
2014-09-25 09:35:50 +0200

    Added base classes for Load Tests and profiler
    * added nailgun profiler
    * added base clases for load test
    * added integration tests
    
    Tests are run only when enviroment variable NAILGUN_LOAD_TEST
    is set on True, also profiler runs only when this variable is set.
    All artifacts from profiler are writen in
    /tmp/nailgun_load_tests/results/.
    
    DocImpact
    Change-Id: I2be46c2a0a51544b12a1d9f2a840d4edc3ede2c4
    Blueprint: 100-nodes-support

---
+++ nailgun/nailgun/test/load/integration/test_cluster.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from mock import patch
+from nailgun.test.base import fake_tasks
+from nailgun.test.load.base import BaseIntegrationLoadTestCase
+
+
+class IntegrationClusterTests(BaseIntegrationLoadTestCase):
+
+    MAX_EXEC_TIME = 60
+
+    def setUp(self):
+        super(IntegrationClusterTests, self).setUp()
+        self.env.create_nodes(self.NODES_NUM, api=True)
+        self.cluster = self.env.create_cluster(api=False)
+        controllers = 3
+        created_c = 0
+        nodes = []
+        self.nodes_ids = []
+        for node in self.env.nodes:
+            if created_c < controllers:
+                nodes.append({'id': node.id,
+                              'role': ['controller'],
+                              'cluster': self.cluster['id'],
+                              'pending_addition': True})
+            else:
+                nodes.append({'id': node.id,
+                              'role': ['compute'],
+                              'cluster': self.cluster['id'],
+                              'pending_addition': True})
+            self.nodes_ids.append(str(node.id))
+        self.put_handler('NodeCollectionHandler', nodes)
+
+    @fake_tasks(fake_rpc=False, mock_rpc=False)
+    @patch('nailgun.rpc.cast')
+    def test_deploy(self, mock_rpc):
+        self.provision(self.cluster['id'], self.nodes_ids)
+        self.deployment(self.cluster['id'], self.nodes_ids)

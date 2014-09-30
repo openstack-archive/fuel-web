04bb987bf5ff3543aeef817b75870a164a7190df
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2014-09-30 15:18:51 +0400

    Some fixes in fuel_agent
    Implements: blueprint image-based-provisioning
    Change-Id: I19f2ccc3a01fd6053c5ee557256a5d4a0acabda0

--- fuel_agent/fuel_agent/utils/fs_utils.py
+++ fuel_agent/fuel_agent/utils/fs_utils.py
@@ -31,10 +31,10 @@ def make_fs(fs_type, fs_options, fs_label, dev):
 
 
 def extend_fs(fs_type, fs_dev):
-    if fs_type in ('ext3', 'ext4'):
+    if fs_type in ('ext2', 'ext3', 'ext4'):
         # ext3,4 file system can be mounted
         # must be checked with e2fsck -f
-        utils.execute('e2fsck', ' -f', fs_dev, check_exit_code=[0])
+        utils.execute('e2fsck', '-yf', fs_dev, check_exit_code=[0])
         utils.execute('resize2fs', fs_dev, check_exit_code=[0])
     elif fs_type == 'xfs':
         # xfs file system must be mounted

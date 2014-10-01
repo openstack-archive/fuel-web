59eec58c036564f869e66bbb9b3becabcaf987b0
Ivan Kliuk <ivan.kliuk@gmail.com>
2014-10-01 06:32:44 +0300

    Refactoring JSON serialization of response object
        Swapping DRY code jsonutils.loads(webob.TestResponse.body) with
        webob.TestResponse.json_body property.
    
    Change-Id: I2f4e0cd3f1f8abc209e19491287f3630b1c4e80d
    Partial-Bug: #1304967

--- nailgun/nailgun/test/integration/test_put_same_json.py
+++ nailgun/nailgun/test/integration/test_put_same_json.py
@@ -14,7 +14,7 @@
 
 from mock import patch
 
-from nailgun.openstack.common import jsonutils as json
+from nailgun.openstack.common import jsonutils
 from nailgun.test import base
 
 
@@ -42,7 +42,7 @@ class TestPutSameJson(base.BaseIntegrationTest):
         """
         response = self.app.put(
             base.reverse(name, kwargs=arguments),
-            json.dumps(data),
+            jsonutils.dumps(data),
             headers=self.default_headers
         )
         self.assertEqual(response.status_code, expected_status)
@@ -55,7 +55,7 @@ class TestPutSameJson(base.BaseIntegrationTest):
             base.reverse(name, kwargs=arguments),
             headers=self.default_headers
         )
-        return json.loads(response.body)
+        return jsonutils.loads(response.body)
 
     def test_release(self):
         release = self.env.create_release()

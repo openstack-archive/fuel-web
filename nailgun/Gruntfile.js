5a97c74fcf1060ed6262a73b4915d567b23f6dec
Vitaly Kramskikh <vkramskikh@mirantis.com>
2014-10-03 22:45:34 +0700

    Integration with Intern
     - you need to start nailgun in fake mode nd have selenium server started
     - then start test by running `grunt intern`
    
    Change-Id: I21d342093f142a1bbe7b8ae4f2c96fad1c125a0b

--- nailgun/Gruntfile.js
+++ nailgun/Gruntfile.js
@@ -299,12 +299,29 @@ module.exports = function(grunt) {
                     moduleType: 'js'
                 }
             }
+        },
+        intern: {
+            unit: {
+                options: {
+                    config: 'ui_tests/intern',
+                    runType: 'runner',
+                    suites: ['ui_tests/unit/example']
+                }
+            },
+            functional: {
+                options: {
+                    config: 'ui_tests/intern',
+                    runType: 'runner',
+                    functionalSuites: ['ui_tests/functional/example']
+                }
+            }
         }
     });
 
     Object.keys(pkg.devDependencies)
         .filter(function(npmTaskName) { return npmTaskName.indexOf('grunt-') === 0; })
         .forEach(grunt.loadNpmTasks.bind(grunt));
+    grunt.loadNpmTasks('intern');
 
     grunt.registerTask('build', [
         'bower',
---
+++ nailgun/ui_tests/functional/example.js
@@ -0,0 +1,32 @@
+define([
+    'intern!object',
+    'intern/chai!assert',
+    'require',
+    'intern/dojo/node!leadfoot/helpers/pollUntil'
+], function (registerSuite, assert, require, pollUntil) {
+    registerSuite({
+        name: 'simple login screen test',
+
+        '#user login attempt': function () {
+            var credentials = 'admin';
+            return this.remote
+                .get('http://127.0.0.1:' + (process.env.SERVER_PORT || 8000))
+                .setFindTimeout(5000)
+                .findByCssSelector('.login-box input[type=text]')
+                    .click()
+                    .type(credentials)
+                    .end()
+                .findByCssSelector('.login-box input[type=password]')
+                    .click()
+                    .type(credentials)
+                    .end()
+                .findByCssSelector('.login-btn')
+                    .click()
+                    .end()
+                .getCurrentUrl()
+                .then(function (url) {
+                    assert.include(url, 'clusters');
+                });
+        }
+    });
+});

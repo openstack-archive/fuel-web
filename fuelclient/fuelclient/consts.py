4e73867c77dd1716d090008f7fa19ffa327cd074
Roman Prykhodchenko <me@romcheg.me>
2014-10-13 12:54:19 +0200

    Basic profiling facilities for CLI
    This patch implements a simple wrapper for
    profiling different CLI commands and saving
    the results.
    
    cProfile is used as a profiling tool; results
    are saved as a call-tree, textual statistics and
    a DOT-graph.
    
    In order to enable profiling NAILGUN_PERFORMANCE_PROFILING
    environment variable should be set to one of 1, True or Yes.
    The value of this variable is case-insensitive.
    
    DocImpact
    Blueprint: 100-nodes-support
    Change-Id: Ia9f6d19960fbe35e034fa85bf6303f3a419a7a75

---
+++ fuelclient/fuelclient/consts.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright 2013-2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from collections import namedtuple
+
+
+def Enum(*values, **kwargs):
+    names = kwargs.get('names')
+    if names:
+        return namedtuple('Enum', names)(*values)
+    return namedtuple('Enum', values)(*values)
+
+
+PERF_TEST_VAR = 'NAILGUN_PERFORMANCE_PROFILING'
+
+
+LOAD_TESTS_PATHS = Enum(
+    '/tmp/fuelclient_load_tests/tests/',
+    '/tmp/fuelclient_load_tests/tests/last/',
+    '/tmp/fuelclient_load_tests/results/',
+    names=(
+        'load_tests_base',
+        'last_load_test',
+        'load_tests_results',
+    )
+)

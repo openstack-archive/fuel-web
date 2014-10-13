4e50c01e185241ac7ae425a1bb9017ab2d9b9f45
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2014-10-13 18:42:34 +0400

    Some fixes in cloud-init templates in fuel-agent
    Added some new fields in config drive scheme.
    
    Change-Id: I76f36e555572a874f8f7b14951692927a7f00ddc
    Implements: blueprint image-based-provisioning

--- fuel_agent/fuel_agent/tests/test_configdrive.py
+++ fuel_agent/fuel_agent/tests/test_configdrive.py
@@ -40,13 +40,13 @@ class TestConfigDriveScheme(test_base.BaseTestCase):
 
     def test_template_data_ok(self):
         cd_common = configdrive.ConfigDriveCommon(
-            'ssh_auth_key', 'hostname', 'fqdn', 'name_servers',
+            ['auth_key1', 'auth_key2'], 'hostname', 'fqdn', 'name_servers',
             'search_domain', 'master_ip', 'master_url', 'udevrules',
             'admin_mac', 'admin_ip', 'admin_mask', 'admin_iface_name',
-            'timezone')
-        cd_puppet = configdrive.ConfigDrivePuppet('master')
+            'timezone', {'repo1': 'repo1_url', 'repo2': 'repo2_url'})
+        cd_puppet = configdrive.ConfigDrivePuppet('master', 0)
         cd_mcollective = configdrive.ConfigDriveMcollective(
-            'pskey', 'vhost', 'host', 'user', 'password', 'connector')
+            'pskey', 'vhost', 'host', 'user', 'password', 'connector', 1)
         self.cd_scheme.common = cd_common
         self.cd_scheme.puppet = cd_puppet
         self.cd_scheme.mcollective = cd_mcollective

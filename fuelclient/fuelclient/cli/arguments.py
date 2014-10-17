da299459434478816710bae375c2249ae3c8ed5e
Dima Shulyak <dshulyak@mirantis.com>
2014-10-17 14:19:50 +0300

    Add cli commands to interact with plugins
    - List all plugins installed
    fuel plugins
    fuel plugins --list
    
    - Install plugin
    fuel plugins --install --plugin /tmp/sample.pb
    1. Extracts metadata from plugin tar
    2. If plugin successfully registered in nailgun
    3. Untar all the files in /var/www/nailgun/plugins
    
    - If you want to update plugin when it is already installed
      add --force flag
    fuel plugins --install --plugin /tmp/sample --force
    The procedure is the same as for simple install, but
    if plugin already registered - it will try to update
    (send PUT request)
    
    - Refactored tests to allow writing unit tests, with mock
      and all that goodness
    
    related to blueprint cinder-neutron-plugins-in-fuel
    
    Change-Id: I642a300abcb9c50ef5039a343064f2ac78e39cd8

--- fuelclient/fuelclient/cli/arguments.py
+++ fuelclient/fuelclient/cli/arguments.py
@@ -367,3 +367,15 @@ def get_task_arg(help_msg):
         "help": help_msg
     }
     return get_arg("task", **default_kwargs)
+
+
+def get_plugin_arg(help_msg):
+    return get_str_arg(
+        "plugin",
+        flags=("--plugin",),
+        help=help_msg
+    )
+
+
+def get_plugin_install_arg(help_msg):
+    return get_boolean_arg("install", help=help_msg)

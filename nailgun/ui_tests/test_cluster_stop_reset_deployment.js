ef35e86d9480d7f1ae06ee65cc7afa2d1d2b521c
Julia Aranovich <jkirnosova@mirantis.com>
2014-10-27 14:15:27 +0400

    Fix for UI casper tests
    Change-Id: I2fcfde273fedd4bcac83b8b7e79704b7d2db4a3d

--- nailgun/ui_tests/test_cluster_stop_reset_deployment.js
+++ nailgun/ui_tests/test_cluster_stop_reset_deployment.js
@@ -35,8 +35,8 @@ casper.then(function() {
 
 casper.then(function() {
     this.test.comment('Testing adding node with controller role');
-    this.click('.cluster-toolbar  .btn-add-nodes');
-    this.test.assertSelectorAppears('.roles-panel', 'Add controller nodes screen appears');
+    this.click('.cluster-toolbar .btn-add-nodes');
+    this.test.assertSelectorAppears('.node-box', 'Add Nodes screen appeared and unallocated nodes loaded');
     this.then(function() {
         this.test.assertEvalEquals(function() {return $('.node-box .node-content').length}, 1, 'Number of unallocated nodes is correct');
         this.evaluate(function() {

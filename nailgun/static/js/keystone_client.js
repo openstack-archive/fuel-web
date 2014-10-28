27b408e4df2fa2c1eb4fd8f78d1aabde02d9ea5e
Przemyslaw Kaminski <pkaminski@mirantis.com>
2014-10-28 10:12:37 +0100

    Fix KeystoneClient issue when token was expired
    KeystoneClient should always first try to use username/password and only if
    they are not provided, do a fallback to this.token. Otherwise for expired
    tokens login is not possible.
    
    Related-Bug: #1375622
    (cherry picked from commit d8d9d96608f759d765ec0b1016e7363229ffc35a)
    
    Change-Id: Ibdaff8d8b7e71e624776d39c95aa8922f1872306

--- nailgun/static/js/keystone_client.js
+++ nailgun/static/js/keystone_client.js
@@ -33,13 +33,13 @@ define(['jquery', 'underscore'], function($, _) {
                 return $.Deferred().resolve();
             }
             var data = {auth: {}};
-            if (this.token) {
-                data.auth.token = {id: this.token};
-            } else if (username && password) {
+            if (username && password) {
                 data.auth.passwordCredentials = {
                     username: username,
                     password: password
                 };
+            } else if (this.token) {
+                data.auth.token = {id: this.token};
             } else {
                 return $.Deferred().reject();
             }
@@ -63,7 +63,7 @@ define(['jquery', 'underscore'], function($, _) {
                 } catch(e) {
                     return $.Deferred().reject();
                 }
-            }, this), _.bind(function(xhr, status, error) {
+            }, this)).fail(_.bind(function(xhr, status, error) {
                 delete this.tokenUpdateTime;
             }, this)).always(_.bind(function() {
                 delete this.tokenUpdateRequest;

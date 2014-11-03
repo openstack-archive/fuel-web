f3ee3ffcfd4228a617b7b609d4ce99de725e1529
Dima Shulyak <dshulyak@mirantis.com>
2014-11-03 15:23:44 +0200

    Explicitly requeue rabbitmq message in case process interupted
    In case if process is interrupted excplit requeue will be
    requested
    
    Dont think that we should consider dead-letter exchanges
    at this point, becauce messaging traffic is low, and only
    thing that we need is reliably process message after
    receiverd restarted
    
    DocImpact
    Change-Id: If530049b6e519ceb03de0cfd82c3449cdb616f50
    Related-Bug: 1383511

---
+++ nailgun/nailgun/test/unit/test_rpc_acknowledge.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from nailgun.errors import errors
+from nailgun.rpc import receiverd
+from nailgun.test import base
+
+
+class TestRpcAcknowledge(base.BaseTestCase):
+
+    def setUp(self):
+        super(TestRpcAcknowledge, self).setUp()
+        self.receiver = mock.Mock()
+        self.connection = mock.Mock()
+        self.consumer = receiverd.RPCConsumer(self.connection, self.receiver)
+        self.msg = mock.Mock()
+        self.body = {'method': 'test',
+                     'args': {}}
+
+    def test_message_acked_if_success(self):
+        self.consumer.consume_msg(self.body, self.msg)
+        self.assertEqual(self.msg.ack.call_count, 1)
+        self.assertEqual(self.receiver.test.call_count, 1)
+
+    def test_message_acked_if_no_task_found(self):
+        def raise_no_task_found(*args, **kwargs):
+            raise errors.NoTaskFound()
+        self.receiver.test.side_effect = raise_no_task_found
+        self.consumer.consume_msg(self.body, self.msg)
+        self.assertEqual(self.receiver.test.call_count, 1)
+        self.assertEqual(self.msg.ack.call_count, 1)
+
+    def test_message_acked_if_exception(self):
+        def raise_exception(*args, **kwargs):
+            raise Exception
+        self.receiver.test.side_effect = raise_exception
+        self.consumer.consume_msg(self.body, self.msg)
+        self.assertEqual(self.receiver.test.call_count, 1)
+        self.assertEqual(self.msg.ack.call_count, 1)
+
+    def test_message_requeued_in_case_of_interrupt(self):
+        def raise_keyboard_interrupt(*args, **kwargs):
+            raise KeyboardInterrupt
+        self.receiver.test.side_effect = raise_keyboard_interrupt
+        self.consumer.consume_msg(self.body, self.msg)
+        self.assertEqual(self.msg.ack.call_count, 0)
+        self.assertEqual(self.msg.requeue.call_count, 1)

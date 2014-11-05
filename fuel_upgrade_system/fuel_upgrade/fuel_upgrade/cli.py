6b7765fc01e2ae06b1954c0cde38789eac47ecc4
Przemyslaw Kaminski <pkaminski@mirantis.com>
2014-11-05 13:20:48 +0100

    fuel-upgrade: add --check-only option
    This is used to perform preliminary upgrade checks only and not perform any
    upgrade.
    
    Change-Id: I205b97678efc9ee888bcc893a9d5e98b85f0db1c
    Related-Bug: #1389667

--- fuel_upgrade_system/fuel_upgrade/fuel_upgrade/cli.py
+++ fuel_upgrade_system/fuel_upgrade/fuel_upgrade/cli.py
@@ -90,6 +90,9 @@ def parse_args(args):
     parser.add_argument(
         '--src', required=True, help='path to update file')
     parser.add_argument(
+        '--check-only', action='store_true',
+        help='only perform pre-upgrade checks')
+    parser.add_argument(
         '--no-checker', action='store_true',
         help='do not check before upgrade')
     parser.add_argument(
@@ -131,19 +134,24 @@ def is_engine_in_list(engines_list, engine_class):
     return False
 
 
-def run_upgrade(args):
-    """Run upgrade on master node
+def check(config, upgraders_to_use):
+    """Perform pre-upgrade checks.
 
     :param args: argparse object
     """
-    # Get admin password
-    if not args.password:
-        args.password = getpass.getpass('Admin Password: ')
+    # Initialize checkers
+    checker_manager = CheckerManager(upgraders_to_use, config)
+    checker_manager.check()
 
     # recheck pasword again
     if not args.password:
         raise errors.CommandError(messages.no_password_provided)
 
+def run_upgrade(args):
+    """Run upgrade on master node
+
+    :param args: argparse object
+    """
     # Initialize config
     config = build_config(args.src, args.password)
     logger.debug('Configuration data: {0}'.format(config))
@@ -153,10 +161,11 @@ def run_upgrade(args):
         SUPPORTED_SYSTEMS[system](config)
         for system in args.systems]
 
-    # Initialize checkers
-    if not args.no_checker:
-        checker_manager = CheckerManager(upgraders_to_use, config)
-        checker_manager.check()
+    if not args.no_checker or args.check_only:
+        check(config, upgraders_to_use)
+
+    if args.check_only:
+        sys.exit()
 
     # Initialize pre upgrade hook manager
     hook_manager = PreUpgradeHookManager(upgraders_to_use, config)

212866da377483b7487bc46f433cd0ce4ebd7074
Dima Shulyak <dshulyak@mirantis.com>
2014-11-05 13:06:24 +0200

    Add support for release network_metadata serialziation
    Patch adds support for release network metadata serialization,
    by adding additional handler on release/<release_id>/networks for PUT/GET
    requestss
    Proper validation for release networking data
    
    Why not to add another field in release handler:
    1. networks_metadata it is ours internal naming
    2. url release/<release_id>/networks makes more sense for client
      applications
    
    Change-Id: I205ec786f1a9d772905ff22e426c1ce8a6d4d6e7
    Closes-Bug: 1381557

---
+++ nailgun/nailgun/api/v1/validators/json_schema/networks.py
@@ -0,0 +1,63 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+# Non-complete JSON Schema for validating IP addresses.
+# Use it for better readability in the main schema.
+_IP_ADDRESS_SCHEMA = {
+    'type': 'string',
+    'format': 'ipv4',
+}
+
+
+# Non-complete JSON schema for validating NET addresses.
+# Use it for better readability in the main schema.
+_NET_ADDRESS_SCHEMA = {
+    'type': 'string',
+
+    # check for valid ip address and route prefix
+    # e.g: 192.168.0.0/24
+    'pattern': '^(({octet}\.){{3}}{octet})({prefix})?$'.format(
+        octet='(2(5[0-5]|[0-4][0-9])|[01]?[0-9][0-9]?)',
+        prefix='/(3[012]|[12]?[0-9])'
+    ),
+}
+
+
+# Non-complete JSON Schema for validating MAC addresses.
+# Use it for better readability in the main schema.
+_MAC_ADDRESS_SCHEMA = {
+    'type': 'string',
+    'pattern': '^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$',
+}
+
+
+NETWORK = {
+    'type': 'object',
+    'properties': {
+        'name': {'type': 'string'},
+        'cidr': _NET_ADDRESS_SCHEMA,
+        'gateway': _IP_ADDRESS_SCHEMA,
+        'ip_range': {'type': 'array'},
+        'vlan_start': {'type': ['null', 'number']},
+        'use_gateway': {'type': 'boolean'},
+        'notation': {'type': ['string', 'null']},
+        'render_type': {'type': ['null', 'string']},
+        'map_priority': {'type': 'number'},
+        'configurable': {'type': 'boolean'},
+        'floating_range_var': {'type': 'string'},
+        'ext_net_data': {'type': 'array'},
+        'assign_vip': {'type': 'boolean'}},
+    'required': ['name']
+}

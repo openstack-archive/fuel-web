54b0c6277255f6061382a596a53b399f2ab13917
Dima Shulyak <dshulyak@mirantis.com>
2014-11-06 15:12:06 +0200

    Add cli commands to work with release networks
    Change-Id: I774ab8341b45a86bbf77d266aa788424936776cc

--- fuelclient/fuelclient/tests/test_release_networks_action.py
+++ fuelclient/fuelclient/tests/test_release_networks_action.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+#
 #    Copyright 2014 Mirantis, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -12,23 +14,21 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from fuelclient.objects.base import BaseObject
+
+from mock import Mock
+from mock import patch
+
+from fuelclient.tests import base
+
+DATA = """
+name: sample
+version: 0.1.0
+"""
 
 
-class Release(BaseObject):
+@patch('fuelclient.client.requests')
+class TestReleaseNetworkActions(base.UnitTestCase):
 
-    class_api_path = "releases/"
-    instance_api_path = "releases/{0}/"
-    networks_path = 'releases/{0}/networks'
-
-    @classmethod
-    def get_all(cls):
-        map(cls.init_with_data, cls.get_all_data())
-
-    def get_networks(self):
-        url = self.networks_path.format(self.id)
-        return self.connection.get_request(url)
-
-    def update_networks(self, data):
-        url = self.networks_path.format(self.id)
-        return self.connection.put_request(url, data)
+    def test_release_network_download(self, mrequests):
+        self.execute(['fuel', 'rel', '--rel', '1', '--network', '--download'])
+        mrequests.get.return_value = Mock(return_value={'config': 'nova_network'})

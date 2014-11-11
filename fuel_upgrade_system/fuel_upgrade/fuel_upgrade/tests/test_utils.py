767411caca9afbbabf9110bdbed957fcce28c561
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2014-11-11 18:47:05 +0300

    Added targetimages into upgrade script
    Change-Id: If408b5e13a02e5531583a9ae64704efa435fa5a3
    Implements: blueprint image-based-provisioning

--- fuel_upgrade_system/fuel_upgrade/fuel_upgrade/tests/test_utils.py
+++ fuel_upgrade_system/fuel_upgrade/fuel_upgrade/tests/test_utils.py
@@ -282,6 +282,23 @@ class TestUtils(BaseTestCase):
         self.called_once(remove_mock)
 
     @mock.patch('fuel_upgrade.utils.os.path.exists', return_value=True)
+    @mock.patch('fuel_upgrade.utils.symlink')
+    def test_symlink_if_src_exists_ok(self, symlink_mock, _):
+        from_path = '/tmp/from/path'
+        to_path = '/tmp/to/path'
+        utils.symlink_if_src_exists(from_path, to_path)
+        symlink_mock.assert_called_once_with(from_path, to_path,
+                                             overwrite=True)
+
+    @mock.patch('fuel_upgrade.utils.os.path.exists', return_value=False)
+    @mock.patch('fuel_upgrade.utils.symlink')
+    def test_symlink_src_not_exists(self, symlink_mock, _):
+        from_path = '/tmp/from/path'
+        to_path = '/tmp/to/path'
+        utils.symlink_if_src_exists(from_path, to_path)
+        self.assertItemsEqual(symlink_mock.call_args_list, [])
+
+    @mock.patch('fuel_upgrade.utils.os.path.exists', return_value=True)
     @mock.patch('fuel_upgrade.utils.os.remove')
     def test_remove_if_exists(self, remove_mock, exists_mock):
         path = '/tmp/some/path'

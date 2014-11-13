4040d50c5664c21130bb6668ac1d2378f68f30b3
Alexander Kislitsky <akislitsky@mirantis.com>
2014-11-13 18:27:47 +0400

    Fix for filtering attributes for statistics
    Change-Id: If9c1db5bde010b05fd6555da74f498f8af883f7d
    Blueprint: send-anon-usage

--- nailgun/nailgun/test/unit/test_statistics.py
+++ nailgun/nailgun/test/unit/test_statistics.py
@@ -46,8 +46,29 @@ class TestStatistics(BaseTestCase):
         libvirt_type = libvirt_dict['value']
 
         self.assertEquals('qemu', libvirt_type)
-        self.assertEquals({'libvirt_type': 'qemu'},
-                          info.get_attributes(cluster.attributes.editable))
+        self.assertDictEqual({'libvirt_type': 'qemu'},
+                             info.get_attributes(cluster.attributes.editable))
+
+    def test_get_empty_attributes(self):
+        info = InstallationInfo()
+        trash_attrs = {'some': 'trash', 'nested': {'n': 't'}}
+        result = info.get_attributes(trash_attrs)
+        self.assertDictEqual({}, result)
+
+    def test_get_attributes_exception_handled(self):
+        info = InstallationInfo()
+        variants = [
+            None,
+            [],
+            {},
+            {'common': None},
+            {'common': []},
+            {'common': {'libvirt_type': {}}},
+            {'common': {'libvirt_type': 3}},
+        ]
+        for attrs in variants:
+            result = info.get_attributes(attrs)
+            self.assertDictEqual({}, result)
 
     def test_clusters_info(self):
         info = InstallationInfo()

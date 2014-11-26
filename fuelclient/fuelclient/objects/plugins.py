80b9139056201d12a65442a823b79955db01e056
Andrew Woodward <awoodward@mirantis.com>
2014-11-26 00:13:32 -0800

    Add install script support for plugins
    In some cases, plugins will need to be able to run install scripts. This
     adds the ability to specify an 'install_script' in the metadata to run
     during install
    
    Change-Id: Ieaf2f8e0f208176b39f6de3d098a4675e853dd70
    Closes-bug: 1395228

--- fuelclient/fuelclient/objects/plugins.py
+++ fuelclient/fuelclient/objects/plugins.py
@@ -13,6 +13,7 @@
 #    under the License.
 
 import os
+import subprocess
 import tarfile
 
 import yaml
@@ -45,6 +46,29 @@ class Plugins(base.BaseObject):
             plugin_tar.name, cls.metadata_config))
 
     @classmethod
+    def run_script(cls, cmd, cwd=None):
+        child = subprocess.popen(
+            cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            shell=True,
+            cwd=cwd)
+
+        if self.connection.debug:
+            for each in child.stdout:
+                self.connection.print_debug(each.rstrip())
+
+        child.wait()
+
+        if exit_code != 0:
+            raise error.GenericProcessingError(
+                u'Shell command executed with "{0}" '
+                'exit code: {1} '.format(exit_code, cmd))
+
+        self.connection.print_debug(
+            u'Command "{0}" successfully executed'.format(cmd))
+
+    @classmethod
     def add_plugin(cls, plugin_meta, plugin_tar):
         return plugin_tar.extractall(EXTRACT_PATH)
 
@@ -65,6 +89,15 @@ class Plugins(base.BaseObject):
             else:
                 resp.raise_for_status()
             cls.add_plugin(metadata, plugin_tar)
+            install_script = metadata.get('install_script')
+            if install_script:
+                path = ''.join((
+                    EXTRACT_PATH,
+                    metadata.get('name',''),
+                    '-',
+                    metadata.get('version', ''),
+                    '/',))
+                cls.run_script(cmd=path + install_script, cwd=path)
         finally:
             plugin_tar.close()
         return resp

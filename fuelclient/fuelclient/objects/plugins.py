14d87bffbcae55de149f1f355f91354542239c5c
Andrew Woodward <awoodward@mirantis.com>
2014-11-26 00:13:32 -0800

    Add install script support for plugins
    In some cases, plugins will need to be able to run install scripts. This
     adds the ability to specify an 'install_script' in the metadata to run
     during install
    
    Change-Id: Ieaf2f8e0f208176b39f6de3d098a4675e853dd70
    Closes-bug: 1395228

--- fuelclient/fuelclient/objects/plugins.py
+++ fuelclient/fuelclient/objects/plugins.py
@@ -13,6 +13,7 @@
 #    under the License.
 
 import os
+import subprocess
 import tarfile
 
 import yaml
@@ -45,6 +46,14 @@ class Plugins(base.BaseObject):
             plugin_tar.name, cls.metadata_config))
 
     @classmethod
+    def run_script(cls, cmd, cwd=None):
+        output = subprocess.check_output(cmd, cwd=cwd)
+
+        if cls.connection.debug:
+            for each in output:
+                cls.connection.print_debug(each.rstrip())
+
+    @classmethod
     def add_plugin(cls, plugin_meta, plugin_tar):
         return plugin_tar.extractall(EXTRACT_PATH)
 
@@ -65,6 +74,15 @@ class Plugins(base.BaseObject):
             else:
                 resp.raise_for_status()
             cls.add_plugin(metadata, plugin_tar)
+            install_script = metadata.get('install_script')
+            if install_script:
+                path = ''.join((
+                    EXTRACT_PATH,
+                    metadata.get('name', ''),
+                    '-',
+                    metadata.get('version', ''),
+                    '/',))
+                cls.run_script(cmd=path + install_script, cwd=path)
         finally:
             plugin_tar.close()
         return resp

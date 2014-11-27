a5b70006aa5811276066110b46c9f2ca882d60db
Nikolay Markov <nmarkov@mirantis.com>
2014-11-27 16:45:06 +0300

    Wrap errors to JSON in Nailgun API
    Partial-Bug: #1391846
    Implements blueprint nailgun-wrap-errors-json
    Related blueprint: nailgun-api-requests-responses-validation
    
    Change-Id: Ie351d6cd6a5ebf929563a2e0c76ef875fa71c2d3

--- nailgun/nailgun/test/integration/test_node_collection_handlers.py
+++ nailgun/nailgun/test/integration/test_node_collection_handlers.py
@@ -155,32 +155,10 @@ class TestHandlers(BaseIntegrationTest):
             headers=self.default_headers,
             expect_errors=True)
         self.assertEqual(resp.status_code, 400)
-        self.assertEqual(resp.body, "Neither MAC nor ID is specified")
-
-        resp = self.app.put(
-            reverse('NodeCollectionHandler'),
-            jsonutils.dumps([{'mac': None,
-                              'manufacturer': 'man1'}]),
-            headers=self.default_headers,
-            expect_errors=True)
-        self.assertEqual(resp.status_code, 400)
-
-        resp = self.app.put(
-            reverse('NodeCollectionHandler'),
-            jsonutils.dumps([{'id': None,
-                              'manufacturer': 'man2'}]),
-            headers=self.default_headers,
-            expect_errors=True)
-        self.assertEqual(resp.status_code, 400)
-
-        resp = self.app.put(
-            reverse('NodeCollectionHandler'),
-            jsonutils.dumps([{'mac': None,
-                              'id': None,
-                              'manufacturer': 'man3'}]),
-            headers=self.default_headers,
-            expect_errors=True)
-        self.assertEqual(resp.status_code, 400)
+        self.assertEqual(
+            resp.json_body["message"],
+            "Neither MAC nor ID is specified"
+        )
 
         resp = self.app.put(
             reverse('NodeCollectionHandler'),
@@ -190,6 +168,14 @@ class TestHandlers(BaseIntegrationTest):
             headers=self.default_headers,
             expect_errors=True)
         self.assertEqual(resp.status_code, 400)
+        self.assertIn(
+            "schema['properties']['mac']",
+            resp.json_body["message"]
+        )
+        self.assertIn(
+            "None is not of type 'string'",
+            resp.json_body["message"]
+        )
 
         resp = self.app.put(
             reverse('NodeCollectionHandler'),
@@ -232,7 +218,10 @@ class TestHandlers(BaseIntegrationTest):
             headers=self.default_headers,
             expect_errors=True)
         self.assertEqual(resp.status_code, 400)
-        self.assertEqual(resp.body, "Invalid ID specified")
+        self.assertEqual(
+            resp.json_body["message"],
+            "Invalid ID specified"
+        )
 
     def test_node_update_agent_discover(self):
         self.env.create_node(
@@ -364,7 +353,7 @@ class TestHandlers(BaseIntegrationTest):
             api=True,
             exclude=["mac"],
             expect_http=400,
-            expect_message="No mac address specified"
+            expected_error="No mac address specified"
         )
         self.assertEqual(node, None)
 

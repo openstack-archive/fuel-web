d5c4ff3df952a92136a27e6867694fc3655ba21f
Vitaly Kramskikh <vkramskikh@mirantis.com>
2014-12-05 19:46:06 +0300

    BackboneViewWrapper PoC
    Change-Id: Ibc19adb631c6f9b28d26174fa1749bdc28ea1b34

--- nailgun/static/js/views/cluster_page.js
+++ nailgun/static/js/views/cluster_page.js
@@ -18,6 +18,7 @@ define(
     'react',
     'utils',
     'models',
+    'jsx!js/backbone_view_wrapper',
     'views/common',
     'jsx!views/cluster_page_subviews',
     'jsx!views/dialogs',
@@ -29,7 +30,7 @@ define(
     'jsx!views/cluster_page_tabs/healthcheck_tab',
     'text!templates/cluster/page.html'
 ],
-function(React, utils, models, commonViews, clusterPageSubviews, dialogViews, NodesTab, NetworkTab, SettingsTab, LogsTab, ActionsTab, HealthCheckTab, clusterPageTemplate) {
+function(React, utils, models, BackboneViewWrapper, commonViews, clusterPageSubviews, dialogViews, NodesTab, NetworkTab, SettingsTab, LogsTab, ActionsTab, HealthCheckTab, clusterPageTemplate) {
     'use strict';
 
     var ClusterPage = commonViews.Page.extend({
@@ -150,7 +151,7 @@ function(React, utils, models, commonViews, clusterPageSubviews, dialogViews, No
 
             var tabs = {
                 nodes: NodesTab,
-                network: NetworkTab,
+                network: BackboneViewWrapper(NetworkTab),
                 settings: SettingsTab,
                 actions: ActionsTab,
                 logs: LogsTab,

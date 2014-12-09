bd9329e4d7b2f0d3c1f4de5eedc9117ed786a7d5
Dmitry Shulyak <dshulyak@mirantis.com>
2014-12-09 19:37:49 +0200

    Rsync puppet/Upload repos tasks serialization
    Implemented backbone for pre/post task serializers,
    with few examples for pre task serializers
    
    - Rsync puppet
    uses mask in arguments, that is processed in serializer
    - Upload repos
    For given task in config - serializes number of tasks,
    one task for each repo in repo_metadata, and one task for
    regenerating metadata after repo was added
    
    implements blueprint granular-deployment-based-on-tasks
    Change-Id: I3baf1b06b7c0fa9d712edff5283c9854a7b26431

--- nailgun/nailgun/orchestrator/deployment_graph.py
+++ nailgun/nailgun/orchestrator/deployment_graph.py
@@ -23,7 +23,7 @@ from nailgun.errors import errors
 from nailgun.logger import logger
 from nailgun import objects
 from nailgun.orchestrator import priority_serializers as ps
-from nailgun.orchestrator import tasks_templates as templates
+from nailgun.orchestrator.tasks_serializer import TaskSerializers
 
 
 class DeploymentGraph(nx.DiGraph):
@@ -155,6 +155,7 @@ class AstuteGraph(object):
         self.tasks = objects.Cluster.get_deployment_tasks(cluster)
         self.graph = DeploymentGraph()
         self.graph.add_tasks(self.tasks)
+        self.serializers = TaskSerializers.init_default()
 
     def only_tasks(self, task_ids):
         self.graph.only_tasks(task_ids)
@@ -269,7 +270,23 @@ class AstuteGraph(object):
             processed_groups.update(current_groups)
             current_groups = groups_subgraph.get_next_groups(processed_groups)
 
-    def serialize_tasks(self, node):
+    def pre_tasks_serialize(self, nodes):
+        """Serialize tasks for pre_deployment hook
+
+        :param nodes: list of node db objects
+        """
+        tasks = self.graph.get_tasks(consts.STAGES.pre_deployment).topology
+        serialized = []
+        for task in tasks:
+            serializer = self.serializers.get_stage_serializer(task)(
+                task, self.cluster, nodes)
+            if not serializer.should_execute():
+                continue
+            for task in serializer.serialize():
+                serialized.append(task)
+        return serialized
+
+    def deploy_task_serialize(self, node):
         """Serialize tasks with necessary for orchestrator attributes
 
         :param node: dict with serialized node
@@ -278,14 +295,12 @@ class AstuteGraph(object):
         serialized = []
         priority = ps.Priority()
         for task in tasks:
-            if task['type'] == consts.ORCHESTRATOR_TASK_TYPES.puppet:
-                item = templates.make_puppet_task(
-                    [node['uid']],
-                    task)
-            elif task['type'] == consts.ORCHESTRATOR_TASK_TYPES.shell:
-                item = templates.make_shell_task(
-                    [node['uid']],
-                    task)
-            item['priority'] = priority.next()
-            serialized.append(item)
+            serializer = self.serializers.get_deploy_serializer(task)(
+                task, node)
+
+            if not serializer.should_execute():
+                continue
+            for item in serializer.serialize():
+                item['priority'] = priority.next()
+                serialized.append(item)
         return serialized

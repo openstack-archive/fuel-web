ccd9531b929a371de1482a0e6cbb68007a37da36
Dmitry Shulyak <dshulyak@mirantis.com>
2014-12-09 19:37:49 +0200

    Rsync puppet/Upload repos tasks serialization
    Implemented backbone for pre/post task serializers,
    with few examples for pre task serializers
    
    - Rsync puppet
    uses mask in arguments, that is processed in serializer
    - Upload repos
    For given task in config - serializes number of tasks,
    one task for each repo in repo_metadata, and one task for
    regenerating metadata after repo was added
    
    implements blueprint granular-deployment-based-on-tasks
    Change-Id: I3baf1b06b7c0fa9d712edff5283c9854a7b26431

--- nailgun/nailgun/orchestrator/graph_configuration.py
+++ nailgun/nailgun/orchestrator/graph_configuration.py
@@ -19,6 +19,8 @@
 DEPLOYMENT_CURRENT = """
 - id: deploy
   type: stage
+- id: pre_deployment
+  type: stage
 - id: primary-controller
   type: group
   role: [primary-controller]
@@ -92,11 +94,31 @@ DEPLOYMENT_CURRENT = """
     puppet_manifest: /etc/puppet/manifests/site.pp
     puppet_modules: /etc/puppet/modules
     timeout: 3600
+
+#PREDEPLOYMENT HOOKS
+
+#after we will introduce condition engine - it will be changed
+- id: upload_mos_repos
+  type: upload_file
+  role: '*'
+  stage: pre_deployment
+
+- id: rsync_mos_puppet
+  type: sync
+  role: '*'
+  stage: pre_deployment
+  requires: [upload_mos_repos]
+  parameters:
+    src: rsync://{MASTER_IP}:/puppet/{OPENSTACK_VERSION}/
+    dst: /etc/puppet
+    timeout: 180
 """
 
 DEPLOYMENT_50 = """
 - id: deploy
   type: stage
+- id: pre_deployment
+  type: stage
 - id: primary-controller
   type: group
   role: [primary-controller]
@@ -169,10 +191,30 @@ DEPLOYMENT_50 = """
     puppet_manifest: /etc/puppet/manifests/site.pp
     puppet_modules: /etc/puppet/modules
     timeout: 3600
+
+#PREDEPLOYMENT HOOKS
+
+#after we will introduce condition engine - it will be changed
+- id: upload_mos_repos
+  type: upload_file
+  role: '*'
+  stage: pre_deployment
+
+- id: rsync_mos_puppet
+  type: sync
+  role: '*'
+  stage: pre_deployment
+  requires: [upload_mos_repos]
+  parameters:
+    src: /etc/puppet/{OPENSTACK_VERSION}/
+    dst: /etc/puppet
+    timeout: 180
 """
 
 PATCHING = """
 - id: deploy
+  type: stage
+- id: pre_deployment
   type: stage
 - id: primary-controller
   type: group
@@ -246,4 +288,22 @@ PATCHING = """
     puppet_manifest: /etc/puppet/manifests/site.pp
     puppet_modules: /etc/puppet/modules
     timeout: 3600
+
+#PREDEPLOYMENT HOOKS
+
+#after we will introduce condition engine - it will be changed
+- id: upload_mos_repos
+  type: upload_file
+  role: '*'
+  stage: pre_deployment
+
+- id: rsync_mos_puppet
+  type: sync
+  role: '*'
+  stage: pre_deployment
+  requires: [upload_mos_repos]
+  parameters:
+    src: /etc/puppet/{OPENSTACK_VERSION}/
+    dst: /etc/puppet
+    timeout: 180
 """

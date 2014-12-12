639de5d3f25e840860c6755f78ce636829777d38
iberezovskiy <iberezovskiy@mirantis.com>
2014-12-12 14:58:16 +0300

    Add API checks for environment with external mongo
    Partial implements blueprint external-mongodb-support
    
    Change-Id: Ia164a45ef8f0efc071b2f3b8826c979c9a381393

---
+++ nailgun/nailgun/test/unit/test_mongo_nodes.py
@@ -0,0 +1,100 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nailgun.consts import TASK_NAMES
+from nailgun.db.sqlalchemy.models import Task
+from nailgun.errors import errors
+from nailgun import objects
+from nailgun.task.task import CheckBeforeDeploymentTask
+from nailgun.test import base
+
+
+class TestMongoNodes(base.BaseTestCase):
+
+    def update_meta(self, ceilometer, mongo):
+        attr_meta = self.env.get_default_attributes_metadata()
+        attr_meta['editable']['additional_components'].update({
+            "ceilometer": {"value": ceilometer},
+            "mongo": {"value": mongo}
+        })
+        return attr_meta
+
+    def test_get_mongo_nodes(self):
+        self.env.create(
+            nodes_kwargs=[{}]
+        )
+        cluster = self.env.clusters[0]
+        nodes = objects.Cluster.get_mongo_nodes(cluster)
+        self.assertEqual(len(nodes), 0)
+
+        self.env.create(
+            nodes_kwargs=[
+                {'pending_roles': ['mongo'],
+                 'status': 'discover',
+                 'pending_addition': True},
+                {'pending_roles': ['mongo'],
+                 'status': 'ready',
+                 'pending_addition': True}
+            ]
+        )
+        cluster = self.env.clusters[1]
+        nodes = objects.Cluster.get_mongo_nodes(cluster)
+        self.assertEqual(len(nodes), 2)
+
+    def test_check_mongo_nodes_with_ext_mongo(self):
+        self.env.create(
+            release_kwargs={
+                'attributes_metadata': self.update_meta(True, True)},
+            nodes_kwargs=[
+                {'pending_roles': ['mongo'],
+                 'status': 'discover',
+                 'pending_addition': True}
+            ]
+        )
+        cluster = self.env.clusters[0]
+        task = Task(name=TASK_NAMES.deploy, cluster=cluster)
+        self.assertRaises(errors.ExtMongoCheckerError,
+                          CheckBeforeDeploymentTask._check_mongo_nodes,
+                          task)
+
+        self.env.create(
+            release_kwargs={
+                'attributes_metadata': self.update_meta(True, True)},
+            nodes_kwargs=[]
+        )
+        cluster = self.env.clusters[1]
+        task = Task(name=TASK_NAMES.deploy, cluster=cluster)
+        self.assertIsNone(CheckBeforeDeploymentTask._check_mongo_nodes(task))
+
+        self.env.create(
+            release_kwargs={
+                'attributes_metadata': self.update_meta(True, False)},
+            nodes_kwargs=[]
+        )
+        cluster = self.env.clusters[2]
+        task = Task(name=TASK_NAMES.deploy, cluster=cluster)
+        self.assertIsNone(CheckBeforeDeploymentTask._check_mongo_nodes(task))
+
+        self.env.create(
+            release_kwargs={
+                'attributes_metadata': self.update_meta(True, False)},
+            nodes_kwargs=[
+                {'pending_roles': ['mongo'],
+                 'status': 'discover',
+                 'pending_addition': True}
+            ]
+        )
+        cluster = self.env.clusters[3]
+        task = Task(name=TASK_NAMES.deploy, cluster=cluster)
+        self.assertIsNone(CheckBeforeDeploymentTask._check_mongo_nodes(task))

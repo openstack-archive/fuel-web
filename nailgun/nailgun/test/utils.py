e7bc6832f4aa78d2813a46cbbec30884688182b5
Kamil Sambor <ksambor@mirantis.com>
2014-12-23 13:11:08 +0100

    Improve running performance unit tests
    * added decorator which run test multiple times and evaluate results
    * added helping functions to read and write statistics from tests
    * modified node performance tests to work faster
    
    Change-Id: I09c11ceeed42a747129239bcd8d90159d17c22ba

---
+++ nailgun/nailgun/test/utils.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import random
+import six
+import string
+
+
+def random_string(lenght, charset=None):
+    """Returns a random string of the specified length
+
+    :param length:  The length of the resulting string.
+    :type lenght:   int.
+    :param charset: A set of characters to use for building random strings.
+    :type charset:  Iterable object. Default: ASCII letters and digits.
+    :return:        str
+
+    """
+    charset = charset or string.ascii_letters + string.digits
+
+    return ''.join([str(random.choice(charset))
+                    for i in six.moves.range(lenght)])

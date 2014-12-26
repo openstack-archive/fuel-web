f680a0764ac7797bae6ab3e2e1d9b985ca41668d
Artem Roma <aroma@mirantis.com>
2014-12-26 19:19:57 +0200

    Add collection of info about VMs to OpenStackInfoCollector
    OpenStackInfoCollector class remastered so that it now provides
    requested info about different OpenStack components.
    
    Change-Id: I2bcf5da19ae68f87b72fb0ca3581b7f233ed271a
    Implements: blueprint send-anon-usage

---
+++ nailgun/nailgun/statistics/utils.py
@@ -0,0 +1,62 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+import six
+
+from contextlib import contextmanager
+
+from nailgun.logger import logger
+
+
+def get_online_controller(cluster):
+    return filter(
+        lambda node: (
+            "controller" in node.roles and node.online is True),
+        cluster.nodes
+    )[0]
+
+
+@contextmanager
+def set_proxy(proxy):
+    """Replace http_proxy environment variable for the scope
+    of context execution. After exit from context old proxy value
+    (if any) is restored
+
+    :param proxy: - proxy url
+    """
+    proxy_old_value = None
+
+    if os.environ.get("http_proxy"):
+        proxy_old_value = os.environ["http_proxy"]
+        logger.warning("http_proxy variable is already set with "
+                       "value: {0}. Change to {1}. Old value "
+                       "will be restored after exit from script's "
+                       "execution context"
+                       .format(proxy_old_value, proxy))
+
+    os.environ["http_proxy"] = proxy
+
+    try:
+        yield
+    except Exception as e:
+        logger.exception("Error while talking to proxy. Details: {0}"
+                         .format(six.text_type(e)))
+    finally:
+        if proxy_old_value:
+            logger.info("Restoring old value for http_proxy")
+            os.environ["http_proxy"] = proxy_old_value
+        else:
+            logger.info("Deleting set http_proxy environment variable")
+            del os.environ["http_proxy"]

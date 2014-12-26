758814d0f62d797c5b2f516b13010698b152acf2
Artem Roma <aroma@mirantis.com>
2014-12-26 19:19:57 +0200

    Info collected from OpenStack installation extended
    OpenStackInfoCollector class extended to make it collect defined set of
    attributes for needed entities.
    
    Change-Id: I2bcf5da19ae68f87b72fb0ca3581b7f233ed271a
    Implements: blueprint send-anon-usage

--- nailgun/nailgun/statistics/openstack_info_collector.py
+++ nailgun/nailgun/statistics/openstack_info_collector.py
@@ -26,6 +26,64 @@ from nailgun import objects
 from nailgun.settings import settings
 
 
+class _Missing(object):
+    def __repr__(self):
+        return "no value"
+
+
+_missing = _Missing()
+
+
+class cached_property(object):
+    """Inspired by werkzeug progect's code:
+    https://github.com/mitsuhiko/werkzeug/blob/master/werkzeug/utils.py#L35-L73
+
+    Quotation from the class' documentation:
+        'A decorator that converts a function into a lazy property.  The
+    function wrapped is called the first time to retrieve the result
+    and then that calculated result is used the next time you access
+    the value::
+        class Foo(object):
+            @cached_property
+            def foo(self):
+                # calculate something important here
+                return 42
+    The class has to have a `__dict__` in order for this property to
+    work.'
+    """
+    def __init__(self, func, name=None, doc=None):
+        self.__name__ = name or func.__name__
+        self.__module__ = func.__module__
+        self.__doc__ = doc or func.__doc__
+        self.func = func
+
+    def __get__(self, obj, type=None):
+        if obj is None:
+            return self
+        value = obj.__dict__.get(self.__name__, _missing)
+        if value is _missing:
+            value = self.func(obj)
+            obj.__dict__[self.__name__] = value
+        return value
+
+
+class ClientProvider(object):
+    """Initialize clients for OpenStack components
+    and expose them as attributes
+    """
+
+    def __init__(self, credentials):
+        self.credentials = credentials
+
+    @cached_property
+    def nova(self):
+        return nova_client.Client(
+            settings.OPENSTACK_API_VERSION["nova"],
+            *self.credentials,
+            service_type=consts.NOVA_SERVICE_TYPE.compute
+        )
+
+
 class OpenStackInfoCollector(object):
     """Introduce interface for collecting
     info from OpenStack installation
@@ -34,39 +92,51 @@ class OpenStackInfoCollector(object):
     for the time of request to OpenStack components
     """
 
-    def __init__(self, cluster, cluster_nodes):
-        self.online_controller = filter(
-            lambda node: "controller" in node.roles and node.online is True,
-            cluster_nodes
-        )[0]
+    collected_attributes = {
+        "nova": {
+            "servers": [
+                "id",
+                "name",
+                "status",
+                "user_id",
+                "tenant_id",
+                "OS-EXT-STS:power_state",
+                "OS-EXT-AZ:availability_zone",
+                "created",
+                "image",
+                "flavor",
+                "os-extended-volumes:volumes_attached",
+                "networks",
+                "diagnostics",
+            ],
+        }
+    }
 
-        proxy_host = self.online_controller.ip
-        proxy_port = settings.OPENSTACK_INFO_COLLECTOR_PROXY_PORT
-        self.proxy = "http://{0}:{1}".format(proxy_host, proxy_port)
+    equally_processed = ("nova",)
 
-        access_data = objects.Cluster.get_creds(cluster)
-
-        os_user = access_data["user"]["value"]
-        os_password = access_data["password"]["value"]
-        os_tenant = access_data["tenant"]["value"]
-
-        self.compute_service_type = consts.NOVA_SERVICE_TYPE.compute
-
-        auth_host = self.get_host_for_auth(cluster)
-        auth_url = "http://{0}:{1}/v2.0/".format(auth_host,
-                                                 settings.AUTH_PORT)
-
-        self.initialize_clients(os_user, os_password, os_tenant, auth_url)
+    def __init__(self, cluster):
+        self.cluster = cluster
+        self.nova = None
 
     @contextmanager
-    def set_proxy(self):
-        if os.environ.get("http_proxy"):
-            raise Exception(
-                "Cannot set 'http_proxy' environment variable "
-                "as it already has a value"
-            )
+    def set_proxy(self, proxy):
+        """Replace http_proxy environment variable for the scope
+        of context execution. After exit from context old proxy value
+        (if any) is restored
 
-        os.environ["http_proxy"] = self.proxy
+        :param proxy: - proxy url
+        """
+        proxy_old_value = None
+
+        if os.environ.get("http_proxy"):
+            proxy_old_value = os.environ["http_proxy"]
+            logger.warning("http_proxy variable is already set with "
+                           "value: {0}. Change to {1}. Old value "
+                           "will be restored after exit from script's "
+                           "execution context"
+                           .format(proxy_old_value, proxy))
+
+        os.environ["http_proxy"] = proxy
 
         try:
             yield
@@ -75,17 +145,52 @@ class OpenStackInfoCollector(object):
                              "OpenStack api. Details: {0}"
                              .format(six.text_type(e)))
         finally:
-            if os.environ.get("http_proxy") == self.proxy:
+            if proxy_old_value:
+                logger.info("Restoring old value for http_proxy")
+                os.environ["http_proxy"] = proxy_old_value
+            else:
+                logger.info("Deleting set http_proxy environment variable")
                 del(os.environ["http_proxy"])
 
-    def initialize_clients(self, *auth_creds):
-        self.nova_client = nova_client.Client(
-            settings.NOVACLIENT_VERSION,
-            *auth_creds,
-            service_type=self.compute_service_type
-        )
+    @property
+    def proxy(self):
+        proxy_host = self.online_controller.ip
+        proxy_port = settings.OPENSTACK_INFO_COLLECTOR_PROXY_PORT
+        proxy = "http://{0}:{1}".format(proxy_host, proxy_port)
 
-    def get_host_for_auth(self, nodes):
+        return proxy
+
+    @cached_property
+    def online_controller(self):
+        online_controller = filter(
+            lambda node: (
+                "controller" in node.roles and node.online is True),
+            self.cluster.nodes
+        )[0]
+
+        return online_controller
+
+    @cached_property
+    def client_provider(self):
+        return ClientProvider(self.auth_creds)
+
+    @property
+    def auth_creds(self):
+        access_data = objects.Cluster.get_creds(self.cluster)
+
+        os_user = access_data["user"]["value"]
+        os_password = access_data["password"]["value"]
+        os_tenant = access_data["tenant"]["value"]
+
+        auth_host = self.get_host_for_auth(self.cluster)
+        auth_url = "http://{0}:{1}/v2.0/".format(auth_host,
+                                                 settings.AUTH_PORT)
+
+        auth_creds = (os_user, os_password, os_tenant, auth_url)
+
+        return auth_creds
+
+    def get_host_for_auth(self):
         return manager.NetworkManager._get_ip_by_network_name(
             self.online_controller, consts.NETWORKS.management
         ).ip_addr
@@ -93,16 +198,46 @@ class OpenStackInfoCollector(object):
     def get_info(self):
         openstack_info = {}
 
-        with self.set_proxy():
-            openstack_info["nova_servers_count"] = len(
-                self.nova_client.servers.list()
-            )
+        with self.set_proxy(self.proxy):
+            for component_name in self.equally_processed:
+                openstack_info[component_name] = \
+                    self._get_component_info(component_name)
+
             openstack_info["images"] = self.get_images_info()
 
         return openstack_info
 
+    def _get_component_info(self, comp_name):
+        comp_info = {}
+
+        try:
+            comp_client = getattr(self.client_provider, comp_name)
+
+            comp_managers = self.collected_attributes[comp_name]
+
+            for manager_name in comp_managers:
+                manager_inst = getattr(comp_client, manager_name)
+
+                manager_info = []
+                for instance in manager_inst.list():
+                    instance_info = {}
+                    for attr_name in comp_managers[manager_name]:
+                        instance_info[attr_name] = getattr(instance, attr_name)
+
+                    manager_info.append(instance_info)
+
+            comp_info[manager_name] = {
+                'instances_info': manager_info,
+                'instances_count': len(manager_inst.list())
+            }
+        except Exception as e:
+            logger.exception("Collecting info from {0} failed. Details: {1}"
+                             .format(comp_name, six.text_type(e)))
+
+        return comp_info
+
     def get_images_info(self):
-        images = self.nova_client.images.list()
+        images = self.client_provider.nova.images.list()
 
         size_attr_name = consts.OPENSTACK_IMAGES_SETTINGS.size_attr_name
 

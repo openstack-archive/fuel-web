6ce5997bc79851c3db3115be34d90bc2f2a309c9
Andriy Popovych <apopovych@mirantis.com>
2014-12-29 00:55:10 +0200

    Change migrations with static data to load
    Now some data for migrations takes from fixtures.
    It's can broke migration state in future. Most obivous
    way to avoid it just "hardcode" data.
    
    Closes-Bug: #1404231
    Change-Id: I5b974fce211dd22741654290a118a00ee5704bf7
    
    Pep8 convention for migration data
    Change-Id: I5b974fce211dd22741654290a118a00ee5704bf7

--- nailgun/nailgun/test/unit/test_utils.py
+++ nailgun/nailgun/test/unit/test_utils.py
@@ -23,7 +23,6 @@ from nailgun.test.base import BaseIntegrationTest
 from nailgun.utils import dict_merge
 from nailgun.utils import extract_env_version
 from nailgun.utils import get_fuel_release_versions
-from nailgun.utils import migration
 
 
 class TestUtils(BaseIntegrationTest):
@@ -46,18 +45,6 @@ class TestUtils(BaseIntegrationTest):
                                            "transparency": 100,
                                            "dict": {"stuff": "hz",
                                                     "another_stuff": "hz"}}})
-
-    def test_upgrade_wizard_data(self):
-        fixture_path = os.path.join(os.path.dirname(__file__), '..', '..',
-                                    'fixtures', 'openstack.yaml')
-
-        wizard_meta = migration.upgrade_release_wizard_metadata_50_to_51(
-            fixture_path=fixture_path
-        )
-        network_settings = [
-            n['data'] for n in wizard_meta['Network']['manager']['values']
-        ]
-        self.assertNotIn('neutron-nsx', network_settings)
 
     def test_extract_env_version(self):
         # format: input, output pairs

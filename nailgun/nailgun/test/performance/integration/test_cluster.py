6af92686bd2240c1081c9f4fbbe05c7a89a2a32d
Maciej Kwiek <mkwiek@mirantis.com>
2015-01-14 12:14:38 +0100

    Added nailgun unit and integration load tests
    Integration tests:
    * add 100 nodes, deploy, add 100 nodes, deploy
    * add 100 nodes, deploy cluster, stop deployment, deploy cluster
    
    Unit tests:
    * Tests for handler ProvisionSelectedNodes
    * Tests for handler NodeGroupCollectionHandler
    * Tests for handler NodeCollectionNICsDefaultHandler
    * Check how NotificationCollectionHandler works with big number
      of notifications
    
    Change-Id: I846aefa3c67495b324bf06f7c9b45383f990ad1d

--- nailgun/nailgun/test/performance/integration/test_cluster.py
+++ nailgun/nailgun/test/performance/integration/test_cluster.py
@@ -15,15 +15,21 @@
 #    under the License.
 import functools
 
+import nailgun
+import mock
+
 from mock import patch
-from nailgun.test.base import fake_tasks
+from nailgun.test.base import fake_tasks, reverse
 from nailgun.test.performance.base import BaseIntegrationLoadTestCase
+from nailgun.db.sqlalchemy.models import Task
 
 
 class IntegrationClusterTests(BaseIntegrationLoadTestCase):
 
     MAX_EXEC_TIME = 60
     MAX_TOTAL_EXEC_TIME = 350
+
+    INITIAL_DEPLOY_TIME = 0
 
     def setUp(self):
         super(IntegrationClusterTests, self).setUp()
@@ -80,3 +86,60 @@ class IntegrationClusterTests(BaseIntegrationLoadTestCase):
             handler_kwargs={'cluster_id': self.cluster['id']}
         )
         self.check_time_exec(func, 10)
+
+    @fake_tasks(fake_rpc=False, mock_rpc=False)
+    @patch('nailgun.rpc.cast')
+    def test_double_deploy(self, mock_rpc):
+        self.provision(self.cluster['id'], self.nodes_ids)
+        self.deployment(self.cluster['id'], self.nodes_ids)
+        new_nodes = self.env.create_nodes(self.NODES_NUM, api=True)
+
+        ids = [str(node['id']) for node in new_nodes]
+
+        self.provision(self.cluster['id'], ids)
+        self.deployment(self.cluster['id'], ids)
+
+    @fake_tasks(fake_rpc=False, mock_rpc=False)
+    @patch('nailgun.rpc.cast')
+    def test_divided_deploy(self, mock_rpc):
+        self.provision(self.cluster['id'], self.nodes_ids)
+        self.deployment(self.cluster['id'], self.nodes_ids)
+
+        new_nodes = self.env.create_nodes(self.NODES_NUM, api=True)
+        ids = [str(node['id']) for node in new_nodes]
+
+        self.provision(self.cluster['id'], ids)
+        self.deployment(self.cluster['id'], ids)
+
+    @fake_tasks(fake_rpc=False, mock_rpc=False)
+    @patch('nailgun.rpc.cast')
+    def test_rerun_stopped_deploy(self, mock_rpc):
+        """ use DeploySelectedNodes from api/v1/handlers/orchestrator.py"""
+        
+        cluster = self.env.clusters[0]
+
+        self.app.put(
+            reverse(
+                'DeploySelectedNodes',
+                kwargs={'cluster_id': cluster.id}),
+            headers=self.default_headers)
+
+        stop_resp = self.app.put(
+            reverse(
+                'ClusterStopDeploymentHandler',
+                kwargs={'cluster_id': cluster.id}),
+            headers=self.default_headers)
+
+        self.tester.assertEqual(202, stop_resp.status_code)
+
+        second_deploy_response = self.app.put(
+            reverse(
+                'DeploySelectedNodes',
+                kwargs={'cluster_id': cluster.id}),
+            headers=self.default_headers)
+
+        task = self.db.query(Task).filter_by(
+            uuid=second_deploy_response.json_body['uuid']
+        ).first()
+
+        self.env.wait_ready(task, 350) 

e930b5ac4148c8fba4cbb458d99c1f96419f637e
Kate Pimenova <kpimenova@mirantis.com>
2015-01-21 18:34:10 +0300

    Move Navbar and Footer under RootComponent
    	- layout from index.html moved to root.jsx
    	- navbar, breadcrumbs and footer are managed from root.jsx now
    
    Related to blueprint backbone-to-react
    Related to blueprint react-router
    
    Change-Id: Ie99e47f5be64665e8702d95b445a3336e35c2198

--- nailgun/static/js/app.js
+++ nailgun/static/js/app.js
@@ -164,31 +164,7 @@ function($, _, i18n, Backbone, React, utils, layoutComponents, Coccyx, coccyxMix
             }, this));
         },
         renderLayout: function() {
-            this.content = $('#content');
-            this.navbar = React.render(React.createElement(layoutComponents.Navbar, {
-                elements: [
-                    {label: 'environments', url: '#clusters'},
-                    {label: 'releases', url: '#releases'},
-                    {label: 'support', url: '#support'}
-                ],
-                user: this.user,
-                version: this.version,
-                statistics: new models.NodesStatistics(),
-                notifications: new models.Notifications()
-            }), $('#navbar')[0]);
-            this.breadcrumbs = React.render(React.createElement(layoutComponents.Breadcrumbs), $('#breadcrumbs')[0]);
-            this.footer = React.render(React.createElement(layoutComponents.Footer, {version: this.version}), $('#footer')[0]);
-            this.content.find('.loading').addClass('layout-loaded');
-        },
-        updateTitle: function() {
-            var newTitle = _.result(this.page, 'title');
-            document.title = i18n('common.title') + (newTitle ? ' - ' + newTitle : '');
-            this.breadcrumbs.update();
-        },
-        toggleElements: function(state) {
-            app.footer.setState({hidden: !state});
-            app.breadcrumbs.setState({hidden: !state});
-            app.navbar.setState({hidden: !state});
+            this.rootComponent = utils.universalMount(RootComponent, {version: this.version, user: this.user}, $('#main-container'));
         },
         loadPage: function(Page, options) {
             return (Page.fetchData ? Page.fetchData.apply(Page, options) : $.Deferred().resolve()).done(_.bind(function(pageOptions) {
@@ -196,13 +172,7 @@ function($, _, i18n, Backbone, React, utils, layoutComponents, Coccyx, coccyxMix
             }, this));
         },
         setPage: function(Page, options) {
-            if (!this.rootComponent) {
-                this.rootComponent = utils.universalMount(RootComponent, {}, this.content);
-            }
             this.page = this.rootComponent.setPage(Page, options);
-            this.navbar.setActive(_.result(this.page, 'navbarActiveElement'));
-            this.updateTitle();
-            this.toggleElements(!this.page.hiddenLayout);
         },
         // pre-route hook
         before: function(currentUrl) {

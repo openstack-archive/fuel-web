56625127d9e8e7f67d36e94f6497bd8927925e3b
Aleksey Kasatkin <akasatkin@mirantis.com>
2015-01-26 14:50:42 +0200

    Add DB table and object for OSWL statistics storing
    blueprint: openstack-workload-statistics
    
    Change-Id: I29020c764b61982ff0c16f011c35e2dac5fd563f

---
+++ nailgun/nailgun/objects/oswl.py
@@ -0,0 +1,71 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nailgun.db import db
+from nailgun.db.sqlalchemy import models
+
+from nailgun import consts
+
+from nailgun.objects import NailgunCollection
+from nailgun.objects import NailgunObject
+
+
+class OpenStackWorkloadStats(NailgunObject):
+
+    #: SQLAlchemy model for OpenStackWorkloadStats
+    model = models.OpenStackWorkloadStats
+
+    @classmethod
+    def update(cls, instance, data):
+        """Form additional info for further instance update.
+
+        Extend corresponding method of the parent class.
+
+        Side effects:
+        overrides already present keys of additional_info attribute
+        of instance if this attribute is present in data argument
+
+        Arguments:
+        instance - instance of ActionLog class that is processed
+        data - dictionary containing keyword arguments for entity to be
+        updated
+
+        return - returned by parent class method value
+        """
+
+        if data.get('additional_info'):
+            add_info = dict(instance.additional_info)
+            add_info.update(data['additional_info'])
+            data['additional_info'] = add_info
+
+        return super(ActionLog, cls).update(instance, data)
+
+    @classmethod
+    def get_by_task_uuid(cls, task_uuid):
+        """Get action_log entry by task_uuid.
+
+        Arguments:
+        task_uuid - uuid of task, using which row is retrieved
+
+        return - matching instance of action_log entity
+        """
+        instance = db().query(models.ActionLog)\
+            .filter_by(task_uuid=task_uuid)\
+            .first()
+
+        return instance
+
+
+class OpenStackWorkloadStatsCollection(NailgunCollection):
+    single = OpenStackWorkloadStats

596dbb41face84e825e4c2f2f84fe4a645ee716c
Aleksey Kasatkin <akasatkin@mirantis.com>
2015-01-26 14:50:42 +0200

    Add DB table and object for OSWL statistics storing
    blueprint: openstack-workload-statistics
    
    Change-Id: I29020c764b61982ff0c16f011c35e2dac5fd563f

---
+++ nailgun/nailgun/objects/oswl.py
@@ -0,0 +1,40 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+from nailgun.db import db
+from nailgun.db.sqlalchemy import models
+from nailgun.objects import NailgunCollection
+from nailgun.objects import NailgunObject
+
+
+class OpenStackWorkloadStats(NailgunObject):
+
+    #: SQLAlchemy model for OpenStackWorkloadStats
+    model = models.OpenStackWorkloadStats
+
+    @classmethod
+    def get_last_by_cluster_id(cls, cluster_id):
+        """Get last entry by cluster_id.
+        """
+        instance = db().query(models.OpenStackWorkloadStats) \
+            .order_by(models.OpenStackWorkloadStats.creation_date.desc()) \
+            .filter_by(cluster_id=cluster_id) \
+            .first()
+
+        return instance
+
+
+class OpenStackWorkloadStatsCollection(NailgunCollection):
+    single = OpenStackWorkloadStats

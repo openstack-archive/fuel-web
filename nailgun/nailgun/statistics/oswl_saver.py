15c12fcac0b0b8e918d98312160475fabac32ac9
Aleksey Kasatkin <akasatkin@mirantis.com>
2015-01-27 19:45:06 +0200

    Add OSWL statictics saving to DB
    blueprint: openstack-workload-statistics
    
    Change-Id: I80942d43432de74150b765fa99b56c9fed0f432d

---
+++ nailgun/nailgun/statistics/oswl_saver.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+from datetime import datetime
+import hashlib
+import json
+
+from nailgun import objects
+
+
+def _added(time, prev, curr, collected):
+    prev_ids = set([res['id'] for res in prev])
+    curr_ids = set([res['id'] for res in curr])
+    added_ids = curr_ids - prev_ids
+    for id in added_ids:
+        collected[id] = {'time': str(time)}
+    return collected
+
+
+def _removed(time, prev, curr, collected):
+    prev_ids = set([res['id'] for res in prev])
+    curr_ids = set([res['id'] for res in curr])
+    removed_ids = prev_ids - curr_ids
+    for res in prev:
+        if res['id'] in removed_ids:
+            collected[res['id']] = res
+            collected[res['id']]['time'] = str(time)
+    return collected
+
+
+def _modified(time, prev, curr, collected):
+    prev_dict = dict([(res['id'], res) for res in prev])
+    curr_dict = dict([(res['id'], res) for res in curr])
+    same = set(prev_dict.keys()) & set(curr_dict.keys())
+    for id in same:
+        if curr_dict[id] != prev_dict[id]:
+            m = prev_dict[id]
+            m['time'] = str(time)
+            collected.setdefault(id, []).append(m)
+    return collected
+
+
+def oswl_data_checksum(data):
+    return hashlib.sha1(json.dumps(data)).hexdigest()
+
+
+def oswl_statistics_save(cluster_id, resource_type, data):
+    """Save OSWL statistics data for given cluster and resource_type to DB.
+    DB changes are not committed here.
+    """
+    dt = datetime.utcnow()
+    rec = objects.OpenStackWorkloadStats.get_last_by_cluster_id_resource_type(
+        cluster_id, resource_type)
+    cs = oswl_data_checksum(data)
+    obj_data = {
+        'update_time': dt.time(),
+
+        'resource_data_current': data,
+        'resource_current_checksum': cs
+    }
+    if rec:
+        if cs == rec.resource_current_checksum:
+            return
+        cur_dict = rec.resource_data_current
+        if rec.creation_date == dt.date():
+            # update record
+            add_dict = dict(rec.resource_data_added)
+            rem_dict = dict(rec.resource_data_removed)
+            mod_dict = dict(rec.resource_data_modified)
+            obj_data.update({
+                'resource_data_added': _added(
+                    dt.time(), cur_dict, data, add_dict),
+                'resource_data_removed': _removed(
+                    dt.time(), cur_dict, data, rem_dict),
+                'resource_data_modified': _modified(
+                    dt.time(), cur_dict, data, mod_dict)
+            })
+            objects.OpenStackWorkloadStats.update(rec, obj_data)
+        else:
+            # create new record
+            obj_data.update({
+                'cluster_id': cluster_id,
+                'resource_type': resource_type,
+                'creation_date': dt.date(),
+
+                'resource_data_added': _added(
+                    dt.time(), cur_dict, data, {}),
+                'resource_data_removed': _removed(
+                    dt.time(), cur_dict, data, {}),
+                'resource_data_modified': _modified(
+                    dt.time(), cur_dict, data, {})
+            })
+            objects.OpenStackWorkloadStats.create(obj_data)
+    else:
+        obj_data.update({
+            'cluster_id': cluster_id,
+            'resource_type': resource_type,
+            'creation_date': dt.date(),
+
+            'resource_data_added': _added(dt.time(), [], data, {}),
+            'resource_data_removed': {},
+            'resource_data_modified': {}
+        })
+        objects.OpenStackWorkloadStats.create(obj_data)

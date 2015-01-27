5a0f7dbfa374bf959cb2e2affcd491a65bfe64f3
Aleksey Kasatkin <akasatkin@mirantis.com>
2015-01-27 19:45:06 +0200

    Add OSWL statictics saving to DB
    blueprint: openstack-workload-statistics
    
    Change-Id: I80942d43432de74150b765fa99b56c9fed0f432d

--- nailgun/nailgun/test/unit/test_statistics.py
+++ nailgun/nailgun/test/unit/test_statistics.py
@@ -25,9 +25,12 @@ from nailgun.test.base import BaseTestCase
 from nailgun import consts
 from nailgun.objects import Cluster
 from nailgun.objects import OpenStackWorkloadStats
+from nailgun.objects import OpenStackWorkloadStatsCollection
 from nailgun.objects import ReleaseCollection
 from nailgun.settings import settings
 from nailgun.statistics.installation_info import InstallationInfo
+from nailgun.statistics.oswl_saver import oswl_data_checksum
+from nailgun.statistics.oswl_saver import oswl_statistics_save
 from nailgun.statistics.statsenderd import StatsSender
 from nailgun.statistics import utils
 
@@ -468,3 +471,179 @@ class TestOSWLObject(BaseTestCase):
             OpenStackWorkloadStats.get_last_by(
                 cluster['id'], consts.OSWL_RESOURCE_TYPES.vm)
         )
+
+
+class TestOSWLServerInfoSaving(BaseTestCase):
+
+    vms_info = {
+        "id": 1,
+        #"status": "running",
+        "power_state": 1,
+        #"created_at": "dt",
+        #"host_id": "111",
+        #"tenant_id": "222",
+        #"image_id": "333",
+        #"flavor_id": "444"
+    }
+
+    @property
+    def empty_data(self):
+        return {
+            'cluster_id': 1,
+            'resource_type': consts.OSWL_RESOURCE_TYPES.vm,
+            'created_date': datetime.datetime.utcnow().date(),
+
+            'resource_data': {'added': {},
+                              'removed': {},
+                              'modified': {},
+                              'current': []},
+            'resource_checksum': oswl_data_checksum([]),
+            'is_sent': False
+        }
+
+    def data_w_default_vm_info(self, time):
+        data = self.empty_data
+        data['resource_data'].update({
+            'added': {'1': {'time': str(time)}},
+            'current': [self.vms_info]
+        })
+        return data
+
+    def check_overall_rec_count(self, count):
+        saved = OpenStackWorkloadStatsCollection.all()
+        self.assertEqual(saved.count(), count)
+        return saved
+
+    def check_data_vs_rec(self, data, rec):
+        data['resource_checksum'] = \
+            oswl_data_checksum(data['resource_data']['current'])
+        for k, v in data.iteritems():
+            self.assertEqual(v, getattr(rec, k))
+
+    def save_data_and_check_record(self, data):
+        oswl_statistics_save(1, consts.OSWL_RESOURCE_TYPES.vm, data)
+        last = OpenStackWorkloadStats.get_last_by(
+            1, consts.OSWL_RESOURCE_TYPES.vm)
+        self.assertEqual(last, self.check_overall_rec_count(1).first())
+        return last
+
+    def add_default_vm_info_and_check(self):
+        last = self.save_data_and_check_record([self.vms_info])
+        time_update = last.updated_time
+        data = self.data_w_default_vm_info(time_update)
+        self.check_data_vs_rec(data, last)
+        return time_update, data
+
+    def test_empty_data(self):
+        last = self.save_data_and_check_record([])
+        self.check_data_vs_rec(self.empty_data, last)
+
+    def test_added_same_info(self):
+        time_update, data = self.add_default_vm_info_and_check()
+
+        # save same info
+        last = self.save_data_and_check_record([self.vms_info])
+        # DB row was not updated
+        self.assertEqual(time_update, last.updated_time)
+        self.check_data_vs_rec(data, last)
+
+    def test_added_one_by_one(self):
+        # VM with id=1 is added
+        time_update1, data = self.add_default_vm_info_and_check()
+
+        # VM with id=2 is added
+        two_vms = [dict(self.vms_info), dict(self.vms_info)]
+        two_vms[1]['id'] = 2
+        last = self.save_data_and_check_record(two_vms)
+
+        time_update2 = last.updated_time
+        data['resource_data'].update({
+            'added': {'1': {'time': str(time_update1)},
+                      '2': {'time': str(time_update2)}},
+            'current': two_vms
+        })
+        self.check_data_vs_rec(data, last)
+
+    def test_added_then_removed(self):
+        # VM is added
+        time_update, data = self.add_default_vm_info_and_check()
+
+        # VM is removed
+        last = self.save_data_and_check_record([])
+
+        time_update = last.updated_time
+        removed = dict(self.vms_info)
+        removed['time'] = str(time_update)
+        data['resource_data'].update({
+            'removed': {'1': removed},
+            'current': []
+        })
+        self.check_data_vs_rec(data, last)
+
+    def test_modified(self):
+        # VM is added
+        time_update, data = self.add_default_vm_info_and_check()
+
+        # VM power state is changed
+        vms_new = [dict(self.vms_info)]
+        vms_new[0]["power_state"] = 0
+        last = self.save_data_and_check_record(vms_new)
+
+        time_update = last.updated_time
+        modified1 = dict(self.vms_info)
+        modified1['time'] = str(time_update)
+        data['resource_data'].update({
+            'modified': {'1': [modified1]},
+            'current': vms_new
+        })
+        self.check_data_vs_rec(data, last)
+
+        # VM power state is changed back
+        last = self.save_data_and_check_record([self.vms_info])
+
+        time_update = last.updated_time
+        modified2 = dict(vms_new[0])
+        modified2['time'] = str(time_update)
+        data['resource_data'].update({
+            'modified': {'1': [modified1, modified2]},
+            'current': [self.vms_info]
+        })
+        self.check_data_vs_rec(data, last)
+
+    def test_add_row_per_day(self):
+        # VM is added
+        last = self.save_data_and_check_record([self.vms_info])
+
+        date_cur = last.created_date
+        time_update = last.updated_time
+        date_1st_rec = date_cur - datetime.timedelta(days=1)
+        # make existing record one day older
+        OpenStackWorkloadStats.update(last,
+                                      {'created_date': date_1st_rec})
+
+        # pass the same data
+        # no new record was created and existing one remains unchanged
+        self.assertEqual(last,
+                         self.save_data_and_check_record([self.vms_info]))
+
+        # VM is removed
+        oswl_statistics_save(1, consts.OSWL_RESOURCE_TYPES.vm, [])
+        saved = self.check_overall_rec_count(2)
+        last = OpenStackWorkloadStats.get_last_by(
+            1, consts.OSWL_RESOURCE_TYPES.vm)
+
+        self.assertEqual(last.created_date, date_cur)
+        for rec in saved:
+            if rec.created_date == date_cur:
+                self.assertEqual(rec, last)
+                # last record contains 'removed' and empty 'added'
+                data = self.empty_data
+                removed = dict(self.vms_info)
+                removed['time'] = str(last.updated_time)
+                data['resource_data']['removed'] = {'1': removed}
+                self.check_data_vs_rec(data, rec)
+            elif rec.created_date == date_1st_rec:
+                # first record contains 'added' and empty 'removed'
+                data = self.data_w_default_vm_info(time_update)
+                data['created_date'] = date_1st_rec
+                self.check_data_vs_rec(data, rec)

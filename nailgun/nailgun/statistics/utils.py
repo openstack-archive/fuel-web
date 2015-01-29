713175dc377242679f1cd62069f9e8062a0a1ff3
Artem Roma <aroma@mirantis.com>
2015-01-29 15:46:13 +0200

    Add OSWL collector
    Periodical collecting OSWL info for particular resource. It is
    supposed to be managed via supervisord so corresponding entry in console
    scripts in setup.py added.
    
    Implements: blueprint openstack-workload-statistics
    
    Change-Id: I781d95c028526f5a8788bd980d1c61f5f98458a9

--- nailgun/nailgun/statistics/utils.py
+++ nailgun/nailgun/statistics/utils.py
@@ -74,7 +74,7 @@ class ClientProvider(object):
             os_password = access_data["password"]["value"]
             os_tenant = access_data["tenant"]["value"]
 
-            auth_host = _get_host_for_auth()
+            auth_host = _get_host_for_auth(self.cluster)
             auth_url = "http://{0}:{1}/{2}/".format(
                 auth_host, settings.AUTH_PORT,
                 settings.OPENSTACK_API_VERSION["keystone"])
@@ -84,9 +84,9 @@ class ClientProvider(object):
         return self._credentials
 
 
-def _get_host_for_auth(self):
+def _get_host_for_auth(cluster):
     return manager.NetworkManager._get_ip_by_network_name(
-        _get_online_controller(self.cluster),
+        _get_online_controller(cluster),
         consts.NETWORKS.management
     ).ip_addr
 
@@ -141,10 +141,10 @@ def _get_value_from_nested_dict(obj_dict, key_path):
 
 def _get_nested_attr(obj, attr_path):
     if attr_path:
-        attr_name = attr_path.pop(0)
+        attr_name = attr_path[0]
         attr_value = getattr(obj, attr_name)
 
-        nested_value = _get_nested_attr(attr_value, attr_path)
+        nested_value = _get_nested_attr(attr_value, attr_path[1:])
 
         if nested_value:
             return nested_value

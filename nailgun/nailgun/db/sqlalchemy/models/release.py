8a7aa681a1489d00164e5f370ea9c840a384b406
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2015-02-02 10:58:07 +0300

    Nailgun: moved repo_matadata to cluster attributes
    We need it to be possible to set priorities (pinning)
    for deb and rpm repositories. This patch moves repo_metadata
    to cluster attributes and changes its format.
    
    Implements: blueprint consume-external-ubuntu
    Change-Id: I5a98469b9547014abec9406a7b36f1cf22ba8086

--- nailgun/nailgun/db/sqlalchemy/models/release.py
+++ nailgun/nailgun/db/sqlalchemy/models/release.py
@@ -17,10 +17,8 @@
 from sqlalchemy import Boolean
 from sqlalchemy import Column
 from sqlalchemy import Enum
-from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
 from sqlalchemy import String
-from sqlalchemy import Text
 from sqlalchemy import Unicode
 from sqlalchemy import UniqueConstraint
 
@@ -32,15 +30,6 @@ from nailgun.db import db
 from nailgun.db.sqlalchemy.models.base import Base
 from nailgun.db.sqlalchemy.models.fields import JSON
 from nailgun.db.sqlalchemy.models.node import Role
-
-
-class ReleaseOrchestratorData(Base):
-    __tablename__ = 'release_orchestrator_data'
-    id = Column(Integer, primary_key=True)
-    release_id = Column(Integer, ForeignKey('releases.id'), nullable=False)
-    repo_metadata = Column(JSON, nullable=False)
-    puppet_manifests_source = Column(Text, nullable=False)
-    puppet_modules_source = Column(Text, nullable=False)
 
 
 class Release(Base):
@@ -85,9 +74,6 @@ class Release(Base):
         backref="release",
         cascade="all,delete"
     )
-    orchestrator_data = relationship("ReleaseOrchestratorData",
-                                     uselist=False,
-                                     cascade="delete")
 
     #TODO(enchantner): get rid of properties
 

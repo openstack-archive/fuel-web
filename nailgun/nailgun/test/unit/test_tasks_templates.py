2889735b8ffbcbd2d85aaa36cb9464470f0f30d2
Vladimir Sharshov (warpc) <vsharshov@mirantis.com>
2015-02-03 15:58:58 +0300

    Respect original plugin tasks order
    Original order:
    
    - puppet1
    - shell1
    - puppet2
    - shell2
    
    Order before this change:
    
    - puppet1
    - puppet2
    - shell1
    - shell2
    
    Order now:
    
    - puppet1
    - shell1
    - puppet2
    - shell2
    
    Change-Id: I203fd5a00e628aac465a55a8e219e5fb130efbd8
    Closes-Bug: #1417537

---
+++ nailgun/nailgun/test/unit/test_tasks_templates.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nailgun.test import base
+
+from nailgun.orchestrator import tasks_templates
+
+
+class TestMakeTask(base.BaseTestCase):
+
+    def test_make_ubuntu_repo_task(self):
+        result = tasks_templates.make_multiversion_ubuntu(
+            'plugin_name',
+            'http://url',
+            [1, 2, 3])
+
+        self.assertEqual(
+            result,
+            {'parameters': {
+                'data': 'deb http://url /',
+                'path': '/etc/apt/sources.list.d/plugin_name.list'},
+             'type': 'upload_file',
+             'uids': [1, 2, 3]})

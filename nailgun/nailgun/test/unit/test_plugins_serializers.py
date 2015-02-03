2f341f355cd125f58e5409f9851ba9a24cb9bac0
Vladimir Sharshov (warpc) <vsharshov@mirantis.com>
2015-02-03 15:58:58 +0300

    Respect original plugin tasks order
    Original order:
    
    - puppet1
    - shell1
    - puppet2
    - shell2
    
    Order before this change:
    
    - puppet1
    - puppet2
    - shell1
    - shell2
    
    Order now:
    
    - puppet1
    - shell1
    - puppet2
    - shell2
    
    Change-Id: I203fd5a00e628aac465a55a8e219e5fb130efbd8
    Closes-Bug: #1417537

--- nailgun/nailgun/test/unit/test_plugins_serializers.py
+++ nailgun/nailgun/test/unit/test_plugins_serializers.py
@@ -1,6 +1,7 @@
+
 # -*- coding: utf-8 -*-
 
-#    Copyright 2014 Mirantis, Inc.
+#    Copyright 2015 Mirantis, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -14,23 +15,55 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import mock
 from nailgun.test import base
 
 from nailgun.orchestrator import plugins_serializers
+from nailgun.orchestrator.plugins_serializers import \
+    BasePluginDeploymentHooksSerializer
 
 
-class TestMakeTask(base.BaseTestCase):
+class TestBasePluginDeploymentHooksSerializer(base.BaseTestCase):
 
-    def test_make_ubuntu_repo_task(self):
-        result = plugins_serializers.make_ubuntu_repo_task(
-            'plugin_name',
-            'http://url',
-            [1, 2, 3])
+    def setUp(self):
+        super(TestBasePluginDeploymentHooksSerializer, self).setUp()
+        cluster_mock = mock.Mock()
+        self.cluster = cluster_mock
+        self.nodes = [
+            {'id': 1, 'role': 'controller'},
+            {'id': 2, 'role': 'compute'}
+        ]
+        self.hook = BasePluginDeploymentHooksSerializer(
+            self.nodes,
+            self.cluster)
 
+    @mock.patch('nailgun.orchestrator.plugins_serializers.get_uids_for_tasks')
+    def test_original_order_of_deployment_tasks(self, get_uids_for_tasks_mock):
+        stage = 'pre_deployment'
+        role = 'controller'
+
+        plugin = mock.Mock()
+        plugin.full_name = 'plugin_name'
+        plugin.tasks = [
+            {'type': 'shell', 'role': role, 'id': '1', 'stage': stage,
+             'parameters': {'cmd': 'test1', 'cwd': '/', 'timeout': 15}},
+            {'type': 'puppet', 'role': role, 'id': '2', 'stage': stage,
+             'parameters': {
+                 'puppet_manifest': 'manifests/site.pp',
+                 'puppet_modules': 'modules',
+                 'cwd': '/etc/puppet/plugins/plugin_name',
+                 'timeout': 150}},
+            {'type': 'shell', 'role': role, 'id': '3', 'stage': stage,
+             'parameters': {'cmd': 'test2', 'cwd': '/', 'timeout': 15}}
+        ]
+
+        get_uids_for_tasks_mock.return_value = [1, 2]
+
+        raw_result = self.hook.deployment_tasks([plugin], stage)
+        result = [r['type'] for r in raw_result]
+        self.assertEqual(result, ['shell', 'puppet', 'shell'])
+        self.assertEqual(raw_result[0]['parameters']['cmd'], 'test1')
         self.assertEqual(
-            result,
-            {'parameters': {
-                'data': 'deb http://url /',
-                'path': '/etc/apt/sources.list.d/plugin_name.list'},
-             'type': 'upload_file',
-             'uids': [1, 2, 3]})
+            raw_result[1]['parameters']['puppet_modules'],
+            'modules')
+        self.assertEqual(raw_result[2]['parameters']['cmd'], 'test2')

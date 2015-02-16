8c5ba4c42e685a63855948ca9eb0cbabd1a70eef
Aleksey Kasatkin <akasatkin@mirantis.com>
2015-02-16 19:00:59 +0200

    Add bond properties
    Blueprint: support-linux-bridges-and-bonds-in-api-and-ui
    
    Change-Id: Ifdf06c376360caae7065805c360e9b2ef2bac55b

--- nailgun/nailgun/test/integration/test_orchestrator_serializer.py
+++ nailgun/nailgun/test/integration/test_orchestrator_serializer.py
@@ -533,9 +533,12 @@ class TestNovaNetworkOrchestratorSerializer61(OrchestratorSerializerTestBase):
         )
         for node in cluster.nodes:
             self.env.make_bond_via_api('lnx_bond',
-                                       consts.BOND_MODES.balance_rr,
+                                       '',
                                        ['eth1', 'eth2'],
-                                       node.id)
+                                       node.id,
+                                       bond_properties={
+                                           'mode': consts.BOND_MODES.balance_rr
+                                       })
         serializer = get_serializer_for_cluster(cluster)
         facts = serializer(AstuteGraph(cluster)).serialize(
             cluster, cluster.nodes)
@@ -579,9 +582,12 @@ class TestNovaNetworkOrchestratorSerializer61(OrchestratorSerializerTestBase):
         )
         for node in cluster.nodes:
             self.env.make_bond_via_api('lnx_bond',
-                                       consts.BOND_MODES.balance_rr,
+                                       '',
                                        ['eth1', 'eth2'],
-                                       node.id)
+                                       node.id,
+                                       bond_properties={
+                                           'mode': consts.BOND_MODES.balance_rr
+                                       })
         serializer = get_serializer_for_cluster(cluster)
         facts = serializer(AstuteGraph(cluster)).serialize(
             cluster, cluster.nodes)
@@ -730,9 +736,12 @@ class TestNeutronOrchestratorSerializer61(OrchestratorSerializerTestBase):
                                   nic_count=3)
         for node in cluster.nodes:
             self.env.make_bond_via_api('lnx_bond',
-                                       consts.BOND_MODES.balance_rr,
+                                       '',
                                        ['eth1', 'eth2'],
-                                       node.id)
+                                       node.id,
+                                       bond_properties={
+                                           'mode': consts.BOND_MODES.balance_rr
+                                       })
         serializer = get_serializer_for_cluster(cluster)
         facts = serializer(AstuteGraph(cluster)).serialize(
             cluster, cluster.nodes)
@@ -856,10 +865,17 @@ class TestNeutronOrchestratorSerializer61(OrchestratorSerializerTestBase):
         cluster = self.create_env(segment_type='gre', ctrl_count=3,
                                   nic_count=3)
         for node in cluster.nodes:
-            self.env.make_bond_via_api('lnx_bond',
-                                       consts.BOND_MODES.balance_rr,
-                                       ['eth1', 'eth2'],
-                                       node.id)
+            self.env.make_bond_via_api(
+                'lnx_bond',
+                '',
+                ['eth1', 'eth2'],
+                node.id,
+                bond_properties={
+                    'mode': consts.BOND_MODES.l_802_3ad,
+                    'xmit_hash_policy': consts.BOND_XMIT_HASH_POLICY.layer2,
+                    'lacp_rate': consts.BOND_LACP_RATES.slow,
+                    'type__': consts.BOND_TYPES.linux
+                })
         serializer = get_serializer_for_cluster(cluster)
         facts = serializer(AstuteGraph(cluster)).serialize(
             cluster, cluster.nodes)
@@ -892,7 +908,9 @@ class TestNeutronOrchestratorSerializer61(OrchestratorSerializerTestBase):
                  'bridge': 'br-ex',
                  'name': 'lnx_bond',
                  'interfaces': ['eth1', 'eth2'],
-                 'bond_properties': {'mode': 'balance-rr'},
+                 'bond_properties': {'mode': '802.3ad',
+                                     'xmit_hash_policy': 'layer2',
+                                     'lacp_rate': 'slow'},
                  'interface_properties': {}}
             ]
             self.assertEqual(

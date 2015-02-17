95f6c2cc59d450621b1545c2d4efe6b7d75eac42
Artem Roma <aroma@mirantis.com>
2015-02-17 16:59:10 +0200

    Volumes info collecting added
    OSWL utilities updated. Code that supports collecting volumes info
    added. This patch is dependent on [1].
    
    [1]: https://review.openstack.org/#/c/156643/
    
    Change-Id: Ifcb44bf990d46ce79cdcc67d533f598146710ec2
    Implements: blueprint openstack-workload-statistics

--- nailgun/nailgun/test/unit/test_statistics.py
+++ nailgun/nailgun/test/unit/test_statistics.py
@@ -593,7 +593,23 @@ class TestOSWLCollectingUtils(BaseTestCase):
                     "created": "some_date_of_creation",
                     "updated": "some_date_of_update"
                 },
-            }
+            },
+            "cinder": {
+                "volumes": {
+                    "id": 3,
+                    "availability_zone": "test_availability_zone",
+                    "encrypted": False,
+                    "bootable": False,
+                    "status": "available",
+                    "volume_type": "test_volume",
+                    "display_description": "This is mocked volume",
+                    "size": 1,
+                    "os-vol-host-attr:host": "test-node",
+                    "snapshot_id": None,
+                    "attachments": "test_attachments",
+                    "os-vol-tenant-attr:tenant_id": "test_tenant",
+                },
+            },
         }
 
         expected = {
@@ -640,7 +656,26 @@ class TestOSWLCollectingUtils(BaseTestCase):
                         "updated_at": "some_date_of_update"
                     }
                 ]
-            }
+            },
+            "volumes": {
+                "regard_resource": "volume",
+                "data": [
+                    {
+                        "id": 3,
+                        "availability_zone": "test_availability_zone",
+                        "encrypted_flag": False,
+                        "bootable_flag": False,
+                        "status": "available",
+                        "volume_type": "test_volume",
+                        "description": "This is mocked volume",
+                        "size": 1,
+                        "host": "test-node",
+                        "snapshot_id": None,
+                        "attachments": "test_attachments",
+                        "tenant_id": "test_tenant",
+                    }
+                ]
+            },
         }
 
         get_proxy_path = ("nailgun.statistics.utils.get_proxy_for_cluster")

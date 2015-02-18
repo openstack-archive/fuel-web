8da14f6254ec704f9104310f348698e2d540a7e7
Aleksey Kasatkin <akasatkin@mirantis.com>
2015-02-18 19:53:25 +0200

    Add routing for multi-cluster-l2
    Blueprint: refactor-l23-linux-bridges
    
    Change-Id: Idc699f971a13dbfbeff00dec862c6b05fabe41fe

--- nailgun/nailgun/test/integration/test_orchestrator_serializer.py
+++ nailgun/nailgun/test/integration/test_orchestrator_serializer.py
@@ -18,8 +18,10 @@ import copy
 from operator import attrgetter
 from operator import itemgetter
 import re
+import six
 
 import mock
+from netaddr import IPAddress
 from netaddr import IPNetwork
 from netaddr import IPRange
 
@@ -913,6 +915,63 @@ class TestNeutronOrchestratorSerializer61(OrchestratorSerializerTestBase):
                 node['network_scheme']['transformations'],
                 transformations
             )
+
+    def test_gre_with_multi_groups(self):
+        cluster = self.create_env(segment_type='gre', ctrl_count=3)
+
+        resp = self.env.create_node_group()
+        group_id = resp.json_body['id']
+
+        self.env.create_nodes_w_interfaces_count(
+            nodes_count=3,
+            if_count=2,
+            roles=['compute'],
+            pending_addition=True,
+            cluster_id=cluster.id,
+            group_id=group_id)
+
+        nets = self.env.neutron_networks_get(cluster.id).json_body
+        print [n['name'] for n in nets['networks']]
+        nets_w_gw = {'management': '199.99.20.0/24',
+                     'storage': '199.98.20.0/24',
+                     'fuelweb_admin': '199.97.20.0/24',
+                     'public': '199.96.20.0/24'}
+        for net in nets['networks']:
+            if net['name'] in nets_w_gw.keys():
+                if net['group_id'] == group_id:
+                    net['cidr'] = nets_w_gw[net['name']]
+                    net['ip_ranges'] = [[
+                        str(IPAddress(IPNetwork(net['cidr']).first + 2)),
+                        str(IPAddress(IPNetwork(net['cidr']).first + 253)),
+                    ]]
+                elif net['name'] == 'fuelweb_admin':
+                    pass
+                net['gateway'] = str(
+                    IPAddress(IPNetwork(net['cidr']).first + 1))
+                print net['name'], net['gateway']
+        resp = self.env.neutron_networks_put(cluster.id, nets)
+        self.assertEqual(resp.status_code, 200)
+
+        objects.NodeCollection.prepare_for_deployment(cluster.nodes)
+        serializer = get_serializer_for_cluster(cluster)
+        facts = serializer(AstuteGraph(cluster)).serialize(
+            cluster, cluster.nodes)
+        for node in facts:
+            node_db = objects.Node.get_by_uid(node['uid'])
+            endpoints = node['network_scheme']['endpoints']
+            br_set = set(['br-storage', 'br-mgmt', 'br-fw-admin'])
+            if objects.Node.should_have_public(node_db):
+                br_set.update(['br-ex', 'br-floating'])
+                self.assertEqual(endpoints['br-floating'], {'IP': 'none'})
+            self.assertEqual(
+                set(endpoints.keys()),
+                br_set
+            )
+            endpoints.pop('br-floating')
+            print endpoints
+            for name, descr in six.iteritems(endpoints):
+                self.assertEqual(set(descr.keys()),
+                                 set(['IP', 'gateway', 'routes']))
 
 
 class TestNovaOrchestratorHASerializer(OrchestratorSerializerTestBase):

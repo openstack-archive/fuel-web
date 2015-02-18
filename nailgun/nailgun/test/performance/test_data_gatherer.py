8a930e2d9a58f77f97b0c3e53acc9f40cbeba55c
Maciej Kwiek <mkwiek@mirantis.com>
2015-02-18 12:40:25 +0100

    Save which performance tests failed
    Names of performance tests that failed are saved in
    nailgun/failed_tests.txt file.
    
    Change-Id: Ia028e32d8543d01cd2b2ed31dae013b9f52b8593
    Implements: blueprint 200-nodes-support

--- nailgun/nailgun/test/performance/test_data_gatherer.py
+++ nailgun/nailgun/test/performance/test_data_gatherer.py
@@ -85,3 +85,15 @@ class PerformanceDataCsvCreator(object):
                 sub[1].items() for sub in six.iteritems(data)
             ] for item in sublist
         ]
+
+
+class FailedTestSaver(object):
+    FAILED_TESTS_FILE_NAME = 'failed_tests.txt'
+
+    def __init__(self, reports):
+        self.failed_ids = [x.nodeid for x in reports if x.outcome != 'passed']
+
+    def save(self):
+        with open(self.FAILED_TESTS_FILE_NAME, 'w') as fail_file:
+            for r in self.failed_ids:
+                fail_file.write('{0}\n'.format(r))

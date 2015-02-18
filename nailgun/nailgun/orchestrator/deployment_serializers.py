a1eb20df4baa98e1e9f6d2b0de12f2ef4387272a
Aleksey Kasatkin <akasatkin@mirantis.com>
2015-02-18 19:53:25 +0200

    Add routing for multi-cluster-l2
    Blueprint: refactor-l23-linux-bridges
    
    Change-Id: Idc699f971a13dbfbeff00dec862c6b05fabe41fe

--- nailgun/nailgun/orchestrator/deployment_serializers.py
+++ nailgun/nailgun/orchestrator/deployment_serializers.py
@@ -1044,6 +1044,21 @@ class NeutronNetworkDeploymentSerializer61(
             return "{0}.{1}".format(iface_name, net_descr['vlan_id'])
 
     @classmethod
+    def generate_routes(cls, node, attrs, nm, netgroup_mapping, netgroups):
+        other_nets = nm.get_networks_not_on_node(node)
+
+        for ngname, brname in netgroup_mapping:
+            netgroup = netgroups[ngname]
+            if netgroup.get('gateway'):
+                via = netgroup['gateway']
+                attrs['endpoints'][brname]['routes'] = []
+                for cidr in other_nets.get(ngname, []):
+                    attrs['endpoints'][brname]['routes'].append({
+                        'net': cidr,
+                        'via': via
+                    })
+
+    @classmethod
     def generate_network_scheme(cls, node):
 
         # Create a data structure and fill it with static values.
@@ -1104,7 +1119,8 @@ class NeutronNetworkDeploymentSerializer61(
             attrs['endpoints']['br-ex']['gateway'] = \
                 netgroups['public']['gateway']
         else:
-            attrs['endpoints']['br-fw-admin']['gateway'] = settings.MASTER_IP
+            attrs['endpoints']['br-fw-admin']['gateway'] = \
+                nm.get_default_gateway(node.id)
 
         # Fill up interfaces.
         for iface in node.nic_interfaces:
@@ -1224,6 +1240,10 @@ class NeutronNetworkDeploymentSerializer61(
                             bond['bridge'] = net['br_name']
                 attrs['transformations'].append(bond)
                 attrs['transformations'].extend(bond_ports)
+
+        if objects.NodeGroupCollection.get_by_cluster_id(
+                node.cluster.id).count() > 1:
+            cls.generate_routes(node, attrs, nm, netgroup_mapping, netgroups)
 
         return attrs
 

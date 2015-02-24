8c15cb67a614034ff2a39d5ae7832c0d73d4f59d
Nikolay Markov <nmarkov@mirantis.com>
2015-02-24 16:48:29 +0300

    Fake registration form
    Change-Id: Ifc25d5a24868b3e9459efac93ec188e1cda7a830

--- nailgun/nailgun/api/v1/urls.py
+++ nailgun/nailgun/api/v1/urls.py
@@ -81,7 +81,8 @@ from nailgun.api.v1.handlers.orchestrator import DeploySelectedNodesWithTasks
 from nailgun.api.v1.handlers.orchestrator import ProvisioningInfo
 from nailgun.api.v1.handlers.orchestrator import ProvisionSelectedNodes
 
-from nailgun.api.v1.handlers.registration import FuelKeyHandler
+from nailgun.api.v1.handlers.registration import FuelRegistrationForm
+
 from nailgun.api.v1.handlers.release import ReleaseCollectionHandler
 from nailgun.api.v1.handlers.release import ReleaseDeploymentTasksHandler
 from nailgun.api.v1.handlers.release import ReleaseHandler
@@ -218,8 +219,8 @@ urls = (
     r'/logs/sources/nodes/(?P<node_id>\d+)/?$',
     LogSourceByNodeCollectionHandler,
 
-    r'/registration/key/?$',
-    FuelKeyHandler,
+    r'/registration/?$',
+    FuelRegistrationForm,
 
     r'/version/?$',
     VersionHandler,

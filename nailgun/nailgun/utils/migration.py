33ccdd464068563defe6d8d6b635b4195eaa43da
Nikolay Markov <nmarkov@mirantis.com>
2015-02-24 16:48:29 +0300

    Fake registration form
    Change-Id: Ifc25d5a24868b3e9459efac93ec188e1cda7a830

--- nailgun/nailgun/utils/migration.py
+++ nailgun/nailgun/utils/migration.py
@@ -285,6 +285,40 @@ def upgrade_attributes_metadata_6_0_to_6_1(attributes_meta):
 def upgrade_master_node_settings_6_0_to_6_1(master_node_settings):
     master_node_settings['statistics']['email']['regex']['source'] = \
         '^\S+@\S+[\.][0-9a-z]+$'
+    master_node_settings['tracking'] = {
+        "email": {
+            "type": "text",
+            "value": "",
+            "label": "Mirantis Account Email",
+            "weight": 10,
+            "regex": {
+                "source": "^\\S+@\\S+$",
+                "error": "Invalid email"
+            },
+            "restrictions": [
+                {
+                    "condition": "not ('mirantis' in version:feature_groups)",
+                    "action": "hide"
+                }
+            ]
+        },
+        "password": {
+            "type": "password",
+            "value": "",
+            "label": "Password",
+            "weight": 20,
+            "regex": {
+                "source": "\\S",
+                "error": "Password cannot be empty"
+            },
+            "restrictions": [
+                {
+                    "condition": "not ('mirantis' in version:feature_groups)",
+                    "action": "hide"
+                }
+            ]
+        }
+    }
 
     return master_node_settings
 

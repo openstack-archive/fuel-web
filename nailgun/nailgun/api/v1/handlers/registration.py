97525d0bf6e628d41ebd90f5e545be6849ae2ecc
Nikolay Markov <nmarkov@mirantis.com>
2015-02-24 16:48:29 +0300

    Fake registration form
    Change-Id: Ifc25d5a24868b3e9459efac93ec188e1cda7a830

--- nailgun/nailgun/api/v1/handlers/registration.py
+++ nailgun/nailgun/api/v1/handlers/registration.py
@@ -18,29 +18,102 @@
 Product registration handlers
 """
 
-import base64
+import web
 
 
 from nailgun.api.v1.handlers.base import BaseHandler
 from nailgun.api.v1.handlers.base import content
+
+from nailgun.errors import errors
+
 from nailgun.openstack.common import jsonutils
-from nailgun.settings import settings
+from nailgun.utils.tracking import FuelTrackingManager
 
 
-class FuelKeyHandler(BaseHandler):
-    """ Fuel key handler"""
+class FuelRegistrationForm(BaseHandler):
+    """Registration form handler"""
 
     @content
     def GET(self):
-        """Returns Fuel Key data
-        :returns: base64 of FUEL commit SHA, release version and Fuel UUID.
+        """Returns Fuel registration form
+        :returns: JSON representation of registration form
         :http: * 200 (OK)
         """
-        key_data = {
-            "sha": str(settings.VERSION['nailgun_sha']),
-            "release": str(settings.VERSION['release']),
-            "uuid": str(settings.FUEL_KEY)
-        }
-        signature = base64.b64encode(jsonutils.dumps(key_data))
-        key_data["signature"] = signature
-        return {"key": base64.b64encode(jsonutils.dumps(key_data))}
+        try:
+            return FuelTrackingManager.get_registration_form()
+        except errors.TrackingError as exc:
+            raise self.http(400, exc.message)
+
+    @content
+    def POST(self):
+        data = web.data()
+        try:
+            json_data = jsonutils.loads(data)
+        except Exception:
+            raise self.http(400, "Invalid JSON received")
+
+        if json_data["credentials"]["first_name"]["value"] == "Darth":
+            raise self.http(
+                400,
+                jsonutils.dumps({
+                    "message": "Death Star destroyed is",
+                    "errors": [
+                        {"field": "first_name", "message": "Yoda disapproves"}
+                    ]
+                })
+            )
+
+        if json_data["credentials"]["email"]["value"] \
+                == "gmail@chucknorris.com":
+            raise self.http(
+                400,
+                jsonutils.dumps({
+                    "message": "Godmode disabled",
+                    "errors": [
+                        {
+                            "field": "email",
+                            "message": "Chuck, you don't need to register"
+                        },
+                        {"field": "first_name", "message": "Really?"}
+                    ]
+                })
+            )
+        return {}
+
+
+class FuelLoginForm(BaseHandler):
+    """Login form handler"""
+
+    @content
+    def GET(self):
+        """Returns Fuel login form
+        :returns: JSON representation of login form
+        :http: * 200 (OK)
+        """
+        try:
+            return FuelTrackingManager.get_login_form()
+        except errors.TrackingError as exc:
+            raise self.http(400, exc.message)
+
+    @content
+    def POST(self):
+        return {}
+
+
+class FuelRestorePasswordForm(BaseHandler):
+    """Restore password form handler"""
+
+    @content
+    def GET(self):
+        """Returns Fuel restore password form
+        :returns: JSON representation of restore password form
+        :http: * 200 (OK)
+        """
+        try:
+            return FuelTrackingManager.get_restore_password_form()
+        except errors.TrackingError as exc:
+            raise self.http(400, exc.message)
+
+    @content
+    def POST(self):
+        return {}

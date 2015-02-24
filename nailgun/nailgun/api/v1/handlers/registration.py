c3d0ec9d9b99e428971164947db0ae2054128723
Nikolay Markov <nmarkov@mirantis.com>
2015-02-24 16:48:29 +0300

    Fake registration form
    Change-Id: Ifc25d5a24868b3e9459efac93ec188e1cda7a830

--- nailgun/nailgun/api/v1/handlers/registration.py
+++ nailgun/nailgun/api/v1/handlers/registration.py
@@ -18,29 +18,223 @@
 Product registration handlers
 """
 
-import base64
+import web
 
 
 from nailgun.api.v1.handlers.base import BaseHandler
 from nailgun.api.v1.handlers.base import content
+
 from nailgun.openstack.common import jsonutils
-from nailgun.settings import settings
 
 
-class FuelKeyHandler(BaseHandler):
-    """ Fuel key handler"""
+class FuelRegistrationForm(BaseHandler):
+    """Registration form handler"""
 
     @content
     def GET(self):
-        """Returns Fuel Key data
-        :returns: base64 of FUEL commit SHA, release version and Fuel UUID.
+        """Returns Fuel registration form
+        :returns: JSON representation of registration form
         :http: * 200 (OK)
         """
-        key_data = {
-            "sha": str(settings.VERSION['nailgun_sha']),
-            "release": str(settings.VERSION['release']),
-            "uuid": str(settings.FUEL_KEY)
+        return {
+            "credentials": {
+                "metadata": {
+                    "label": "Sign Up",
+                    "weight": 10
+                },
+                "first_name": {
+                    "type": "text",
+                    "label": "First Name",
+                    "value": "",
+                    "weight": 10,
+                    "regex": {
+                        "source": "\\S",
+                        "error": "First name should not be empty"
+                    }
+                },
+                "last_name": {
+                    "type": "text",
+                    "label": "Last Name",
+                    "value": "",
+                    "weight": 20
+                },
+                "email": {
+                    "type": "text",
+                    "label": "Corporate email",
+                    "value": "",
+                    "weight": 30,
+                    "description": "For best experience please avoid "
+                                   "using public emails, such as @gmail.com, "
+                                   "@yahoo.com, etc.",
+                    "regex": {
+                        "source": "^\\S+@\\S+$",
+                        "error": "Invalid email"
+                    }
+                },
+                "company": {
+                    "type": "text",
+                    "label": "Company",
+                    "value": "",
+                    "weight": 40,
+                    "regex": {
+                        "source": "\\S",
+                        "error": "Company should not be empty"
+                    }
+                },
+                "phone": {
+                    "type": "text",
+                    "label": "Phone number",
+                    "value": "",
+                    "weight": 50
+                },
+                "job": {
+                    "type": "text",
+                    "label": "Job Title",
+                    "value": "",
+                    "weight": 60
+                },
+                "country": {
+                    "type": "select",
+                    "label": "Select Country",
+                    "value": "us",
+                    "weight": 70,
+                    "values": [
+                        {
+                            "data": "us",
+                            "label": "US && Canada"
+                        }
+                    ]
+                },
+                "region": {
+                    "type": "select",
+                    "label": "US & Canade State/Province",
+                    "value": "",
+                    "weight": 80,
+                    "values": [
+                        {
+                            "data": "Alabama",
+                            "label": "Alabama"
+                        }
+                    ]
+                },
+                "agree": {
+                    "type": "checkbox",
+                    "value": "",
+                    "label": "I Read and Accept Terms & Conditions",
+                    "description": "https://software.mirantis.com/"
+                                   "blank/terms-and-conditions/"
+                }
+            }
         }
-        signature = base64.b64encode(jsonutils.dumps(key_data))
-        key_data["signature"] = signature
-        return {"key": base64.b64encode(jsonutils.dumps(key_data))}
+
+    @content
+    def POST(self):
+        data = web.data()
+        try:
+            json_data = jsonutils.loads(data)
+        except Exception:
+            raise self.http(400, "Invalid JSON received")
+
+        if json_data["credentials"]["first_name"]["value"] == "Darth":
+            raise self.http(
+                400,
+                jsonutils.dumps({
+                    "message": "Death Star destroyed is",
+                    "errors": [
+                        {"field": "first_name", "message": "Yoda disapproves"}
+                    ]
+                })
+            )
+
+        if json_data["credentials"]["email"]["value"] \
+                == "gmail@chucknorris.com":
+            raise self.http(
+                400,
+                jsonutils.dumps({
+                    "message": "Godmode disabled",
+                    "errors": [
+                        {
+                            "field": "email",
+                            "message": "Chuck, you don't need to register"
+                        },
+                        {"field": "first_name", "message": "Really?"}
+                    ]
+                })
+            )
+        return {}
+
+
+class FuelLoginForm(BaseHandler):
+    """Login form handler"""
+
+    @content
+    def GET(self):
+        """Returns Fuel login form
+        :returns: JSON representation of login form
+        :http: * 200 (OK)
+        """
+        return {
+            "credentials": {
+                "metadata": {
+                    "label": "Login",
+                    "weight": 10
+                },
+                "email": {
+                    "type": "text",
+                    "label": "Email",
+                    "value": "",
+                    "weight": 10,
+                    "regex": {
+                        "source": "^\\S+@\\S+$",
+                        "error": "Invalid email"
+                    }
+                },
+                "password": {
+                    "type": "password",
+                    "label": "Password",
+                    "value": "",
+                    "weight": 20,
+                    "regex": {
+                        "source": "\\S",
+                        "error": "Password cannot be empty"
+                    }
+                }
+            }
+        }
+
+    @content
+    def POST(self):
+        return {}
+
+
+class FuelRestorePasswordForm(BaseHandler):
+    """Restore password form handler"""
+
+    @content
+    def GET(self):
+        """Returns Fuel restore password form
+        :returns: JSON representation of restore password form
+        :http: * 200 (OK)
+        """
+        return {
+            "credentials": {
+                "metadata": {
+                    "label": "Restore password",
+                    "weight": 10
+                },
+                "email": {
+                    "type": "text",
+                    "label": "Email",
+                    "value": "",
+                    "weight": 10,
+                    "regex": {
+                        "source": "^\\S+@\\S+$",
+                        "error": "Invalid email"
+                    }
+                },
+            }
+        }
+
+    @content
+    def POST(self):
+        return {}

35284100ae95128f448b373ce69b71d213693ad3
Dmitry Shulyak <dshulyak@mirantis.com>
2015-02-27 10:48:50 +0200

    Add api to operate Role object
    Standart api calls added that will allow to create or
    modify Role database object
    
    URI for this object will be served on:
    
      /releases/<release_id>/roles/<role_name>
    
    Role name serves as unique identifier amongst roles for releases
    
    DocImpact
    related to bp create-role-with-nailgun-api
    
    Change-Id: Ifd7aadfc7440c00a2b797647551766d942694f67

---
+++ nailgun/nailgun/objects/serializers/role.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nailgun.objects.serializers.base import BasicSerializer
+
+
+class RoleSerializer(BasicSerializer):
+
+    @classmethod
+    def serialize(cls, role, fields=None):
+        release = role.release
+        meta = release.roles_metadata[role.name]
+        volumes = release.volumes_metadata['volumes_roles_mapping'][role.name]
+
+        return {
+            'id': role.id,
+            'name': role.name,
+            'meta': meta,
+            'volumes_roles_mapping': volumes}

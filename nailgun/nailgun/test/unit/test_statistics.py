3f0b13dd18d6543bb7fd3f74503cb27708f1c2f0
Aleksey Kasatkin <akasatkin@mirantis.com>
2015-03-03 14:04:58 +0200

    Use separate user for collecting OSWL data
    Implements: blueprint openstack-workload-statistics
    
    Change-Id: I6b1ca082581c6d2502a5c7b7194b05adc0dd219b

--- nailgun/nailgun/test/unit/test_statistics.py
+++ nailgun/nailgun/test/unit/test_statistics.py
@@ -933,8 +933,8 @@ class TestOpenStackClientProvider(BaseTestCase):
         cluster = self.env.create_cluster(api=False)
         updated_attributes = {
             "editable": {
-                "access": {
-                    "user": {"value": expected_username},
+                "workloads_collector": {
+                    "username": {"value": expected_username},
                     "password": {"value": expected_password},
                     "tenant": {"value": expected_tenant}
                 }

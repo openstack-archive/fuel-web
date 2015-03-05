c5d014625b1aeaf0866d9b7cb68e5fc36056177d
Andriy Popovych <apopovych@mirantis.com>
2015-03-05 10:08:48 +0200

    Test cases for provisioning_serializer
    Change-Id: Ifa0aca52935668e8d7bf0bc4cd8b853eaa11bc3f
    Related-Bug: #1283207

--- nailgun/nailgun/test/integration/test_provisioning_serializer.py
+++ nailgun/nailgun/test/integration/test_provisioning_serializer.py
@@ -14,33 +14,85 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-
 from nailgun.db.sqlalchemy.models import Node
 from nailgun.orchestrator.provisioning_serializers import serialize
+from nailgun.settings import settings
 from nailgun.test.base import BaseIntegrationTest
 
 
 class TestProvisioningSerializer(BaseIntegrationTest):
 
-    def test_ubuntu_serializer(self):
-        release = self.env.create_release(
-            api=False,
-            operating_system='Ubuntu')
+    def setUp(self):
+        super(TestProvisioningSerializer, self).setUp()
+        self.env.create()
+        self.cluster_db = self.env.clusters[0]
+        self.env.create_nodes_w_interfaces_count(
+            1, 1,
+            **{
+                'roles': ['controller'],
+                'pending_addition': True,
+                'cluster_id': self.cluster_db.id
+            }
+        )
+        self.env.create_nodes_w_interfaces_count(
+            1, 1,
+            **{
+                'roles': ['compute'],
+                'pending_addition': True,
+                'cluster_id': self.cluster_db.id
+            }
+        )
+        self.attributes = self.cluster_db.attributes.editable
+        self.serialized_cluster = serialize(
+            self.cluster_db, self.cluster_db.nodes)
 
-        self.env.create(
-            cluster_kwargs={
-                'release_id': release.id},
-            nodes_kwargs=[
-                {'roles': ['controller'], 'pending_addition': True},
-                {'roles': ['compute'], 'pending_addition': True}])
+    def test_cluster_info_serialization(self):
+        engine = self.serialized_cluster['engine']
+        self.assertDictEqual(engine, {
+            'url': settings.COBBLER_URL,
+            'username': settings.COBBLER_USER,
+            'password': settings.COBBLER_PASSWORD,
+            'master_ip': settings.MASTER_IP,
+            'provision_method': 'cobbler'
+        })
 
-        cluster_db = self.env.clusters[0]
-        serialized_cluster = serialize(cluster_db, cluster_db.nodes)
-
-        for node in serialized_cluster['nodes']:
+    def test_node_serialization(self):
+        for node in self.serialized_cluster['nodes']:
             node_db = self.db.query(Node).filter_by(
                 fqdn=node['hostname']
             ).first()
+            # Get interface (in our case we created only one for each node)
+            intr_db = node_db.nic_interfaces[0]
+            intr_name = intr_db.name
+            intr_mac = intr_db.mac
+            kernal_params = self.attributes.get('kernel_params', {}) \
+                .get('kernel', {}).get('value')
+
+            self.assertEqual(node['uid'], node_db.uid)
+            self.assertEqual(node['power_address'], node_db.ip)
+            self.assertEqual(node['name'], "node-{0}".format(node_db.id))
+            self.assertEqual(node['hostname'], node_db.fqdn)
             self.assertEqual(
-                node['kernel_options']['netcfg/choose_interface'],
-                node_db.admin_interface.mac)
+                node['power_pass'], settings.PATH_TO_BOOTSTRAP_SSH_KEY)
+
+            self.assertDictEqual(node['kernel_options'], {
+                'netcfg/choose_interface': node_db.admin_interface.mac,
+                'udevrules': '{0}_{1}'.format(intr_mac, intr_name)
+            })
+
+            self.assertDictEqual(node['ks_meta']['pm_data'], {
+                'ks_spaces': node_db.attributes.volumes,
+                'kernel_params': kernal_params
+            })
+            # Check node interfaces section
+            self.assertEqual(
+                node['interfaces'][intr_name]['mac_address'], intr_mac)
+            self.assertEqual(
+                node['interfaces'][intr_name]['static'], '0')
+            self.assertEqual(
+                node['interfaces'][intr_name]['dns_name'], node_db.fqdn)
+            # Check node interfaces extra section
+            self.assertEqual(node['interfaces_extra'][intr_name], {
+                'peerdns': 'no',
+                'onboot': 'yes'
+            })

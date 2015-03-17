cd2e75a949ecb4fae6ad0c3bc77c598020d51c8e
Aleksey Kasatkin <akasatkin@mirantis.com>
2015-03-17 16:59:43 +0200

    vlan_splinters and disable_offloading are removed from settings
    Also, disable_offloading is added to default NIC configuration.
    vlan_splinters fixture is added for 6.0 tests.
    
    Closes-Bug: #1432830
    Closes-Bug: #1432832
    Closes-Bug: #1428767
    
    Change-Id: I28ce5413fc6271f11ebb78b195d40c6282deae79

--- nailgun/nailgun/test/integration/test_orchestrator_serializer.py
+++ nailgun/nailgun/test/integration/test_orchestrator_serializer.py
@@ -26,6 +26,7 @@ from netaddr import IPAddress
 from netaddr import IPNetwork
 from netaddr import IPRange
 from oslo.serialization import jsonutils
+import yaml
 
 from nailgun import consts
 from nailgun.db.sqlalchemy.models import Cluster
@@ -1745,6 +1746,50 @@ class TestNeutronOrchestratorSerializer(OrchestratorSerializerTestBase):
             self.assertIn('gateway', ep['br-storage'])
             self.assertIn('gateway', ep['br-mgmt'])
 
+
+class TestVlanSplinters(OrchestratorSerializerTestBase):
+
+    @property
+    def vlan_splinters_meta(self):
+        meta = """
+        vlan_splinters:
+          metadata:
+            toggleable: true
+            enabled: false
+            label: "VLAN Splinters"
+            weight: 50
+            restrictions:
+              - condition: "cluster:net_provider != 'neutron'"
+                action: "hide"
+          vswitch:
+            value: "disabled"
+            label: "Open VSwitch VLAN Splinters feature"
+            weight: 55
+            type: "radio"
+            values:
+              - data: "soft"
+                label: "Enable OVS VLAN splinters soft trunks workaround"
+                description: "Configure OVS to use VLAN splinters workaround
+                  with soft trunk detection. This may resolve issues that
+                  might be encountered when using VLAN tags with OVS and
+                  Neutron on Kernels <3.3 (CentOS)"
+              - data: "hard"
+                label: "Enable OVS VLAN splinters hard trunks workaround"
+                description: "Configure OVS to use VLAN splinters workaround
+                  with hard trunk allocation. Offers similar effect as soft
+                  trunks workaround, but forces each trunk to be predefined.
+                  This may work better than soft trunks especially if you
+                  still see network problems using soft trunks"
+              - data: "kernel_lt"
+                label: "EXPERIMENTAL: Use Fedora longterm kernel"
+                description: "Install the Fedora 3.10 longterm kernel instead
+                  of the default 2.6.32 kernel. This should remove any need
+                  for VLAN Splinters workarounds as the 3.10 kernel has better
+                  support for OVS VLANs. This kernel may not work with all
+                  hardware platforms, use caution."
+        """
+        return yaml.load(meta)
+
     def _create_cluster_for_vlan_splinters(self, segment_type='gre'):
         meta = {
             'interfaces': [
@@ -1758,7 +1803,8 @@ class TestNeutronOrchestratorSerializer(OrchestratorSerializerTestBase):
         cluster = self.env.create(
             cluster_kwargs={
                 'net_provider': 'neutron',
-                'net_segment_type': segment_type
+                'net_segment_type': segment_type,
+                'editable_attributes': self.vlan_splinters_meta
             },
             nodes_kwargs=[
                 {'roles': ['controller'], 'pending_addition': True,

4353e730da932a23415c061a55bc794988370048
Vitaly Kramskikh <vkramskikh@mirantis.com>
2015-03-30 20:22:44 +0300

    Twitter Bootstrap 3 Markup
    Change-Id: Ib7d4f90355af5d53618ae56f7fdce858d44bfcef

--- nailgun/static/plugins/vmware/vmware_tab.jsx
+++ nailgun/static/plugins/vmware/vmware_tab.jsx
@@ -33,37 +33,25 @@ define(
         onChange: function(name, value) {
             this.props.model.set(name, value);
             this.setState({model: this.props.model});
-            _.defer(function() {dispatcher.trigger('vcenter_model_update'); });
+            _.defer(function() {dispatcher.trigger('vcenter_model_update');});
         },
         render: function() {
             var metadata = this.props.metadata,
-                options = this.props.options || [],
-                errors = this.props.model.validationError,
-                errorText = errors ? errors[metadata.name] : null;
-            var classes = utils.classNames({
-                'settings-group table-wrapper parameter-box': true,
-                password: metadata.type == 'password',
-                'has-error': !!errorText
-            });
+                value = this.props.model.get(metadata.name);
             return (
-                <div key={metadata.name} className={classes}>
-                    <controls.Input
-                        key={metadata.name}
-                        {... _.pick(metadata, 'name', 'type', 'label')}
-                        value={this.props.model.get(metadata.name)}
-                        checked={this.props.model.get(metadata.name)}
-                        description={errorText || metadata.description}
-                        descriptionClassName={utils.classNames({'validation-error': errorText})}
-                        toggleable={metadata.type == 'password'}
-                        wrapperClassName='tablerow-wrapper'
-                        onChange={this.onChange}
-                        disabled={this.props.disabled}
-                    >
-                        {options.map(function(index) {
-                            return <option key={index.label} value={index.value}>{index.label}</option>;
-                        }, this)}
-                    </controls.Input>
-                </div>
+                <controls.Input
+                    {... _.pick(metadata, 'name', 'type', 'label', 'description')}
+                    value={value}
+                    checked={value}
+                    toggleable={metadata.type == 'password'}
+                    onChange={this.onChange}
+                    disabled={this.props.disabled}
+                    error={(this.props.model.validationError || {})[metadata.name]}
+                >
+                    {this.props.options && this.props.options.map(function(index) {
+                        return <option key={index.label} value={index.value}>{index.label}</option>;
+                    })}
+                </controls.Input>
             );
         }
     });
@@ -93,17 +81,14 @@ define(
     var GenericSection = React.createClass({
         mixins: [controls.tooltipMixin],
         render: function() {
-            var model = this.props.model;
-            if (!model) {
-                return null;
-            }
+            if (!this.props.model) return null;
             return (
-                <div className={this.props.className}>
-                    <legend className='vmware'>
+                <div className='col-xs-12 forms-box'>
+                    <h3>
                         {this.props.title}
                         {this.renderTooltipIcon()}
-                    </legend>
-                    <FieldGroup model={model} disabled={this.props.disabled}/>
+                    </h3>
+                    <FieldGroup model={this.props.model} disabled={this.props.disabled}/>
                 </div>
             );
         }
@@ -111,28 +96,31 @@ define(
 
     var NovaCompute = React.createClass({
         render: function() {
-            var model = this.props.model;
-            if (!model) {
-                return null;
-            }
-            var removeButtonClasses = utils.classNames({'btn btn-link': true, hide: this.props.isRemovable});
+            if (!this.props.model) return null;
             return (
                 <div className='nova-compute'>
                     <h4>
-                        <button className='btn btn-link'
-                            disabled={this.props.disabled}
-                            onClick={_.bind(function() {this.props.onAdd(model)}, this)}>
-                            <i className='icon-plus-circle'></i>
-                        </button>
-                        <button className={removeButtonClasses}
-                            disabled={this.props.disabled}
-                            onClick={_.bind(function() {this.props.onRemove(model)}, this)}>
-                            <i className='icon-minus-circle'></i>
-                        </button>
-                        &thinsp;
+                        <div className='btn-group'>
+                            <button
+                                className='btn btn-link'
+                                disabled={this.props.disabled}
+                                onClick={_.bind(function() {this.props.onAdd(this.props.model)}, this)}
+                            >
+                                <i className='glyphicon glyphicon-plus-sign' />
+                            </button>
+                            {!this.props.isRemovable &&
+                                <button
+                                    className='btn btn-link'
+                                    disabled={this.props.disabled}
+                                    onClick={_.bind(function() {this.props.onRemove(this.props.model)}, this)}
+                                >
+                                    <i className='glyphicon glyphicon-minus-sign' />
+                                </button>
+                            }
+                        </div>
                         {i18n('vmware.nova_compute')}
                     </h4>
-                    <FieldGroup model={model} disabled={this.props.disabled}/>
+                    <FieldGroup model={this.props.model} disabled={this.props.disabled}/>
                 </div>
             );
         }
@@ -163,26 +151,26 @@ define(
         },
         renderComputes: function(actions) {
             var novaComputes = this.props.model.get('nova_computes'),
-                isSingleInstance = (novaComputes.length == 1),
+                isSingleInstance = novaComputes.length == 1,
                 disabled = actions.disable.nova_computes;
 
             return (
-                <div className='idented'>
-                    <legend className='vmware'>{i18n('vmware.nova_computes')}</legend>
-                    {
-                        novaComputes.map(function(value) {
-                            return (
-                                <NovaCompute
-                                    key={value.cid}
-                                    model={value}
-                                    onAdd={this.addNovaCompute}
-                                    onRemove={this.removeNovaCompute}
-                                    isRemovable={isSingleInstance}
-                                    disabled={disabled.result || this.props.disabled}
-                                />
-                            );
-                        }, this)
-                    }
+                <div className='col-xs-offset-1'>
+                    <h3>
+                        {i18n('vmware.nova_computes')}
+                    </h3>
+                    {novaComputes.map(function(compute) {
+                        return (
+                            <NovaCompute
+                                key={compute.cid}
+                                model={compute}
+                                onAdd={this.addNovaCompute}
+                                onRemove={this.removeNovaCompute}
+                                isRemovable={isSingleInstance}
+                                disabled={disabled.result || this.props.disabled}
+                            />
+                        );
+                    }, this)}
                 </div>
             );
         },
@@ -201,20 +189,16 @@ define(
     var AvailabilityZones = React.createClass({
         mixins: [controls.tooltipMixin],
         render: function() {
-            if (!this.props.collection) {
-                return null;
-            }
+            if (!this.props.collection) return null;
             return (
-                <div className='availability-zones'>
-                    <legend className='vmware'>
+                <div className='col-xs-12 forms-box'>
+                    <h3>
                         {i18n('vmware.availability_zones')}
                         {this.renderTooltipIcon()}
-                    </legend>
-                    {
-                        this.props.collection.map(function(model) {
-                            return <AvailabilityZone key={model.cid} model={model} disabled={this.props.disabled}/>;
-                        }, this)
-                    }
+                    </h3>
+                    {this.props.collection.map(function(model) {
+                        return <AvailabilityZone key={model.cid} model={model} disabled={this.props.disabled}/>;
+                    }, this)}
                 </div>
             );
         }
@@ -306,41 +290,37 @@ define(
                 defaultsDisabled = !editable || !this.hasDefaultsChanges;
 
             return (
-                <div className='vmware'>
-                    <div className='wrapper'>
-                        <h3>{i18n('vmware.title')}</h3>
-                        {!hide.availability_zones.result &&
-                            <AvailabilityZones
-                                collection={model.get('availability_zones')}
-                                disabled={!editable || disable.availability_zones.result}
-                                tooltipText={disable.availability_zones.message}
-                            />
-                        }
-                        {!hide.network.result &&
-                            <GenericSection
-                                model={model.get('network')}
-                                className='network'
-                                title={i18n('vmware.network')}
-                                disabled={!editable || disable.network.result}
-                                tooltipText={disable.network.message}
-                            />
-                        }
-                        {!hide.glance.result &&
-                            <GenericSection
-                                model={model.get('glance')}
-                                className='glance'
-                                title={i18n('vmware.glance')}
-                                disabled={!editable || disable.glance.result}
-                                tooltipText={disable.glance.message}
-                            />
-                        }
-                    </div>
-                    <div className='page-control-box'>
-                        <div className='page-control-button-placeholder'>
-                            <button className='btn btn-load-defaults' onClick={this.onLoadDefaults} disabled={defaultsDisabled}>
+                <div className='row'>
+                    <div className='title'>{i18n('vmware.title')}</div>
+                    {!hide.availability_zones.result &&
+                        <AvailabilityZones
+                            collection={model.get('availability_zones')}
+                            disabled={!editable || disable.availability_zones.result}
+                            tooltipText={disable.availability_zones.message}
+                        />
+                    }
+                    {!hide.network.result &&
+                        <GenericSection
+                            model={model.get('network')}
+                            title={i18n('vmware.network')}
+                            disabled={!editable || disable.network.result}
+                            tooltipText={disable.network.message}
+                        />
+                    }
+                    {!hide.glance.result &&
+                        <GenericSection
+                            model={model.get('glance')}
+                            title={i18n('vmware.glance')}
+                            disabled={!editable || disable.glance.result}
+                            tooltipText={disable.glance.message}
+                        />
+                    }
+                    <div className='col-xs-12 page-buttons'>
+                        <div className='btn-group pull-right'>
+                            <button className='btn btn-default btn-load-defaults' onClick={this.onLoadDefaults} disabled={defaultsDisabled}>
                                 {i18n('vmware.reset_to_defaults')}
                             </button>
-                            <button className='btn btn-revert-changes' onClick={this.onCancel} disabled={!this.hasChanges}>
+                            <button className='btn btn-default btn-revert-changes' onClick={this.onCancel} disabled={!this.hasChanges}>
                                 {i18n('vmware.cancel')}
                             </button>
                             <button className='btn btn-success btn-apply-changes' onClick={this.onSave} disabled={saveDisabled}>

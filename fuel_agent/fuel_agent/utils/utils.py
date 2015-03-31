537f2cd42014f9deebfd9a93d36dbb769c8afa79
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2015-03-31 19:25:20 +0300

    IBP: Added driver and objects for building images
    As far as building of OS images is nothing more than
    just a stage of the whole OS installing procedure
    it is sounds rational to implement this in terms
    of fuel-agent. Besides, we already have plenty of utilities
    which could be useful during building of images.
    And some tasks are the same like pre-configuring
    some files inside target OS.
    
    Related-bug: #1433193
    Implements: blueprint ibp-build-ubuntu-images
    Change-Id: I3fadfb16e06e4ee16926da29b7b83ca005500698

--- fuel_agent/fuel_agent/utils/utils.py
+++ fuel_agent/fuel_agent/utils/utils.py
@@ -200,3 +200,8 @@ def init_http_request(url, byte_range=0):
                 'Exceeded maximum http request retries for %s' % url)
     response_obj.raise_for_status()
     return response_obj
+
+
+def makedirs_if_not_exists(path, mode=0o755):
+    if not os.path.isdir(path):
+        os.makedirs(path, mode=mode)

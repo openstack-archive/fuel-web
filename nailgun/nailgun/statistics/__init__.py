cf9391aa515d8a6e3826560fa2402a4f14ed5f66
Artem Roma <aroma@mirantis.com>
2015-03-31 17:16:09 +0300

    White list filtering reused for oswl collecting
    Change-Id: I6177cbf83079b89ad25bd8a1ca5072d0e1c027ab
    Closes-Bug: #1438716

--- nailgun/nailgun/statistics/__init__.py
+++ nailgun/nailgun/statistics/__init__.py
@@ -11,3 +11,9 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
+from collections import namedtuple
+
+
+WhiteListRule = namedtuple(
+    'WhiteListItem', ['path', 'map_to_name', 'transform_func'])

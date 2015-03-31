9f385b9424c9d3d26a4f5d4ed5d9f2f84a2ed0d9
Sebastian Kalinowski <sebastian@kalinowski.eu>
2015-03-31 09:49:43 +0200

    Shotgun will always try to save stdout and stderr
    Added CCStringIO - "carbon copy" StringIO which in addition
    to normal behavior can pass any write to different file-like
    object.
    It is used as a stdout and stderr for executing commands via fabric
    and when errors occur durring command execution it is used as a source
    of stdout and stderr. In addition it also proxies everything to
    regular sys.stdout and sys.stderr.
    
    Change-Id: I022128141ed142561213a9633bc0fe0f8ccc2685
    Closes-Bug: #1434574

--- shotgun/shotgun/utils.py
+++ shotgun/shotgun/utils.py
@@ -19,6 +19,7 @@ import os
 import re
 import shlex
 import socket
+from StringIO import StringIO
 import subprocess
 
 
@@ -116,3 +117,29 @@ def execute(command, to_filename=None, env=None):
             process[-2].stdout.close()
     stdout, stderr = process[-1].communicate()
     return (process[-1].returncode, stdout, stderr)
+
+
+class CCStringIO(StringIO):
+    """A "carbon copy" StringIO.
+
+    It's capable of multiplexing its writes to other buffer objects.
+
+    Taken from fabric.tests.mock_streams.CarbonCopy
+    """
+
+    def __init__(self, buffer='', writers=None):
+        """If ``writers`` is given and is a file-like object or an
+        iterable of same, it/they will be written to whenever this
+        StringIO instance is written to.
+        """
+        StringIO.__init__(self, buffer)
+        if writers is None:
+            writers = []
+        elif hasattr(writers, 'write'):
+            writers = [writers]
+        self.writers = writers
+
+    def write(self, s):
+        StringIO.write(self, s)
+        for writer in self.writers:
+            writer.write(s)

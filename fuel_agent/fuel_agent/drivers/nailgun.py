327cb76e0a084686a73d51c688fc27f08d84b2cc
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2015-03-31 19:25:20 +0300

    IBP: Added driver and objects for building images
    As far as building of OS images is nothing more than
    just a stage of the whole OS installing procedure
    it is sounds rational to implement this in terms
    of fuel-agent. Besides, we already have plenty of utilities
    which could be useful during building of images.
    And some tasks are the same like pre-configuring
    some files inside target OS.
    
    Related-bug: #1433193
    Implements: blueprint ibp-build-ubuntu-images
    Change-Id: I3fadfb16e06e4ee16926da29b7b83ca005500698

--- fuel_agent/fuel_agent/drivers/nailgun.py
+++ fuel_agent/fuel_agent/drivers/nailgun.py
@@ -15,7 +15,13 @@
 import math
 import os
 import six
+import yaml
 
+from six.moves.urllib.parse import urljoin
+from six.moves.urllib.parse import urlparse
+from six.moves.urllib.parse import urlsplit
+
+from fuel_agent.drivers.base import BaseDataDriver
 from fuel_agent.drivers import ks_spaces_validator
 from fuel_agent import errors
 from fuel_agent import objects
@@ -23,9 +29,6 @@ from fuel_agent.openstack.common import log as logging
 from fuel_agent.utils import hardware_utils as hu
 from fuel_agent.utils import utils
 
-from six.moves.urllib.parse import urljoin
-from six.moves.urllib.parse import urlparse
-import yaml
 
 LOG = logging.getLogger(__name__)
 
@@ -60,11 +63,9 @@ def match_device(hu_disk, ks_disk):
     return False
 
 
-class Nailgun(object):
+class Nailgun(BaseDataDriver):
     def __init__(self, data):
-        # Here data is expected to be raw provisioning data
-        # how it is given by nailgun
-        self.data = data
+        super(Nailgun, self).__init__(data)
         self.partition_scheme = self.parse_partition_scheme()
         self.configdrive_scheme = self.parse_configdrive_scheme()
         # parsing image scheme needs partition scheme has been parsed
@@ -406,3 +407,102 @@ class Nailgun(object):
                 md5=image_meta.get(mount_point, {}).get('md5'),
             )
         return image_scheme
+
+
+class NailgunBuildImage(BaseDataDriver):
+
+    TRUSTY_PACKAGES = [
+        "bash-completion",
+        "curl",
+        "daemonize",
+        "build-essential",
+        "gdisk",
+        "grub-pc",
+        "linux-firmware",
+        "linux-firmware-nonfree",
+        "linux-image-generic-lts-trusty",
+        "linux-headers-generic-lts-trusty",
+        "lvm2",
+        "mdadm",
+        "nailgun-agent",
+        "nailgun-mcagents",
+        "nailgun-net-check",
+        "ntp",
+        "openssh-client",
+        "openssh-server",
+        "telnet",
+        "ubuntu-minimal",
+        "ubuntu-standard",
+        "virt-what",
+        "acl",
+        "anacron",
+        "bridge-utils",
+        "bsdmainutils",
+        "cloud-init",
+        "debconf-utils",
+        "ruby-augeas",
+        "ruby-shadow",
+        "ruby-json",
+        "mcollective",
+        "puppet",
+        "python-amqp",
+        "ruby-ipaddress",
+        "ruby-netaddr",
+        "ruby-openstack",
+        "ruby-stomp",
+        "vim",
+        "vlan",
+        "uuid-runtime",
+    ]
+
+    def __init__(self, data):
+        super(NailgunBuildImage, self).__init__(data)
+        self.parse_schemes()
+        self.parse_operating_system()
+
+    def parse_operating_system(self):
+        if self.data.get('codename') != 'trusty':
+            raise errors.WrongInputDataError(
+                'Currently, we only support Ubuntu trusty but given'
+                'codename is {0}'.format(self.data.get('codename')))
+
+        packages = self.TRUSTY_PACKAGES
+        if 'packages' in self.data:
+            packages = self.data['packages']
+
+        repos = []
+        for repo in self.data['repos']:
+            repos.append(objects.DEBRepo(
+                name=repo['name'],
+                uri=repo['uri'],
+                suite=repo['suite'],
+                section=repo['section'],
+                priority=repo['priority']))
+
+        self.operating_system = objects.Ubuntu(repos=repos, packages=packages)
+
+    def parse_schemes(self):
+        self.image_scheme = objects.ImageScheme()
+        self.partition_scheme = objects.PartitionScheme()
+
+        for mount, image in six.iteritems(self.data['image_data']):
+            filename = os.path.basename(urlsplit(image['uri']).path)
+            # Loop does not allocate any loop device
+            # during initialization.
+            device = objects.Loop()
+
+            self.image_scheme.add_image(
+                uri='file://' + os.path.join(self.data['output'], filename),
+                format=image['format'],
+                container=image['container'],
+                target_device=device)
+
+            self.partition_scheme.add_fs(
+                device=device,
+                mount=mount,
+                fs_type=image['format'])
+
+            if mount == '/':
+                metadata_filename = filename.split('.', 1)[0] + '.yaml'
+                self.metadata_uri = 'file://' + os.path.join(
+                    self.data['output'], metadata_filename)

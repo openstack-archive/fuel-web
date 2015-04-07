6204196e426a10a171f3df94684c1eeb254d0197
Valyavskiy Viacheslav <vvalyavskiy@mirantis.com>
2015-04-07 20:32:56 +0300

    admin interface bonding nailgun/backend
    Implements: blueprint admin-network-on-bond
    
    Change-Id: I2267c40e6f588061f8cceb68187419b0e1aa1b1f

--- nailgun/nailgun/objects/node.py
+++ nailgun/nailgun/objects/node.py
@@ -688,6 +688,24 @@ class Node(NailgunObject):
             return Cluster.get_network_manager(instance.cluster)
 
     @classmethod
+    def get_admin_physical_iface(cls, instance):
+        """Returns node's physical iface.
+        In case if we have bonded admin iface, first
+        of the bonded ifaces will be returned
+
+        :param instance: Node instance
+        :returns: interface instance
+        """
+        admin_iface = cls.get_network_manager(instance) \
+            .get_admin_interface(instance)
+
+        if admin_iface.type != consts.NETWORK_INTERFACE_TYPES.bond:
+            return admin_iface
+
+        iface = filter(lambda i: i.mac == instance.mac, admin_iface.slaves)
+        return iface[0] if iface else admin_iface.slaves[-1]
+
+    @classmethod
     def remove_from_cluster(cls, instance):
         """Remove Node from Cluster.
         Also drops networks assignment for Node and clears both

3e77f375dec93db1b807ed0fa685bfb9ba8060cf
Sylwester Brzeczkowski <sbrzeczkowski@mirantis.com>
2015-04-17 15:42:01 +0200

    Delete logs on master after node or env deletion
    Added delete_node_logs function to utils which is called in
    NailgunReciever.remove_nodes_resp after node deletion
    (just before removing it from database).
    
    Closes-Bug: #1428825
    Change-Id: I828e515c7f5d1e39373d351809e583cd732836c1

--- nailgun/nailgun/task/task.py
+++ nailgun/nailgun/task/task.py
@@ -45,6 +45,7 @@ from nailgun.orchestrator import stages
 from nailgun.settings import settings
 from nailgun.task.fake import FAKE_THREADS
 from nailgun.task.helpers import TaskHelper
+from nailgun.utils import logs as logs_utils
 from nailgun.utils.restrictions import VmwareAttributesRestriction
 from nailgun.utils.zabbix import ZabbixManager
 
@@ -261,7 +262,7 @@ class ProvisionTask(object):
                 node
             ).get_admin_network_group_id(node.id)
 
-            TaskHelper.prepare_syslog_dir(node, admin_net_id)
+            logs_utils.prepare_syslog_dir(node, admin_net_id)
 
         rpc_message = make_astute_message(
             task,

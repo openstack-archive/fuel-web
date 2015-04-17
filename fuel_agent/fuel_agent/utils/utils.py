4b6959c415e5998fab4f7b021a7ed788b8cd827a
Alexander Gordeev <agordeev@mirantis.com>
2015-04-17 14:25:55 +0300

    IBP: add retries to utils.execute
    utils.execute should be able to retry command execution in case of
    failure.
    
    Controlled by additional 'attempts' key parameter, which is defaultly
    set to 1.
    
    Added 10 attempts to execute debootstrap/apt-get in build_utils.py.
    That should protect from minor networking issues during downloading
    of packages.
    
    Partially implements: blueprint ibp-build-ubuntu-images
    
    Change-Id: I5c547d613fcf8596f95b64623fe65150ae52c521

--- fuel_agent/fuel_agent/utils/utils.py
+++ fuel_agent/fuel_agent/utils/utils.py
@@ -57,6 +57,11 @@ u_opts = [
         default=1048576,
         help='Block size of data to read for calculating checksum',
     ),
+    cfg.FloatOpt(
+        'execute_retry_delay',
+        default=2.0,
+        help='Delay in seconds before the next exectuion will retry',
+    ),
 ]
 
 CONF = cfg.CONF
@@ -70,6 +75,7 @@ def execute(*cmd, **kwargs):
     commands = [c.strip() for c in re.split(ur'\|', command)]
     env = os.environ
     env['PATH'] = '/bin:/usr/bin:/sbin:/usr/sbin'
+    attempts = kwargs.pop('attempts', 1)
     check_exit_code = kwargs.pop('check_exit_code', [0])
     ignore_exit_code = False
     to_filename = kwargs.get('to_filename')
@@ -85,36 +91,45 @@ def execute(*cmd, **kwargs):
     if to_filename:
         to_file = open(to_filename, 'wb')
 
-    process = []
-    for c in commands:
+    for attempt in reversed(xrange(attempts)):
         try:
-            # NOTE(eli): Python's shlex implementation doesn't like unicode.
-            # We have to convert to ascii before shlex'ing the command.
-            # http://bugs.python.org/issue6988
-            encoded_command = c.encode('ascii')
+            process = []
+            for c in commands:
+                try:
+                    # NOTE(eli): Python's shlex implementation doesn't like
+                    # unicode. We have to convert to ascii before shlex'ing
+                    # the command. http://bugs.python.org/issue6988
+                    encoded_command = c.encode('ascii')
 
-            process.append(subprocess.Popen(
-                shlex.split(encoded_command),
-                env=env,
-                stdin=(process[-1].stdout if process else None),
-                stdout=(to_file
-                        if (len(process) == len(commands) - 1) and to_file
-                        else subprocess.PIPE),
-                stderr=(subprocess.PIPE),
-                cwd=cwd
-            ))
-        except OSError as e:
-            raise errors.ProcessExecutionError(exit_code=1, stdout='',
-                                               stderr=e, cmd=command)
-        if len(process) >= 2:
-            process[-2].stdout.close()
-    stdout, stderr = process[-1].communicate()
-    if not ignore_exit_code and process[-1].returncode not in check_exit_code:
-        raise errors.ProcessExecutionError(exit_code=process[-1].returncode,
-                                           stdout=stdout,
-                                           stderr=stderr,
-                                           cmd=command)
-    return (stdout, stderr)
+                    process.append(subprocess.Popen(
+                        shlex.split(encoded_command),
+                        env=env,
+                        stdin=(process[-1].stdout if process else None),
+                        stdout=(to_file
+                                if ((len(process) == len(commands) - 1) and
+                                    to_file)
+                                else subprocess.PIPE),
+                        stderr=(subprocess.PIPE),
+                        cwd=cwd
+                    ))
+                except (OSError, ValueError) as e:
+                    raise errors.ProcessExecutionError(exit_code=1, stdout='',
+                                                       stderr=e, cmd=command)
+                if len(process) >= 2:
+                    process[-2].stdout.close()
+            stdout, stderr = process[-1].communicate()
+            if (not ignore_exit_code and
+               process[-1].returncode not in check_exit_code):
+                    raise errors.ProcessExecutionError(
+                        exit_code=process[-1].returncode, stdout=stdout,
+                        stderr=stderr, cmd=command)
+            return (stdout, stderr)
+        except errors.ProcessExecutionError as e:
+                LOG.warning('Failed to execute command: %s', e)
+                if not attempt:
+                    raise
+                else:
+                    time.sleep(CONF.execute_retry_delay)
 
 
 def parse_unit(s, unit, ceil=True):

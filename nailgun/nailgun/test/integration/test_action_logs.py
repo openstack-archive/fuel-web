64e269c61eab86578a422a3004154dbc88f8bdb2
Alexander Kislitsky <akislitsky@mirantis.com>
2015-04-22 13:45:38 +0300

    Only UTC datetime used for action_log creation
    Backport from 6.1
    
    Using of local datetime removed from ActionLogs construction process.
    ConnectionMonitorMiddleware added into test application.
    Separate test for action_logs added.
    Closing DB connections added into test_db_refresh test.
    
    Change-Id: I8e3e7a4bdf9f2d4f82a49022b7a0fca266bb183b
    Closes-Bug: #1446734

---
+++ nailgun/nailgun/test/integration/test_action_logs.py
@@ -0,0 +1,79 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import datetime
+
+from nailgun.test.base import BaseIntegrationTest
+
+from nailgun import consts
+from nailgun import objects
+from nailgun.openstack.common import jsonutils
+from nailgun.test.base import fake_tasks
+from nailgun.test.base import reverse
+
+
+class TestActionLogs(BaseIntegrationTest):
+
+    @fake_tasks()
+    def test_only_utc_datetime_used(self):
+        self.env.create(
+            api=False,
+            nodes_kwargs=[
+                {'roles': ['controller'], 'pending_addition': True},
+                {'roles': ['compute'], 'pending_addition': True},
+            ]
+        )
+
+        deployment_task = self.env.launch_deployment()
+        self.env.wait_ready(deployment_task)
+
+        cluster = self.env.clusters[0]
+
+        self.app.put(
+            reverse('ClusterHandler', kwargs={'obj_id': cluster.id}),
+            jsonutils.dumps({'name': 'xx'}),
+            headers=self.default_headers
+        )
+
+        action_logs = objects.ActionLogCollection.filter_by(None)
+        dt = datetime.datetime.utcnow()
+        for action_log in action_logs:
+            self.assertGreaterEqual(dt, action_log.start_timestamp)
+            self.assertGreaterEqual(dt, action_log.end_timestamp)
+
+    @fake_tasks()
+    def test_all_action_logs_types_saved(self):
+        # Creating nailgun_tasks
+        self.env.create(
+            api=False,
+            nodes_kwargs=[
+                {'roles': ['controller'], 'pending_addition': True},
+                {'roles': ['compute'], 'pending_addition': True},
+            ]
+        )
+        deployment_task = self.env.launch_deployment()
+        self.env.wait_ready(deployment_task)
+
+        # Creating http_request
+        cluster = self.env.clusters[0]
+        self.app.put(
+            reverse('ClusterHandler', kwargs={'obj_id': cluster.id}),
+            jsonutils.dumps({'name': 'xx'}),
+            headers=self.default_headers
+        )
+
+        # Filtering action types
+        action_logs = objects.ActionLogCollection.filter_by(None).all()
+        action_types = [al.action_type for al in action_logs]
+        self.assertSetEqual(set(consts.ACTION_TYPES), set(action_types))

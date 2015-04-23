154604f958092444b7ae65e30a854f3ca213a76b
Denis Egorenko <degorenko@mirantis.com>
2015-04-23 18:42:04 +0300

    Fedora kernel install in ibp mode
    Nailgun puts 'kernel_lt' flag in provisioning data and
    if this flag is set, it is supposed that we need to install
    kernel-lt package which is fedora kernel 3.10.
    We assume both kernels are available in centos OS image:
    2.6 and 3.10. We can just configure grub to boot with
    one of those kernels. If flag 'kernel_lt' is NOT set, we
    use 'vmlinuz-2.6' regexp for looking up for kernel file.
    Otherwise we use default regexp 'vmlinuz' and sort available
    kernels in backward direction, i.e. we use the newest available
    kernel.
    
    Co-Authored-By: Vladimir Kozhukalov <vkozhukalov@mirantis.com>
    Change-Id: I1f7eee934440ce32d6e733c417e82f578b6d0c18
    Partially-closes-bug: #1398643

--- fuel_agent/fuel_agent/tests/test_utils.py
+++ fuel_agent/fuel_agent/tests/test_utils.py
@@ -180,3 +180,33 @@ class ExecuteTestCase(testtools.TestCase):
         utils.makedirs_if_not_exists('/fake/path')
         mock_isdir.assert_called_once_with('/fake/path')
         self.assertEqual(mock_makedirs.mock_calls, [])
+
+    @mock.patch('fuel_agent.utils.utils.os.listdir')
+    def test_guess_filename(self, mock_oslistdir):
+        mock_oslistdir.return_value = ['file1', 'file2', 'file3']
+        filename = utils.guess_filename('/some/path', '^file2.*')
+        self.assertEqual(filename, 'file2')
+        mock_oslistdir.assert_called_once_with('/some/path')
+
+    @mock.patch('fuel_agent.utils.utils.os.listdir')
+    def test_guess_filename_not_found(self, mock_oslistdir):
+        mock_oslistdir.return_value = ['file1', 'file2', 'file3']
+        filename = utils.guess_filename('/some/path', '^file4.*')
+        self.assertIsNone(filename)
+        mock_oslistdir.assert_called_once_with('/some/path')
+
+    @mock.patch('fuel_agent.utils.utils.os.listdir')
+    def test_guess_filename_not_exact_match(self, mock_oslistdir):
+        mock_oslistdir.return_value = ['file1', 'file2', 'file3']
+        filename = utils.guess_filename('/some/path', '^file.*')
+        # by default files are sorted in backward direction
+        self.assertEqual(filename, 'file3')
+        mock_oslistdir.assert_called_once_with('/some/path')
+
+    @mock.patch('fuel_agent.utils.utils.os.listdir')
+    def test_guess_filename_not_exact_match_forward_sort(self, mock_oslistdir):
+        mock_oslistdir.return_value = ['file1', 'file2', 'file3']
+        filename = utils.guess_filename('/some/path', '^file.*', reverse=False)
+        # by default files are sorted in backward direction
+        self.assertEqual(filename, 'file1')
+        mock_oslistdir.assert_called_once_with('/some/path')

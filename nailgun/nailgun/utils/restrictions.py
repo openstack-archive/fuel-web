59886696635db1cbdf2bd34e7d07f42b32fbea5c
Przemyslaw Kaminski <pkaminski@mirantis.com>
2015-04-28 10:45:13 +0200

    Validate cluster attributes
    Add validation for cluster attributes. Do it so that it doesn't break
    compatibility with current tests. Validate regexpes if they are provided.
    
    Change-Id: I9c1fc04dab45aecf3e32615172654d11844df7c3
    Closes-Bug: #1447407

--- nailgun/nailgun/utils/restrictions.py
+++ nailgun/nailgun/utils/restrictions.py
@@ -260,11 +260,10 @@ class AttributesRestriction(RestrictionMixin):
                     # TODO(apopovych): handle restriction message
                     return
                 else:
-                    attr_regex = data.get('regex', {})
-                    if attr_regex:
-                        pattern = re.compile(attr_regex.get('source'))
-                        if not pattern.match(data.get('value')):
-                            yield attr_regex.get('error')
+                    regex_error = cls.validate_regex(data)
+                    if regex_error is not None:
+                        yield regex_error
+
                     for key, value in six.iteritems(data):
                         if key not in ['restrictions', 'regex']:
                             for err in find_errors(value):
@@ -275,6 +274,14 @@ class AttributesRestriction(RestrictionMixin):
                         yield err
 
         return list(find_errors())
+
+    @staticmethod
+    def validate_regex(data):
+        attr_regex = data.get('regex', {})
+        if attr_regex:
+            pattern = re.compile(attr_regex.get('source'))
+            if not pattern.match(data.get('value')):
+                return attr_regex.get('error')
 
 
 class VmwareAttributesRestriction(RestrictionMixin):

a23222170d5aa9d618d8536edf396366eaccc52f
Igor Zinovik <izinovik@mirantis.com>
2015-04-29 17:40:39 +0300

    Replace all occurrences of $ with $$ in vCenter input values
    - OpenStack services may reference to previously defined variable in
      configuration files using $ (dollar sign), e.g. 'metadata_host =
      $my_ip'.  Interpolation can be avoided by using $$.  Passwords often
      contain metachars and $ is one of them.  We must replace all $
      occurrences with $$, otherwise service will fail to start because it
      cannot interpolate non-existing variable.
    - add method escape_dollar() to VMwareDeploymentSerializerMixin class
      that implements conversion
    - also handle username and regular expression that is used for datastore
      search
    - provide input values with '$' in test fixture vmware_attributes.json
    - modify integration test
    
    Change-Id: I0d7d9f2d7f0ccaa3310c865a7f467c377c067442
    Closes-bug: #1436083

--- nailgun/nailgun/orchestrator/deployment_serializers.py
+++ nailgun/nailgun/orchestrator/deployment_serializers.py
@@ -73,15 +73,23 @@ class VmwareDeploymentSerializerMixin(object):
             network = vmware_attributes.get('network', {})
 
             for zone in availability_zones:
+
+                vc_user = self.escape_dollar(zone.get('vcenter_username', ''))
+                vc_password = self.escape_dollar(zone.get('vcenter_password',
+                                                          ''))
+
                 for compute in zone.get('nova_computes', {}):
+                    datastore_regex = \
+                        self.escape_dollar(compute.get('datastore_regex', ''))
+
                     compute_item = {
                         'availability_zone_name': zone.get('az_name', ''),
                         'vc_host': zone.get('vcenter_host', ''),
-                        'vc_user': zone.get('vcenter_username', ''),
-                        'vc_password': zone.get('vcenter_password', ''),
+                        'vc_user': vc_user,
+                        'vc_password': vc_password,
                         'service_name': compute.get('service_name', ''),
                         'vc_cluster': compute.get('vsphere_cluster', ''),
-                        'datastore_regex': compute.get('datastore_regex', '')
+                        'datastore_regex': datastore_regex
                     }
 
                     compute_instances.append(compute_item)
@@ -89,8 +97,8 @@ class VmwareDeploymentSerializerMixin(object):
                 cinder_item = {
                     'availability_zone_name': zone.get('az_name', ''),
                     'vc_host': zone.get('vcenter_host', ''),
-                    'vc_user': zone.get('vcenter_username', ''),
-                    'vc_password': zone.get('vcenter_password', '')
+                    'vc_user': vc_user,
+                    'vc_password': vc_password
                 }
                 cinder_instances.append(cinder_item)
 
@@ -109,15 +117,30 @@ class VmwareDeploymentSerializerMixin(object):
                 }
 
             if glance_instance:
+                glance_username = \
+                    self.escape_dollar(glance_instance
+                                       .get('vcenter_username', ''))
+                glance_password = \
+                    self.escape_dollar(glance_instance
+                                       .get('vcenter_password', ''))
+
                 vmware_data['glance'] = {
                     'vc_host': glance_instance.get('vcenter_host', ''),
-                    'vc_user': glance_instance.get('vcenter_username', ''),
-                    'vc_password': glance_instance.get('vcenter_password', ''),
+                    'vc_user': glance_username,
+                    'vc_password': glance_password,
                     'vc_datacenter': glance_instance.get('datacenter', ''),
                     'vc_datastore': glance_instance.get('datastore', '')
                 }
 
         return vmware_data
+
+    @classmethod
+    def escape_dollar(self, data):
+        """In order to disable variable interpolation in
+        values that we write to configuration files during
+        deployment we must replace all $ (dollar sign) occurrences.
+        """
+        return data.replace('$', '$$')
 
 
 class NetworkDeploymentSerializer(object):

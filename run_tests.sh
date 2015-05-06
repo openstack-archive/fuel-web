06911a6bafeb9efd60dead0da7d6017bd6db90d0
Nick Bogdanov <nbogdanov@mirantis.com>
2015-05-06 15:44:01 +0300

    Intern-based functional tests
    This commit replaces CasperJS with Intern
    
    Change-Id: I21d342093f142a1bbe7b8ae4f2c96fad1c125a0b

--- run_tests.sh
+++ run_tests.sh
@@ -88,7 +88,6 @@ NAILGUN_ROOT=$ROOT/nailgun
 TESTRTESTS="nosetests"
 FLAKE8="flake8"
 PEP8="pep8"
-CASPERJS="./node_modules/.bin/casperjs"
 GULP="./node_modules/.bin/gulp"
 LINTUI="${GULP} lint"
 
@@ -310,7 +309,7 @@ function run_ui_unit_tests {
 #   $@ -- tests to be run; with no arguments all tests will be run
 function run_ui_func_tests {
   local SERVER_PORT=$UI_SERVER_PORT
-  local TESTS_DIR=$ROOT/nailgun/ui_tests
+  local TESTS_DIR=$ROOT/nailgun/static/tests/functional
   local TESTS=$TESTS_DIR/test_*.js
   local artifacts=$ARTIFACTS/ui_func
   local config=$artifacts/test.yaml
@@ -347,7 +346,7 @@ function run_ui_func_tests {
       { echo 'Failed to start Nailgun'; return 1; }
 
     SERVER_PORT=$SERVER_PORT \
-    ${CASPERJS} test --includes="$TESTS_DIR/helpers.js" --fail-fast "$testcase"
+    ${GULP} functional-tests --suites=$testcase
     if [ $? -ne 0 ]; then
       result=1
       break
@@ -484,6 +483,7 @@ function syncdb {
 
   if [[ $# -ne 0 && $defaults = true ]]; then
     NAILGUN_CONFIG=$config tox -evenv -- python manage.py loaddefault > /dev/null
+    NAILGUN_CONFIG=$config tox -evenv -- python manage.py loaddata nailgun/fixtures/sample_environment.json > /dev/null
   fi
 
   popd >> /dev/null

8f0b65a2d8ae81eae5ef5cdfd8fcd115200af5b4
Nick Bogdanov <nbogdanov@mirantis.com>
2015-05-06 15:44:01 +0300

    Intern-based functional tests
    This commit replaces CasperJS with Intern
    
    Change-Id: I21d342093f142a1bbe7b8ae4f2c96fad1c125a0b

---
+++ nailgun/static/tests/functional/pages/welcome.js
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2015 Mirantis, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ **/
+
+define(['../../helpers'], function(Helpers) {
+    'use strict';
+    function WelcomePage(remote) {
+        this.remote = remote;
+    }
+
+    WelcomePage.prototype = {
+        constructor: WelcomePage,
+        skip: function(strictCheck) {
+            return this.remote
+                .getCurrentUrl()
+                .then(function(url) {
+                    if (url == Helpers.serverUrl + '/#welcome') {
+                        return this.parent
+                            .setFindTimeout(2000)
+                            .findByCssSelector('.welcome-button-box button')
+                                .click()
+                                .end()
+                            .waitForDeletedByCssSelector('.welcome-button-box button')
+                            .then(
+                                function() {return true},
+                                function() {return !strictCheck}
+                            );
+                    } else {
+                        return true;
+                    }
+                });
+        }
+    };
+    return WelcomePage;
+});
---
+++ nailgun/static/tests/functional/test_login_logout.js
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2014 Mirantis, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ **/
+
+define([
+    'intern!object',
+    'intern/chai!assert',
+    'tests/functional/pages/login',
+    'tests/functional/pages/common'
+], function(registerSuite, assert, LoginPage, Common) {
+    'use strict';
+
+    registerSuite(function() {
+        var loginPage, common;
+        return {
+            name: 'Login page',
+            setup: function() {
+                loginPage = new LoginPage(this.remote);
+                common = new Common(this.remote);
+            },
+            beforeEach: function() {
+                this.remote
+                    .then(function() {
+                        return common.getOut();
+                    });
+            },
+            'Login with incorrect credentials': function() {
+                return this.remote
+                    .then(function() {
+                        return loginPage.login('login', '*****');
+                    })
+                    .findByCssSelector('div.login-fields-box p.text-danger')
+                    .isDisplayed()
+                    .then(function(errorShown) {
+                        assert.ok(errorShown, 'Error message is expected to be displayed');
+                    });
+            },
+            'Login with proper credentials': function() {
+                return this.remote
+                    .then(function() {
+                        return loginPage.login();
+                    })
+                    .waitForDeletedByClassName('login-btn');
+            }
+        };
+    });
+});

9e1b5bb59a1160942af0bcfc9faf575be914fce1
Artem Roma <aroma@mirantis.com>
2015-05-07 19:49:59 +0300

    Rename OpenStack user for oswl collecting
    Change-Id: I9bfec323074779d899018ebe031e8bfc9049ff07
    Closes-Bug: #1452762

--- nailgun/nailgun/test/integration/test_orchestrator_serializer.py
+++ nailgun/nailgun/test/integration/test_orchestrator_serializer.py
@@ -2457,7 +2457,7 @@ class TestDeploymentAttributesSerialization61(BaseDeploymentSerializer):
                               'metadata',
                               'tenant',
                               'create_user']))
-        self.assertEqual(oswl_user['user'], 'workloads_collector')
+        self.assertEqual(oswl_user['user'], 'fuel_stats_user')
         self.assertEqual(oswl_user['enabled'], True)
         self.assertEqual(len(oswl_user['password']), 8)
         self.assertEqual(oswl_user['tenant'], 'services')
@@ -2476,7 +2476,7 @@ class TestDeploymentAttributesSerialization61(BaseDeploymentSerializer):
                               'metadata',
                               'tenant',
                               'create_user']))
-        self.assertEqual(oswl_user['user'], 'workloads_collector')
+        self.assertEqual(oswl_user['user'], 'fuel_stats_user')
         self.assertEqual(oswl_user['enabled'], True)
         self.assertEqual(len(oswl_user['password']), 8)
         self.assertEqual(oswl_user['tenant'], 'services')

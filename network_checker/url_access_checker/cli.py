ede2f9919e62a2abb52354bd5665159747d57b84
Maciej Kwiek <mkwiek@mirantis.com>
2015-05-13 11:36:11 +0200

    Add url_access_checker to network checker
    This change introduces new entry point for network_checker, which allows
    to check if it is possible to access urls give to the command.
    
    Usage: urlchecker check <url1> <url2> <url3> ...
    
    It will be used to check repository connectivity from slave nodes.
    
    python-requests is added to nailgun-net-check package requirements in
    both rpm spec and debian `control` file.
    
    Change-Id: Idc04f74ad7364dee452e9151391654f828e2342d
    Partial-Bug: #1439686

--- network_checker/url_access_checker/cli.py
+++ network_checker/url_access_checker/cli.py
@@ -1,4 +1,4 @@
-#    Copyright 2013 Mirantis, Inc.
+#    Copyright 2015 Mirantis, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -12,39 +12,26 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import logging
-import os
 import sys
-# fixed in cmd2 >=0.6.6
-os.environ['EDITOR'] = '/usr/bin/nano'
 
 from cliff.commandmanager import CommandManager
 
 from fuel_network_checker import base_app
 
 
-class DhcpApp(base_app.BaseApp):
-    DEFAULT_VERBOSE_LEVEL = 0
-    LOG_FILENAME = '/var/log/dhcp_checker.log'
+class UrlAccessCheckApp(base_app.BaseApp):
+    LOG_FILENAME = '/var/log/url_access_checker.log'
 
     def __init__(self):
-        super(DhcpApp, self).__init__(
-            description='Dhcp check application',
+        super(UrlAccessCheckApp, self).__init__(
+            description='Url access check application',
             version='0.1',
-            command_manager=CommandManager('dhcp.check'),
+            command_manager=CommandManager('urlaccesscheck'),
         )
-
-    def configure_logging(self):
-        super(DhcpApp, self).configure_logging()
-
-        # set scapy logger level only to ERROR
-        # due to a lot of spam
-        runtime_logger = logging.getLogger('scapy.runtime')
-        runtime_logger.setLevel(logging.ERROR)
 
 
 def main(argv=sys.argv[1:]):
-    myapp = DhcpApp()
+    myapp = UrlAccessCheckApp()
     return myapp.run(argv)
 
 

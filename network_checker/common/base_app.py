794f6a434d55bc1395d969014735d91c77313175
Maciej Kwiek <mkwiek@mirantis.com>
2015-05-13 11:36:11 +0200

    Add url_access_checker to network checker
    This change introduces new entry point for network_checker, which allows
    to check if it is possible to access urls give to the command.
    
    Usage: urlchecker check <url1> <url2> <url3> ...
    
    It will be used to check repository connectivity from slave nodes.
    
    python-requests is added to nailgun-net-check package requirements in
    both rpm spec and debian `control` file.
    
    Change-Id: Idc04f74ad7364dee452e9151391654f828e2342d
    Partial-Bug: #1439686

--- network_checker/common/base_app.py
+++ network_checker/common/base_app.py
@@ -1,4 +1,4 @@
-#    Copyright 2013 Mirantis, Inc.
+#    Copyright 2015 Mirantis, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -11,43 +11,37 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-
+#
 import logging
 from logging import handlers
-import os
-import sys
-# fixed in cmd2 >=0.6.6
-os.environ['EDITOR'] = '/usr/bin/nano'
 
-from cliff.commandmanager import CommandManager
-
-from common import base_app
+from cliff.app import App
 
 
-class DhcpApp(base_app.BaseApp):
+class BaseApp(App):
     DEFAULT_VERBOSE_LEVEL = 0
-    LOG_FILENAME = '/var/log/dhcp_checker.log'
-
-    def __init__(self):
-        super(DhcpApp, self).__init__(
-            description='Dhcp check application',
-            version='0.1',
-            command_manager=CommandManager('dhcp.check'),
-        )
 
     def configure_logging(self):
-        super(DhcpApp, self).configure_logging()
+        super(BaseApp, self).configure_logging()
+        logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(
+            '%(asctime)s %(levelname)s (%(module)s) %(message)s',
+            "%Y-%m-%d %H:%M:%S")
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.ERROR)
+        stream_handler.setFormatter(formatter)
+
+        file_handler = handlers.TimedRotatingFileHandler(
+            self.LOG_FILENAME)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+
+        logger.addHandler(stream_handler)
+        logger.addHandler(file_handler)
 
         # set scapy logger level only to ERROR
         # due to a lot of spam
         runtime_logger = logging.getLogger('scapy.runtime')
         runtime_logger.setLevel(logging.ERROR)
-
-
-def main(argv=sys.argv[1:]):
-    myapp = DhcpApp()
-    return myapp.run(argv)
-
-
-if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))

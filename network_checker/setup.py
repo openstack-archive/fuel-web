8368039778683ce995f9b3ded1d5ecb9d1c053e1
Maciej Kwiek <mkwiek@mirantis.com>
2015-05-13 11:36:11 +0200

    Add url_access_checker to network checker
    This change introduces new entry point for network_checker, which allows
    to check if it is possible to access urls give to the command.
    
    Usage: urlchecker check <url1> <url2> <url3> ...
    
    It will be used to check repository connectivity from slave nodes.
    
    python-requests is added to nailgun-net-check package requirements in
    both rpm spec and debian `control` file.
    
    Change-Id: Idc04f74ad7364dee452e9151391654f828e2342d
    Partial-Bug: #1439686

--- network_checker/setup.py
+++ network_checker/setup.py
@@ -34,16 +34,19 @@ setuptools.setup(
             'net_probe.py = network_checker.net_check.api:main',
             'fuel-netcheck = network_checker.cli:main',
             'dhcpcheck = dhcp_checker.cli:main',
+            'urlaccesscheck = url_access_checker.cli:main',
         ],
         'dhcp.check': [
             'discover = dhcp_checker.commands:ListDhcpServers',
             'request = dhcp_checker.commands:ListDhcpAssignment',
             'vlans = dhcp_checker.commands:DhcpWithVlansCheck'
-
         ],
         'network_checker': [
             'multicast = network_checker.multicast.api:MulticastChecker',
             'simple = network_checker.tests.simple:SimpleChecker'
-        ]
+        ],
+        'urlaccesscheck': [
+            'check = url_access_checker.commands:CheckUrls'
+        ],
     },
 )

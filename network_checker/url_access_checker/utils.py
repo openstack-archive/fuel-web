906f286f474d4d85ae1f5e5b35fad13a942415ec
Dmitry Shulyak <dshulyak@mirantis.com>
2015-05-15 11:53:56 +0300

    Add url access verification that will setup network
    Added command that will perform setup and teardown of
    required networking configuration.
    
    Network configuration will perform next things:
    
    - set interface up if required
    - create vlan tagged interface and set it up
    - add required ipv4 settings for interface
    - add default route
    
    After verification teardown will be performed
    
    Change-Id: I910c15c2b39a917eb8428bb69271b5dde364b639
    Partial-Bug: 1439686

---
+++ network_checker/url_access_checker/utils.py
@@ -0,0 +1,35 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from logging import getLogger
+import subprocess
+
+from url_access_checker.errors import CommandFailed
+
+logger = getLogger(__name__)
+
+
+def execute(cmd, check_errors=True):
+    logger.debug('Executing command %s', cmd)
+    command = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = command.communicate()
+    msg = 'Command {0} executed. RC {1}, stdout {2}, stderr {3}'.format(
+        cmd, command.returncode, stdout, stderr)
+    if check_errors and command.returncode != 0:
+        logger.error(msg)
+        raise CommandFailed(msg)
+    else:
+        logger.debug(msg)
+    return command.returncode, stdout, stderr

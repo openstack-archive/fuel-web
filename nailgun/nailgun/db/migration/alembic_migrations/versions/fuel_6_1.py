fc50bb307f1579a585d64c32684788ea4e783e70
Andrew Woodward <awoodward@mirantis.com>
2015-06-11 17:32:18 -0500

    [WIP] Drop the Enum for vip_types to allow for more vip allocations
    vip_types is forced into an Enum, but this prevents allocating more
    addresses when seperating components.
    
    Partial blueprint: detach-components-from-controllers
    
    Change-Id: Icbff48cde56a781c99f5a5e7f09c314543ee96fe

--- nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_6_1.py
+++ nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_6_1.py
@@ -154,13 +154,9 @@ def downgrade():
 def upgrade_schema():
     connection = op.get_bind()
 
-    vrouter_enum = sa.Enum('haproxy', 'vrouter',
-                           name='network_vip_types')
-    vrouter_enum.create(op.get_bind(), checkfirst=False)
-
     op.add_column(
         'ip_addrs',
-        sa.Column('vip_type', vrouter_enum, nullable=True)
+        sa.Column('vip_type', sa.String(length=32), nullable=True)
     )
 
     op.add_column(

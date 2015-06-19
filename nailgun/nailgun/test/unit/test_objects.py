4cae52c41187ea3e04ab02dd4139a0b12aa94223
Ivan Kliuk <ivan.kliuk@gmail.com>
2015-06-19 10:34:35 -0500

    Enable public IP in new/custom roles
      Update logic of objects.Nodeshould_have_public
      Update JSON schema for validation of 'public_ip_required' field
      Add 'public_ip_required' field to 'roles_metadata' of openstack.yaml
    
    Partial blueprint: detach-components-from-controllers
    
    Change-Id: I0b942b66b5ff3d22577a7cf94ee2448df97c78f4

--- nailgun/nailgun/test/unit/test_objects.py
+++ nailgun/nailgun/test/unit/test_objects.py
@@ -28,6 +28,7 @@ from oslo.serialization import jsonutils
 
 from nailgun.test.base import BaseIntegrationTest
 from nailgun.test.base import BaseTestCase
+from nailgun.test.base import BaseUnitTest
 from nailgun.test.base import reverse
 
 from nailgun.errors import errors

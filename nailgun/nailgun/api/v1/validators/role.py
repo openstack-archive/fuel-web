4e5e889f82e6710e56803cd934a9a93f3859d3ca
Ivan Kliuk <ivan.kliuk@gmail.com>
2015-06-20 17:55:19 +0300

    Validation of id values for volumes_roles_mapping
      Any string can be set as 'id' in 'volumes_roles_mapping' and validation of
      node's role on PUT-request passes without an error.
    
    * Add an additional validation step into 'RoleValidator.validate_update'.
    * Add unit tests for node roles validation.
    
    Change-Id: I2f199f742931c582eb6ea35a3030d470724864c0
    Closes-Bug: #1467097

--- nailgun/nailgun/api/v1/validators/role.py
+++ nailgun/nailgun/api/v1/validators/role.py
@@ -46,7 +46,21 @@ class RoleValidator(BasicValidator):
 
     @classmethod
     def validate_update(cls, data, instance):
-        return cls.validate(data, instance=instance)
+        parsed = cls.validate(data, instance=instance)
+
+        allowed_ids = [m['id'] for m in instance.volumes_metadata['volumes']]
+        missing_volume_ids = []
+        for volume in parsed['volumes_roles_mapping']:
+            if volume['id'] not in allowed_ids:
+                missing_volume_ids.append(volume['id'])
+
+        if missing_volume_ids:
+            raise errors.InvalidData(
+                "Wrong data in volumes_roles_mapping. Volumes with ids {0} are"
+                " not in the list of allowed volumes {1}".format(
+                    missing_volume_ids, allowed_ids))
+
+        return parsed
 
     @classmethod
     def validate_create(cls, data):

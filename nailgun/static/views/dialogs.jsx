a6b38e078312f0dbbcbddc0f2daf6a917776bf4b
Kate Pimenova <kpimenova@mirantis.com>
2015-06-22 13:39:24 +0300

    Dismiss changes dialog added on Support page
      - created new common method for noLeaveCheck in component mixin
    
    Closes-Bug: #1441093
    
    Change-Id: Ide7f29382e130fb21e3fee52e439c70df7109936

--- nailgun/static/views/dialogs.jsx
+++ nailgun/static/views/dialogs.jsx
@@ -829,14 +829,14 @@ function($, _, i18n, Backbone, React, utils, models, dispatcher, controls, compo
         getDefaultProps: function() {return {title: i18n('dialog.dismiss_settings.title'), defaultMessage: i18n('dialog.dismiss_settings.default_message')};},
         proceed: function() {
             this.close();
-            dispatcher.trigger('networkConfigurationUpdated', _.bind(this.props.cb, this.props));
+            if (this.props.cb) this.props.cb();
+            this.props.redirect();
         },
         renderBody: function() {
-            var message = this.props.verification ? i18n('dialog.dismiss_settings.verify_message') : this.props.defaultMessage;
             return (
                 <div className='text-danger dismiss-settings-dialog'>
                     {this.renderImportantLabel()}
-                    {message}
+                    {this.props.message || this.props.defaultMessage}
                 </div>
             );
         },
@@ -846,7 +846,7 @@ function($, _, i18n, Backbone, React, utils, models, dispatcher, controls, compo
                     {i18n('dialog.dismiss_settings.stay_button')}
                 </button>
             ];
-            if (!this.props.verification) buttons.push(
+            if (!this.props.hideLeaveButton) buttons.push(
                 <button key='leave' className='btn btn-danger' onClick={this.proceed}>
                     {i18n('dialog.dismiss_settings.leave_button')}
                 </button>

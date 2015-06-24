2cd8c7fcb8a1f6e7d7d70b8e5d3ed04ced96e4b9
Alexei Sheplyakov <asheplyakov@mirantis.com>
2015-06-24 17:24:16 +0300

    fuel-agent: check if lvcreate supports --yes switch (WIP)
    lvcreate --yes switch has been added in the recent versions of lvm2
    which are not available on Ubuntu 14.04.
    
    blueprint fuel-bootstrap-on-ubuntu
    
    Change-Id: Iaf35d190fcb9afd083b181769cf691b0a6e8f42f

--- fuel_agent/fuel_agent/tests/test_lvm_utils.py
+++ fuel_agent/fuel_agent/tests/test_lvm_utils.py
@@ -308,10 +308,41 @@ class TestLvmUtils(test_base.BaseTestCase):
         mock_vgdisplay.return_value = [{'name': 'vgname', 'free': 2000},
                                        {'name': 'some'}]
         mock_lvdisplay.return_value = [{'name': 'some'}]
+        mock_exec.side_effect = [
+            # pretend lvcreate supports --yes option
+            ('--yes', ''),
+            ('', '')
+        ]
+        expected_calls = [
+            mock.call('lvcreate', '--help'),
+            mock.call('lvcreate', '--yes', '-L', '1000m',
+                      '-n', 'lvname', 'vgname',
+                      check_exit_code=[0])
+        ]
         lu.lvcreate('vgname', 'lvname', 1000)
-        mock_exec.assert_called_once_with('lvcreate', '--yes', '-L', '1000m',
-                                          '-n', 'lvname', 'vgname',
-                                          check_exit_code=[0])
+        self.assertEqual(mock_exec.call_args_list, expected_calls)
+
+    @mock.patch.object(lu, 'vgdisplay')
+    @mock.patch.object(lu, 'lvdisplay')
+    @mock.patch.object(utils, 'execute')
+    def test_lvcreate_ok_old_lvm(self, mock_exec, mock_lvdisplay, mock_vgdisplay):
+        mock_vgdisplay.return_value = [{'name': 'vgname', 'free': 2000},
+                                       {'name': 'some'}]
+        mock_lvdisplay.return_value = [{'name': 'some'}]
+        mock_exec.side_effect = [
+            # pretend lvcreate does not support --yes option
+            # (which is the case for Ubuntu 14.04)
+            ('--foo', ''),
+            ('', '')
+        ]
+        expected_calls = [
+            mock.call('lvcreate', '--help'),
+            mock.call('lvcreate', '-L', '1000m',
+                      '-n', 'lvname', 'vgname',
+                      check_exit_code=[0])
+        ]
+        lu.lvcreate('vgname', 'lvname', 1000)
+        self.assertEqual(mock_exec.call_args_list, expected_calls)
 
     @mock.patch.object(lu, 'vgdisplay')
     @mock.patch.object(utils, 'execute')
@@ -348,10 +379,15 @@ class TestLvmUtils(test_base.BaseTestCase):
         mock_vgdisplay.return_value = [{'name': 'vgname', 'free': 2000},
                                        {'name': 'some', 'free': 2000}]
         mock_lvdisplay.return_value = [{'name': 'lvname', 'vg': 'some'}]
+        mock_exec.side_effect = [('--yes', ''), ('', '')]
+        expected_calls = [
+            mock.call('lvcreate', '--help'),
+            mock.call('lvcreate', '--yes', '-L', '1000m',
+                      '-n', 'lvname', 'vgname',
+                      check_exit_code=[0])
+        ]
         lu.lvcreate('vgname', 'lvname', 1000)
-        mock_exec.assert_called_once_with('lvcreate', '--yes', '-L', '1000m',
-                                          '-n', 'lvname', 'vgname',
-                                          check_exit_code=[0])
+        self.assertEqual(mock_exec.call_args_list, expected_calls)
 
     @mock.patch.object(utils, 'execute')
     def test_lvdisplay(self, mock_exec):

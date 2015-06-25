47602389cedcc00aafc278cecffad23d2de7dc20
Andriy Popovych <popovych.andrey@gmail.com>
2015-06-25 10:42:11 +0300

    [Nailgun] Plugin Sync API
    Current commit provide implementation of API call for plugin
    metadata synchronization from config files into DB
    
    * New PluginSync handler, validator and new url
    * New plugin wrapper for 3.0.0 plugin version with
      sync metadata method
    
    Change-Id: I223ec14afbfa0852f231ad8dd2c5289758997b0d
    Implements: blueprint role-as-a-plugin

--- nailgun/nailgun/test/unit/test_object_plugin.py
+++ nailgun/nailgun/test/unit/test_object_plugin.py
@@ -23,15 +23,7 @@ from nailgun.test import base
 class TestPluginCollection(base.BaseTestCase):
 
     def test_all_newest(self):
-        for version in ['1.0.0', '2.0.0', '0.0.1']:
-            plugin_data = self.env.get_default_plugin_metadata(
-                version=version,
-                name='multiversion_plugin')
-            Plugin.create(plugin_data)
-
-        single_plugin_data = self.env.get_default_plugin_metadata(
-            name='single_plugin')
-        Plugin.create(single_plugin_data)
+        self._create_test_plugins()
 
         newest_plugins = PluginCollection.all_newest()
         self.assertEqual(len(newest_plugins), 2)
@@ -46,4 +38,28 @@ class TestPluginCollection(base.BaseTestCase):
         self.assertEqual(len(single_plugin), 1)
         self.assertEqual(len(multiversion_plugin), 1)
 
-        self.assertEqual(multiversion_plugin[0].version, '2.0.0')
+        self.assertEqual(multiversion_plugin[0].version, '3.0.0')
+
+    def test_get_by_uids(self):
+        plugin_ids = self._create_test_plugins()
+        ids = plugin_ids[:2]
+        plugins = PluginCollection.get_by_uids(ids)
+        self.assertEqual(len(list(plugins)), 2)
+        self.assertListEqual(
+            [plugin.id for plugin in plugins], ids)
+
+    def _create_test_plugins(self):
+        plugin_ids = []
+        for version in ['1.0.0', '2.0.0', '0.0.1', '3.0.0']:
+            plugin_data = self.env.get_default_plugin_metadata(
+                version=version,
+                name='multiversion_plugin')
+            plugin = Plugin.create(plugin_data)
+            plugin_ids.append(plugin.id)
+
+        single_plugin_data = self.env.get_default_plugin_metadata(
+            name='single_plugin')
+        plugin = Plugin.create(single_plugin_data)
+        plugin_ids.append(plugin.id)
+
+        return plugin_ids

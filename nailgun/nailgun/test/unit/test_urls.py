3333d567785d850e5f2674757bfa8e8d814caa05
Evgeniy L <eli@mirantis.com>
2015-06-26 10:58:08 +0300

    Move volume manager specific urls into extension
    * added a field for extension where urls can be
      defined
    * move urls into volume manager extension
    * removed TODO from test_api_doc_generation test
    * removed TestRemovedIn51Handler test because:
    ** it does not test anything, correct work
       of removal of related handler is tested in
       in other tests in the same file
    ** the test relies on patching of global variable
       which causes a lot of problems, now you cannot
       change global variable so it gets propagated
       into web.py
    * added exception H304 in tox configuration, it's
      required because extensions should be written in
      a way that they can import their modules without
      specifing full path to extension, which currently
      includes nailgun module as a root module. It will
      be easier to extract it later from nailgun into
      separate module/repo
    
    Implements blueprint: volume-manager-refactoring
    
    Change-Id: I6fd5a63e6946ef87604f1f91a99351c24de45689

---
+++ nailgun/nailgun/test/unit/test_urls.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from nailgun.api.v1.urls import get_all_urls
+from nailgun.api.v1.urls import get_extensions_urls
+
+from nailgun.test.base import BaseTestCase
+
+
+class FakeHandler(object):
+    pass
+
+
+class TestUrls(BaseTestCase):
+
+    @mock.patch('nailgun.api.v1.urls.get_extensions_urls')
+    def test_get_all_urls(self, mock_get_extensions_urls):
+        mock_get_extensions_urls.return_value = {
+            'urls': (r'/ext/url/', 'FakeHandler'),
+            'handlers': [{
+                'class': FakeHandler,
+                'name': 'FakeHandler'}]}
+
+        result = get_all_urls()
+        # Urls
+        all_urls = result[0]
+        # Variables
+        all_vars = result[1]
+
+        self.assertIn('/ext/url/', all_urls[-2])
+        self.assertIn('FakeHandler', all_urls[-1])
+
+        self.assertEqual(all_vars['FakeHandler'], FakeHandler)
+
+    @mock.patch('nailgun.api.v1.urls.get_all_extensions')
+    def test_get_extensions_urls(self, mock_get_all_extensions):
+        extension = mock.MagicMock(urls=[
+            {'uri': '/ext/uri', 'handler': FakeHandler}])
+        mock_get_all_extensions.return_value = [extension]
+
+        self.assertEqual(
+            get_extensions_urls(),
+            {'urls': ['/ext/uri', 'FakeHandler'],
+             'handlers': [{'class': FakeHandler, 'name': 'FakeHandler'}]})

1e51b919e366f24e42e2a31ca79629506f89a13b
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2015-06-27 21:32:47 +0300

    IBP: fixed custom separator for cciss and nvme
    Not for merge and review, just for testing
    
    Change-Id: I26688478cf67c37c44741393fa6a82cfb42c957d
    Closes-Bug: #1469440

--- fuel_agent/fuel_agent/objects/partition.py
+++ fuel_agent/fuel_agent/objects/partition.py
@@ -94,7 +94,7 @@ class Parted(object):
         if self.next_type() == 'extended':
             return None
         separator = ''
-        if any(self.name.startswith(n) for n in ('cciss', 'nvme', 'loop')):
+        if any(n in self.name for n in ('cciss', 'nvme', 'loop')):
             separator = 'p'
         return '%s%s%s' % (self.name, separator, self.next_count())
 

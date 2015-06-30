08211a21eae0164a2b3977ca1e7bff9627b3a97d
Evgeniy L <eli@mirantis.com>
2015-06-30 14:43:56 +0300

    Move volumes data from the core into volume manager extension
    Migration the data from core into an extension is not
    a trivial problem, ideally extension should know nothing
    about core's fields in the database and vice versa.
    
    We had several options:
    1. move data migration into an extension, it's a bad
       option, because in migration we should drop
       node.attributes.volumes column, it's a very bad
       thing to do, because only core's migrations should
       be responsible for managing of core's db schema
    2. implement everything in the core, cannot be implemented
       for the same reasons as for 1st option, in this
       case we should run extension's db migration in
       order to create required fields and then
       in core migration hardcode table's name to migrate
       the data
    3. use shared table, in this case core's responsibility
       is to just put all of the data from core's models
       into predefined table and drop node.attributes.volumes column,
       extension migration script's responsibility is to check
       if in predefined table there are data for this specific
       extension, and retrieve these data if there are any
    
    We've decided to use shared table (3rd option) because it's
    better option then two others.
    
    Another change is we are not going to use extension version
    as a prefix for extension tables. As result we will
    not have two extension with the same name, but different
    version installed at the same time. Using version in
    table names dramatically increase complexity of extension
    update.
    
    Implements blueprint: volume-manager-refactoring
    Change-Id: I6f03a535edab18e6d9e53a47fc4abdd381a3495f

---
+++ nailgun/nailgun/extensions/consts.py
@@ -0,0 +1,20 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# NOTE(eli): this is the name of table which was added
+# in Fuel 7.0 in order to migrate the data from the core
+# into extensions. This "shared" table allows us not
+# not hardcode information about tables which are specific
+# for extension or core into extension's and core's migrations.
+extensions_migration_buffer_table_name = 'extensions_migration_buffer'

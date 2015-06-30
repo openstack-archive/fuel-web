2b483ed49a7d9eb887b3a1d2e54afdf3926ea31c
Ryan Moe <rmoe@mirantis.com>
2015-06-30 07:49:10 -0700

    Add serializer for network role template
    Serializer switching between general and template is added also.
    When template is loaded, template serializer is being used.
    General serializer is in use otherwise.
    
    Change-Id: I1820d3cd80a33b292f9e2b75d8f0dea3bad7c8ac
    Implements: blueprint templates-for-networking

--- nailgun/nailgun/test/integration/test_orchestrator_serializer_70.py
+++ nailgun/nailgun/test/integration/test_orchestrator_serializer_70.py
@@ -16,6 +16,7 @@
 
 import mock
 import six
+import yaml
 
 from nailgun import consts
 from nailgun.db.sqlalchemy import models
@@ -24,6 +25,10 @@ from nailgun import objects
 from nailgun.orchestrator.deployment_graph import AstuteGraph
 from nailgun.orchestrator.deployment_serializers import \
     get_serializer_for_cluster
+from nailgun.orchestrator.neutron_serializers import \
+    NeutronNetworkDeploymentSerializer70
+from nailgun.orchestrator.neutron_serializers import \
+    NeutronNetworkTemplateSerializer70
 from nailgun.test.integration.test_orchestrator_serializer import \
     BaseDeploymentSerializer
 
@@ -309,3 +314,262 @@ class TestDeploymentSerializationForNovaNetwork70(BaseDeploymentSerializer):
 
     def test_generate_vmware_attributes_data(self):
         self.check_generate_vmware_attributes_data()
+
+
+class TestNetworkTemplateSerializer70(BaseDeploymentSerializer):
+
+    @mock.patch.object(models.Release, 'environment_version',
+                       new_callable=mock.PropertyMock(return_value='7.0'))
+    def setUp(self, *args):
+        super(TestNetworkTemplateSerializer70, self).setUp()
+        self.cluster = self.create_env('ha_compact')
+
+        self.net_template = """
+            adv_net_template:
+              default:
+                nic_mapping:
+                  default:
+                    if1: eth0
+                    if2: eth1
+                    if3: eth2
+                    if4: eth3
+                templates_for_node_role:
+                    controller:
+                      - common
+                      - public
+                    compute:
+                      - common
+                      - storage
+                      - private
+                    cinder:
+                      - common
+                      - storage
+                network_assignments:
+                    storage:
+                      ep: br-storage
+                    private:
+                      ep: br-prv
+                    public:
+                      ep: br-ex
+                    management:
+                      ep: br-mgmt
+                    fuelweb_admin:
+                      ep: br-fw-admin
+                network_scheme:
+                  common:
+                    transformations:
+                      - action: add-br
+                        name: br-fw-admin
+                      - action: add-port
+                        bridge: br-fw-admin
+                        name: <% if1 %>
+                      - action: add-br
+                        name: br-mgmt
+                      - action: add-port
+                        bridge: br-mgmt
+                        name: <% if2 %>
+                    endpoints:
+                      - br-mgmt
+                      - br-fw-admin
+                    roles:
+                      management: br-mgmt
+                      admin/pxe: br-fw-admin
+                      swift/api: br-mgmt
+                      neutron/api: br-mgmt
+                      sahara/api: br-mgmt
+                      ceilometer/api: br-mgmt
+                      cinder/api: br-mgmt
+                      keystone/api: br-mgmt
+                      glance/api: br-mgmt
+                      heat/api: br-mgmt
+                      nova/api: br-mgmt
+                      murano/api: br-mgmt
+                      horizon: br-mgmt
+                      mgmt/api: br-mgmt
+                      mgmt/memcache: br-mgmt
+                      mgmt/database: br-mgmt
+                  public:
+                    transformations:
+                      - action: add-br
+                        name: br-ex
+                      - action: add-br
+                        name: br-floating
+                        provider: ovs
+                      - action: add-patch
+                        bridges:
+                        - br-floating
+                        - br-ex
+                        provider: ovs
+                        mtu: 65000
+                      - action: add-port
+                        bridge: br-ex
+                        name: <% if2 %>
+                    endpoints:
+                      - br-ex
+                    roles:
+                      public/vip: br-ex
+                      swift/public: br-ex
+                      neutron/floating: br-floating
+                      ceph/radosgw: br-ex
+                  storage:
+                    transformations:
+                      - action: add-br
+                        name: br-storage
+                      - action: add-port
+                        bridge: br-storage
+                        name: <% if3 %>
+                      - action: add-port
+                        bridge: br-mgmt
+                        name: <% if4 %>
+                    endpoints:
+                      - br-storage
+                    roles:
+                      cinder/iscsi: br-storage
+                      swift/replication: br-storage
+                      ceph/replication: br-storage
+                  private:
+                    transformations:
+                      - action: add-br
+                        name: br-prv
+                        provider: ovs
+                      - action: add-br
+                        name: br-aux
+                      - action: add-patch
+                        bridges:
+                        - br-prv
+                        - br-aux
+                        provider: ovs
+                        mtu: 65000
+                      - action: add-port
+                        bridge: br-aux
+                        name: <% if4 %>
+                    endpoints:
+                      - br-prv
+                    roles:
+                      neutron/private: br-prv
+        """
+        objects.Cluster.set_network_template(
+            self.cluster,
+            yaml.safe_load(self.net_template)
+        )
+        objects.NodeCollection.prepare_for_deployment(self.env.nodes)
+        cluster_db = self.db.query(models.Cluster).get(self.cluster['id'])
+
+        serializer = get_serializer_for_cluster(self.cluster)
+        self.serialized_for_astute = serializer(
+            AstuteGraph(cluster_db)).serialize(self.cluster, cluster_db.nodes)
+
+    def create_env(self, mode):
+        return self.env.create(
+            cluster_kwargs={
+                'api': False,
+                'mode': mode,
+                'net_provider': 'neutron',
+                'net_segment_type': 'vlan'},
+            nodes_kwargs=[
+                {'roles': ['controller'],
+                 'pending_addition': True,
+                 'name': self.node_name},
+                {'roles': ['compute', 'cinder'],
+                 'pending_addition': True,
+                 'name': self.node_name}
+            ])
+
+    @mock.patch.object(models.Release, 'environment_version',
+                       new_callable=mock.PropertyMock(return_value='7.0'))
+    def test_get_net_provider_serializer(self, _):
+        serializer = get_serializer_for_cluster(self.cluster)
+        self.cluster.network_config.configuration_template = None
+
+        net_serializer = serializer.get_net_provider_serializer(self.cluster)
+        self.assertEquals(net_serializer, NeutronNetworkDeploymentSerializer70)
+
+        self.cluster.network_config.configuration_template = \
+            self.net_template
+        net_serializer = serializer.get_net_provider_serializer(self.cluster)
+        self.assertEquals(net_serializer, NeutronNetworkTemplateSerializer70)
+
+    def test_multiple_node_roles_network_roles(self, *args):
+        expected_roles = {}
+        # controller node
+        expected_roles[self.cluster.nodes[0].fqdn] = {
+            'management': 'br-mgmt',
+            'admin/pxe': 'br-fw-admin',
+            'swift/api': 'br-mgmt',
+            'neutron/api': 'br-mgmt',
+            'sahara/api': 'br-mgmt',
+            'ceilometer/api': 'br-mgmt',
+            'cinder/api': 'br-mgmt',
+            'keystone/api': 'br-mgmt',
+            'glance/api': 'br-mgmt',
+            'heat/api': 'br-mgmt',
+            'nova/api': 'br-mgmt',
+            'murano/api': 'br-mgmt',
+            'horizon': 'br-mgmt',
+            'mgmt/api': 'br-mgmt',
+            'mgmt/memcache': 'br-mgmt',
+            'mgmt/database': 'br-mgmt',
+            'public/vip': 'br-ex',
+            'swift/public': 'br-ex',
+            'neutron/floating': 'br-floating',
+            'ceph/radosgw': 'br-ex'
+        }
+        # compute/cinder node
+        expected_roles[self.cluster.nodes[1].fqdn] = {
+            'management': 'br-mgmt',
+            'admin/pxe': 'br-fw-admin',
+            'swift/api': 'br-mgmt',
+            'neutron/api': 'br-mgmt',
+            'sahara/api': 'br-mgmt',
+            'ceilometer/api': 'br-mgmt',
+            'cinder/api': 'br-mgmt',
+            'keystone/api': 'br-mgmt',
+            'glance/api': 'br-mgmt',
+            'heat/api': 'br-mgmt',
+            'nova/api': 'br-mgmt',
+            'murano/api': 'br-mgmt',
+            'horizon': 'br-mgmt',
+            'mgmt/api': 'br-mgmt',
+            'mgmt/memcache': 'br-mgmt',
+            'mgmt/database': 'br-mgmt',
+            'cinder/iscsi': 'br-storage',
+            'swift/replication': 'br-storage',
+            'ceph/replication': 'br-storage',
+            'neutron/private': 'br-prv'
+        }
+
+        for node in self.serialized_for_astute:
+            roles = node['network_scheme']['roles']
+            self.assertEqual(roles, expected_roles[node['fqdn']])
+
+    @mock.patch.object(models.Release, 'environment_version',
+                       new_callable=mock.PropertyMock(return_value='7.0'))
+    def test_multiple_node_roles_transformations(self, *args):
+        node = self.cluster.nodes[1]
+
+        serializer = get_serializer_for_cluster(self.cluster)
+        net_serializer = serializer.get_net_provider_serializer(self.cluster)
+
+        transformations = net_serializer.generate_transformations(node)
+
+        # Two node roles with the same template should only generate one
+        # transformation.
+        admin_brs = filter(lambda t: t.get('name') == 'br-fw-admin',
+                           transformations)
+        self.assertEquals(1, len(admin_brs))
+
+        # Templates are applied in the order as defined in the template.
+        # storage network template is applied after the 4 transformations
+        # in common
+        self.assertEquals('br-storage', transformations[4]['name'])
+
+        # Ensure all ports connected to br-mgmt happen after the bridge
+        # has been created
+        br_mgmt_ports = []
+        for idx, tx in enumerate(transformations):
+            if tx.get('name') == 'br-mgmt' and tx['action'] == 'add-br':
+                br_mgmt_idx = idx
+            if tx.get('bridge') == 'br-mgmt' and tx['action'] == 'add-port':
+                br_mgmt_ports.append(idx)
+
+        self.assertTrue(all([br_mgmt_idx < port for port in br_mgmt_ports]))

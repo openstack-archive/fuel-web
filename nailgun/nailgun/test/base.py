93feddadb89594b3b49d42ff0c09ec3b0e9c269a
Valyavskiy Viacheslav <vvalyavskiy@mirantis.com>
2015-07-01 16:23:27 +0300

    Add additional restrictions
    Change-Id: I70dcaed66607728b76ae3d93c59048c310ddb22c
    Implements: blueprint admin-network-on-bond

--- nailgun/nailgun/test/base.py
+++ nailgun/nailgun/test/base.py
@@ -334,7 +334,8 @@ class EnvironmentManager(object):
             if_list = [
                 {
                     "name": "eth{0}".format(i),
-                    "mac": self.generate_random_mac()
+                    "mac": self.generate_random_mac(),
+                    "pxe_interface": not bool(i)
                 }
                 for i in range(if_count)]
             self.set_interfaces_in_meta(meta, if_list)
@@ -407,7 +408,8 @@ class EnvironmentManager(object):
                     'name': 'eth{0}'.format(i),
                     'mac': self.generate_random_mac(),
                     'current_speed': 100,
-                    'max_speed': 1000
+                    'max_speed': 1000,
+                    'pxe_interface': False
                 }
             )
         self.set_admin_ip_for_for_single_interface(nics)

2824e0a6157726b030cf1dd22f6b1471dea33dad
Nick Bogdanov <nbogdanov@mirantis.com>
2015-07-02 16:59:23 +0300

    Offload modes UI control
    Related to blueprint selectable-offloading-type
    
    Change-Id: If918f4192976d267aa7cb72fb4b854eaddca3e40

---
+++ nailgun/static/views/cluster_page_tabs/nodes_tab_screens/offloading_modes_control.jsx
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2015 Mirantis, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ **/
+define(
+[
+    'jquery',
+    'underscore',
+    'react',
+    'i18n'
+],
+function($, _, React, i18n) {
+    'use strict';
+
+    var OffloadingModesControl = React.createClass({
+        propTypes: {
+            interface: React.PropTypes.object
+        },
+        getInitialState: function() {
+            return {
+                isVisible: false
+            };
+        },
+        toggleVisibility: function() {
+            this.setState({isVisible: !this.state.isVisible});
+        },
+        setModeState: function(mode, state) {
+            mode.state = state;
+            if (!_.isEmpty(mode.sub)) {
+                mode.sub.forEach((function(mode) {this.setModeState(mode, state)}).bind(this));
+            }
+        },
+        checkModes: function(mode, sub) {
+            var changedState = sub.reduce(
+                    (function(state, childMode) {
+                        if (!_.isEmpty(childMode.sub)) {
+                            this.checkModes(childMode, childMode.sub);
+                        }
+                        return (state === 0 || state === childMode.state) ? childMode.state : -1;
+                    }).bind(this),
+                    0
+                ),
+                oldState;
+
+            if (mode && mode.state != changedState) {
+                oldState = mode.state;
+                mode.state = oldState === false ? null : (changedState === false ? false : oldState);
+            }
+        },
+        findMode: function(name, modes) {
+            var result,
+                mode,
+                i,
+                l;
+            for (i = 0, l = modes.length; i < l; i++) {
+                mode = modes[i];
+                if (mode.name == name) {
+                    return mode;
+                } else if (!_.isEmpty(mode.sub)) {
+                    result = this.findMode(name, mode.sub);
+                    if (result) {
+                        break;
+                    }
+                }
+            }
+            return result;
+        },
+        onModeStateChange: function(name, state) {
+            var modes = _.deepClone(this.props.interface.get('offloading_modes') || []),
+                mode = this.findMode(name, modes);
+
+            return (function() {
+                if (!_.isEmpty(mode)) {
+                    this.setModeState(mode, state);
+                    this.checkModes(null, modes);
+                    this.props.interface.set('offloading_modes', modes);
+                }
+            }).bind(this);
+        },
+        makeOffloadingModesExcerpt: function() {
+            var icons = {
+                true: 'glyphicon glyphicon-ok-sign text-success',
+                false: 'glyphicon glyphicon-remove-sign text-danger',
+                null: 'glyphicon glyphicon-question-sign'
+            };
+            return this.props.interface.get('offloading_modes').map(function(mode, index) {
+                return <span>
+                    {(index ? ', ' : '') + mode.name}
+                    <i className={icons[mode.state]}></i>
+                </span>
+            });
+        },
+        renderChildModes: function(modes, level) {
+            return modes.map((function(mode) {
+                var lines = [
+                    <tr key={mode.name}>
+                        <td className={'level' + level}>{mode.name}</td>
+                        {[true, false, null].map((function(modeState) {
+                            return <td key={mode.name + modeState}>
+                                {mode.state === modeState ?
+                                    <i className='glyphicon glyphicon-ok text-success'></i>
+                                    :
+                                    <button
+                                        className='btn-link'
+                                        onClick={this.onModeStateChange(mode.name, modeState)}>
+                                        <i className='glyphicon glyphicon-ok'></i>
+                                    </button>
+                                }
+                            </td>
+                        }).bind(this))}
+                    </tr>
+                ];
+                if (mode.sub) {
+                    return _.union([lines, this.renderChildModes(mode.sub, level + 1)]);
+                }
+                return lines;
+            }).bind(this));
+        },
+        render: function() {
+            var ns = 'cluster_page.nodes_tab.configure_interfaces.',
+                modes = this.props.interface.get('offloading_modes') || [];
+            return <div className='offloading-modes'>
+                <div>
+                    <button className='btn btn-default' onClick={this.toggleVisibility}>
+                        {i18n(ns + 'offloading_modes')}: {this.makeOffloadingModesExcerpt()}
+                    </button>
+                    {this.state.isVisible &&
+                        <table className='table'>
+                            <thead>
+                                <tr>
+                                    <th>{i18n(ns + 'offloading_mode')}</th>
+                                    <th>{i18n(ns + 'offloading_enabled')}</th>
+                                    <th>{i18n(ns + 'offloading_disabled')}</th>
+                                    <th>{i18n(ns + 'offloading_default')}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.renderChildModes(modes, 1)}
+                            </tbody>
+                        </table>
+                    }
+                </div>
+            </div>
+        }
+    });
+
+    return OffloadingModesControl;
+});

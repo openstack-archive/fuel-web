abaf6e91733a76c9a514a696ee65f831f026af9a
Sebastian Kalinowski <sebastian@kalinowski.eu>
2015-07-03 11:09:02 +0200

    Add simple data driver for partitioning info
    This new "simple" data driver takes serialized partitioning info
    that is provided before provisioning from external tool.
    It does not make any calculations for that data and expect it to have
    all required informations about partitioning.
    Other changes:
    * added unittest2 to use some of it features
    * added requests_mock to mock http requests
    * added objects conversion to/from dictionary to make serialization
      easier
    * added a common "interface" for data drivers
    * fixed test for "do_build_image" - now a correct data driver is used
    
    Change-Id: If0c60073f9565584ed81fb9e0b620bd76a251c01
    Implements: blueprint volume-manager-refactoring

---
+++ fuel_agent/fuel_agent/objects/base.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import abc
+import six
+
+
+class abstractclassmethod(classmethod):
+
+    __isabstractmethod__ = True
+
+    def __init__(self, callable):
+        callable.__isabstractmethod__ = True
+        super(abstractclassmethod, self).__init__(callable)
+
+
+@six.add_metaclass(abc.ABCMeta)
+class Serializable(object):
+
+    @abc.abstractmethod
+    def to_dict(self):
+        pass
+
+    @abstractclassmethod
+    def from_dict(cls, data):
+        pass

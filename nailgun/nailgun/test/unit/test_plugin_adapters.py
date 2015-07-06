86e90616dc40b1c5cae8a62357947b5bc55ba5e7
Artem Roma <aroma@mirantis.com>
2015-07-06 11:57:26 +0300

    Add plugin deployment tasks to deployment graph
    `get_deployment_tasks` for nailgun cluster object modified so that it
    now by default (if deployment graph was not defined for the cluster)
    returns summary list of deployment task for release and all enabled for
    the cluster plugins.
    
    Change-Id: I4e0b063a80a6d94bfb792b80cd6880a2a471f135
    Implements: blueprint role-as-a-plugin

--- nailgun/nailgun/test/unit/test_plugin_adapters.py
+++ nailgun/nailgun/test/unit/test_plugin_adapters.py
@@ -136,6 +136,23 @@ class TestPluginBase(base.BaseTestCase):
                 self.assertEqual(
                     getattr(self.plugin, key), val)
 
+    def test_get_deployment_tasks(self):
+        self.plugin.deployment_tasks = \
+            self.env.get_default_plugin_deployment_tasks()
+
+        depl_task = self.plugin_adapter.deployment_tasks[0]
+        self.assertEqual(depl_task['parameters'].get('cwd'),
+                         self.plugin_adapter.slaves_scripts_path)
+
+    def test_get_deployment_tasks_params_not_changed(self):
+        expected = 'path/to/some/dir'
+        self.plugin.deployment_tasks = \
+            self.env.get_default_plugin_deployment_tasks(
+                parameters={'cwd': expected}
+            )
+        depl_task = self.plugin_adapter.deployment_tasks[0]
+        self.assertEqual(depl_task['parameters'].get('cwd'), expected)
+
 
 class TestPluginV1(TestPluginBase):
 

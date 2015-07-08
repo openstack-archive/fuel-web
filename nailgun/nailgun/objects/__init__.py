605dd6dccfeb60ecf276620d2434e0ceaba68395
Vladimir Sharshov (warpc) <vsharshov@mirantis.com>
2015-07-08 20:29:51 +0300

    API for dashboard entries
     - Dashboard entry DB model and DB migration logic added,
     - Handler, json schema and validation added,
     - Serializer added,
     - Dashboard entires info added into statistics,
     - Public url for Dashboard entry creation.
    
    Implements blueprint post-deployment-dashboard
    Change-Id: I5e613bb35366227d1265fbc698e2efeddf931cc7

--- nailgun/nailgun/objects/__init__.py
+++ nailgun/nailgun/objects/__init__.py
@@ -56,3 +56,6 @@ from nailgun.objects.network_group import NetworkGroupCollection
 
 from nailgun.objects.component import Component
 from nailgun.objects.component import ComponentCollection
+
+from nailgun.objects.dashboard_entry import DashboardEntry
+from nailgun.objects.dashboard_entry import DashboardEntryCollection

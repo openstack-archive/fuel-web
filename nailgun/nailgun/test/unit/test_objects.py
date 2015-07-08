62d39f274f73fa71c4c7c7356a818aa33b2facb1
Andriy Popovych <popovych.andrey@gmail.com>
2015-07-08 12:04:25 +0300

    [Nailgun] Mix plugin volume metadata with core
    Implement functionality for merging plugin volumes metadata
    with release volumes metadata and using it in Volumes Manager.
    
    Implements: blueprint role-as-a-plugin
    Change-Id: Ie8e1236e4aa38b8220d937febaa50d146c9caa2a

--- nailgun/nailgun/test/unit/test_objects.py
+++ nailgun/nailgun/test/unit/test_objects.py
@@ -17,6 +17,8 @@
 import copy
 import datetime
 import hashlib
+import mock
+
 from itertools import cycle
 from itertools import ifilter
 import uuid
@@ -43,6 +45,7 @@ from nailgun.network.neutron import NeutronManager
 from nailgun.network.neutron import NeutronManager70
 
 from nailgun import objects
+from nailgun.plugins.manager import PluginManager
 
 
 class TestObjects(BaseIntegrationTest):
@@ -827,6 +830,37 @@ class TestClusterObject(BaseTestCase):
         with self.assertRaisesRegexp(errors.PluginsTasksOverlapping,
                                      expected_message):
             objects.Cluster.get_deployment_tasks(cluster)
+
+    def test_get_volumes_metadata(self):
+        plugin_volumes_metadata = {
+            'volumes_roles_mapping': {
+                'test_plugin_1': [
+                    {'allocate_size': 'min', 'id': 'test_plugin_1'}
+                ],
+                'test_plugin_2': [
+                    {'allocate_size': 'min', 'id': 'test_plugin_2'}
+                ],
+            },
+            'volumes': [
+                {'id': 'test_plugin_1', 'type': 'vg'},
+                {'id': 'test_plugin_2', 'type': 'vg'}
+            ]
+        }
+        with mock.patch.object(
+                PluginManager, 'get_volumes_metadata') as plugin_volumes:
+            plugin_volumes.return_value = plugin_volumes_metadata
+            expected_volumes_metadata = copy.deepcopy(
+                self.env.releases[0].volumes_metadata)
+            expected_volumes_metadata['volumes_roles_mapping'].update(
+                plugin_volumes_metadata['volumes_roles_mapping'])
+            expected_volumes_metadata['volumes'].extend(
+                plugin_volumes_metadata['volumes'])
+
+            volumes_metadata = objects.Cluster.get_volumes_metadata(
+                self.env.clusters[0])
+
+            self.assertDictEqual(
+                volumes_metadata, expected_volumes_metadata)
 
 
 class TestClusterObjectGetRoles(BaseTestCase):

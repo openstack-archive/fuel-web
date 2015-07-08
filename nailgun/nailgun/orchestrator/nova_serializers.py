b2dc2fa83bf6a69663bc2d930db9f175887846bd
Aleksey Kasatkin <akasatkin@mirantis.com>
2015-07-08 14:34:59 +0300

    Support Nova-Network with granular network roles
    Implements: blueprint templates-for-networking
    Change-Id: I898c5b1733f0be923cfa9dc6098c1d6344efb93e

--- nailgun/nailgun/orchestrator/nova_serializers.py
+++ nailgun/nailgun/orchestrator/nova_serializers.py
@@ -21,6 +21,7 @@ from netaddr import IPNetwork
 
 from nailgun import consts
 from nailgun.objects import Cluster
+from nailgun.objects import Node
 from nailgun.orchestrator.base_serializers import NetworkDeploymentSerializer
 
 
@@ -303,3 +304,127 @@ class NovaNetworkDeploymentSerializer61(NovaNetworkDeploymentSerializer):
                                          fixed_sub_iface)
 
         return attrs
+
+
+class NovaNetworkDeploymentSerializer70(NovaNetworkDeploymentSerializer61):
+
+    @classmethod
+    def network_provider_node_attrs(cls, cluster, node):
+        """Serialize node, then it will be
+        merged with common attributes
+        """
+        node_attrs = super(NovaNetworkDeploymentSerializer70,
+                           cls).network_provider_node_attrs(cluster, node)
+        node_attrs['network_metadata'] = cls.generate_network_metadata(cluster)
+        return node_attrs
+
+    @classmethod
+    def generate_network_scheme(cls, node):
+        attrs = super(NovaNetworkDeploymentSerializer70,
+                      cls).generate_network_scheme(node)
+
+        attrs['roles']['mgmt/corosync'] = 'br-mgmt'
+        attrs['roles']['mgmt/database'] = 'br-mgmt'
+        attrs['roles']['mgmt/messaging'] = 'br-mgmt'
+        attrs['roles']['mgmt/api'] = 'br-mgmt'
+        attrs['roles']['mgmt/memcache'] = 'br-mgmt'
+        attrs['roles']['mgmt/vip'] = 'br-mgmt'
+
+        attrs['roles']['nova/api'] = 'br-mgmt'
+        attrs['roles']['murano/api'] = 'br-mgmt'
+        attrs['roles']['sahara/api'] = 'br-mgmt'
+        attrs['roles']['ceilometer/api'] = 'br-mgmt'
+        attrs['roles']['heat/api'] = 'br-mgmt'
+        attrs['roles']['keystone/api'] = 'br-mgmt'
+        attrs['roles']['horizon'] = 'br-mgmt'
+        attrs['roles']['glance/api'] = 'br-mgmt'
+
+        attrs['roles']['public/vip'] = 'br-ex'
+        attrs['roles']['ceph/radosgw'] = 'br-ex'
+
+        attrs['roles']['admin/pxe'] = 'br-fw-admin'
+
+        attrs['roles']['ceph/replication'] = 'br-storage'
+        attrs['roles']['ceph/public'] = 'br-mgmt'
+
+        attrs['roles']['swift/replication'] = 'br-storage'
+        attrs['roles']['swift/api'] = 'br-mgmt'
+
+        attrs['roles']['cinder/iscsi'] = 'br-storage'
+        attrs['roles']['cinder/api'] = 'br-mgmt'
+
+        attrs['roles']['mongo/db'] = 'br-mgmt'
+
+        return attrs
+
+    @classmethod
+    def generate_network_metadata(cls, cluster):
+        nodes = dict()
+        nm = Cluster.get_network_manager(cluster)
+
+        for n in Cluster.get_nodes_not_for_deletion(cluster):
+            name = Node.make_slave_name(n)
+            node_roles = Node.all_roles(n)
+
+            ip_by_net = {
+                'fuelweb_admin': None,
+                'storage': None,
+                'management': None,
+                'public': None
+            }
+            for net in ip_by_net:
+                netgroup = nm.get_node_network_by_netname(n, net)
+                if netgroup.get('ip'):
+                    ip_by_net[net] = netgroup['ip'].split('/')[0]
+
+            netw_roles = {
+                'admin/pxe': ip_by_net['fuelweb_admin'],
+                'fw-admin': ip_by_net['fuelweb_admin'],
+
+                'keystone/api': ip_by_net['management'],
+                'swift/api': ip_by_net['management'],
+                'sahara/api': ip_by_net['management'],
+                'ceilometer/api': ip_by_net['management'],
+                'cinder/api': ip_by_net['management'],
+                'glance/api': ip_by_net['management'],
+                'heat/api': ip_by_net['management'],
+                'nova/api': ip_by_net['management'],
+                'murano/api': ip_by_net['management'],
+                'horizon': ip_by_net['management'],
+
+                'management': ip_by_net['management'],
+                'mgmt/api': ip_by_net['management'],
+                'mgmt/database': ip_by_net['management'],
+                'mgmt/messaging': ip_by_net['management'],
+                'mgmt/corosync': ip_by_net['management'],
+                'mgmt/memcache': ip_by_net['management'],
+                'mgmt/vip': ip_by_net['management'],
+
+                'mongo/db': ip_by_net['management'],
+
+                'ceph/public': ip_by_net['management'],
+
+                'storage': ip_by_net['storage'],
+                'ceph/replication': ip_by_net['storage'],
+                'swift/replication': ip_by_net['storage'],
+                'cinder/iscsi': ip_by_net['storage'],
+
+                'ex': ip_by_net['public'],
+                'public/vip': ip_by_net['public'],
+                'ceph/radosgw': ip_by_net['public'],
+            }
+
+            nodes[name] = {
+                "uid": n.uid,
+                "fqdn": n.fqdn,
+                "name": name,
+                "user_node_name": n.name,
+                "swift_zone": n.uid,
+                "node_roles": node_roles,
+                "network_roles": netw_roles
+            }
+
+        return dict(
+            nodes=nodes,
+            vips=nm.assign_vips_for_net_groups(cluster)
+        )

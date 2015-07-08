bb36b027696c2a40a13904217e79fc64913d726c
Fedor Zhadaev <fzhadaev@mirantis.com>
2015-07-08 12:57:21 +0300

    Configurable hostnames for physical nodes
    * Extended db model with field 'hostname'
    * Added hostname validation in API node handler
    
    Change-Id: If18d00612140dcaaeec23bea172dcf6490186e5d
    Implements: blueprint node-naming

--- nailgun/nailgun/api/v1/validators/node.py
+++ nailgun/nailgun/api/v1/validators/node.py
@@ -218,6 +218,20 @@ class NodeValidator(BasicValidator):
                     log_message=True
                 )
 
+        if d.get("hostname") is not None and \
+                objects.Node.get_by_hostname(d["hostname"]):
+            if instance:
+                node = instance
+            else:
+                node = objects.Node.get_by_mac_or_uid(
+                    mac=d.get("mac"),
+                    node_uid=d.get("id")
+                )
+            if d["hostname"] != getattr(node, 'hostname'):
+                raise errors.InvalidData(
+                    "Duplicate hostname '%s'." % d["hostname"]
+                )
+
         if "roles" in d:
             if instance:
                 node = instance

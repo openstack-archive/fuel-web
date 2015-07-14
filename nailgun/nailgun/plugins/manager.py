6a21e558d54e6ba7d829d20fb0d1247abfae8b7d
Alexander Saprykin <asaprykin@mirantis.com>
2015-07-14 16:36:59 +0300

    [Nailgun] Load network roles from plugin metadata
    'get_network_roles' returns combined roles list from
    cluster.release and plugin objects
    
    Implements: blueprint vip-reservation
    Change-Id: I92d0de688449b1426197ccdddd3108a72885eb21

--- nailgun/nailgun/plugins/manager.py
+++ nailgun/nailgun/plugins/manager.py
@@ -83,6 +83,30 @@ class PluginManager(object):
         return cluster_plugins
 
     @classmethod
+    def get_network_roles(cls, cluster):
+        core_roles = cluster.release.network_roles_metadata
+        known_roles = set(role['id'] for role in core_roles)
+
+        plugin_roles = []
+        conflict_roles = {}
+        for plugin in cluster.plugins:
+            for role in plugin.network_roles_metadata:
+                role_id = role['id']
+                if role_id in known_roles:
+                    conflict_roles[role_id] = plugin.name
+                known_roles.add(role_id)
+            plugin_roles.extend(plugin.network_roles_metadata)
+
+        if conflict_roles:
+            raise errors.NetworkRoleConflict(
+                "Cannot override existing network roles: '{0}' in "
+                "plugins: '{1}'".format(
+                    ', '.join(conflict_roles),
+                    ', '.join(set(conflict_roles.values()))))
+
+        return plugin_roles
+
+    @classmethod
     def sync_plugins_metadata(cls, plugin_ids=None):
         """Sync metadata for plugins by given ids. If there is not
         ids all newest plugins will be synced

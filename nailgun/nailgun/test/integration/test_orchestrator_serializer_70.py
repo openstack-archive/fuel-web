15f00067687f8341f01c821c95e1c92296f15c38
Andriy Popovych <popovych.andrey@gmail.com>
2015-07-17 10:40:54 +0300

    Support for compute-vmware
    New role 'compute-vmware' was introduced. Also vmware deployment
    serializer is changed for supporting 'target-node'
    
    Change-Id: I4e35f2e980737a0e64abbba585e2f2aaaa3f27c3

--- nailgun/nailgun/test/integration/test_orchestrator_serializer_70.py
+++ nailgun/nailgun/test/integration/test_orchestrator_serializer_70.py
@@ -164,6 +164,19 @@ class TestDeploymentAttributesSerialization70(BaseDeploymentSerializer):
     def test_generate_vmware_attributes_data(self):
         self.check_generate_vmware_attributes_data()
 
+        result = self.serializer.serialize_node(
+            self.env.nodes[0], 'compute-vmware')
+
+        self.assertEqual(
+            result['vcenter']['computes'][0]['target_node'],
+            "test_target_node")
+        self.assertEqual(
+            result['vcenter']['computes'][2]['target_node'],
+            "")
+        self.assertEqual(
+            result['vcenter']['computes'][4]['target_node'],
+            "controllers")
+
 
 class TestDeploymentSerializationForNovaNetwork70(BaseDeploymentSerializer):
     @mock.patch.object(models.Release, 'environment_version',
@@ -311,6 +324,19 @@ class TestDeploymentSerializationForNovaNetwork70(BaseDeploymentSerializer):
 
     def test_generate_vmware_attributes_data(self):
         self.check_generate_vmware_attributes_data()
+
+        result = self.serializer.serialize_node(
+            self.env.nodes[0], 'compute-vmware')
+
+        self.assertEqual(
+            result['vcenter']['computes'][0]['target_node'],
+            "test_target_node")
+        self.assertEqual(
+            result['vcenter']['computes'][2]['target_node'],
+            "")
+        self.assertEqual(
+            result['vcenter']['computes'][4]['target_node'],
+            "controllers")
 
 
 class TestPluginDeploymentTasksInjection(base.BaseIntegrationTest):

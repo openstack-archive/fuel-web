b72993e239493b39b84812faaae31afa0366bb06
Kate Pimenova <kpimenova@mirantis.com>
2015-07-20 15:40:49 +0300

    Confuguration button UI fix
       - buttons for not deployed cluster: "Configure Disks/Interfases"
       - buttons for deployed cluster switches between "Disk/Interface Configuration" and
         "Configure Disks/Interfases" in case of possibility to edit this screens
       - error dialog messages changed
    
    Closes-Bug:#1463699
    
    Change-Id: Icd6a68a28885bbcc6e6ea00be16098fa16df19ac

--- nailgun/static/views/dialogs.jsx
+++ nailgun/static/views/dialogs.jsx
@@ -797,10 +797,10 @@ function($, _, i18n, Backbone, React, utils, models, dispatcher, controls, compo
                     {node.get('cluster') &&
                         <div className='btn-group' role='group'>
                             <button className='btn btn-default btn-edit-disks' onClick={_.partial(this.goToConfigurationScreen, 'disks')}>
-                                {i18n('dialog.show_node.disk_configuration_button')}
+                                {i18n('dialog.show_node.disk_configuration')}
                             </button>
                             <button className='btn btn-default btn-edit-networks' onClick={_.partial(this.goToConfigurationScreen, 'interfaces')}>
-                                {i18n('dialog.show_node.network_configuration_button')}
+                                {i18n('dialog.show_node.network_configuration')}
                             </button>
                         </div>
                     }

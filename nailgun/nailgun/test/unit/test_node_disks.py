c51d9c597993688c6b2993dcd6047a1d8078a394
Alexander Kislitsky <akislitsky@mirantis.com>
2015-07-21 16:50:38 +0300

    Mount points added for controller
    As workaround before volume manager refactoring we add
    /var and /var/log mount points for role controller.
    
    Change-Id: I65c1c2f627a0124ed1f2d09d82df397012fe87f3
    Closes-Bug: #1278964

--- nailgun/nailgun/test/unit/test_node_disks.py
+++ nailgun/nailgun/test/unit/test_node_disks.py
@@ -509,7 +509,9 @@ class TestNodeVolumesInformationHandler(BaseIntegrationTest):
     def test_volumes_information_for_controller_role(self):
         node_db = self.create_node('controller')
         response = self.get(node_db.id)
-        self.check_volumes(response, ['os', 'image', 'mysql'])
+        self.check_volumes(
+            response, ['os', 'image', 'mysql',
+                       'controller_var', 'controller_log'])
 
     def test_volumes_information_for_ceph_role(self):
         node_db = self.create_node('ceph-osd')
@@ -624,6 +626,16 @@ class TestVolumeManager(BaseIntegrationTest):
         self.non_zero_size(mysql_sum_size)
         return mysql_sum_size
 
+    def volumes_sum_size(self, disks, vg):
+        sum_size = 0
+        for disk in only_disks(disks):
+            volumes = filter(
+                lambda volume: volume.get('vg') == vg, disk['volumes']
+            )
+            sum_size += sum(v['size'] for v in volumes)
+        self.non_zero_size(sum_size)
+        return sum_size
+
     def reserved_size(self, spaces):
         reserved_size = 0
         for disk in only_disks(spaces):
@@ -730,10 +742,14 @@ class TestVolumeManager(BaseIntegrationTest):
         os_sum_size = self.os_size(disks)
         mysql_sum_size = self.mysql_size(disks)
         glance_sum_size = self.glance_size(disks)
+        var_sum_size = self.volumes_sum_size(disks, 'controller_var')
+        log_sum_size = self.volumes_sum_size(disks, 'controller_log')
         reserved_size = self.reserved_size(disks)
 
         self.assertEqual(disks_size_sum - reserved_size,
-                         os_sum_size + glance_sum_size + mysql_sum_size)
+                         os_sum_size + glance_sum_size +
+                         mysql_sum_size + var_sum_size +
+                         log_sum_size)
         self.logical_volume_sizes_should_equal_all_phisical_volumes(
             VolumeManagerExtension.get_node_volumes(node))
         self.check_disk_size_equal_sum_of_all_volumes(

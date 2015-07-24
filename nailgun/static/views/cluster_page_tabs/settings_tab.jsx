05a89d87d5682dc60947252e39d8f2b1ed703681
Julia Aranovich <jkirnosova@mirantis.com>
2015-07-24 13:56:39 +0300

    Urlify controls description
    Closes-Bug: #1476706
    
    Change-Id: Ic4e71c322fb0e552960152ca0b7d73e89cc05a25

--- nailgun/static/views/cluster_page_tabs/settings_tab.jsx
+++ nailgun/static/views/cluster_page_tabs/settings_tab.jsx
@@ -444,7 +444,6 @@ function($, _, i18n, React, utils, models, Expression, componentMixins, controls
                     <div>
                         {_.map(sortedSettings, function(settingName) {
                             var setting = group[settingName];
-                            if (setting.type == 'hidden') return null;
 
                             var path = this.props.makePath(this.props.groupName, settingName);
 
@@ -491,9 +490,10 @@ function($, _, i18n, React, utils, models, Expression, componentMixins, controls
                                     />;
                                 }
                                 return <controls.Input
-                                    {... _.pick(setting, 'type', 'label', 'description')}
+                                    {... _.pick(setting, 'type', 'label')}
                                     key={settingName}
                                     name={settingName}
+                                    description={setting.description && utils.urlify(setting.description)}
                                     children={setting.type == 'select' ? this.composeOptions(setting.values) : null}
                                     defaultValue={setting.value}
                                     defaultChecked={_.isBoolean(setting.value) ? setting.value : false}

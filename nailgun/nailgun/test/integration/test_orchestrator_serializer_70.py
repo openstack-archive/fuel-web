9b71a7277be193e1de1c935e953411ca84ca492f
Alexander Saprykin <asaprykin@mirantis.com>
2015-07-24 11:35:18 +0300

    Fix prepare for deployment for 7.0 version
    * Rename NodeCollection.prepare_for_deployment to
      prepare_for_6_1_deployment.
    * Add prepare_for_deployment method.
    * Fix tests for orchestrator serializer
    * Fix Private networks description. 'render_addr_mask' is not relevant for 7.0 serializer.
    
    Closes-Bug: #1477643
    Change-Id: I27133d15fff23d0ac0c9755c7356bac7d730b317

--- nailgun/nailgun/test/integration/test_orchestrator_serializer_70.py
+++ nailgun/nailgun/test/integration/test_orchestrator_serializer_70.py
@@ -19,6 +19,7 @@ import six
 import yaml
 
 from nailgun import consts
+from nailgun.db import db
 from nailgun.db.sqlalchemy import models
 from nailgun.network.manager import NetworkManager
 from nailgun import objects
@@ -34,6 +35,12 @@ from nailgun.orchestrator.neutron_serializers import \
     NeutronNetworkTemplateSerializer70
 from nailgun.test.integration.test_orchestrator_serializer import \
     BaseDeploymentSerializer
+from nailgun.test.integration.test_orchestrator_serializer import \
+    TestDeploymentHASerializer61
+from nailgun.test.integration.test_orchestrator_serializer import \
+    TestNovaOrchestratorSerializer
+from nailgun.test.integration.test_orchestrator_serializer import \
+    TestSerializeInterfaceDriversData
 
 
 class BaseTestDeploymentAttributesSerialization70(BaseDeploymentSerializer):
@@ -54,13 +61,14 @@ class BaseTestDeploymentAttributesSerialization70(BaseDeploymentSerializer):
 
     # Must be set in subclasses
     segmentation_type = None
+    env_version = '2015.1.0-7.0'
+    prepare_for_deployment = objects.NodeCollection.prepare_for_deployment
 
     def setUp(self):
         super(BaseTestDeploymentAttributesSerialization70, self).setUp()
         self.cluster = self.create_env('ha_compact')
 
-        objects.NodeCollection.prepare_for_deployment(
-            self.env.nodes, self.segmentation_type)
+        self.prepare_for_deployment(self.env.nodes)
         self.cluster_db = self.db.query(models.Cluster).get(self.cluster['id'])
         serializer_type = get_serializer_for_cluster(self.cluster_db)
         self.serializer = serializer_type(AstuteGraph(self.cluster_db))
@@ -70,7 +78,7 @@ class BaseTestDeploymentAttributesSerialization70(BaseDeploymentSerializer):
 
     def create_env(self, mode):
         return self.env.create(
-            release_kwargs={'version': '2015.1.0-7.0'},
+            release_kwargs={'version': self.env_version},
             cluster_kwargs={
                 'mode': mode,
                 'net_provider': 'neutron',
@@ -226,7 +234,7 @@ class TestDeploymentSerializationForNovaNetwork70(
 
     def create_env(self, mode):
         return self.env.create(
-            release_kwargs={'version': '2015.1.0-7.0'},
+            release_kwargs={'version': self.env_version},
             cluster_kwargs={
                 'mode': mode,
                 'net_provider': consts.CLUSTER_NET_PROVIDERS.nova_network},
@@ -716,6 +724,9 @@ class TestPluginDeploymentTasksInjection(base.BaseIntegrationTest):
 
 class TestRolesSerializationWithPlugins(BaseDeploymentSerializer):
 
+    env_version = '2015.1.0-7.0'
+    prepare_for_deployment = objects.NodeCollection.prepare_for_deployment
+
     ROLES = yaml.safe_load("""
         test_role:
           name: "Some plugin role"
@@ -758,7 +769,7 @@ class TestRolesSerializationWithPlugins(BaseDeploymentSerializer):
 
         self.env.create(
             release_kwargs={
-                'version': '2015.1.0-7.0',
+                'version': self.env_version,
             },
             cluster_kwargs={
                 'mode': 'ha_compact',
@@ -784,7 +795,7 @@ class TestRolesSerializationWithPlugins(BaseDeploymentSerializer):
             pending_addition=True)
         self.db.flush()
 
-        objects.NodeCollection.prepare_for_deployment(self.cluster.nodes)
+        self.prepare_for_deployment(self.cluster.nodes)
 
         serializer = self._get_serializer(self.cluster)
         serialized_data = serializer.serialize(
@@ -815,7 +826,7 @@ class TestRolesSerializationWithPlugins(BaseDeploymentSerializer):
             pending_addition=True)
         self.db.flush()
 
-        objects.NodeCollection.prepare_for_deployment(self.cluster.nodes)
+        self.prepare_for_deployment(self.cluster.nodes)
 
         serializer = self._get_serializer(self.cluster)
         serialized_data = serializer.serialize(
@@ -835,6 +846,9 @@ class TestRolesSerializationWithPlugins(BaseDeploymentSerializer):
 
 class TestNetworkTemplateSerializer70(BaseDeploymentSerializer):
 
+    env_version = '2015.1.0-7.0'
+    prepare_for_deployment = objects.NodeCollection.prepare_for_deployment
+
     def setUp(self, *args):
         super(TestNetworkTemplateSerializer70, self).setUp()
         self.cluster = self.create_env('ha_compact')
@@ -844,7 +858,7 @@ class TestNetworkTemplateSerializer70(BaseDeploymentSerializer):
             self.cluster,
             self.net_template
         )
-        objects.NodeCollection.prepare_for_deployment(self.env.nodes)
+        self.prepare_for_deployment(self.env.nodes)
         cluster_db = self.db.query(models.Cluster).get(self.cluster['id'])
 
         serializer = get_serializer_for_cluster(self.cluster)
@@ -853,7 +867,7 @@ class TestNetworkTemplateSerializer70(BaseDeploymentSerializer):
 
     def create_env(self, mode):
         return self.env.create(
-            release_kwargs={'version': '11111-7.0'},
+            release_kwargs={'version': self.env_version},
             cluster_kwargs={
                 'api': False,
                 'mode': mode,
@@ -1086,3 +1100,80 @@ class TestNetworkTemplateSerializer70(BaseDeploymentSerializer):
         # serializer should not fail if we delete one of default network
         # what is not used in template
         net_serializer.generate_network_metadata(self.cluster)
+
+
+class TestCustomNetGroupIpAllocation(BaseDeploymentSerializer):
+
+    env_version = '2015.1.0-7.0'
+    prepare_for_deployment = objects.NodeCollection.prepare_for_deployment
+
+    def setUp(self):
+        super(TestCustomNetGroupIpAllocation, self).setUp()
+        self.cluster = self.create_env()
+
+    def create_env(self):
+        return self.env.create(
+            release_kwargs={'version': self.env_version},
+            cluster_kwargs={
+                'api': False,
+                'net_provider': 'neutron',
+                'net_segment_type': 'gre'},
+            nodes_kwargs=[
+                {'roles': ['controller']},
+                {'roles': ['compute']},
+            ])
+
+    def _create_network_group(self, **kwargs):
+        ng = {
+            'release': self.cluster.release.id,
+            'name': 'test',
+            'vlan_start': 50,
+            'cidr': '172.16.122.0/24',
+            'gateway': '172.16.122.1',
+            'group_id': objects.Cluster.get_default_group(self.cluster).id,
+            'meta': {
+                'notation': 'ip_ranges'
+            }
+        }
+        ng.update(kwargs)
+
+        net_group = models.NetworkGroup(**ng)
+        ip_range = models.IPAddrRange(
+            first='172.16.122.2',
+            last='172.16.122.255'
+        )
+        ip_range.network_group = net_group
+
+        db().add(ip_range)
+        db().flush()
+
+    def test_ip_allocation(self):
+        self._create_network_group()
+        self.prepare_for_deployment(self.env.nodes)
+
+        ip_addrs_count = db().query(models.IPAddr).filter(
+            models.IPAddr.ip_addr.like('172.16.122.%')).count()
+        self.assertEqual(ip_addrs_count, 2)
+
+
+class TestSerializer70Mixin(object):
+
+    env_version = "2015.1.0-7.0"
+
+    def prepare_for_deployment(self, nodes, *_):
+        objects.NodeCollection.prepare_for_deployment(nodes)
+
+
+class TestNovaOrchestratorSerializer70(TestSerializer70Mixin,
+                                       TestNovaOrchestratorSerializer):
+    pass
+
+
+class TestSerializeInterfaceDriversData70(TestSerializer70Mixin,
+                                          TestSerializeInterfaceDriversData):
+    pass
+
+
+class TestDeploymentHASerializer70(TestSerializer70Mixin,
+                                   TestDeploymentHASerializer61):
+    pass

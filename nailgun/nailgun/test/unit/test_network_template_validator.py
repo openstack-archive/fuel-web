3f64d4daf4870fb61c012ffb2329db7114569dd0
Ivan Kliuk <ivan.kliuk@gmail.com>
2015-07-24 09:46:52 +0300

    Additional tests for network template validator
    * Extract often used assert-methods into 'BaseValidatorTest' class;
    * Add comprehensive tests for JSON-shema validation.
    
    Change-Id: Iaf7f36c30fbd86f389ed62b457632de0c1dc05cb
    Related-bug: #1476779

--- nailgun/nailgun/test/unit/test_network_template_validator.py
+++ nailgun/nailgun/test/unit/test_network_template_validator.py
@@ -15,13 +15,15 @@
 from oslo_serialization import jsonutils
 
 from nailgun.api.v1.validators.network import NetworkTemplateValidator
-from nailgun.errors import errors
-from nailgun.test.base import BaseTestCase
+from nailgun.test.base import BaseValidatorTest
 
 
-class TestNetworkTemplateValidator(BaseTestCase):
-    def test_ok(self):
-        network_template = jsonutils.dumps({
+class TestNetworkTemplateValidator(BaseValidatorTest):
+    validator = NetworkTemplateValidator
+
+    def setUp(self):
+        super(BaseValidatorTest, self).setUp()
+        self.nt = {
             "adv_net_template": {
                 "node_group_1": {
                     "nic_mapping": {"default": {}},
@@ -43,56 +45,68 @@ class TestNetworkTemplateValidator(BaseTestCase):
                     }
                 },
             }
-        })
+        }
 
-        NetworkTemplateValidator.validate(network_template)
+    def test_ok(self):
+        dumped = jsonutils.dumps(self.nt)
+        self.validator.validate(dumped)
 
     def test_no_key_adv_net_template(self):
-        network_template = jsonutils.dumps({"adv_net_template": {}})
-
-        with self.assertRaises(errors.InvalidData) as context:
-            NetworkTemplateValidator.validate(network_template)
-
+        context = self.get_invalid_data_context({"adv_net_template": {}})
         self.assertEqual(
             context.exception.message, "No node groups are defined")
 
     def test_no_defined_templates(self):
-        network_template = jsonutils.dumps({
-            "adv_net_template": {
-                "node_group_1": {
-                    "nic_mapping": {"default": {}},
-                    "templates_for_node_role": {},
-                    "network_assignments": {},
-                    "network_scheme": {}
-                }
-            }
-        })
-
-        with self.assertRaises(errors.InvalidData) as context:
-            NetworkTemplateValidator.validate(network_template)
-
+        self.nt['adv_net_template']['node_group_1']['network_scheme'] = {}
+        self.nt['adv_net_template']['node_group_1'][
+            'templates_for_node_role'] = {}
+        context = self.get_invalid_data_context(self.nt)
         self.assertEqual(
             context.exception.message,
             "No templates are defined for node group node_group_1")
 
     def test_templates_not_found(self):
-        network_template = jsonutils.dumps({
-            "adv_net_template": {
-                "node_group_1": {
-                    "nic_mapping": {"default": {}},
-                    "templates_for_node_role": {
-                        "controller": ["public", "common"]
-                    },
-                    "network_assignments": {},
-                    "network_scheme": {}
-                }
-            }
-        })
-
-        with self.assertRaises(errors.InvalidData) as context:
-            NetworkTemplateValidator.validate(network_template)
-
+        self.nt['adv_net_template']['node_group_1']['network_scheme'] = {}
+        context = self.get_invalid_data_context(self.nt)
         self.assertEqual(
             context.exception.message,
             "Requested templates public, common were "
             "not found for node group node_group_1")
+
+    def test_network_scheme_additional_property(self):
+        self.nt['adv_net_template']['node_group_1']['network_scheme'][
+            'public'].update({'test_key': {}})
+        self.assertRaisesAdditionalProperty(self.nt, 'test_key')
+
+    def test_network_scheme_transformations_invalid_type(self):
+        self.nt['adv_net_template']['node_group_1']['network_scheme'][
+            'public'].update({'transformations': {}})
+        self.assertRaisesInvalidType(self.nt, {}, 'array')
+
+    def test_network_scheme_endpoints_invalid_type(self):
+        self.nt['adv_net_template']['node_group_1']['network_scheme'][
+            'public'].update({'endpoints': {}})
+        self.assertRaisesInvalidType(self.nt, {}, 'array')
+
+    def test_network_scheme_roles_invalid_type(self):
+        self.nt['adv_net_template']['node_group_1']['network_scheme'][
+            'public'].update({'roles': 1})
+        self.assertRaisesInvalidType(self.nt, 1, 'object')
+
+    def test_network_scheme_invalid_type(self):
+        self.nt['adv_net_template']['node_group_1']['network_scheme'] = 1
+        self.assertRaisesInvalidType(self.nt, 1, 'object')
+
+    def test_nic_mapping(self):
+        self.nt['adv_net_template']['node_group_1']['nic_mapping'] = 1
+        self.assertRaisesInvalidType(self.nt, 1, 'object')
+
+    def test_templates_list_invalid_type(self):
+        self.nt['adv_net_template']['node_group_1'][
+            'templates_for_node_role'] = 1
+        self.assertRaisesInvalidType(self.nt, 1, 'object')
+
+    def test_templates_list_invalid_type_in_list(self):
+        self.nt['adv_net_template']['node_group_1'][
+            'templates_for_node_role'] = {"anything": [1]}
+        self.assertRaisesInvalidType(self.nt, 1, 'string')

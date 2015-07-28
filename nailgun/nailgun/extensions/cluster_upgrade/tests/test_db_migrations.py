3578b4be632935f244c9107fb12fe18c8e3effda
Ilya Kharin <akscram@gmail.com>
2015-07-28 05:03:32 +0300

    Introduce extension to upgrade clusters
    The patch adds an extension which implements the procedure of upgrading
    clusters from one major release to another. As a first step of the
    procedure the extension provides an ability to create a seed cluster
    with the same settings as the original.
    
    Implements blueprint: nailgun-api-env-upgrade-extensions
    
    Change-Id: I22d51a3ffd51a7c88bdcbde0eef6f47b65def1c8

---
+++ nailgun/nailgun/extensions/cluster_upgrade/tests/test_db_migrations.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import alembic
+
+from nailgun import db
+from nailgun.db.migration import make_alembic_config_from_extension
+from nailgun.test import base
+
+from .. import extension
+
+
+_test_revision = '3b5d115d7e49'
+
+
+def setup_module(module):
+    alembic_config = make_alembic_config_from_extension(
+        extension.ClusterUpgradeExtension)
+    db.dropdb()
+    alembic.command.upgrade(alembic_config, _test_revision)
+
+
+class TestAddRelations(base.BaseAlembicMigrationTest):
+
+    def test_works_without_core_migrations(self):
+        columns = [
+            t.name for t in
+            self.meta.tables['cluster_upgrade_relations'].columns]
+
+        self.assertItemsEqual(columns, [
+            'id',
+            'orig_cluster_id',
+            'seed_cluster_id',
+        ])

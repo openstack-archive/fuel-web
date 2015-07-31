c87aecff34a55603778dfaf3f652cb3910f4900b
Fedor Zhadaev <fzhadaev@mirantis.com>
2015-07-31 18:21:31 +0300

    Fix bug with changing offloading modes for bonds
    Fixes bug: #1480169
    Change-Id: I0f5f3d195540777e449a6bacba67991afcc32f3d

---
+++ nailgun/nailgun/db/sqlalchemy/models/mutable.py
@@ -0,0 +1,42 @@
+from sqlalchemy.ext.mutable import Mutable
+
+
+class MutableList(Mutable, list):
+    @classmethod
+    def coerce(cls, key, value):
+        """Convert plain lists to MutableList."""
+
+        if not isinstance(value, MutableList):
+            if isinstance(value, list):
+                return MutableList(value)
+
+            # this call will raise ValueError
+            return Mutable.coerce(key, value)
+        else:
+            return value
+
+    def __setitem__(self, key, value):
+        """Detect list set events and emit change events."""
+
+        list.__setitem__(self, key, value)
+        self.changed()
+
+    def __delitem__(self, key):
+        """Detect list del events and emit change events."""
+
+        list.__delitem__(self, key)
+        self.changed()
+
+    def append(self, value):
+        list.append(self, value)
+        self.changed()
+
+    def extend(self, iterable):
+        list.extend(self, iterable)
+        self.changed()
+
+    def __getstate__(self):
+        return list(self)
+
+    def __setstate__(self, state):
+        self[:] = state

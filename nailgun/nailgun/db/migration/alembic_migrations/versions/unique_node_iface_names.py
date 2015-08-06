d099425f6c4065a015b204eb31682575a6aa4f32
Alexander Kuleshov <akuleshov@mirantis.com>
2015-08-06 20:31:28 +0300

    Ensure node interface names are unique within node
    Add a constraint on interface names that ensures that they are unique
    within node. Make it deferrable so that chain renaming is possible
    (e.g. (0, eth0) > (0, eth1); (1, eth1) > (1, eth0))
    
    Change-Id: I32da84e8e83dfec12fd9facd7e3fb6094b93481f

---
+++ nailgun/nailgun/db/migration/alembic_migrations/versions/unique_node_iface_names.py
@@ -0,0 +1,49 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""unique_node_iface_names
+
+Revision ID: 3f42d9f4198f
+Revises: 1e50a4903910
+Create Date: 2015-08-06 20:28:27.042711
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3f42d9f4198f'
+down_revision = '1e50a4903910'
+
+from alembic import op
+
+
+def upgrade():
+    op.create_unique_constraint('ensure_unique_node_bond_names',
+                                'node_bond_interfaces',
+                                ['node_id', 'name'],
+                                deferrable='True',
+                                initially='DEFERRED')
+    op.create_unique_constraint('ensure_unique_node_nic_names',
+                                'node_nic_interfaces',
+                                ['node_id', 'name'],
+                                deferrable='True',
+                                initially='DEFERRED')
+
+
+def downgrade():
+    op.drop_constraint('ensure_unique_node_nic_names',
+                       'node_nic_interfaces',
+                       type_='unique')
+    op.drop_constraint('ensure_unique_node_bond_names',
+                       'node_bond_interfaces',
+                       type_='unique')

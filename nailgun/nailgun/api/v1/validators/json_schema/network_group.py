bd3f077a5c13769a7af6905853410b5156e1bb98
Maciej Kwiek <mkwiek@mirantis.com>
2015-08-07 13:16:30 +0200

    vlan_start is nullable in schema for netgroup
    Also made schema require most of it's columns. This is done to make
    requests fail at validation step instead of on saving the model to
    database.
    
    Change-Id: I4d0edf99427194045269e2d98fca2ae9e0746331
    Closes-Bug: #1478948

--- nailgun/nailgun/api/v1/validators/json_schema/network_group.py
+++ nailgun/nailgun/api/v1/validators/json_schema/network_group.py
@@ -24,10 +24,12 @@ single_scheme = {
         "id": {"type": "integer"},
         "name": {"type": "string"},
         "release": base_types.NULLABLE_ID,
-        "vlan_start": {"type": "number"},
+        "vlan_start": {"type": ["null", "number"]},
         "cidr": base_types.NET_ADDRESS,
         "gateway": base_types.NULLABLE_IP_ADDRESS,
         "group_id": base_types.NULLABLE_ID,
         "meta": {"type": "object"}
-    }
+    },
+    "required": ["name", "release", "vlan_start", "cidr", "gateway", "meta",
+                 "group_id"]
 }

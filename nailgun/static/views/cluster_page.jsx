9cac1e0b157078fd6bd495ecd5e53bf80375c194
Nick Bogdanov <nbogdanov@mirantis.com>
2015-08-11 15:07:47 +0300

    Preserves cluster settings selected subtab
    When user navigates cluster page tabs selected settings subtab
    preserved (settings tab state partially stored in cluster page)
    
    Change-Id: I0fca033e934dbe871ee8234445ca1efeb3f9dda7
    Closes-Bug: #1482280

--- nailgun/static/views/cluster_page.jsx
+++ nailgun/static/views/cluster_page.jsx
@@ -142,6 +142,14 @@ function($, _, i18n, Backbone, React, utils, models, dispatcher, componentMixins
                 });
             }
         },
+        getInitialState: function() {
+            var settings = this.props.cluster.get('settings');
+            return {
+                activeGroupName: _.min(_.keys(settings.attributes), function(groupName) {
+                    return settings.get(groupName + '.metadata.weight');
+                })
+            };
+        },
         removeFinishedNetworkTasks: function(callback) {
             var request = this.removeFinishedTasks(this.props.cluster.tasks({group: 'network'}));
             if (callback) request.always(callback);
@@ -194,6 +202,15 @@ function($, _, i18n, Backbone, React, utils, models, dispatcher, componentMixins
                 return !tabData.tab.isVisible || tabData.tab.isVisible(cluster);
             });
         },
+        setActiveSettingsGroupName: function(value) {
+            if (_.isUndefined(value)) {
+                var settings = this.props.cluster.get('settings');
+                value = _.min(_.keys(settings.attributes), function(groupName) {
+                    return settings.get(groupName + '.metadata.weight');
+                });
+            };
+            this.setState({activeGroupName: value});
+        },
         render: function() {
             var cluster = this.props.cluster,
                 availableTabs = this.getAvailableTabs(cluster),
@@ -227,7 +244,13 @@ function($, _, i18n, Backbone, React, utils, models, dispatcher, componentMixins
                         </div>
                     </div>
                     <div key={tab.url + cluster.id} className={'content-box tab-content ' + tab.url + '-tab'}>
-                        <Tab ref='tab' cluster={cluster} tabOptions={this.props.tabOptions} {...this.props.tabData} />
+                        <Tab
+                            ref='tab'
+                            cluster={cluster}
+                            tabOptions={this.props.tabOptions}
+                            setActiveGroupName={this.setActiveSettingsGroupName}
+                            activeGroupName={this.state.activeGroupName}
+                            {...this.props.tabData} />
                     </div>
                 </div>
             );

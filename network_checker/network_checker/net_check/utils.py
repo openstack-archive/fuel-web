17db592ee7ee63f11b4bd112077ec44c0babccd8
Dmitry Shulyak <dshulyak@mirantis.com>
2015-08-11 14:10:36 +0300

    Introduce timeout for a collecting information from pcap files
    Timeout for collecting procedure will ensure that net_probe.py
    will exit earlier than timeout will be raied in net_probe.rb.
    
    Change-Id: I69e53809bca717919998dc06b4c9315c5642d184
    Closes-Bug: 1466020

---
+++ network_checker/network_checker/net_check/utils.py
@@ -0,0 +1,52 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from contextlib import contextmanager
+from functools import partial
+import logging
+import signal
+
+
+log = logging.getLogger(__name__)
+
+
+class TimeoutException(KeyboardInterrupt):
+    """Exception should be raised if timeout is exceeded."""
+
+
+def timeout_handler(timeout, signum, frame):
+    raise TimeoutException("Timeout {0} seconds exceeded".format(timeout))
+
+
+@contextmanager
+def signal_timeout(timeout, raise_exc=True):
+    """Timeout handling using signals
+    :param timeout: timeout in seconds, integer
+    :param raise_exc: bool to control suppressing of exception
+    """
+    handler = partial(timeout_handler, timeout)
+
+    signal.signal(signal.SIGALRM, handler)
+    signal.alarm(timeout)
+
+    try:
+        yield
+    except TimeoutException as exc:
+        if raise_exc:
+            raise
+        else:
+            log.warning(str(exc))
+    finally:
+        signal.alarm(0)
+        signal.signal(signal.SIGALRM, signal.SIG_DFL)

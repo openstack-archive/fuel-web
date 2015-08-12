6510bca5805145d3fa02a5305812d4f955fe9efd
Kseniya Tychkova <ktychkova@mirantis.com>
2015-08-12 18:37:50 +0400

    replace jinja2 with python templating
    replace jinja2 in nailgun.nailgun.objects.node.Node.apply_network_template
    to python templating
    
    jinja2 was used to substitute tokenized aliases with NICs' names in template
    python Template is able to do same substitution by deriving subclasses of Template
    jinja2 is removed from here because other jinja2 features should not be accessible with templates
    
    fix is covered by unit test
    test_add_node_with_cluster_network_template in test_node_assignment_handler.TestAssignmentHandlers
    
    Change-Id: I15e18b55654e38c29dedc84f1d480517513f62fa
    Closes-Bug: #1483309

---
+++ nailgun/nailgun/test/unit/test_network_template.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+try:
+    from unittest.case import TestCase
+except ImportError:
+    # Runing unit-tests in production environment
+    from unittest2.case import TestCase
+
+from nailgun.network.template import NetworkTemplate
+
+
+class TestNetworkTemplate(TestCase):
+    def test_simple_substitution(self):
+        template = NetworkTemplate("a: <%a%>, b: <%b%>")
+        substituted_string = template.safe_substitute(a='aaa', b='bbb')
+
+        self.assertEqual(substituted_string, "a: aaa, b: bbb")
+
+    def test_substitution_with_spaces(self):
+        template = NetworkTemplate("a: <% a%>, b: <%b %>, "
+                                   "c: <% c %>, d: <%   d%>")
+        substituted_string = template.safe_substitute(a='aaa', b='bbb',
+                                                      c='ccc', d='ddd')
+
+        self.assertEqual(substituted_string, "a: aaa, b: bbb, c: ccc, d: ddd")
+
+    def test_substitution_with_no_match(self):
+        template = NetworkTemplate("a: <%a b: <% b % >")
+        substituted_string = template.safe_substitute(a='aaa', b='bbb')
+
+        self.assertEqual(substituted_string, "a: <%a b: <%b % >")

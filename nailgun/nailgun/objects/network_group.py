e529f08bbaa2ff3b9be2c649e9d358071b8c2e21
Artem Roma <aroma@mirantis.com>
2015-08-13 19:08:31 +0300

    Add cleanup on delete action for NetworkGroupHandler
    Delete and update methods for NetworkGroup nailgun object added with
    custom actions to support removing and regenerating stale data (IPAddrs
    and IPAddrRanges)
    
    Change-Id: Id0b1aebb329da1fd895c7edd9d7a88f137fa36e6
    Closes-Bug: #1484476

--- nailgun/nailgun/objects/network_group.py
+++ nailgun/nailgun/objects/network_group.py
@@ -22,7 +22,10 @@ from nailgun.db import db
 from nailgun.db.sqlalchemy import models
 
 from nailgun.errors import errors
+from nailgun.logger import logger
 
+
+from nailgun.objects import Cluster
 from nailgun.objects import NailgunCollection
 from nailgun.objects import NailgunObject
 
@@ -76,6 +79,85 @@ class NetworkGroup(NailgunObject):
             db().add(ip_range)
             db().flush()
         return instance
+
+    @classmethod
+    def update(cls, instance, data):
+        notation = instance.meta.get('notation')
+
+        # if notation data is present change ip ranges and remove
+        # stalled ip adresses for the network group
+        if notation:
+            cls.cleanup(instance)
+
+        if notation == 'ip_ranges' and data.get('meta', {}).get('ip_range'):
+            cls._update_ip_range(instance.id, data['meta']['ip_range'])
+
+        if notation == 'cidr' and data.get('cidr'):
+            if instance.gateway is not None:
+                use_gateway = True
+            else:
+                use_gateway = False
+
+            cls._update_ip_range_for_cidr(
+                instance.id, data.get('cidr'), use_gateway)
+
+            cluster = instance.nodegroup.cluster
+            Cluster.add_pending_changes(cluster, 'networks')
+
+        return super(NetworkGroup, cls).update(instance, data)
+
+    @classmethod
+    def _update_ip_range(cls, network_group_id, ip_range):
+        # delete old ip range
+        db().query(models.IPAddrRange).filter_by(
+            network_group_id=network_group_id).delete()
+
+        new_ip_range = models.IPAddrRange(
+            network_group_id=network_group_id,
+            first=ip_range[0],
+            last=ip_range[1])
+
+        db().add(new_ip_range)
+        db().flush()
+
+    @classmethod
+    def _update_ip_range_for_cidr(cls, network_group_id, cidr, use_gateway):
+        # delete old ip range
+        db().query(models.IPAddrRange).filter_by(
+            network_group_id=network_group_id).delete()
+
+        first_idx = 2 if use_gateway else 1
+        new_cidr = IPNetwork(cidr)
+
+        new_ip_range = models.IPAddrRange(
+            network_group_id=network_group_id,
+            first=str(new_cidr[first_idx]),
+            last=str(new_cidr[-2]))
+
+        db().add(new_ip_range)
+        db().flush()
+
+    @classmethod
+    def delete(cls, instance):
+        """Delete network group and do cleanup: remove ip range and
+        ip adresses associated with the group
+        """
+        # NOTE(aroma): ip range data will be removing from db
+        # automatically due to relations restrictions
+
+        cls.cleanup(instance)
+        super(NetworkGroup, cls).delete(instance)
+
+    @classmethod
+    def cleanup(cls, instance):
+        """Remove all IPs that were assigned for the network group
+        """
+        logger.debug("Deleting old IPs for network with id=%s, cidr=%s",
+                     instance.id, instance.cidr)
+        db().query(models.IPAddr)\
+            .filter_by(network=instance.id)\
+            .delete()
+        db().flush()
 
 
 class NetworkGroupCollection(NailgunCollection):
--- nailgun/nailgun/test/unit/test_network_group_handler.py
+++ nailgun/nailgun/test/unit/test_network_group_handler.py
@@ -14,8 +14,11 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from netaddr import IPNetwork
+
 from oslo_serialization import jsonutils
 
+from nailgun.db.sqlalchemy import models
 from nailgun import objects
 from nailgun.test.base import BaseIntegrationTest
 from nailgun.utils import reverse
@@ -142,6 +145,51 @@ class TestHandlers(BaseIntegrationTest):
         )
         self.assertEqual(204, resp.status_code)
 
+    def test_delete_network_group_cleanup_ip_range(self):
+        ng_id = self._create_network_group(
+            meta={
+                "notation": "ip_ranges",
+                "ip_range": ["10.3.0.33", "10.3.0.158"]
+            }
+        ).json["id"]
+        self.app.delete(
+            reverse(
+                'NetworkGroupHandler',
+                kwargs={'obj_id': ng_id}
+            ),
+            headers=self.default_headers,
+        )
+        ip_range = self.db.query(models.IPAddrRange)\
+            .filter_by(network_group_id=ng_id)\
+            .first()
+        self.assertIsNone(ip_range)
+
+    def test_delete_network_group_cleanup_ip_addrs(self):
+        ng_id = self._create_network_group().json["id"]
+        node = self.env.create_node(api=False)
+
+        ip_address = []
+        for ip_addr in ('10.3.0.2', '10.3.0.3'):
+            ip_addr_data = {'network': ng_id,
+                            'node': node.id,
+                            'ip_addr': ip_addr}
+            ip_address.append(ip_addr_data)
+
+        self.db.add_all([models.IPAddr(**ips) for ips in ip_address])
+        self.db.flush()
+
+        self.app.delete(
+            reverse(
+                'NetworkGroupHandler',
+                kwargs={'obj_id': ng_id}
+            ),
+            headers=self.default_headers,
+        )
+        ips_db = self.db.query(models.IPAddr)\
+            .filter_by(network=ng_id)\
+            .all()
+        self.assertFalse(ips_db)
+
     def test_cannot_delete_admin_network_group(self):
         admin = objects.Cluster.get_network_manager().get_admin_network_group()
         resp = self.app.delete(
@@ -179,6 +227,60 @@ class TestHandlers(BaseIntegrationTest):
         updated_ng = jsonutils.loads(resp.body)
 
         self.assertEquals('test2', updated_ng['name'])
+
+    def test_update_network_group_ipranges_regenerated(self):
+        resp = self._create_network_group(
+            meta={
+                "notation": "ip_ranges",
+                "ip_range": ["10.3.0.33", "10.3.0.158"]
+            }
+        )
+        new_ng = jsonutils.loads(resp.body)
+        new_ng["name"] = "test"
+
+        new_ip_range = ["172.16.0.1", "172.16.0.10"]
+        new_ng["meta"]["ip_range"] = new_ip_range
+
+        self.app.put(
+            reverse(
+                'NetworkGroupHandler',
+                kwargs={'obj_id': new_ng['id']}
+            ),
+            jsonutils.dumps(new_ng),
+            headers=self.default_headers
+        )
+
+        ip_range = self.db.query(models.IPAddrRange)\
+            .filter_by(network_group_id=new_ng["id"])\
+            .one()
+        self.assertEqual(ip_range.first, new_ip_range[0])
+        self.assertEqual(ip_range.last, new_ip_range[1])
+
+    def test_update_network_group_ipranges_regenerated_for_cidr(self):
+        resp = self._create_network_group()
+        new_ng = jsonutils.loads(resp.body)
+
+        new_cidr = "10.3.0.1/20"
+        new_ng['cidr'] = new_cidr
+        new_ng['name'] = 'test'
+
+        generated_range = IPNetwork("10.3.0.1/20")
+        new_ip_range = [str(generated_range[2]), str(generated_range[-2])]
+
+        self.app.put(
+            reverse(
+                'NetworkGroupHandler',
+                kwargs={'obj_id': new_ng['id']}
+            ),
+            jsonutils.dumps(new_ng),
+            headers=self.default_headers
+        )
+
+        ip_range = self.db.query(models.IPAddrRange)\
+            .filter_by(network_group_id=new_ng["id"])\
+            .one()
+        self.assertEqual(ip_range.first, new_ip_range[0])
+        self.assertEqual(ip_range.last, new_ip_range[1])
 
     def test_duplicate_network_name_on_creation(self):
         resp = self._create_network_group()

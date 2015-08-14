90f6cc8238f70b46a330b9b0efc74f350c61f206
Nick Bogdanov <nbogdanov@mirantis.com>
2015-08-14 16:48:40 +0300

    Upgrades Intern to 3.0.6
    This change updates Intern testing framework from 2.2.2
    to 3.0.6 version and updates ScreenshotOnFailReporter
    to use newer API.
    
    Change-Id: I368cb18756c9d27a95c1b615d4473d8486ada184

--- nailgun/static/tests/functional/test_node_view.js
+++ nailgun/static/tests/functional/test_node_view.js
@@ -17,12 +17,13 @@
 define([
     'intern!object',
     'intern/chai!assert',
+    'intern/dojo/node!leadfoot/helpers/pollUntil',
     'tests/functional/pages/node',
     'tests/functional/pages/modal',
     'tests/functional/pages/common',
     'tests/functional/pages/cluster',
     'tests/functional/helpers'
-], function(registerSuite, assert, NodeComponent, ModalWindow, Common, ClusterPage) {
+], function(registerSuite, assert, pollUntil, NodeComponent, ModalWindow, Common, ClusterPage) {
     'use strict';
 
     registerSuite(function() {
@@ -146,8 +147,10 @@ define([
                     .assertElementNotExists('.node', 'Node has been removed');
             },
             'Additional tests for unallocated node': function() {
+                var self = this;
                 return this.remote
                     .clickByCssSelector('button.btn-add-nodes')
+                    .then(pollUntil(self.remote.isElementVisibleByClass, ['node'], 3000))
                     .assertElementAppears('.node-list', 2000, 'Unallocated node list loaded')
                     .then(function() {
                         return node.openCompactNodeExtendedView();

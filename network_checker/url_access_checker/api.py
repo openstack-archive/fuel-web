6b7fd52b808391be1c142d58067395d336b859d5
Maciej Kwiek <mkwiek@mirantis.com>
2015-08-19 15:28:10 +0200

    Add file protocol handling to url checker
    Handling of file protocol is needed for checking existence of local
    repositories.
    
    Change-Id: Idfe9d527c081f1dcb1f0fcbb2446a0852196608f
    Closes-Bug: #1484191

--- network_checker/url_access_checker/api.py
+++ network_checker/url_access_checker/api.py
@@ -13,6 +13,7 @@
 #    under the License.
 
 import json
+import os
 import socket
 
 import requests
@@ -21,10 +22,16 @@ import url_access_checker.errors as errors
 
 
 def check_urls(urls, proxies=None, timeout=60):
-    """Checks a set of urls to see if they are up and returning 200
+    """Checks a set of urls to see if they are valid
+
+    Url is valid if
+    - it returns 200 upon requesting it with http (if it doesn't specify
+    protocol as "file")
+    - it is an existing file or directory (if the protocol used in url is
+    "file")
 
     Arguments:
-    url -- a string containing url for testing
+    urls -- an iterable containing urls for testing
     proxies -- proxy servers to use for the request
     timeout -- the max time to wait for a response, default 60 seconds
     """
@@ -43,7 +50,7 @@ def _get_response_tuple(url, proxies=None, timeout=60):
     """Return a tuple which contains a result of url test
 
     Arguments:
-    url -- a string containing url for testing
+    url -- a string containing url for testing, can be local file
     proxies -- proxy servers to use for the request
     timeout -- the max time to wait for a response, default 60 seconds
 
@@ -51,21 +58,26 @@ def _get_response_tuple(url, proxies=None, timeout=60):
         result[0] -- boolean value, True if the url is deemed failed
         result[1] -- unchange url argument
     """
-    try:
-        # requests seems to correctly handle various corner cases:
-        # proxies=None or proxies={} mean 'use the default' rather than
-        # "don't use proxy". To force a direct connection one should pass
-        # proxies={'http': None}.
-        # Setting the timeout for requests.get(...) sets max request time. We
-        # want to set a value to prevent this process from hanging as the
-        # default timeout is None which can lead to bad things when processes
-        # never exit. LP#1478138
-        response = requests.get(url, proxies=proxies, timeout=timeout)
-        return (response.status_code != 200, url)
-    except (requests.exceptions.ConnectionError,
-            requests.exceptions.Timeout,
-            requests.exceptions.HTTPError,
-            requests.exceptions.ProxyError,
-            ValueError,
-            socket.timeout):
-        return (True, url)
+    file_prefix = 'file://'
+    if url.startswith(file_prefix):
+        path = url[len(file_prefix):]
+        return (not os.path.exists(path), url)
+    else:
+        try:
+            # requests seems to correctly handle various corner cases:
+            # proxies=None or proxies={} mean 'use the default' rather than
+            # "don't use proxy". To force a direct connection one should pass
+            # proxies={'http': None}.
+            # Setting the timeout for requests.get(...) sets max request time.
+            # We want to set a value to prevent this process from hanging as
+            # the default timeout is None which can lead to bad things when
+            # processes never exit. LP#1478138
+            response = requests.get(url, proxies=proxies, timeout=timeout)
+            return (response.status_code != 200, url)
+        except (requests.exceptions.ConnectionError,
+                requests.exceptions.Timeout,
+                requests.exceptions.HTTPError,
+                requests.exceptions.ProxyError,
+                ValueError,
+                socket.timeout):
+            return (True, url)

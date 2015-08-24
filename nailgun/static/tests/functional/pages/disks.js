98e42969cbd3ce6868b5884a88d8e04e4938bfd9
Kate Pimenova <kpimenova@mirantis.com>
2015-08-24 18:47:43 +0300

    [intern] Test Node Disks
    Related to blueprint ui-functional-tests-with-intern
    
    Change-Id: Ida75100e9ba9a0ec222e02a467d6658e0a8f9834

---
+++ nailgun/static/tests/functional/pages/disks.js
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2015 Mirantis, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ **/
+
+define(function() {
+    'use strict';
+    function NodeDisks(remote) {
+        this.remote = remote;
+        this.sdaDisk = '.disk-box[data-disk=sda]';
+        this.sdaDiskOS = this.sdaDisk + ' .disk-utility-box [data-volume=os]';
+        this.sdaDiskImage = this.sdaDisk + ' .disk-utility-box [data-volume=image]';
+    }
+
+    NodeDisks.prototype = {
+        constructor: NodeDisks,
+        goToNodeDisks: function() {
+            return this.remote
+                .setFindTimeout(5000)
+                .findAllByCssSelector('.node.pending_addition > label')
+                    // Check node
+                    .click()
+                    .end()
+                .findByCssSelector('.btn-configure-disks')
+                    // Click to "Configure Disks" button
+                    .click()
+                    .end()
+                .findByCssSelector('.edit-node-disks-screen')
+                    // Check if "Edit Node Disks page" opens
+                    .end();
+        },
+        getButton: function(selector) {
+            return this.remote
+                .setFindTimeout(1000)
+                .findByCssSelector(selector)
+        }
+    };
+    return NodeDisks;
+});

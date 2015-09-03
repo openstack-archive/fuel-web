2aee6292f5b6e9ee49fcfe030425440af412ddab
Vitaliy Mygal <xmig.mir@gmail.com>
2015-09-03 14:18:20 +0300

    Store attributes of plugins in a separate table
    This is redesign of plugins architecture in order to store
    the plugin's attributes in a separate table, not in cluster
    attributes, so it will be possible to remove connection between
    plugin and cluster when a plugin gets deleted.
    
    Added ability to work with different versions of a plugin.
    User can choose the preferred version in UI.
    
    The test "test_plugin_generator" was removed because no longer
    relevant.
    
    Closes-Bug: #1440046
    Implements: blueprint store-plugins-attributes
    Change-Id: I52115f130bf1c7c80c66e18d0bf9f7acb16dd56c

--- nailgun/nailgun/orchestrator/tasks_serializer.py
+++ nailgun/nailgun/orchestrator/tasks_serializer.py
@@ -109,7 +109,8 @@ class ExpressionBasedTask(DeploymentHook):
     @property
     def _expression_context(self):
         return {'cluster': self.cluster,
-                'settings': self.cluster.attributes.editable}
+                'settings':
+                objects.Cluster.get_attributes(self.cluster).editable}
 
     def should_execute(self):
         if 'condition' not in self.task:
@@ -260,7 +261,7 @@ class GenerateHaproxyKeys(GenericRolesHook):
         uids = self.get_uids()
         self.task['parameters']['cmd'] = self.task['parameters']['cmd'].format(
             CLUSTER_ID=self.cluster.id,
-            CN_HOSTNAME=self.cluster.attributes.editable
+            CN_HOSTNAME=objects.Cluster.get_attributes(self.cluster).editable
             ['public_ssl']['hostname']['value'])
         yield templates.make_shell_task(uids, self.task)
 

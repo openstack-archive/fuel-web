4f72249965fa7df928bf999eb94224acc1e4a706
Vitaliy Mygal <xmig.mir@gmail.com>
2015-09-03 14:18:20 +0300

    Store attributes of plugins in a separate table
    This is redesign of plugins architecture in order to store
    the plugin's attributes in a separate table, not in cluster
    attributes, so it will be possible to remove connection between
    plugin and cluster when a plugin gets deleted.
    
    Closes-Bug: #1440046
    Implements: blueprint store-plugins-attributes
    Change-Id: I52115f130bf1c7c80c66e18d0bf9f7acb16dd56c

--- nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_8_0.py
+++ nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_8_0.py
@@ -12,7 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-"""empty message
+"""Fuel 8.0
 
 Revision ID: 43b2cb64dae6
 Revises: 1e50a4903910
@@ -25,9 +25,169 @@ revision = '43b2cb64dae6'
 down_revision = '1e50a4903910'
 
 
+from alembic import op
+from nailgun.db.sqlalchemy.models import fields
+from oslo_serialization import jsonutils
+import six
+import sqlalchemy as sa
+
+
 def upgrade():
-    pass
+    cluster_plugins_upgrade()
 
 
 def downgrade():
-    pass
+    cluster_plugins_downgrade()
+
+
+def cluster_plugins_upgrade():
+    op.alter_column(
+        'cluster_plugins',
+        'cluster_id',
+        nullable=False
+    )
+    op.drop_constraint(
+        'cluster_plugins_cluster_id_fkey',
+        'cluster_plugins',
+        type_='foreignkey'
+    )
+    op.create_foreign_key(
+        'cluster_plugins_cluster_id_fkey',
+        'cluster_plugins', 'clusters',
+        ['cluster_id'], ['id'],
+        ondelete='CASCADE'
+    )
+    op.add_column(
+        'cluster_plugins',
+        sa.Column(
+            'enabled',
+            sa.Boolean,
+            nullable=False,
+            server_default='false'
+        )
+    )
+    op.add_column(
+        'cluster_plugins',
+        sa.Column(
+            'attributes',
+            fields.JSON(),
+            nullable=False,
+            server_default='{}'
+        )
+    )
+
+    # Iterate over all editable cluster attributes,
+    # and set entry in 'cluster_plugins' table
+
+    connection = op.get_bind()
+
+    q_get_plugins = sa.text('''
+        SELECT id, name FROM plugins
+    ''')
+    q_get_cluster_attributes = sa.text('''
+        SELECT cluster_id, editable FROM attributes
+    ''')
+    q_update_cluster_attributes = sa.text('''
+        UPDATE attributes
+        SET editable = :editable
+        WHERE cluster_id = :cluster_id
+    ''')
+    q_get_cluster_plugins = sa.text('''
+        SELECT id FROM cluster_plugins
+        WHERE cluster_id = :cluster_id AND plugin_id = :plugin_id
+    ''')
+    q_update_cluster_plugins = sa.text('''
+        UPDATE cluster_plugins
+        SET enabled = :enabled, attributes = :attributes
+        WHERE cluster_id = :cluster_id AND plugin_id = :plugin_id
+    ''')
+    q_insert_cluster_plugins = sa.text('''
+        INSERT INTO cluster_plugins
+            (cluster_id, plugin_id, enabled, attributes)
+        VALUES
+            (:cluster_id, :plugin_id, :enabled, :attributes)
+    ''')
+
+    plugins = dict(
+        (id_, name) for id_, name in connection.execute(q_get_plugins))
+    for cluster_id, editable in connection.execute(q_get_cluster_attributes):
+        editable = jsonutils.loads(editable)
+        for plugin_id, plugin_name in six.iteritems(plugins):
+            if plugin_name in editable:
+                attributes = editable.pop(plugin_name)
+                del attributes['metadata']['plugin_id']
+
+                action = q_update_cluster_plugins\
+                    if connection.execute(
+                        q_get_cluster_plugins,
+                        cluster_id=cluster_id,
+                        plugin_id=plugin_id,
+                    ).first()\
+                    else q_insert_cluster_plugins
+
+                connection.execute(
+                    action,
+                    cluster_id=cluster_id,
+                    plugin_id=plugin_id,
+                    enabled=attributes['metadata'].pop('enabled'),
+                    attributes=jsonutils.dumps(attributes)
+                )
+        connection.execute(
+            q_update_cluster_attributes,
+            cluster_id=cluster_id,
+            editable=jsonutils.dumps(editable)
+        )
+
+
+def cluster_plugins_downgrade():
+    connection = op.get_bind()
+
+    q_get_cluster_attributes = sa.text('''
+        SELECT clusters.id, attributes.editable
+        FROM attributes JOIN clusters ON (attributes.cluster_id = clusters.id)
+    ''')
+    q_get_plugins = sa.text('''
+        SELECT plugins.id, plugins.name, plugins.title,
+          cluster_plugins.enabled, cluster_plugins.attributes
+        FROM plugins JOIN cluster_plugins
+          ON (plugins.id = cluster_plugins.plugin_id)
+        WHERE cluster_plugins.cluster_id = :cluster_id
+    ''')
+    q_update_cluster_attributes = sa.text('''
+        UPDATE attributes
+        SET editable = :editable
+        WHERE cluster_id = :cluster_id
+    ''')
+
+    for cluster_id, editable in connection.execute(q_get_cluster_attributes):
+        editable = jsonutils.loads(editable)
+        plugins = connection.execute(q_get_plugins, cluster_id=cluster_id)
+        for p_id, p_name, p_title, p_enabled, p_attr in plugins:
+            p_attr = jsonutils.loads(p_attr)
+            p_attr['metadata'].update({
+                'plugin_id': p_id,
+                'enabled': p_enabled,
+                'label': p_title
+            })
+            editable.update({p_name: p_attr})
+            connection.execute(q_update_cluster_attributes,
+                               cluster_id=cluster_id,
+                               editable=jsonutils.dumps(editable))
+
+    op.drop_column('cluster_plugins', 'attributes')
+    op.drop_column('cluster_plugins', 'enabled')
+    op.drop_constraint(
+        'cluster_plugins_cluster_id_fkey',
+        'cluster_plugins',
+        type_='foreignkey'
+    )
+    op.create_foreign_key(
+        'cluster_plugins_cluster_id_fkey',
+        'cluster_plugins', 'clusters',
+        ['cluster_id'], ['id']
+    )
+    op.alter_column(
+        'cluster_plugins',
+        'cluster_id',
+        nullable=None
+    )
---
+++ nailgun/nailgun/test/unit/test_migration_fuel_8_0.py
@@ -0,0 +1,238 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import alembic
+from oslo_serialization import jsonutils
+import sqlalchemy as sa
+
+from nailgun.db import db
+from nailgun.db import dropdb
+from nailgun.db.migration import ALEMBIC_CONFIG
+from nailgun.test import base
+
+
+_prepare_revision = '1e50a4903910'
+_test_revision = '43b2cb64dae6'
+
+
+def setup_module(module):
+    dropdb()
+    alembic.command.upgrade(ALEMBIC_CONFIG, _prepare_revision)
+    prepare()
+    alembic.command.upgrade(ALEMBIC_CONFIG, _test_revision)
+
+
+def prepare():
+    meta = base.reflect_db_metadata()
+
+    result = db.execute(
+        meta.tables['releases'].insert(),
+        [{
+            'name': 'test_name',
+            'version': '2014.2.2-6.1',
+            'operating_system': 'ubuntu',
+            'state': 'available',
+            'roles': jsonutils.dumps([
+                'controller',
+                'compute',
+                'mongo',
+            ]),
+            'roles_metadata': jsonutils.dumps({
+                'controller': {
+                    'name': 'Controller',
+                    'description': 'Controller role',
+                    'has_primary': True,
+                },
+                'zabbix-server': {
+                    'name': 'Zabbix Server',
+                    'description': 'Zabbix Server role'
+                },
+                'cinder': {
+                    'name': 'Cinder',
+                    'description': 'Cinder role'
+                },
+                'mongo': {
+                    'name': 'Telemetry - MongoDB',
+                    'description': 'mongo is',
+                    'has_primary': True,
+                }
+            }),
+            'attributes_metadata': jsonutils.dumps({}),
+            'networks_metadata': jsonutils.dumps({
+                'bonding': {
+                    'properties': {
+                        'linux': {
+                            'mode': [
+                                {
+                                    "values": ["balance-rr",
+                                               "active-backup",
+                                               "802.3ad"]
+                                },
+                                {
+                                    "values": ["balance-xor",
+                                               "broadcast",
+                                               "balance-tlb",
+                                               "balance-alb"],
+                                    "condition": "'experimental' in "
+                                                 "version:feature_groups"
+                                }
+                            ]
+                        }
+                    }
+                },
+            }),
+            'is_deployable': True,
+        }])
+    releaseid = result.inserted_primary_key[0]
+
+    result = db.execute(
+        meta.tables['clusters'].insert(),
+        [{
+            'name': 'test_env',
+            'release_id': releaseid,
+            'mode': 'ha_compact',
+            'status': 'new',
+            'net_provider': 'neutron',
+            'grouping': 'roles',
+            'fuel_version': '6.1',
+        }])
+    clusterid = result.inserted_primary_key[0]
+
+    result = db.execute(
+        meta.tables['plugins'].insert(),
+        [{
+            'name': 'test_plugin_a',
+            'title': 'Test plugin A',
+            'version': '1.0.0',
+            'description': 'Test plugin A for Fuel',
+            'homepage': 'http://fuel_plugins.test_plugin.com',
+            'package_version': '3.0.0',
+            'groups': jsonutils.dumps(['tgroup']),
+            'authors': jsonutils.dumps(['tauthor']),
+            'licenses': jsonutils.dumps(['tlicense']),
+            'releases': jsonutils.dumps([
+                {'repository_path': 'repositories/ubuntu'}
+            ]),
+            'fuel_version': jsonutils.dumps(['6.1', '7.0']),
+        }]
+    )
+    pluginid_a = result.inserted_primary_key[0]
+
+    result = db.execute(
+        meta.tables['plugins'].insert(),
+        [{
+            'name': 'test_plugin_b',
+            'title': 'Test plugin B',
+            'version': '1.0.0',
+            'description': 'Test plugin B for Fuel',
+            'homepage': 'http://fuel_plugins.test_plugin.com',
+            'package_version': '3.0.0',
+            'groups': jsonutils.dumps(['tgroup']),
+            'authors': jsonutils.dumps(['tauthor']),
+            'licenses': jsonutils.dumps(['tlicense']),
+            'releases': jsonutils.dumps([
+                {'repository_path': 'repositories/ubuntu'}
+            ]),
+            'fuel_version': jsonutils.dumps(['6.1', '7.0']),
+        }]
+    )
+    pluginid_b = result.inserted_primary_key[0]
+
+    db.execute(
+        meta.tables['cluster_plugins'].insert(),
+        [
+            {
+                'cluster_id': clusterid,
+                'plugin_id': pluginid_a
+            },
+            {
+                'cluster_id': clusterid,
+                'plugin_id': pluginid_b
+            }
+        ]
+    )
+
+    db.execute(
+        meta.tables['attributes'].insert(),
+        [{
+            'cluster_id': clusterid,
+            'editable': jsonutils.dumps({
+                'test_plugin_a': {
+                    'metadata': {
+                        'plugin_id': pluginid_a,
+                        'enabled': True,
+                        'toggleable': True,
+                        'weight': 70,
+                    }
+                },
+                'test_plugin_b': {
+                    'metadata': {
+                        'plugin_id': pluginid_b,
+                        'enabled': False,
+                        'toggleable': True,
+                        'weight': 80,
+                    }
+                }
+            }),
+            'generated': jsonutils.dumps({}),
+        }])
+
+    db.commit()
+
+
+class TestClusterPluginsMigration(base.BaseAlembicMigrationTest):
+
+    def _get_enabled(self, plugin_name):
+        plugins = self.meta.tables['plugins']
+        cluster_plugins = self.meta.tables['cluster_plugins']
+
+        query = sa.select([cluster_plugins.c.enabled])\
+            .select_from(
+                sa.join(
+                    cluster_plugins, plugins,
+                    cluster_plugins.c.plugin_id == plugins.c.id))\
+            .where(plugins.c.name == plugin_name)
+        return db.execute(query).fetchone()[0]
+
+    def test_plugin_a_is_enabled(self):
+        enabled = self._get_enabled('test_plugin_a')
+        self.assertTrue(enabled)
+
+    def test_plugin_b_is_disabled(self):
+        enabled = self._get_enabled('test_plugin_b')
+        self.assertFalse(enabled)
+
+    def test_moving_plugin_attributes(self):
+        clusters = self.meta.tables['clusters']
+        attributes = self.meta.tables['attributes']
+        plugins = self.meta.tables['plugins']
+        cluster_plugins = self.meta.tables['cluster_plugins']
+
+        query = sa.select([attributes.c.editable])\
+            .select_from(
+                sa.join(
+                    attributes, clusters,
+                    attributes.c.cluster_id == clusters.c.id))
+        result = jsonutils.loads(db.execute(query).fetchone()[0])
+        self.assertItemsEqual(result, {})
+
+        query = sa.select([cluster_plugins.c.attributes])\
+            .select_from(
+                sa.join(
+                    cluster_plugins, plugins,
+                    cluster_plugins.c.plugin_id == plugins.c.id))\
+            .where(plugins.c.name == 'test_plugin_a')
+        result = jsonutils.loads(db.execute(query).fetchone()[0])
+        self.assertItemsEqual(result['metadata'],
+                              {'weight': 70, 'toggleable': True})

7dc81ee43aca4b6ffa035f269f0de7acdafcfef9
Vitaliy Mygal <xmig.mir@gmail.com>
2015-09-03 14:18:20 +0300

    Store attributes of plugins in a separate table
    This is redesign of plugins architecture in order to store
    the plugin's attributes in a separate table, not in cluster
    attributes, so it will be possible to remove connection between
    plugin and cluster when a plugin gets deleted.
    
    Added ability to work with different versions of a plugin.
    
    The test "test_plugin_generator" was removed because no longer
    relevant.
    
    Closes-Bug: #1440046
    Implements: blueprint store-plugins-attributes
    Change-Id: I52115f130bf1c7c80c66e18d0bf9f7acb16dd56c

--- nailgun/nailgun/task/task.py
+++ nailgun/nailgun/task/task.py
@@ -1221,7 +1221,7 @@ class CheckBeforeDeploymentTask(object):
     def _check_vmware_consistency(cls, task):
         """Checks vmware attributes consistency and proper values
         """
-        attributes = task.cluster.attributes.editable
+        attributes = objects.Cluster.get_attributes(task.cluster).editable
         vmware_attributes = task.cluster.vmware_attributes
         # Old(< 6.1) clusters haven't vmware support
         if vmware_attributes:

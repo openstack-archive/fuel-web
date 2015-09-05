f4687fbe15095b7934fbbe6d59be15ef2e3a0990
Aleksey Kasatkin <akasatkin@mirantis.com>
2015-09-05 12:14:01 -0500

    Remap Admin network when PXE interface is changed
    1. Check whether PXE interface was changed and remap Admin-pxe network accordingly.
    2. Fix _get_pxe_iface_name to check all meta info related to PXE interface.
    3. Update node's IP and MAC before checking it against interfaces meta.
    4. Fix 'pxe' flag assignment in tests.
    
    Change-Id: I6bec445dbc56c9caad12926a2733c59e82e58e36
    Partial-bug: #1466148

--- nailgun/nailgun/test/base.py
+++ nailgun/nailgun/test/base.py
@@ -269,6 +269,7 @@ class EnvironmentManager(object):
         mac = kwargs.get('mac', self.generate_random_mac())
         if default_metadata['interfaces']:
             default_metadata['interfaces'][0]['mac'] = mac
+            default_metadata['interfaces'][0]['pxe'] = True
             if not metadata or not meta_ifaces:
                 for iface in default_metadata['interfaces'][1:]:
                     if 'mac' in iface:

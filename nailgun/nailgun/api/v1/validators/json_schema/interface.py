7ade7a38e3326b188a9d664c2cf29e34dccf2a46
Ivan Kliuk <ivan.kliuk@gmail.com>
2015-09-06 03:24:46 +0300

    JSON-schema validation for network assignment
    Change-Id: I028b50e32d7bf1b9d02674b861e2e5ff24b4f2d5
    Closes-bug: #1425907

---
+++ nailgun/nailgun/api/v1/validators/json_schema/interface.py
@@ -0,0 +1,62 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nailgun.api.v1.validators.json_schema import base_types
+
+
+INTERFACE = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "object",
+    "additionalProperties": False,
+    "properties": {
+        "assigned_networks": {
+            "type": "array"
+        },
+        "bus_info": base_types.NULLABLE_STRING,
+        "current_speed": base_types.NULLABLE_STRING,
+        "driver": base_types.NULLABLE_STRING,
+        "id": base_types.POSITIVE_INTEGER,
+        "interface_properties": {
+            "type": "object",
+            "additionalProperties": False,
+            "properties": {
+                "disable_offloading": {"type": "boolean"},
+                "mt": base_types.NULLABLE_STRING,
+                "mtu": base_types.NULLABLE_POSITIVE_INTEGER
+            }
+        },
+        "mac": base_types.MAC_ADDRESS,
+        "max_speed": base_types.NULLABLE_STRING,
+        "name": {"type": "string"},
+        "offloading_modes": {
+            "type": "array"
+        },
+        "pxe": {"type": "boolean"},
+        "state": base_types.NULLABLE_STRING,
+        "type": {"type": "string"},
+        "interfaces": {"type": "array"},
+    }
+}
+
+INTERFACES = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "object",
+    "additionalProperties": False,
+    "properties": {
+        "interfaces": {
+            "type": "array",
+            "items": INTERFACE
+        }
+    }
+}

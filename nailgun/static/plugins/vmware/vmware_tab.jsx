c547b717fd168407355adaef342a250eb42da6c8
Andriy Popovych <popovych.andrey@gmail.com>
2015-09-07 18:57:18 +0300

    Fix wrong target_node value after deployment
    targetNode shouldn't be overwritten if it's not empty
    
    Change-Id: Ic19b9226fa07a0d0820c86aea71794f7ba09fc9a
    Closes-Bug: #1491744

--- nailgun/static/plugins/vmware/vmware_tab.jsx
+++ nailgun/static/plugins/vmware/vmware_tab.jsx
@@ -110,17 +110,19 @@ define(
 
             // add nodes of 'compute-vmware' type to targetNode select
             var targetNode = this.props.model.get('target_node') || {};
-            targetNode.options = [{id: 'controllers', label: 'controllers'}];
-            var nodes = this.props.cluster.get('nodes').filter(function(node) {
-                return _.contains(node.get('pending_roles'), 'compute-vmware');
-            });
-            nodes.forEach(function(node) {
-                targetNode.options.push({
-                    id: node.get('hostname'),
-                    label: node.get('name') + ' (' + node.get('mac').substr(9) + ')'
+            if (_.isEmpty(targetNode)) {
+                targetNode.options = [{id: 'controllers', label: 'controllers'}];
+                var nodes = this.props.cluster.get('nodes').filter(function(node) {
+                    return _.contains(node.get('pending_roles'), 'compute-vmware');
                 });
-            });
-            this.props.model.set('target_node', targetNode);
+                nodes.forEach(function(node) {
+                    targetNode.options.push({
+                        id: node.get('hostname'),
+                        label: node.get('name') + ' (' + node.get('mac').substr(9) + ')'
+                    });
+                });
+                this.props.model.set('target_node', targetNode);
+            }
 
             return (
                 <div className='nova-compute'>

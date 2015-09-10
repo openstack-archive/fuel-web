6f27ef84692fd91166578be55f9749f17530aa01
Vitaly Kramskikh <vkramskikh@mirantis.com>
2015-09-10 16:40:44 -0500

    Karma test-runner intergration
    Related to blueprint webpack
    
    Change-Id: I3905c46b42feef311762364cb81e0dcc5076e0af

--- nailgun/static/tests/unit/node_interfaces_offloading_modes.js
+++ nailgun/static/tests/unit/node_interfaces_offloading_modes.js
@@ -1,10 +1,4 @@
-define([
-    'intern!object',
-    'intern/chai!assert',
-    'underscore',
-    'sinon',
-    'views/cluster_page_tabs/nodes_tab_screens/offloading_modes_control'
-], function(registerSuite, assert, _, sinon, OffloadingModes) {
+define(['views/cluster_page_tabs/nodes_tab_screens/offloading_modes_control'], function(OffloadingModes) {
     'use strict';
 
     var offloadingModesConrol,
@@ -23,10 +17,8 @@ define([
             }
         };
 
-    registerSuite({
-        name: 'Offloadning Modes control',
-
-        beforeEach: function() {
+    suite('Offloadning Modes control', function() {
+        setup(function() {
             TestMode22 = {name: 'TestName22', state: false, sub: []};
             TestMode31 = {name: 'TestName31', state: null, sub: []};
             fakeOffloadingModes = [
@@ -54,25 +46,26 @@ define([
             offloadingModesConrol = new OffloadingModes({
                 interface: fakeInterface
             });
-        },
-        'Finding mode by name': function() {
+        });
+
+        test('Finding mode by name', function() {
             var mode = offloadingModesConrol.findMode(TestMode22.name, fakeOffloadingModes);
             assert.deepEqual(mode, TestMode22, 'Mode can be found by name');
-        },
-        'Set mode state logic': function() {
+        });
+        test('Set mode state logic', function() {
             offloadingModesConrol.setModeState(TestMode31, true);
             assert.strictEqual(TestMode31.state, true, 'Mode state is changing');
-        },
-        'Set submodes states logic': function() {
+        });
+        test('Set submodes states logic', function() {
             var mode = offloadingModesConrol.findMode('TestName1', fakeOffloadingModes);
             offloadingModesConrol.setModeState(mode, false);
             assert.strictEqual(TestMode31.state, false, 'Parent state changing leads to all child modes states changing');
-        },
-        'Disabled reversed logic': function() {
+        });
+        test('Disabled reversed logic', function() {
             var mode = offloadingModesConrol.findMode('TestName2', fakeOffloadingModes);
             offloadingModesConrol.setModeState(TestMode22, true);
             offloadingModesConrol.checkModes(null, fakeOffloadingModes);
             assert.strictEqual(mode.state, null, 'Parent state changing leads to all child modes states changing');
-        }
+        });
     });
 });

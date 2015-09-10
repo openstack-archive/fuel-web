1bae535241c86ab95033177538916ce126d5fabb
Alexandra Morozova <astepanchuk@mirantis.com>
2015-09-10 08:33:41 +0200

    [Intern] Wizard tests
    Related to blueprint ui-functional-tests-with-intern
    Change-Id: Ie51b7da2b93f11ee1f2b67490cec127bba7afdc7

---
+++ nailgun/static/tests/functional/test_wizard.js
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2015 Mirantis, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ **/
+
+define([
+    'intern!object',
+    'intern/chai!assert',
+    'tests/helpers',
+    'tests/functional/pages/common',
+    'tests/functional/pages/modal'
+], function(registerSuite, assert, helpers, Common, Modal) {
+    'use strict';
+
+    registerSuite(function() {
+        var common,
+            modal;
+
+        return {
+            name: 'Wizard Page',
+            setup: function() {
+                common = new Common(this.remote);
+                modal = new Modal(this.remote);
+                return this.remote
+                    .then(function() {
+                        return common.getIn();
+                    });
+            },
+            beforeEach: function() {
+                var clusterName = common.pickRandomName('Temp');
+                return this.remote
+                    .clickByCssSelector('.create-cluster')
+                    .then(function() {
+                        return modal.waitToOpen();
+                    })
+                    .setInputValue('[name=name]', clusterName);
+            },
+            afterEach: function() {
+                return this.remote
+                    .clickByCssSelector('.close');
+            },
+            'Test steps manipulations': function() {
+                return this.remote
+                    .then(function() {
+                        return common.assertElementExists('.wizard-step.active', 'There is only one active and available step at the beginning');
+                    })
+                    // Compute
+                    .pressKeys('\uE007')
+                    // Network
+                    .pressKeys('\uE007')
+                    // Storage
+                    .pressKeys('\uE007')
+                    // Additional Services
+                    .pressKeys('\uE007')
+                    // Finish
+                    .pressKeys('\uE007')
+                    .findAllByCssSelector('.wizard-step.available')
+                        .then(function(elements) {
+                            return assert.isTrue(elements.length == 5, 'All steps are available at the end');
+                        })
+                        .end()
+                    .clickLinkByText('Compute')
+                    .clickByCssSelector('input[value=kvm]')
+                    .findAllByCssSelector('.wizard-step.available')
+                        .then(function(elements) {
+                            return assert.isTrue(elements.length == 5, 'All steps are available if no changes to restrictions are made');
+                        })
+                        .end()
+                    .clickByCssSelector('input[type=checkbox]')
+                    .findAllByCssSelector('.wizard-step.available')
+                        .then(function(elements) {
+                            return assert.isTrue(elements.length == 1, 'Only one step is available after changing hypervisor');
+                        })
+                        .end();
+            }
+        };
+    });
+});

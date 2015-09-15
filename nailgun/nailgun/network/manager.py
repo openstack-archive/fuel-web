5186c0fa3edc88506273c1913f8bc0555fbebab7
=Nikita Koshikov <mk455v@att.com>
2015-09-15 14:14:04 -0500

    Update dnsmasq to setup dhcp when Admin networks are changed
    When parameters of any Admin network are changed or Admin network is
    deleted new UpdateDnsmasq task is called to setup dnsmasq with new
    configuration for dhcp.
    
    Co-Authored-By: Aleksei Kasatkin <akasatkin@mirantis.com>
    
    Partial-Bug: #1495593
    
    Change-Id: I532f21508c78bd142fbd0aa1754bcb7b617b121b

--- nailgun/nailgun/network/manager.py
+++ nailgun/nailgun/network/manager.py
@@ -1257,6 +1257,7 @@ class NetworkManager(object):
         )
         db().add(new_admin)
         db().flush()
+        cls.update_range_from_cidr(new_admin, new_admin.cidr, True)
 
     @classmethod
     def create_network_group(cls, cluster, net, gid=None):
@@ -1311,9 +1312,8 @@ class NetworkManager(object):
     @classmethod
     def update_networks(cls, network_configuration):
         if 'networks' in network_configuration:
+            admin_id = cls.get_admin_network_group_id()
             for ng in network_configuration['networks']:
-                if ng['id'] == cls.get_admin_network_group_id():
-                    continue
 
                 ng_db = db().query(NetworkGroup).get(ng['id'])
 
@@ -1490,12 +1490,47 @@ class NetworkManager(object):
         """Returns IPs related to network with provided ID.
         """
         return [x[0] for x in
-                db().query(IPAddr.ip_addr).filter_by(
-                    network=network_id)]
+                db().query(
+                    IPAddr.ip_addr
+                ).filter(
+                    IPAddr.network == network_id,
+                    or_(
+                        IPAddr.node.isnot(None),
+                        IPAddr.vip_type.isnot(None)
+                    )
+                )]
+
+    @classmethod
+    def get_admin_networks(cls, cluster_nodegroup_info=False):
+        admin_db = db().query(
+            NetworkGroup
+        ).filter(
+            NetworkGroup.name == consts.NETWORKS.fuelweb_admin
+        )
+        result = []
+        for net in admin_db:
+            net_info = {
+                'id': net.id,
+                'cidr': net.cidr,
+                'gateway': net.gateway,
+                'ip_ranges': [[ir.first, ir.last]
+                              for ir in net.ip_ranges]
+            }
+            if cluster_nodegroup_info:
+                net_info.update({
+                    'node_group_id': net.group_id,
+                    'node_group_name':
+                        net.nodegroup.name if net.group_id else None,
+                    'cluster_id':
+                        net.nodegroup.cluster_id if net.group_id else None,
+                    'cluster_name':
+                        net.nodegroup.cluster.name if net.group_id else None,
+                })
+            result.append(net_info)
+        return result
 
 
 class AllocateVIPs70Mixin(object):
-
 
     @classmethod
     def _build_advanced_vip_info(cls, vip_info, role, address):

c809014919b589ee684c9ffdee0b72c8be9e85f7
Bulat Gaifullin <gaifullinbf@gmail.com>
2015-09-24 13:39:05 +0300

    Plugin is able to extend existing network roles
    The same network role from different plugins can be merged
    according to policy of merge, that allows each plugin to extend the VIP for role.
    
    Change-Id: I81e773e53ab67b4e7d424805134c0d86dcc7a43a
    Closes-Bug: #1487011

---
+++ nailgun/nailgun/test/unit/test_plugins_merge_policies.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+from nailgun import consts
+from nailgun.plugins.merge_policies import NetworkRoleMergePolicy
+from nailgun.plugins.merge_policies import UnresolvableConflict
+from nailgun.test.base import BaseTestCase
+import six
+
+
+class TestNetworkRoleMergePolicy(BaseTestCase):
+    def setUp(self):
+        super(TestNetworkRoleMergePolicy, self).setUp()
+        self.policy = NetworkRoleMergePolicy()
+
+    @staticmethod
+    def _make_plugin_network_role(**kwargs):
+        properties = {
+            'subnet': True,
+            'gateway': False,
+            'vip': []
+        }
+
+        properties.update(kwargs)
+
+        return {
+            'id': 'test_network_role',
+            'default_mapping': consts.NETWORKS.public,
+            'properties': properties
+        }
+
+    def test_apply_path(self):
+        target = self._make_plugin_network_role(vip=[{'name': 'test_vip_a'}])
+        patch = self._make_plugin_network_role(vip=[{'name': 'test_vip_b'}])
+        self.policy.apply_patch(target, patch)
+        expected = self._make_plugin_network_role(
+            vip=[{'name': 'test_vip_a'}, {'name': 'test_vip_b'}]
+        )
+
+        self.assertDictEqual(expected, target)
+
+    def test_apply_for_conflicted_text_value(self):
+        policy = NetworkRoleMergePolicy()
+        policy.mergeable = frozenset(("test",))
+        target = self._make_plugin_network_role(test=six.text_type('test'))
+        patch = self._make_plugin_network_role(test='test1')
+        self.assertNotRaises(
+            UnresolvableConflict,
+            policy.apply_patch,
+            target,
+            patch
+        )
+        self.assertEqual(target['properties']['test'], 'test1')
+
+    def test_apply_patch_fail(self):
+        with self.assertRaisesRegexp(UnresolvableConflict, 'subnet'):
+            self.policy.apply_patch(
+                self._make_plugin_network_role(subnet=True),
+                self._make_plugin_network_role(subnet=False)
+            )
+
+        with self.assertRaisesRegexp(UnresolvableConflict, 'prop1'):
+            self.policy.apply_patch(
+                self._make_plugin_network_role(prop1=0.1),
+                self._make_plugin_network_role(prop1=1)
+            )

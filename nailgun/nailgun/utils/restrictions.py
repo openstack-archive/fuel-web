fee6481f34dc84a18b6429b135d3c4b80801a082
Andrey Shestakov <ashestakov@mirantis.com>
2015-10-08 17:20:36 +0300

    Add restrictions for network groups
    This change introduces new field 'restrictions' for networks
    
    On cluster create and on attributes update, network metadata validates
    restrictions if specified.
    
    Networks with restrictions should be created or removed depends on cluster
    attributes matches specified restrictions.
    
    Change-Id: I65a0d27e7c4c95360d4745f7e9cdd948bb20fdd3

--- nailgun/nailgun/utils/restrictions.py
+++ nailgun/nailgun/utils/restrictions.py
@@ -164,7 +164,8 @@ class RestrictionMixin(object):
     """Mixin with restriction processing functionality"""
 
     @classmethod
-    def check_restrictions(cls, models, restrictions, action=None):
+    def check_restrictions(cls, models, restrictions, action=None,
+                           strict=True):
         """Check if attribute satisfied restrictions
 
         :param models: objects which represent models in restrictions
@@ -190,7 +191,7 @@ class RestrictionMixin(object):
             # Filter which restriction satisfied condition
             satisfied = filter(
                 lambda item: Expression(
-                    item.get('condition'), models).evaluate(),
+                    item.get('condition'), models, strict=strict).evaluate(),
                 filterd_by_action_restrictions)
 
         return {

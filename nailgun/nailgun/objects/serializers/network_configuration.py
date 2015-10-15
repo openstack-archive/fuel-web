06b136a93942bf3879e9f03b497afc8ee8623f82
Andrey Shestakov <ashestakov@mirantis.com>
2015-10-15 14:48:30 +0300

    [Ironic] Add baremetal network for ironic
    This change adds:
    * new optional baremetal network
    * baremetal sections in Neutron L3 configuration
    * UI fixes for baremetal network
    
    Ironic requires separate baremetal network on infra nodes and neutron flat
    network mapped to this physical network.
    
    New baremetal network is optional and depends on Ironic component enabled in
    settings.
    
    Change-Id: I742bf69d3f3ac5e4b4ecd39a12e3171c566563e8

--- nailgun/nailgun/objects/serializers/network_configuration.py
+++ nailgun/nailgun/objects/serializers/network_configuration.py
@@ -82,7 +82,18 @@ class NeutronNetworkConfigurationSerializer(NetworkConfigurationSerializer):
         'configuration_template')
 
     @classmethod
+    def add_baremetal_fields(cls, cluster):
+        if (cluster.attributes.editable['additional_components'].
+                get(('ironic'), {}).get('value')):
+            cls.network_cfg_fields += ('baremetal_gateway', 'baremetal_range')
+        else:
+            cls.network_cfg_fields = tuple(filter(
+                lambda x: x not in ('baremetal_gateway', 'baremetal_range'),
+                cls.network_cfg_fields))
+
+    @classmethod
     def serialize_for_cluster(cls, cluster):
         result = cls.serialize_net_groups_and_vips(cluster)
+        cls.add_baremetal_fields(cluster)
         result['networking_parameters'] = cls.serialize_network_params(cluster)
         return result

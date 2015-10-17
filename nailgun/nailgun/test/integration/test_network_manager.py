1dd29c99c477950468c8485e695ec1723fcc31f8
Ivan Kliuk <ivan.kliuk@gmail.com>
2015-10-17 10:31:24 +0300

    Support of IP ranges for all networks by default
    Change-Id: I8cfc0e0622b83bd8047ba68fd5791c2008ed38e0
    Closes-bug: #1506820

--- nailgun/nailgun/test/integration/test_network_manager.py
+++ nailgun/nailgun/test/integration/test_network_manager.py
@@ -656,7 +656,8 @@ class TestNetworkManager(BaseNetworkManagerTest):
                 "meta": {"notation": consts.NETWORK_NOTATION.cidr},
             },
             consts.NETWORKS.management: {
-                "meta": {"use_gateway": True}
+                "meta": {"notation": consts.NETWORK_NOTATION.cidr,
+                         "use_gateway": True}
             }
         }
         cluster = self.env.create_cluster(

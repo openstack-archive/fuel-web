c4def20db947f4c5137badc368ea1ae0771c80ff
Andrey Tykhonov <atykhonov@mirantis.com>
2015-10-19 16:30:19 +0300

    Validate nodegroup name on creation and updating
    Prevent duplicated nodegroup names for the same environment.
    
    Change-Id: I360540d6407c28a59cc1fb6a0fba5ede1e12b27b
    Closes-bug: #1420658

--- nailgun/nailgun/api/v1/validators/node_group.py
+++ nailgun/nailgun/api/v1/validators/node_group.py
@@ -15,6 +15,7 @@
 
 from nailgun.api.v1.validators.base import BasicValidator
 from nailgun import consts
+from nailgun.db import db
 from nailgun.errors import errors
 from nailgun import objects
 
@@ -26,10 +27,42 @@ class NodeGroupValidator(BasicValidator):
     single_schema = node_group.single_schema
 
     @classmethod
+    def _validate_unique_name(cls, data, query_updater=None):
+        """Validate node group name to be unique.
+
+        Validate whether node group name is unique for specific
+        environment. Prevent to have duplicated node group names for
+        the some environment.
+
+        Arguments:
+            data (dict): data which contains node group name and cluster_id.
+            query_updater: is a optional function which modifies the query
+                           used in the method for validation.
+        Returns:
+            None
+
+        Raises:
+            errors.NotAllowed: in case of validation fails (duplicated node
+                group name is provided).
+        """
+        nodegroup_query = objects.NodeGroupCollection.filter_by(
+            None, name=data['name'], cluster_id=data['cluster_id'])
+        if query_updater:
+            query_updater(nodegroup_query)
+        nodegroup_exists = db.query(nodegroup_query.exists()).scalar()
+        if nodegroup_exists:
+            raise errors.NotAllowed(
+                "Node group '{0}' already exists "
+                "in environment {1}.".format(
+                    data['name'], data['cluster_id']))
+
+    @classmethod
     def validate(cls, data):
         data = cls.validate_json(data)
         cluster = objects.Cluster.get_by_uid(
             data['cluster_id'], fail_if_not_found=True)
+
+        cls._validate_unique_name(data)
 
         if cluster.net_provider == consts.CLUSTER_NET_PROVIDERS.nova_network:
             raise errors.NotAllowed(
@@ -47,5 +80,12 @@ class NodeGroupValidator(BasicValidator):
             )
 
     @classmethod
-    def validate_update(cls, data, **kwargs):
-        return cls.validate_json(data)
+    def validate_update(cls, data, instance=None):
+        data = cls.validate_json(data)
+
+        if instance:
+            filter_itself = lambda q: q.filter(
+                objects.NodeGroup.model.id != instance.id)
+            cls._validate_unique_name(data, filter_itself)
+
+        return data

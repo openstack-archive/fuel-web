76e6e5e69223ecdc0f924ec9583396ae2ec269f2
Ivan Kliuk <ivan.kliuk@gmail.com>
2015-10-19 19:14:59 +0300

    Bump serializers to 8.0
    * Add 'DeploymentHASerializer80' and 'NeutronNetworkDeploymentSerializer80'.
    * Corresponding unit tests are updated.
    
    Change-Id: I15f5a895244dc5f841878986fa155c5c46784bbe

--- nailgun/nailgun/orchestrator/deployment_serializers.py
+++ nailgun/nailgun/orchestrator/deployment_serializers.py
@@ -49,6 +49,8 @@ from nailgun.orchestrator.neutron_serializers import \
 from nailgun.orchestrator.neutron_serializers import \
     NeutronNetworkDeploymentSerializer70
 from nailgun.orchestrator.neutron_serializers import \
+    NeutronNetworkDeploymentSerializer80
+from nailgun.orchestrator.neutron_serializers import \
     NeutronNetworkTemplateSerializer70
 from nailgun.orchestrator.nova_serializers import \
     NovaNetworkDeploymentSerializer
@@ -506,6 +508,10 @@ class DeploymentHASerializer70(DeploymentHASerializer61):
         return {}
 
 
+class DeploymentHASerializer80(DeploymentHASerializer70):
+    pass
+
+
 def get_serializer_for_cluster(cluster):
     """Returns a serializer depends on a given `cluster`.
 
@@ -532,6 +538,9 @@ def get_serializer_for_cluster(cluster):
         '7.0': {
             # Multinode is not supported anymore
             'ha': DeploymentHASerializer70,
+        },
+        '8.0': {
+            'ha': DeploymentHASerializer80,
         }
     }
 

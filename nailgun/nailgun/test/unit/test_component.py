d56be9382fbb3f635849a96501f3309f2b0b5e3c
Andriy Popovych <popovych.andrey@gmail.com>
2015-10-20 16:45:03 +0300

    Component API
    Provide handler for component API and all releated logic.
    Implement serialization format for components.
    
    Change-Id: I11ae12f193f64592692fea0663b481f93dc6d665
    Implements: blueprint component-registry

---
+++ nailgun/nailgun/test/unit/test_component.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nailgun import consts
+from nailgun.objects import ComponentCollection
+from nailgun.objects.serializers.component import ComponentSerializer
+from nailgun.test import base
+
+
+class BaseComponentTestCase(base.BaseTestCase):
+
+    def setUp(self):
+        super(BaseComponentTestCase, self).setUp()
+        self.release = self.env.create_release(
+            version='2015.1-8.0',
+            operating_system='Ubuntu',
+            modes=[consts.CLUSTER_MODES.ha_compact])
+        self.plugin = self.env.create_plugin(
+            name='compatible_plugin',
+            fuel_version=['8.0'],
+            releases=[{
+                'repository_path': 'repositories/ubuntu',
+                'version': '2015.1-8.0',
+                'os': 'ubuntu',
+                'mode': ['ha'],
+                'deployment_scripts_path': 'deployment_scripts/'}])
+        self.core_component = self.env.create_component(
+            release=self.release,
+            name='test_component_1')
+        self.plugin_component = self.env.create_component(
+            plugin=self.plugin,
+            name='test_component_2',
+            type='additional_service')
+
+
+class TestComponentCollection(BaseComponentTestCase):
+
+    def test_get_all_by_release(self):
+        self.incompatible_plugin = self.env.create_plugin()
+        self.env.create_component(
+            plugin=self.incompatible_plugin,
+            name='incompatible_component')
+        components = ComponentCollection.get_all_by_release(self.release.id)
+        for component in components:
+            self.assertIn(
+                component.name, ['test_component_1', 'test_component_2'])
+            self.assertNotIn(component.name, ['incompatible_component'])
+        self.assertEqual(len(components), 2)
+
+
+class TestComponentSerializer(BaseComponentTestCase):
+
+    def test_core_component_serialization(self):
+        release_id = self.release.id
+        component_data = ComponentSerializer.serialize(
+            self.core_component)
+        self.assertDictEqual(component_data, {
+            'name': 'test_component_1',
+            'type': 'hypervisor',
+            'compatible': {
+                'additional_services': [],
+                'networks': [],
+                'storages': [],
+                'hypervisors': []
+            },
+            'plugin_id': None,
+            'releases_ids': [release_id]})
+
+    def test_plugin_component_serialization(self):
+        plugin_id = self.plugin.id
+        component_data = ComponentSerializer.serialize(
+            self.plugin_component)
+        self.assertDictEqual(component_data, {
+            'name': 'test_component_2',
+            'type': 'additional_service',
+            'compatible': {
+                'additional_services': [],
+                'networks': [],
+                'storages': [],
+                'hypervisors': []
+            },
+            'plugin_id': plugin_id,
+            'releases_ids': []})

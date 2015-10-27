6e438cfe75542d8ed18341747004b9acb5c9c0b2
Ryan Moe <rmoe@mirantis.com>
2015-10-27 14:54:43 -0700

    Move all db queries from network manager to objects
    Change-Id: I4ce965f227c54577659e64f598ff5cdf4c868ed6

--- nailgun/nailgun/objects/cluster.py
+++ nailgun/nailgun/objects/cluster.py
@@ -170,7 +170,6 @@ class Cluster(NailgunObject):
             errors.OutOfVLANs,
             errors.OutOfIPs,
             errors.NoSuitableCIDR,
-            errors.InvalidNetworkPool
         ) as exc:
             db().delete(new_cluster)
             raise errors.CannotCreate(exc.message)
@@ -1035,6 +1034,21 @@ class Cluster(NailgunObject):
                    consts.CLUSTER_STATUSES.operational,
                    consts.CLUSTER_STATUSES.error]
         return instance.status not in allowed
+
+    @classmethod
+    def has_bonds(cls, cluster_id):
+        return db().query(models.Node).filter(
+            models.Node.cluster_id == cluster_id
+        ).filter(
+            models.Node.bond_interfaces.any()).count() > 0
+
+    @classmethod
+    def get_single_controller(cls, cluster_id):
+        return db().query(models.Node).filter_by(
+            cluster_id=cluster_id
+        ).filter(
+            models.Node.roles.any('controller')
+        ).first()
 
 
 class ClusterCollection(NailgunCollection):

ad7b1ac8c61f4b9ced35959f4a52e7907e357db9
Ryan Moe <rmoe@mirantis.com>
2015-10-27 14:54:43 -0700

    Move all db queries from network manager to objects
    All network-related database queries are moved into the appropriate
    object methods. This is being done to make it possible to have an external
    network management service.
    
    Change-Id: I4ce965f227c54577659e64f598ff5cdf4c868ed6

--- nailgun/nailgun/objects/cluster.py
+++ nailgun/nailgun/objects/cluster.py
@@ -1335,6 +1335,22 @@ class Cluster(NailgunObject):
         attrs = cls.get_editable_attributes(instance, False)
         return attrs['common'].get('task_deploy', {}).get('value')
 
+    @classmethod
+    def get_network_groups_by_node_group(cls, instance_id):
+        query = (db().query(
+            models.Node.id,
+            models.NetworkGroup.id,
+            models.NetworkGroup.name,
+            models.NetworkGroup.meta)
+            .join(models.NodeGroup.nodes)
+            .join(models.NodeGroup.networks)
+            .filter(models.NodeGroup.cluster_id == instance_id,
+                    models.NetworkGroup.name != consts.NETWORKS.fuelweb_admin)
+            .order_by(models.NetworkGroup.id)
+        )
+
+        return query
+
 
 class ClusterCollection(NailgunCollection):
     """Cluster collection."""

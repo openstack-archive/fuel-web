33f90f727a7e01441a957ee376a8498425b4dfb7
Ryan Moe <rmoe@mirantis.com>
2015-10-27 14:54:43 -0700

    Move all db queries from network manager to objects
    All network-related database queries are moved into the appropriate
    object methods. This is being done to make it possible to have an external
    network management service.
    
    Blueprint: external-network-config
    Change-Id: I4ce965f227c54577659e64f598ff5cdf4c868ed6

---
+++ nailgun/nailgun/objects/bond.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+from nailgun.db import db
+from nailgun.db.sqlalchemy import models
+from nailgun.objects import NailgunCollection
+from nailgun.objects import NailgunObject
+from nailgun.objects.serializers.base import BasicSerializer
+
+
+class Bond(NailgunObject):
+
+    model = models.NodeBondInterface
+    serializer = BasicSerializer
+
+    @classmethod
+    def assign_networks(cls, instance, networks):
+        """Assigns networks to specified Bond interface.
+
+        :param instance: Bond object
+        :type instance: Bond model
+        :param networks: List of networks to assign
+        :type networks: list
+        :returns: None
+        """
+        for net in networks:
+            net_assignment = models.NetworkBondAssignment()
+            net_assignment.network_id = net['id']
+            net_assignment.bond_id = instance.id
+            db().add(net_assignment)
+        db().commit()
+
+    @classmethod
+    def update(cls, instance, data):
+        """Update existing Bond with specified parameters
+
+        :param instance: object (model) instance
+        :param data: dictionary of key-value pairs as object fields
+        :returns: instance of an object (model)
+        """
+        instance.update(data)
+        instance.offloading_modes = data.get('offloading_modes', {})
+        db().add(instance)
+        db().commit()
+        return instance
+
+
+class BondCollection(NailgunCollection):
+
+    single = Bond

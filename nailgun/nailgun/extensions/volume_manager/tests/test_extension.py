af72eff4482a2603e8232308b043d312bcae915f
Dmitry Stepanenko <dstepanenko@mirantis.com>
2015-11-02 17:54:51 +0300

    Resetting environment shouldn't reset user defined disk layout
    Fix cluster reset mechanism to not change disk partitions.
    Prohibited resetting of disks if node is not in discover state.
    
    DocImpact
    Change-Id: I9db43d0f0fa54ac4f4d626552b0553ee7a02ca69
    Closes-Bug: #1423328

--- nailgun/nailgun/extensions/volume_manager/tests/test_extension.py
+++ nailgun/nailgun/extensions/volume_manager/tests/test_extension.py
@@ -15,21 +15,42 @@
 #    under the License.
 
 import mock
+from nailgun import consts
 from nailgun.test.base import BaseTestCase
 
 from ..extension import VolumeManagerExtension
 from ..objects.volumes import VolumeObject
 
 
-@mock.patch.object(VolumeObject, 'delete_by_node_ids')
 class TestExtension(BaseTestCase):
 
+    @mock.patch.object(VolumeObject, 'delete_by_node_ids')
     def test_on_node_delete(self, obj_call_mock):
         node_mock = mock.MagicMock(id=1)
         VolumeManagerExtension.on_node_delete(node_mock)
         obj_call_mock.assert_called_once_with([1])
 
+    @mock.patch.object(VolumeObject, 'delete_by_node_ids')
     def test_on_node_collection_delete(self, obj_call_mock):
         ids = [1, 2, 3]
         VolumeManagerExtension.on_node_collection_delete(ids)
         obj_call_mock.assert_called_once_with(ids)
+
+    @mock.patch.object(VolumeManagerExtension, 'set_default_node_volumes')
+    def test_on_node_update(self, mock_set_default_node_volumes):
+        ext = VolumeManagerExtension()
+        should_reset_env_statuses = set([consts.NODE_STATUSES.discover])
+        should_not_reset_env_statuses = \
+            set(consts.NODE_STATUSES) - should_reset_env_statuses
+
+        for status in should_reset_env_statuses:
+            node = mock.MagicMock(status=status)
+            mock_set_default_node_volumes.reset_mock()
+            ext.on_node_update(node)
+            ext.set_default_node_volumes.assert_called_once_with(node)
+
+        for status in should_not_reset_env_statuses:
+            node = mock.MagicMock(status=status)
+            mock_set_default_node_volumes.reset_mock()
+            ext.on_node_update(node)
+            self.assertFalse(ext.set_default_node_volumes.called)

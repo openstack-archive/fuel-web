ce36655be600e73d49b3932a633a0c417dc21f3f
Maksim Malchuk <mmalchuk@mirantis.com>
2015-11-03 15:30:30 +0300

    Controlled logging from nailgun
    This commit adds functionality to control logging from nailgun
    by APP_LOGLEVEL value in the '/etc/nailgun/settings.yaml'
    
    Also it reduces log level of notices indicating hardware_info_locked
    when trying to fetch metadata to debug because they are much
    too frequent.
    
    Change-Id: I9d2ff84df1d663be363d2f1f7dc7d5e141bfdf27
    Closes-Bug: #1510151

--- nailgun/nailgun/settings.py
+++ nailgun/nailgun/settings.py
@@ -14,8 +14,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import logging
 import os
-
 import yaml
 
 from nailgun.logger import logger
@@ -65,6 +65,10 @@ class NailgunSettings(object):
             })
             logger.info("Static dir is %s" % self.config.get("STATIC_DIR"))
             logger.info("Template dir is %s" % self.config.get("TEMPLATE_DIR"))
+
+        loglevel = self.config.get("APP_LOGLEVEL")
+        if isinstance(loglevel, basestring):
+            logger.setLevel(getattr(logging, loglevel.upper()))
 
     def update(self, dct):
         self.config.update(dct)

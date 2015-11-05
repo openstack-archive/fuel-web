c56671829c449cb141f8ffcab51359105321df7c
Alexander Rozdymakha <arozdymakha@mirantis.com>
2015-11-05 18:01:01 +0200

    Added support of regexp patterns for groups in deployment_tasks.yaml
    Regular expressions (perl format) should be used to specify wildcard.
    '/' used on the end of group name to distinguish regexp from simple string.
    
    groups: '/.*/' # any group
    groups: '/^(?!controller$)/' # all groups except controller
    groups: '/(?=controller)(?=^((?!^primary).)*$)/' #all controller,
            except primary-controller
    groups: '/mongo|compute/' # mongo or compute
    
    DocImpact
    Closes-Bug: #1480930
    
    Change-Id: I1f279fbd585775fc7b5af1ecbbaa24be21d831c9

--- nailgun/nailgun/utils/role_resolver.py
+++ nailgun/nailgun/utils/role_resolver.py
@@ -16,64 +16,13 @@
 
 import abc
 from collections import defaultdict
-import re
 
 import six
 
 from nailgun import consts
 from nailgun.logger import logger
 from nailgun import objects
-
-
-@six.add_metaclass(abc.ABCMeta)
-class NameMatchPolicy(object):
-    @abc.abstractmethod
-    def match(self, name):
-        """Tests that name is acceptable.
-
-        :param name: the name to test
-        :type name: str
-        :returns: True if yes otherwise False
-        """
-
-    @staticmethod
-    def create(pattern):
-        """Makes name match policy.
-
-        the string wrapped with '/' treats as pattern
-        '/abc/' - pattern
-        'abc' - the string for exact match
-
-        :param pattern: the pattern to match
-        :return: the NameMatchPolicy instance
-        """
-        if pattern.startswith("/") and pattern.endswith("/"):
-            return PatternMatch(pattern[1:-1])
-        return ExactMatch(pattern)
-
-
-class ExactMatch(NameMatchPolicy):
-    """Tests that name exact match to argument."""
-
-    def __init__(self, name):
-        """Initializes.
-
-        :param name: the name to match
-        """
-        self.name = name
-
-    def match(self, name):
-        return self.name == name
-
-
-class PatternMatch(NameMatchPolicy):
-    """Tests that pattern matches to argument."""
-
-    def __init__(self, patten):
-        self.pattern = re.compile(patten)
-
-    def match(self, name):
-        return self.pattern.match(name)
+from nailgun.policy.name_match import NameMatchingPolicy
 
 
 @six.add_metaclass(abc.ABCMeta)
@@ -140,7 +89,7 @@ class RoleResolver(BaseRoleResolver):
         elif isinstance(roles, (list, tuple)):
             result = set()
             for role in roles:
-                pattern = NameMatchPolicy.create(role)
+                pattern = NameMatchingPolicy.create(role)
                 for node_role, nodes_ids in six.iteritems(self.__mapping):
                     if pattern.match(node_role):
                         result.update(nodes_ids)

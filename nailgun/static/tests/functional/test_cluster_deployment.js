3450d989586be6f1fc733a2e7e048e5d854637d7
Julia Aranovich <jkirnosova@mirantis.com>
2015-11-05 19:51:32 +0300

    Separate provisioning in Fuel UI
    Related to blueprint allow-choosing-nodes-for-provisioning-and-deployment
    
    Change-Id: Idf2a26935352e8f7a5fac402c3a93a8d461f9434

--- nailgun/static/tests/functional/test_cluster_deployment.js
+++ nailgun/static/tests/functional/test_cluster_deployment.js
@@ -60,12 +60,52 @@ define([
                 return this.remote
                     .assertElementNotExists(dashboardPage.deployButtonSelector, 'No deployment should be possible without nodes added')
             },
-            'Discard changes': function() {
+            'Provision nodes': function() {
                 return this.remote
                     .then(function() {
-                        // Adding three controllers
                         return common.addNodesToCluster(1, ['Controller']);
                     })
+                    .then(function() {
+                        return clusterPage.goToTab('Dashboard');
+                    })
+                    .assertElementAppears('.dashboard-tab', 200, 'Dashboard tab opened')
+                    .clickByCssSelector('.deploy-btn-group .dropdown-toggle')
+                    .clickByCssSelector('.btn-provision')
+                    .then(function() {
+                        return modal.waitToOpen();
+                    })
+                    .then(function() {
+                        return modal.checkTitle('Provision Nodes');
+                    })
+                    .then(function() {
+                        return modal.clickFooterButton('Start Provisioning');
+                    })
+                    .then(function() {
+                        return modal.waitToClose();
+                    })
+                    .assertElementAppears('div.deploy-process div.progress', 2000, 'Provisioning started')
+                    .assertElementDisappears('div.deploy-process div.progress', 5000, 'Provisioning finished')
+                    .assertElementContainsText('div.alert-warning strong', 'Success', 'Provisioning successfully finished')
+                    .then(function() {
+                        return clusterPage.isTabLocked('Networks');
+                    })
+                    .then(function(isLocked) {
+                        assert.isFalse(isLocked, 'Networks tab is not locked after nodes were provisioned');
+                    })
+                    .then(function() {
+                        return clusterPage.isTabLocked('Settings');
+                    })
+                    .then(function(isLocked) {
+                        assert.isFalse(isLocked, 'Settings tab is not locked after nodes were provisioned');
+                    })
+                    .then(function() {
+                        return clusterPage.goToTab('Dashboard');
+                    })
+                    .assertElementExists('.deploy-btn', 'Provisioned nodes can be deployed')
+                    .assertElementNotExists('.deploy-btn-group .dropdown-toggle', 'Only deployment available for provisioned nodes');
+            },
+            'Discard changes': function() {
+                return this.remote
                     .then(function() {
                         return clusterPage.goToTab('Dashboard');
                     })

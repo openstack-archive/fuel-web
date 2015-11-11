004dbd79e2d4c67bc7523e8ee0b90fefe750b507
Andrey Shestakov <ashestakov@mirantis.com>
2015-11-11 16:09:16 +0200

    [Ironic] Add transformations for Ironic
    This change introduces L2 transformations for baremetal network
    and L3 neutron network.
    
    Change-Id: I565c703dca6cedaa74f0bf426d82ecde79257ecb
    Implements: blueprint fuel-integrate-ironic (partially)

--- nailgun/nailgun/test/integration/test_orchestrator_serializer_80.py
+++ nailgun/nailgun/test/integration/test_orchestrator_serializer_80.py
@@ -18,6 +18,7 @@ from nailgun import consts
 from nailgun.db.sqlalchemy import models
 from nailgun import objects
 
+from nailgun.orchestrator.deployment_graph import AstuteGraph
 from nailgun.orchestrator.deployment_serializers import \
     get_serializer_for_cluster
 from nailgun.orchestrator.neutron_serializers import \
@@ -60,6 +61,71 @@ class TestSerializer80Mixin(object):
 
     def prepare_for_deployment(self, nodes, *_):
         objects.NodeCollection.prepare_for_deployment(nodes)
+
+
+class TestDeploymentAttributesSerialization80(BaseDeploymentSerializer):
+    env_version = '2015.1.0-8.0'
+
+    def setUp(self):
+        super(TestDeploymentAttributesSerialization80, self).setUp()
+        self.cluster = self.env.create(
+            release_kwargs={'version': self.env_version},
+            cluster_kwargs={
+                'mode': consts.CLUSTER_MODES.ha_compact,
+                'net_provider': consts.CLUSTER_NET_PROVIDERS.neutron,
+                'net_segment_type': consts.NEUTRON_SEGMENT_TYPES.vlan})
+        self.cluster_db = self.db.query(models.Cluster).get(self.cluster['id'])
+        serializer_type = get_serializer_for_cluster(self.cluster_db)
+        self.serializer = serializer_type(AstuteGraph(self.cluster_db))
+
+    def test_baremetal_transformations(self):
+        self.env._set_additional_component(self.cluster_db, 'ironic', True)
+        self.env.create_node(cluster_id=self.cluster_db.id,
+                             roles=['primary-controller'])
+        self.prepare_for_deployment(self.env.nodes)
+        serialized_for_astute = self.serializer.serialize(
+            self.cluster_db, self.cluster_db.nodes)
+        for node in serialized_for_astute:
+            transformations = node['network_scheme']['transformations']
+            baremetal_brs = filter(lambda t: t.get('name') ==
+                                   consts.DEFAULT_BRIDGES_NAMES.br_baremetal,
+                                   transformations)
+            baremetal_ports = filter(lambda t: t.get('name') == "eth0.104",
+                                     transformations)
+            expected_patch = {
+                'action': 'add-patch',
+                'bridges': [consts.DEFAULT_BRIDGES_NAMES.br_ironic,
+                            consts.DEFAULT_BRIDGES_NAMES.br_baremetal],
+                'provider': 'ovs'}
+            self.assertEqual(len(baremetal_brs), 1)
+            self.assertEqual(len(baremetal_ports), 1)
+            self.assertEqual(baremetal_ports[0]['bridge'],
+                             consts.DEFAULT_BRIDGES_NAMES.br_baremetal)
+            self.assertIn(expected_patch, transformations)
+
+    def test_baremetal_neutron_attrs(self):
+        self.env._set_additional_component(self.cluster_db, 'ironic', True)
+        self.env.create_node(cluster_id=self.cluster_db.id,
+                             roles=['primary-controller'])
+        self.prepare_for_deployment(self.env.nodes)
+        serialized_for_astute = self.serializer.serialize(
+            self.cluster_db, self.cluster_db.nodes)
+        for node in serialized_for_astute:
+            expected_network = {
+                "network_type": "flat",
+                "segment_id": None,
+                "router_ext": False,
+                "physnet": "physnet-ironic"
+            }
+            self.assertEqual(expected_network,
+                node['quantum_settings']['predefined_networks'][
+                    'baremetal']['L2']
+            )
+            self.assertIn("physnet-ironic",
+                          node['quantum_settings']['L2']['phys_nets'])
+            self.assertEqual(consts.DEFAULT_BRIDGES_NAMES.br_ironic,
+                             (node['quantum_settings']['L2']['phys_nets']
+                              ["physnet-ironic"]["bridge"]))
 
 
 class TestSerializeInterfaceDriversData80(

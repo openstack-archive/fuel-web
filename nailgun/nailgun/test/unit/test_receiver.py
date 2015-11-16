a4993f05d03f0822ee3047c90b7f506b6f76418d
Valentin Kaplov <vkaplov@mirantis.com>
2015-11-16 20:27:47 +0300

    Added provision notifications
    Added notification of provisioning results. Success and errors.
    
    Change-Id: Ia500a9c2c36d146d0ba9dba008502801fccd9ca3
    Partial-Bug: 1515891

--- nailgun/nailgun/test/unit/test_receiver.py
+++ nailgun/nailgun/test/unit/test_receiver.py
@@ -86,7 +86,10 @@ class TestNailgunReceiver(base.BaseTestCase):
         mnotify.assert_called_with(
             task_resp['status'],
             u'Deployment has failed. Method granular_deploy.',
-            self.cluster.id)
+            self.cluster.id,
+            task_uuid=None,
+            node_id=None
+        )
 
     @patch('nailgun.objects.Task.update_verify_networks')
     def test_check_repositories_resp_success(self, update_verify_networks):
@@ -154,3 +157,85 @@ class TestNailgunReceiver(base.BaseTestCase):
             self.db.flush()
             NailgunReceiver.task_in_orchestrator(**resp)
             self.assertEqual(status, self.task.status)
+
+    @patch('nailgun.rpc.receiver.notifier.notify')
+    def test_notify_provision(self, notify_checker):
+        NailgunReceiver._notify(
+            self.task,
+            "done",
+            "Test error.",
+            "123",
+            self.task.uuid
+        )
+        notify_checker.assert_called_with(
+            "done",
+            u'Test error.',
+            self.task.cluster_id,
+            node_id="123",
+            task_uuid=self.task.uuid
+        )
+
+    @patch('nailgun.rpc.receiver.notifier.notify')
+    def test_notify_provision_sub_task(self, notify_checker):
+        sub_task = self.env.create_task(
+            name=consts.TASK_NAMES.provision,
+            status=consts.TASK_STATUSES.ready,
+            cluster_id=self.cluster.id,
+            parent_id=self.task.id
+        )
+        NailgunReceiver._notify(
+            sub_task,
+            "done",
+            "Test error.",
+            "123",
+            sub_task.uuid
+        )
+        self.assertRaises(
+            AssertionError,
+            notify_checker.assert_called_with,
+            "done",
+            u'Test error.',
+            sub_task.cluster_id,
+            node_id="123",
+            task_uuid=sub_task.uuid
+        )
+
+    @patch('nailgun.rpc.receiver.notifier.notify')
+    def test_notify_deployment(self, notify_checker):
+        NailgunReceiver._notify(
+            self.task,
+            "done",
+            "Test error.",
+            "123",
+            self.task.uuid
+        )
+        notify_checker.assert_called_with(
+            "done",
+            u'Test error.',
+            self.task.cluster_id,
+            node_id="123",
+            task_uuid=self.task.uuid
+        )
+
+    @patch('nailgun.rpc.receiver.notifier.notify')
+    def test_notify_deployment_sub_task(self, notify_checker):
+        sub_task = self.env.create_task(
+            name=consts.TASK_NAMES.deployment,
+            status=consts.TASK_STATUSES.ready,
+            cluster_id=self.cluster.id,
+            parent_id=self.task.id
+        )
+        NailgunReceiver._notify(
+            sub_task,
+            "done",
+            "Test error.",
+            "123",
+            sub_task.uuid
+        )
+        notify_checker.assert_called_with(
+            "done",
+            u'Test error.',
+            sub_task.cluster_id,
+            node_id="123",
+            task_uuid=sub_task.uuid
+        )

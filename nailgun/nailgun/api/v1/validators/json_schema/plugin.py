a00a7403039a30d08e669f2632cb6f2c68df75e6
Bulat Gaifullin <gaifullinbf@gmail.com>
2015-11-23 21:28:35 +0300

    Allow to install plugin to operational environment
    - Added attribute 'is_runtime' to plugin. this attribute means
      that plugin can be installed to operational env.
    - Make attribute 'is_locked' public and remove logic to calculate
      the cluster is locked or not from UI.
    
    Change-Id: I372bfb2c502bcd5927533b03aea1557cf49d9afb
    Closes-Bug: #1519050

--- nailgun/nailgun/api/v1/validators/json_schema/plugin.py
+++ nailgun/nailgun/api/v1/validators/json_schema/plugin.py
@@ -50,7 +50,8 @@ PLUGIN_SCHEMA = {
         'homepage': {'type': 'string'},
         'releases': {
             'type': 'array',
-            'items': PLUGIN_RELEASE_SCHEMA}
+            'items': PLUGIN_RELEASE_SCHEMA},
+        'hotplug': {"type": "boolean"},
     },
     'required': [
         'name',
--- nailgun/nailgun/test/base.py
+++ nailgun/nailgun/test/base.py
@@ -493,7 +493,7 @@ class EnvironmentManager(object):
         resp = self.neutron_networks_put(cluster_id, netconfig)
         return resp
 
-    def create_plugin(self, api=False, cluster=None, **kwargs):
+    def create_plugin(self, api=False, cluster=None, enabled=True, **kwargs):
         plugin_data = self.get_default_plugin_metadata(**kwargs)
 
         if api:
@@ -512,7 +512,9 @@ class EnvironmentManager(object):
         # Enable plugin for specific cluster
         if cluster:
             cluster.plugins.append(plugin)
-            ClusterPlugins.set_attributes(cluster.id, plugin.id, enabled=True)
+            ClusterPlugins.set_attributes(
+                cluster.id, plugin.id, enabled=enabled
+            )
         return plugin
 
     def create_cluster_plugin_link(self, **kwargs):
@@ -767,6 +769,7 @@ class EnvironmentManager(object):
             'licenses': ['License 1'],
             'authors': ['Author1'],
             'homepage': 'http://some-plugin-url.com/',
+            'hotplug': False,
             'releases': [
                 {'repository_path': 'repositories/ubuntu',
                  'version': '2014.2-6.0', 'os': 'ubuntu',

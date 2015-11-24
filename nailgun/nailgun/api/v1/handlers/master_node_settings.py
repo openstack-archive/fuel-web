b251951f54fe997bf49bea367cbe2a98a640edf3
Julia Aranovich <jkirnosova@mirantis.com>
2015-11-24 16:50:50 +0300

    Store settings of Fuel UI Nodes page in DB
    "settings" attribute of "master_node_settings" DB model
    extended with "ui_settings" property to store
    node list configuration from Nodes page in Fuel UI
    
    Related to blueprint view-all-nodes
    
    Co-Authored-By: Alexander Kislitsky <akislitsky@mirantis.com>
    
    Change-Id: I4900c88a02877c4d621e8e835c9aa97097c01fb4

--- nailgun/nailgun/api/v1/handlers/master_node_settings.py
+++ nailgun/nailgun/api/v1/handlers/master_node_settings.py
@@ -12,6 +12,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from oslo_serialization import jsonutils
+
 from nailgun.api.v1.handlers.base import content
 from nailgun.api.v1.handlers.base import DBSingletonHandler
 from nailgun.api.v1.validators.master_node_settings \
@@ -30,28 +32,51 @@ class MasterNodeSettingsHandler(DBSingletonHandler):
 
     not_found_error = "Settings are not found in DB"
 
-    def _handle_stats_opt_in(self):
+    def _handle_stats_opt_in(self, settings_data=None):
+        """Creates task on stats user creation or removal
 
-        if self.single.must_send_stats():
+        :param settings_data: dict with master node settings.
+        Current data from DB will be used if master_node_settings_data is None
+        """
+        must_send = self.single.must_send_stats(
+            master_node_settings_data=settings_data)
+        if must_send:
             logger.debug("Handling customer opt-in to sending statistics")
             manager = CreateStatsUserTaskManager()
         else:
             logger.debug("Handling customer opt-out to sending statistics")
             manager = RemoveStatsUserTaskManager()
-
         try:
             manager.execute()
         except Exception:
             logger.exception("Stats user operation failed")
 
-    @content
-    def PUT(self):
-        result = super(MasterNodeSettingsHandler, self).PUT()
-        self._handle_stats_opt_in()
+    def _get_new_opt_in_status(self):
+        """Extracts opt in status from request
+
+        Returns None if no opt in status in the request
+
+        :return: bool or None
+        """
+        data = self.checked_data(self.validator.validate_update)
+        return data.get('settings', {}).get('statistics', {}).\
+            get('send_anonymous_statistic', {}).get('value')
+
+    def _perform_update(self, http_method):
+        old_opt_in = self.single.must_send_stats()
+        new_opt_in = self._get_new_opt_in_status()
+        result = http_method()
+
+        if new_opt_in is not None and old_opt_in != new_opt_in:
+            self._handle_stats_opt_in(settings_data=jsonutils.loads(result))
         return result
 
     @content
+    def PUT(self):
+        return self._perform_update(
+            super(MasterNodeSettingsHandler, self).PUT)
+
+    @content
     def PATCH(self):
-        result = super(MasterNodeSettingsHandler, self).PATCH()
-        self._handle_stats_opt_in()
-        return result
+        return self._perform_update(
+            super(MasterNodeSettingsHandler, self).PATCH)

393e8efa7ade71546041c33d0f042ccd2f4fe8d6
Ivan Kliuk <ivan.kliuk@gmail.com>
2015-11-24 10:58:41 +0200

    [WIP] VIP allocation functionality in the database
    * Rename 'vip_type' field to 'vip_info' of table 'ip_addrs'.
    * Change type 'vip_type' JSON.
    * Add database migrations.
    * Add unit tests for migrations.
    * Fix unit tests of network manager module.
    
    Change-Id: I0e7bbf2c27b8d85e89a225e719bda5f0d273ca08
    Partial-bug: #1482399

--- nailgun/nailgun/objects/cluster.py
+++ nailgun/nailgun/objects/cluster.py
@@ -1209,6 +1209,26 @@ class Cluster(NailgunObject):
             instance.nodes if nodes is None else nodes
         )
 
+    @classmethod
+    def get_vip_info(cls, instance, vip_name):
+        """Returns VIP info by given 'vip_name'
+
+        :param instance: Cluster instance
+        :type instance: Cluster DB instance
+        :param vip_name: VIP name
+        :type vip_name: basestring
+        :returns: VIP information collected into dict object
+        """
+        for meta in cls.get_network_roles(instance):
+            if meta['properties']['vip']:
+                for vip in meta['properties']['vip']:
+                    if vip['name'] == vip_name:
+                        vip.update({
+                            'network_role': meta['id'],
+                            'manual': False
+                        })
+                        return vip
+
 
 class ClusterCollection(NailgunCollection):
     """Cluster collection."""

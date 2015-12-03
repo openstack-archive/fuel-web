88d721efcf5ff6902026067293f35056215543f0
Alexander Kislitsky <akislitsky@mirantis.com>
2015-12-03 19:57:42 +0300

    Performance of network manager operation improved
    We have extra SQLs generated in the NetworkManager when passing
    node_id instead already loaded SQLAlchemy node object.
    We load nodes for processing eager and pass SQLAchemy objects
    instead node ids for performance boost.
    Bulk insert used in IPs assiging process.
    
    Change-Id: I0518a5879c775d568de5652dbdd856a0cede80ce
    Closes-Bug: #1498365

--- nailgun/nailgun/test/performance/unit/test_cluster_operations.py
+++ nailgun/nailgun/test/performance/unit/test_cluster_operations.py
@@ -225,7 +225,7 @@ class ClusterOperationsLoadTest(base.BaseUnitLoadTestCase):
             [],
             handler_kwargs={'cluster_id': self.cluster['id']}
         )
-        self.check_time_exec(func, 10)
+        self.check_time_exec(func)
 
     @fake_tasks()
     @base.evaluate_unit_performance

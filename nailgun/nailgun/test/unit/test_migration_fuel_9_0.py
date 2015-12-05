e19a9a8902a05485b2f45820468297731a3fb8fb
Ivan Kliuk <ivan.kliuk@gmail.com>
2015-12-05 17:16:38 +0200

    New VIP-related fields in the database
    * Add 'is_user_defined' field to 'ip_addrs' table.
    * Rename 'vip_type' field to 'vip_name' of table 'ip_addrs'.
    * Add 'vip_namespace' field to 'ip_addrs' table.
    * Copy vip namespaces from plugin table network roles to 'vip_namespace' field
      according to the unique vip name.
    * Add database migrations.
    * Add unit test for migrations.
    
    Change-Id: Ia3e1d7f6e08dbebcb182de75eeaf58ddf6be4a8d
    Partial-bug: #1482399

--- nailgun/nailgun/test/unit/test_migration_fuel_9_0.py
+++ nailgun/nailgun/test/unit/test_migration_fuel_9_0.py
@@ -12,5 +12,232 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import alembic
+from oslo_serialization import jsonutils
+import sqlalchemy as sa
+
+from nailgun.db import db
+from nailgun.db import dropdb
+from nailgun.db.migration import ALEMBIC_CONFIG
+from nailgun.test import base
+
 _prepare_revision = '43b2cb64dae6'
 _test_revision = '11a9adc6d36a'
+
+
+def setup_module():
+    dropdb()
+    alembic.command.upgrade(ALEMBIC_CONFIG, _prepare_revision)
+    prepare()
+    alembic.command.upgrade(ALEMBIC_CONFIG, _test_revision)
+
+
+def prepare():
+    meta = base.reflect_db_metadata()
+
+    result = db.execute(
+        meta.tables['releases'].insert(),
+        [{
+            'name': 'test_name',
+            'version': '2015.1-8.0',
+            'operating_system': 'ubuntu',
+            'state': 'available',
+            'networks_metadata': jsonutils.dumps({
+                'neutron': {
+                    'networks': [
+                        {
+                            'assign_vip': True,
+                        },
+                    ]
+                },
+                'nova_network': {
+                    'networks': [
+                        {
+                            'assign_vip': False,
+                        },
+                    ]
+                },
+
+            }),
+            'network_roles_metadata': jsonutils.dumps([{
+                'id': 'admin/vip',
+                'default_mapping': 'fuelweb_admin',
+                'properties': {
+                    'subnet': True,
+                    'gateway': False,
+                    'vip': [
+                        {
+                            'name': 'release-vip1',
+                            'ip_addr': '192.168.42.42'
+                        },
+                        {
+                            'name': 'release-vip2',
+                            'ip_addr': '192.168.42.42',
+                            'namespace': 'release-vip2-namespace'
+                        },
+                        {
+                            'name': 'no-ip-addr-vip'
+                        }
+                    ]
+                }
+            }]),
+            'is_deployable': True,
+        }])
+    releaseid = result.inserted_primary_key[0]
+
+    db.execute(
+        meta.tables['clusters'].insert(),
+        [{
+            'name': 'test_env',
+            'release_id': releaseid,
+            'mode': 'ha_compact',
+            'status': 'new',
+            'net_provider': 'neutron',
+            'grouping': 'roles',
+            'fuel_version': '8.0',
+        }])
+
+    db.execute(
+        meta.tables['ip_addrs'].insert(),
+        [
+            {
+                'ip_addr': '192.168.0.2',
+                'vip_type': 'management'
+            },
+            {
+                'ip_addr': '192.168.1.2',
+                'vip_type': 'haproxy'
+            },
+            {
+                'ip_addr': '192.168.11.2',
+                'vip_type': 'my-vip1',
+                'namespace': 'my-namespace1'
+            },
+            {
+                'ip_addr': '192.168.12.2',
+                'vip_type': 'my-vip2',
+                'namespace': 'my-namespace2'
+            },
+            {
+                'ip_addr': '192.168.13.2',
+                'vip_type': 'my-vip3',
+                'namespace': 'my-namespace3'
+            },
+            {
+                'ip_addr': '192.168.14.2',
+                'vip_type': 'my-vip4',
+                'namespace': 'my-namespace4'
+            }
+        ])
+
+    db.execute(
+        meta.tables['network_groups'].insert(),
+        [{
+            'name': 'public',
+            'release': releaseid,
+            'meta': jsonutils.dumps({'assign_vip': True})
+        }])
+
+    db.execute(
+        meta.tables['plugins'].insert(),
+        [{
+            'name': 'test_plugin_a',
+            'title': 'Test plugin A',
+            'version': '2.0.0',
+            'description': 'Test plugin A for Fuel',
+            'homepage': 'http://fuel_plugins.test_plugin.com',
+            'package_version': '4.0.0',
+            'groups': jsonutils.dumps(['tgroup']),
+            'authors': jsonutils.dumps(['tauthor']),
+            'licenses': jsonutils.dumps(['tlicense']),
+            'releases': jsonutils.dumps([
+                {'repository_path': 'repositories/ubuntu'}
+            ]),
+            'fuel_version': jsonutils.dumps(['8.0']),
+            'network_roles_metadata': jsonutils.dumps([{
+                'id': 'admin/vip',
+                'default_mapping': 'fuelweb_admin',
+                'properties': {
+                    'subnet': True,
+                    'gateway': False,
+                    'vip': [
+                        {
+                            'name': 'my-vip1',
+                            'namespace': 'my-namespace1',
+                        },
+                        {
+                            'name': 'my-vip2',
+                            'namespace': 'my-namespace2',
+                        }
+                    ]
+                }
+            }])
+        }]
+    )
+
+    db.execute(
+        meta.tables['plugins'].insert(),
+        [{
+            'name': 'test_plugin_b',
+            'title': 'Test plugin B',
+            'version': '2.0.0',
+            'description': 'Test plugin B for Fuel',
+            'homepage': 'http://fuel_plugins.test_plugin.com',
+            'package_version': '4.0.0',
+            'groups': jsonutils.dumps(['tgroup']),
+            'authors': jsonutils.dumps(['tauthor']),
+            'licenses': jsonutils.dumps(['tlicense']),
+            'releases': jsonutils.dumps([
+                {'repository_path': 'repositories/ubuntu'}
+            ]),
+            'fuel_version': jsonutils.dumps(['8.0']),
+            'network_roles_metadata': jsonutils.dumps([{
+                'id': 'admin/vip',
+                'default_mapping': 'fuelweb_admin',
+                'properties': {
+                    'subnet': True,
+                    'gateway': False,
+                    'vip': [
+                        {
+                            'name': 'my-vip3',
+                            'namespace': 'my-namespace3',
+                        },
+                        {
+                            'name': 'my-vip4',
+                            'namespace': 'my-namespace4',
+                        }
+                    ]
+                }
+            }])
+        }]
+    )
+
+    db.commit()
+
+
+class TestVipMigration(base.BaseAlembicMigrationTest):
+    def test_ip_addrs_vip_name_exists(self):
+        result = db.execute(
+            sa.select([self.meta.tables['ip_addrs'].c.vip_name]))
+        self.assertEqual(result.scalar(), "management")
+
+    def test_ip_addrs_vip_namespace_exists(self):
+        result = db.execute(
+            sa.select([
+                self.meta.tables['ip_addrs'].c.vip_name,
+                self.meta.tables['ip_addrs'].c.vip_namespace
+            ]))
+        result = list(result)
+        self.assertItemsEqual(
+            (
+                ('management', None,),
+                ('haproxy', None,),
+                ('my-vip1', 'my-namespace1',),
+                ('my-vip2', 'my-namespace2',),
+                ('my-vip3', 'my-namespace3',),
+                ('my-vip4', 'my-namespace4',),
+                ('release-vip1', None,),
+                ('release-vip2', 'release-vip2-namespace',),
+            ),
+            result
+        )

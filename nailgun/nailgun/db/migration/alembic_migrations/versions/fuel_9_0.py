18625e031df9cf6fc63bd2451320bbf5022006eb
Ivan Kliuk <ivan.kliuk@gmail.com>
2015-12-05 17:16:38 +0200

    New VIP-related fields in the database
    * Add 'is_user_defined' field to 'ip_addrs' table.
    * Rename 'vip_type' field to 'vip_name' of table 'ip_addrs'.
    * Add 'vip_namespace' field to 'ip_addrs' table.
    * Copy vip namespaces from plugin table network roles to 'vip_namespace' field
      according to the unique vip name.
    * Add database migrations.
    * Add unit test for migrations.
    
    Change-Id: Ia3e1d7f6e08dbebcb182de75eeaf58ddf6be4a8d
    Partial-bug: #1482399

--- nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_9_0.py
+++ nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_9_0.py
@@ -20,17 +20,98 @@ Create Date: 2015-12-15 17:20:49.519542
 
 """
 
+from alembic import op
+import sqlalchemy as sa
+
 # revision identifiers, used by Alembic.
+from oslo_serialization import jsonutils
+
 revision = '11a9adc6d36a'
 down_revision = '43b2cb64dae6'
 
-from alembic import op  # noqa
-import sqlalchemy as sa  # noqa
-
 
 def upgrade():
-    pass
+    upgrade_ip_address()
+    copy_vip_namespaces_from_network_roles()
 
 
 def downgrade():
-    pass
+    downgrade_ip_address()
+
+
+def upgrade_ip_address():
+
+    op.add_column(
+        'ip_addrs',
+        sa.Column(
+            'is_user_defined',
+            sa.Boolean,
+            nullable=False,
+            default=False,
+            server_default="false"
+        )
+    )
+
+    op.add_column(
+        'ip_addrs',
+        sa.Column(
+            'vip_namespace',
+            sa.String(length=25),
+            nullable=True,
+            default=None,
+            server_default=None
+        )
+    )
+
+    op.alter_column(
+        'ip_addrs',
+        'vip_type',
+        new_column_name='vip_name'
+    )
+
+
+def copy_vip_namespaces_from_network_roles():
+    connection = op.get_bind()
+
+    plugins_select = sa.text("SELECT network_roles_metadata from plugins")
+    network_roles_metadata = connection.execute(plugins_select)
+
+    vip_name_to_namespace = {}
+
+    for network_roles_json in network_roles_metadata:
+        if not network_roles_json:
+            continue
+        network_roles = jsonutils.loads(network_roles_json[0])
+        for network_role in network_roles:
+            vips = network_role.get('properties', {}).get('vip', [])
+            for vip in vips:
+                vip_name_to_namespace[vip['name']] = vip.get('namespace')
+
+    ip_addrs_select = sa.text("SELECT id, vip_name from ip_addrs")
+    ip_addrs = connection.execute(ip_addrs_select)
+
+    ip_addrs_update = sa.sql.text(
+        "UPDATE ip_addrs SET vip_namespace = :vip_namespace WHERE id = :id")
+
+    for vip_id, vip_name in ip_addrs:
+        vip_namespace = vip_name_to_namespace.get(vip_name)
+        if vip_namespace:
+            connection.execute(
+                ip_addrs_update,
+                id=vip_id,
+                vip_namespace=vip_namespace
+            )
+
+
+def downgrade_ip_address():
+    op.alter_column(
+        'ip_addrs',
+        'vip_name',
+        new_column_name='vip_type',
+        type_=sa.String(length=25),
+        server_default=None,
+        nullable=True
+    )
+
+    op.drop_column('ip_addrs', 'is_user_defined')
+    op.drop_column('ip_addrs', 'vip_namespace')
--- nailgun/nailgun/test/unit/test_migration_fuel_9_0.py
+++ nailgun/nailgun/test/unit/test_migration_fuel_9_0.py
@@ -12,5 +12,269 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import alembic
+from nailgun import consts
+from oslo_serialization import jsonutils
+import sqlalchemy as sa
+
+from nailgun.db import db
+from nailgun.db import dropdb
+from nailgun.db.migration import ALEMBIC_CONFIG
+from nailgun.test import base
+
 _prepare_revision = '43b2cb64dae6'
 _test_revision = '11a9adc6d36a'
+
+
+def setup_module():
+    dropdb()
+    alembic.command.upgrade(ALEMBIC_CONFIG, _prepare_revision)
+    prepare()
+    alembic.command.upgrade(ALEMBIC_CONFIG, _test_revision)
+
+
+def prepare():
+    meta = base.reflect_db_metadata()
+
+    roles_metadata = jsonutils.dumps({
+        "mongo": {
+            "name": "Mongo",
+            "description": "Mongo role"
+        }
+    })
+
+    result = db.execute(
+        meta.tables['releases'].insert(),
+        [{
+            'name': 'test_name',
+            'version': '2015.1-8.0',
+            'operating_system': 'ubuntu',
+            'state': 'available',
+            'roles_metadata': roles_metadata,
+            'attributes_metadata': jsonutils.dumps({
+                'editable': {
+                    'storage': {
+                        'volumes_lvm': {},
+                    },
+                    'common': {},
+                },
+                'generated': {
+                    'cobbler': {'profile': {
+                        'generator_arg': 'ubuntu_1204_x86_64'}}},
+            }),
+            'networks_metadata': jsonutils.dumps({
+                'neutron': {
+                    'networks': [
+                        {
+                            'assign_vip': True,
+                        },
+                    ]
+                },
+                'nova_network': {
+                    'networks': [
+                        {
+                            'assign_vip': False,
+                        },
+                    ]
+                },
+
+            }),
+            'is_deployable': True,
+        }])
+    releaseid = result.inserted_primary_key[0]
+
+    result = db.execute(
+        meta.tables['clusters'].insert(),
+        [{
+            'name': 'test_env',
+            'release_id': releaseid,
+            'mode': 'ha_compact',
+            'status': 'new',
+            'net_provider': 'neutron',
+            'grouping': 'roles',
+            'fuel_version': '8.0',
+        }])
+    cluster_id = result.inserted_primary_key[0]
+
+    db.execute(
+        meta.tables['attributes'].insert(),
+        [{
+            'cluster_id': cluster_id,
+            'editable': '{"common": {}}',
+            'generated': '{"cobbler": {"profile": "ubuntu_1204_x86_64"}}',
+        }])
+
+    db.execute(
+        meta.tables['ip_addrs'].insert(),
+        [
+            {
+                'ip_addr': '192.168.0.2',
+                'vip_type': 'management'
+            },
+            {
+                'ip_addr': '192.168.1.2',
+                'vip_type': 'haproxy'
+            },
+            {
+                'ip_addr': '192.168.11.2',
+                'vip_type': 'my-vip'
+            },
+            {
+                'ip_addr': '192.168.12.2',
+                'vip_type': 'my-vip1'
+            },
+            {
+                'ip_addr': '192.168.13.2',
+                'vip_type': 'my-vip2'
+            }
+        ])
+
+    db.execute(
+        meta.tables['network_groups'].insert(),
+        [{
+            'name': 'public',
+            'release': releaseid,
+            'meta': jsonutils.dumps({'assign_vip': True})
+        }])
+
+    result = db.execute(
+        meta.tables['plugins'].insert(),
+        [{
+            'name': 'test_plugin_a',
+            'title': 'Test plugin A',
+            'version': '2.0.0',
+            'description': 'Test plugin A for Fuel',
+            'homepage': 'http://fuel_plugins.test_plugin.com',
+            'package_version': '4.0.0',
+            'groups': jsonutils.dumps(['tgroup']),
+            'authors': jsonutils.dumps(['tauthor']),
+            'licenses': jsonutils.dumps(['tlicense']),
+            'releases': jsonutils.dumps([
+                {'repository_path': 'repositories/ubuntu'}
+            ]),
+            'fuel_version': jsonutils.dumps(['8.0']),
+            'network_roles_metadata': jsonutils.dumps([{
+                'id': 'admin/vip',
+                'default_mapping': consts.NETWORKS.fuelweb_admin,
+                'properties': {
+                    'subnet': True,
+                    'gateway': False,
+                    'vip': [
+                        {
+                            'name': 'my-vip',
+                            'namespace': 'my-namespace',
+                        },
+                        {
+                            'name': 'my-vip1',
+                            'namespace': 'my-namespace1',
+                        }
+                    ]
+                }
+            }])
+        }]
+    )
+    pluginid_a = result.inserted_primary_key[0]
+
+    result = db.execute(
+        meta.tables['plugins'].insert(),
+        [{
+            'name': 'test_plugin_b',
+            'title': 'Test plugin B',
+            'version': '2.0.0',
+            'description': 'Test plugin B for Fuel',
+            'homepage': 'http://fuel_plugins.test_plugin.com',
+            'package_version': '4.0.0',
+            'groups': jsonutils.dumps(['tgroup']),
+            'authors': jsonutils.dumps(['tauthor']),
+            'licenses': jsonutils.dumps(['tlicense']),
+            'releases': jsonutils.dumps([
+                {'repository_path': 'repositories/ubuntu'}
+            ]),
+            'fuel_version': jsonutils.dumps(['8.0']),
+            'network_roles_metadata': jsonutils.dumps([{
+                'id': 'admin/vip',
+                'default_mapping': consts.NETWORKS.fuelweb_admin,
+                'properties': {
+                    'subnet': True,
+                    'gateway': False,
+                    'vip': [
+                        {
+                            'name': 'my-vip',
+                            'namespace': 'my-namespace',
+                        },
+                        {
+                            'name': 'my-vip2',
+                            'namespace': 'my-namespace2',
+                        }
+                    ]
+                }
+            }])
+        }]
+    )
+    pluginid_b = result.inserted_primary_key[0]
+
+    db.execute(
+        meta.tables['cluster_plugins'].insert(),
+        [
+            {
+                'cluster_id': cluster_id,
+                'plugin_id': pluginid_a,
+                'enabled': True
+            },
+            {
+                'cluster_id': cluster_id,
+                'plugin_id': pluginid_b,
+                'enabled': True
+            }
+        ]
+    )
+
+    db.execute(
+        meta.tables['attributes'].insert(),
+        [{
+            'cluster_id': cluster_id,
+            'editable': jsonutils.dumps({
+                'test_plugin_a': {
+                    'metadata': {
+                        'plugin_id': pluginid_a,
+                        'enabled': True,
+                        'toggleable': True,
+                        'weight': 70,
+                    }
+                },
+                'test_plugin_b': {
+                    'metadata': {
+                        'plugin_id': pluginid_b,
+                        'enabled': True,
+                        'toggleable': True,
+                        'weight': 80,
+                    }
+                }
+            }),
+            'generated': jsonutils.dumps({}),
+        }])
+
+    db.commit()
+
+
+class TestVipMigration(base.BaseAlembicMigrationTest):
+    def test_ip_addrs_vip_name_exists(self):
+        result = db.execute(
+            sa.select([self.meta.tables['ip_addrs'].c.vip_name]))
+        self.assertEqual(result.scalar(), "management")
+
+    def test_ip_addrs_vip_namespace_exists(self):
+        result = db.execute(
+            sa.select([
+                self.meta.tables['ip_addrs'].c.vip_name,
+                self.meta.tables['ip_addrs'].c.vip_namespace
+            ]))
+        result = list(result)
+        self.assertIn(('my-vip', 'my-namespace',), result)
+        self.assertIn(('my-vip1', 'my-namespace1',), result)
+        self.assertIn(('my-vip2', 'my-namespace2',), result)
+
+    def test_ip_addrs_is_user_assigned_exists(self):
+        result = db.execute(
+            sa.select([self.meta.tables['ip_addrs'].c.is_user_defined]))
+        self.assertEqual(False, result.scalar())

767831fa74ebf8f4d32cef5e67cbe8d4a3ed227f
Ivan Kliuk <ivan.kliuk@gmail.com>
2015-12-05 17:16:38 +0200

    [WIP] New VIP-related fields in the database
    * Add 'user_defined' field to 'ip_addrs' table.
    * Rename 'vip_type' field to 'vip_name' of table 'ip_addrs'.
    * Add database migrations.
    * Add unit test for migrations.
    
    Change-Id: Ia3e1d7f6e08dbebcb182de75eeaf58ddf6be4a8d
    Partial-bug: #1482399

--- nailgun/nailgun/test/unit/test_migration_fuel_9_0.py
+++ nailgun/nailgun/test/unit/test_migration_fuel_9_0.py
@@ -12,5 +12,45 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import alembic
+import sqlalchemy as sa
+
+from nailgun.db import db
+from nailgun.db import dropdb
+from nailgun.db.migration import ALEMBIC_CONFIG
+from nailgun.test import base
+
 _prepare_revision = '43b2cb64dae6'
 _test_revision = '11a9adc6d36a'
+
+
+def setup_module():
+    dropdb()
+    alembic.command.upgrade(ALEMBIC_CONFIG, _test_revision)
+    prepare()
+
+
+def prepare():
+    meta = base.reflect_db_metadata()
+
+    db.execute(
+        meta.tables['ip_addrs'].insert(),
+        [{
+            'ip_addr': '192.168.0.2',
+            'vip_name': 'vrouter',
+            'user_defined': True,
+        }])
+
+    db.commit()
+
+
+class TestVipMigration(base.BaseAlembicMigrationTest):
+    def test_ip_addrs_vip_name_exists(self):
+        result = db.execute(
+            sa.select([self.meta.tables['ip_addrs'].c.vip_name]))
+        self.assertEqual(result.scalar(), "vrouter")
+
+    def test_ip_addrs_user_assigned_exists(self):
+        result = db.execute(
+            sa.select([self.meta.tables['ip_addrs'].c.user_defined]))
+        self.assertEqual(result.scalar(), True)

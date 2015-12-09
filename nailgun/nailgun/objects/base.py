d35d3a0c78909f780210154dcf72231a557acf6c
Ryan Moe <rmoe@mirantis.com>
2015-12-09 06:09:46 -0800

    Proxy calls to the database from Nailgun objects
    This change introduces a set of objects whose responsibility is to
    proxy queries to the database. This is a pre-requisite to having an
    external network configuration service.
    
    Blueprint: network-config-refactoring
    Change-Id: If54f5d2d0abab1f8cb4e3c0baadb36902c0f059e

--- nailgun/nailgun/objects/base.py
+++ nailgun/nailgun/objects/base.py
@@ -446,3 +446,241 @@ class NailgunCollection(object):
         :returns: instance of an object (model)
         """
         return cls.single.create(data)
+
+
+class ProxiedNailgunObject(NailgunObject):
+    """Base class for objects"""
+
+    #: Serializer class for object
+    serializer = BasicSerializer
+
+    #: SQLAlchemy model for object
+    model = None
+
+    @classmethod
+    def get_by_uid(cls, uid, fail_if_not_found=False, lock_for_update=False):
+        """Get instance by it's uid (PK in case of SQLAlchemy)
+
+        :param uid: uid of object
+        :param fail_if_not_found: raise an exception if object is not found
+        :param lock_for_update: lock returned object for update (DB mutex)
+        :returns: instance of an object (model)
+        """
+        res = cls.proxy.get(uid, lock_for_update=lock_for_update)
+        if not res and fail_if_not_found:
+            raise errors.ObjectNotFound(
+                "Object '{0}' with UID={1} is not found in DB".format(
+                    cls.__name__,
+                    uid
+                )
+            )
+        return res
+
+    @classmethod
+    def create(cls, data):
+        """Create object instance with specified parameters in DB
+
+        :param data: dictionary of key-value pairs as object fields
+        :returns: instance of an object (model)
+        """
+        return cls.proxy.create(data)
+
+    @classmethod
+    def update(cls, instance, data):
+        """Update existing instance with specified parameters
+
+        :param instance: object (model) instance
+        :param data: dictionary of key-value pairs as object fields
+        :returns: instance of an object (model)
+        """
+        return cls.proxy.update(instance, data)
+
+    @classmethod
+    def delete(cls, instance):
+        """Delete object (model) instance
+
+        :param instance: object (model) instance
+        :returns: None
+        """
+        cls.proxy.delete(instance)
+
+
+class ProxiedNailgunCollection(NailgunCollection):
+    """Base class for object collections"""
+
+    #: Single object class
+    single = ProxiedNailgunObject
+
+    @classmethod
+    def all(cls):
+        """Get all instances of this object (model)
+
+        :returns: iterable (SQLAlchemy query)
+        """
+        return cls.single.proxy.get_all()
+
+    @classmethod
+    def _query_order_by(cls, query, order_by):
+        """Adds order by clause into SQLAlchemy query
+
+        :param query: SQLAlchemy query
+        :param order_by: tuple of model fields names for ORDER BY criterion
+        to SQLAlchemy query. If name starts with '-' desc ordering applies,
+        else asc.
+        """
+        params = {
+            'options': {
+                'order_by': order_by
+            }
+        }
+
+        query = cls.single.proxy.filter(params)
+        return query
+
+    @classmethod
+    def filter_by(cls, iterable, **kwargs):
+        """Filter given iterable by specified kwargs.
+
+        In case if iterable=None filters all object instances
+
+        :param iterable: iterable (SQLAlchemy query)
+        :param order_by: tuple of model fields names for ORDER BY criterion
+            to SQLAlchemy query. If name starts with '-' desc ordering applies,
+            else asc.
+        :returns: filtered iterable (SQLAlchemy query)
+        """
+        if iterable is not None:
+            use_iterable = iterable
+        else:
+            use_iterable = cls.all()
+        if cls._is_query(use_iterable):
+            return use_iterable.filter_by(**kwargs)
+        elif cls._is_iterable(use_iterable):
+            return ifilter(
+                lambda i: all(
+                    (getattr(i, k) == v for k, v in kwargs.iteritems())
+                ),
+                use_iterable
+            )
+        else:
+            raise TypeError("First argument should be iterable")
+
+    @classmethod
+    def filter_by_not(cls, iterable, **kwargs):
+        """Filter given iterable by specified kwargs with negation.
+
+        In case if `iterable` is `None` filters all object instances.
+
+        :param iterable: iterable (SQLAlchemy query)
+        :returns: filtered iterable (SQLAlchemy query)
+        """
+        use_iterable = iterable or cls.all()
+        if cls._is_query(use_iterable):
+            conditions = []
+            for key, value in kwargs.iteritems():
+                conditions.append(
+                    getattr(cls.single.model, key) == value
+                )
+            return use_iterable.filter(not_(and_(*conditions)))
+        elif cls._is_iterable(use_iterable):
+            return ifilter(
+                lambda i: not all(
+                    (getattr(i, k) == v for k, v in kwargs.iteritems())
+                ),
+                use_iterable
+            )
+        else:
+            raise TypeError("First argument should be iterable")
+
+    @classmethod
+    def lock_for_update(cls, iterable):
+        """Use SELECT FOR UPDATE on a given iterable (query).
+
+        In case if iterable=None returns all object instances
+
+        :param iterable: iterable (SQLAlchemy query)
+        :returns: filtered iterable (SQLAlchemy query)
+        """
+        use_iterable = iterable or cls.all()
+        if cls._is_query(use_iterable):
+            return use_iterable.with_lockmode('update')
+        elif cls._is_iterable(use_iterable):
+            # we can't lock abstract iterable, so returning as is
+            # for compatibility
+            return use_iterable
+        else:
+            raise TypeError("First argument should be iterable")
+
+    @classmethod
+    def filter_by_list(cls, iterable, field_name, list_of_values,
+                       order_by=()):
+        """Filter given iterable by list of list_of_values.
+
+        In case if iterable=None filters all object instances
+
+        :param iterable: iterable (SQLAlchemy query)
+        :param field_name: filtering field name
+        :param list_of_values: list of values for objects filtration
+        :returns: filtered iterable (SQLAlchemy query)
+        """
+        field_getter = operator.attrgetter(field_name)
+        use_iterable = iterable or cls.all()
+        if cls._is_query(use_iterable):
+            result = use_iterable.filter(
+                field_getter(cls.single.model).in_(list_of_values)
+            )
+            result = cls.order_by(result, order_by)
+            return result
+        elif cls._is_iterable(use_iterable):
+            return ifilter(
+                lambda i: field_getter(i) in list_of_values,
+                use_iterable
+            )
+        else:
+            raise TypeError("First argument should be iterable")
+
+    @classmethod
+    def filter_by_id_list(cls, iterable, uid_list):
+        """Filter given iterable by list of uids.
+
+        In case if iterable=None filters all object instances
+
+        :param iterable: iterable (SQLAlchemy query)
+        :param uid_list: list of uids for objects
+        :returns: filtered iterable (SQLAlchemy query)
+        """
+        return cls.filter_by_list(
+            iterable,
+            'id',
+            uid_list,
+        )
+
+    @classmethod
+    def eager_base(cls, iterable, options):
+        """Eager load linked object instances (SQLAlchemy FKs).
+
+        In case if iterable=None applies to all object instances
+
+        :param iterable: iterable (SQLAlchemy query)
+        :param options: list of sqlalchemy eagerload types
+        :returns: iterable (SQLAlchemy query)
+        """
+        use_iterable = iterable or cls.all()
+        if options:
+            return use_iterable.options(*options)
+        return use_iterable
+
+    @classmethod
+    def eager(cls, iterable, fields):
+        """Eager load linked object instances (SQLAlchemy FKs).
+
+        By default joinedload will be applied to every field.
+        If you want to use custom eagerload method - use eager_base
+        In case if iterable=None applies to all object instances
+
+        :param iterable: iterable (SQLAlchemy query)
+        :param fields: list of links (model FKs) to eagerload
+        :returns: iterable (SQLAlchemy query)
+        """
+        options = [joinedload(field) for field in fields]
+        return cls.eager_base(iterable, options)

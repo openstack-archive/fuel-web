15aacafc45ecccc53b3ca91166a49d7ecabfb665
Ryan Moe <rmoe@mirantis.com>
2015-12-09 06:09:46 -0800

    Proxy calls to the database from Nailgun objects
    This change introduces a set of objects whose responsibility is to
    proxy queries to the database. This is a pre-requisite to having an
    external network configuration service.
    
    Change-Id: If54f5d2d0abab1f8cb4e3c0baadb36902c0f059e

---
+++ nailgun/nailgun/network/proxy/__init__.py
@@ -0,0 +1,4 @@
+from nailgun.network.proxy.base import BaseProxy
+from nailgun.network.proxy.ip_address import IPAddrProxy
+from nailgun.network.proxy.nic import NetworkNICAssignmentProxy
+from nailgun.network.proxy.nic import NICProxy

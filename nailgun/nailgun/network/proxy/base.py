2fb67630f97397f78161bf89a2784d2026416d31
Ryan Moe <rmoe@mirantis.com>
2015-12-09 06:09:46 -0800

    Proxy calls to the database from Nailgun objects
    This change introduces a set of objects whose responsibility is to
    proxy queries to the database. This is a pre-requisite to having an
    external network configuration service.
    
    Blueprint: external-network-config
    Change-Id: If54f5d2d0abab1f8cb4e3c0baadb36902c0f059e

---
+++ nailgun/nailgun/network/proxy/base.py
@@ -0,0 +1,53 @@
+from nailgun.db import db
+from nailgun.network.proxy import utils
+
+
+class BaseProxy(object):
+
+    def get(self, obj_id, lock_for_update=False):
+        q = db().query(self.model)
+        if lock_for_update:
+            q = q.with_for_update()
+        return q.get(obj_id)
+
+    def get_all(self):
+        return db().query(self.model)
+
+    def bulk_create(self, data):
+        for instance in data:
+            new_obj = self.model()
+            for key, value in instance.items():
+                setattr(new_obj, key, value)
+            db().add(new_obj)
+        db().flush()
+
+    def create(self, data):
+        new_obj = self.model()
+        for key, value in data.items():
+            setattr(new_obj, key, value)
+        db().add(new_obj)
+        db().flush()
+        return new_obj
+
+    def filter(self, data):
+        q = utils.Query(self.model, data)
+        return q.render()
+
+    def update(self, instance, data):
+        instance.update(data)
+        db().add(instance)
+        db().flush()
+        return instance
+
+    def delete(self, instance):
+        db().delete(instance)
+        db().flush()
+
+    def bulk_delete(self, ids):
+        db().query(self.model).filter(
+            self.model.id.in_(ids)).delete('fetch')
+        db().flush()
+
+    def filter_delete(self, data):
+        q = utils.Query(self.model, data)
+        q.render().delete()

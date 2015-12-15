e1402a74fc6f02f71e8b1394af55d0e006699f84
Ivan Kliuk <ivan.kliuk@gmail.com>
2015-12-15 17:17:02 +0200

    Add Alembic migrations scaffold for Fuel 9.0
    Change-Id: I1e4002c0c98d8d77589313c87fbdc553e0c9f2ee
    Closes-bug: #1526377

---
+++ nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_9_0.py
@@ -0,0 +1,36 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Fuel 9.0
+
+Revision ID: 11a9adc6d36a
+Revises: 2f879fa32f00
+Create Date: 2015-12-05 17:52:49.519542
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '11a9adc6d36a'
+down_revision = '43b2cb64dae6'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass

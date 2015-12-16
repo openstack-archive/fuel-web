2490c805588bbe02f038bc8c71934ba330182c6d
Andriy Popovych <popovych.andrey@gmail.com>
2015-12-16 22:51:18 +0200

    Create duplex relations for component
    Incompatible/compatible relations is duplex. If some component incompatible/
    compatible with other component it means the last also should have such
    relations.
    
    Closes-Bug: #1526353
    Change-Id: I0e946329a34480798d7871611363f45cb406904d

--- nailgun/nailgun/test/unit/test_objects.py
+++ nailgun/nailgun/test/unit/test_objects.py
@@ -1666,28 +1666,33 @@ class TestRelease(BaseTestCase):
                 'mode': ['ha'],
                 'deployment_scripts_path': 'deployment_scripts/'}],
             components_metadata=self.env.get_default_components(
-                name='storage:test_component_2')
+                name='storage:test_component_2',
+                label='Test storage',
+                incompatible=[{
+                    'name': 'hypervisor:test_component_1',
+                    'message': 'component_2 not compatible with component_1'}])
         )
 
         components = objects.Release.get_all_components(release)
 
-        self.assertListEqual(components, [
-            {
-                'name': 'hypervisor:test_component_1',
-                'compatible': [
-                    {'name': 'hypervisors:*'},
-                    {'name': 'storages:object:block:swift'}],
-                'incompatible': [
-                    {'name': 'networks:*'},
-                    {'name': 'additional_services:*'}]},
-            {
-                'name': 'storage:test_component_2',
-                'compatible': [
-                    {'name': 'hypervisors:*'},
-                    {'name': 'storages:object:block:swift'}],
-                'incompatible': [
-                    {'name': 'networks:*'},
-                    {'name': 'additional_services:*'}]}])
+        self.assertListEqual(components, [{
+            'name': 'hypervisor:test_component_1',
+            'compatible': [
+                {'name': 'hypervisors:*'},
+                {'name': 'storages:object:block:swift'}],
+            'incompatible': [
+                {'name': 'networks:*'},
+                {'name': 'additional_services:*'},
+                {'name': 'storage:test_component_2',
+                 'message': 'Not compatible with Test storage'}]}, {
+            'name': 'storage:test_component_2',
+            'label': 'Test storage',
+            'compatible': [
+                {'name': 'hypervisors:*'},
+                {'name': 'storages:object:block:swift'}],
+            'incompatible': [
+                {'name': 'hypervisor:test_component_1',
+                 'message': 'component_2 not compatible with component_1'}]}])
 
 
 class TestOpenstackConfig(BaseTestCase):

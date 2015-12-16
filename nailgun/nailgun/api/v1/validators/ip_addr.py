4c3af39f86c904e12c6fdef74366d4ccf4f4e25a
Ilya Kutukov <ikutukov@mirantis.com>
2015-12-16 14:25:37 +0300

    Basic VIP management handlers added to nailgun API
    New VIP handlers is introduced that allows list VIP for cluster and get or update
    specific VIP.
    
    /clusters/:cluster_id/network_configuration/ips/vips
    /clusters/:cluster_id/network_configuration/ips/:ip_address_id/vips
    
    Partial-Bug: #1482399
    Implements Blueprint: allow-any-vip
    Change-Id: I4a4058c112bbdb26ab410e5e97b4e1e3afe7a082

---
+++ nailgun/nailgun/api/v1/validators/ip_addr.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#    Copyright 2013 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_serialization import jsonutils
+
+from nailgun.api.v1.validators.base import BasicValidator
+from nailgun.api.v1.validators.json_schema import ip_addr
+from nailgun.errors import errors
+from nailgun.objects import IPAddrCollection
+
+
+class IPAddrValidator(BasicValidator):
+    single_schema = ip_addr.IP_ADDR_UPDATE_SCHEMA
+    collection_schema = ip_addr.IP_ADDRS_UPDATE_SCHEMA
+
+    @classmethod
+    def validate_collection_update(cls, data, cluster_id=None):
+        data_to_update = cls.validate_json(data)
+        existing_vips = IPAddrCollection.get_vips_by_cluster_id(cluster_id)
+        existing_vips_id = set(vip.id for vip in existing_vips)
+
+        records_with_no_id = [
+            record for record in data_to_update if not record.get('id')]
+
+        if records_with_no_id:
+            raise errors.InvalidData(
+                "The following records have no 'id' field:\n{0}".format(
+                    "\n".join(jsonutils.dumps(r) for r in records_with_no_id)
+                )
+            )
+
+        ids_to_update = set(record.get('id') for record in data_to_update)
+        not_found_ids = ids_to_update.difference(existing_vips_id)
+
+        if not_found_ids:
+            raise errors.InvalidData(
+                "IP addresses with ID(s) {0} were not found in "
+                "cluster {1}".format(list(not_found_ids), cluster_id)
+            )
+
+        return data_to_update
---
+++ nailgun/nailgun/objects/ip_addr.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nailgun.db.sqlalchemy import models
+from nailgun.objects import NailgunCollection
+from nailgun.objects import NailgunObject
+from nailgun.objects.serializers.ip_addr import IPAddrSerializer
+
+
+class IPAddr(NailgunObject):
+
+    model = models.IPAddr
+    serializer = IPAddrSerializer
+
+
+class IPAddrCollection(NailgunCollection):
+
+    single = IPAddr
+
+    @classmethod
+    def get_by_cluster_id(cls, cluster_id):
+        """Get records filtered by cluster identifier.
+
+        Or returns all records if no cluster_id is provided.
+
+        :param cluster_id: cluster identifier or None to get all records
+        :type cluster_id: int|None
+        :return: vips query
+        :rtype: SQLAlchemy Query
+        """
+        q = cls.all()
+        if cluster_id is not None:
+            q = q.join(models.NetworkGroup)\
+                .join(models.NodeGroup)\
+                .filter(models.NodeGroup.cluster_id == int(cluster_id))
+        return q
+
+    @classmethod
+    def get_vips_by_cluster_id(cls, cluster_id):
+        """Get VIP filtered by cluster ID.
+
+        VIP is determined by not NULL vip_name field of IPAddr model.
+
+        :param cluster_id: cluster identifier or None to get all records
+        :type cluster_id: int|None
+        :return: vips query
+        :rtype: SQLAlchemy Query
+        """
+        return cls.get_by_cluster_id(cluster_id)\
+            .filter(models.IPAddr.vip_name.isnot(None))
+
+    @classmethod
+    def update_vips(cls, new_data_list):
+        """Perform batch update of VIP data.
+
+        :param new_data_list:
+        :type new_data_list: list(dict)
+        :return: vips query
+        :rtype: SQLAlchemy Query
+        """
+        # create dictionary where key is id
+        data_by_ids = {item['id']: item for item in new_data_list}
+
+        # get db instances
+        q = cls.filter_by_list(None, 'id', list(data_by_ids.keys()))
+
+        cls.lock_for_update(q).all()
+        for existing_instance in q:
+            cls.single.update(
+                existing_instance,
+                data_by_ids[existing_instance.id]
+            )
+        return q

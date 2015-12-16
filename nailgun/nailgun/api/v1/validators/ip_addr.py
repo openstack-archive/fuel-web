c5d9360269e8c9d2d78e32ca922831d7144776a6
Ilya Kutukov <ikutukov@mirantis.com>
2015-12-16 14:25:37 +0300

    Basic VIP management handlers added to nailgun API
    New VIP handlers is introduced that allows list VIP for cluster and get or update
    specific VIP.
    
    /clusters/:cluster_id/network_configuration/ips/vips
    /clusters/:cluster_id/network_configuration/ips/:ip_address_id/vips
    
    Partial-Bug: #1482399
    Implements Blueprint: allow-any-vip
    Change-Id: I4a4058c112bbdb26ab410e5e97b4e1e3afe7a082

---
+++ nailgun/nailgun/api/v1/validators/ip_addr.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+#    Copyright 2013 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import six
+
+from oslo_serialization import jsonutils
+
+from nailgun.api.v1.validators.base import BasicValidator
+from nailgun.api.v1.validators.json_schema import ip_addr
+from nailgun.db.sqlalchemy import models
+from nailgun.errors import errors
+from nailgun import objects
+
+
+class IPAddrValidator(BasicValidator):
+    single_schema = ip_addr.IP_ADDR_UPDATE_SCHEMA
+    collection_schema = ip_addr.IP_ADDRS_UPDATE_SCHEMA
+    updatable_fields = (
+        "ip_addr",
+        "is_user_defined",
+        "vip_namespace",
+    )
+
+    @classmethod
+    def validate_update(cls, data, existing_obj):
+        """Validate single IP address entry update information.
+
+        :param data: new data
+        :type data: dict
+        :param existing_obj: existing object
+        :type existing_obj: instance if fuel.objects.IPAddr
+        :return: validated data
+        :rtype: dict
+        """
+        if isinstance(data, six.string_types):
+            data = cls.validate_json(data)
+
+        existing_data = dict(existing_obj)
+
+        bad_fields = []
+        for field, value in six.iteritems(data):
+            old_value = existing_data.get(field)
+            # field that not allowed to be changed is changed
+            if value != old_value and field not in cls.updatable_fields:
+                bad_fields.append(field)
+
+        if bad_fields:
+            bad_fields_verbose = ", ".join(repr(bf) for bf in bad_fields)
+            raise errors.InvalidData(
+                "\n".join([
+                    "The following fields: {0} are not allowed to be "
+                    "updated for record: {1}".format(
+                        bad_fields_verbose,
+                        jsonutils.dumps(data)
+                    )
+                ])
+            )
+        return data
+
+    @classmethod
+    def validate_collection_update(cls, data, cluster_id):
+        """Validate IP address collection update information.
+
+        :param data: new data
+        :type data: list(dict)
+        :param cluster_id: if od objects.Cluster instance
+        :type cluster_id: int
+        :return: validated data
+        :rtype: list(dict)
+        """
+
+        error_messages = []
+        data_to_update = cls.validate_json(data)
+        existing_instances = objects.IPAddrCollection.get_vips_by_cluster_id(
+            cluster_id)
+
+        for record in data_to_update:
+            instance = existing_instances.filter(
+                models.IPAddr.id == record.get('id')
+            ).first()
+
+            if instance:
+                try:
+                    cls.validate_update(record, instance)
+                except errors.InvalidData as e:
+                    error_messages.append(e.message)
+            else:
+                error_messages.append(
+                    "IPAddr with (ID={0}) does not exist or does not "
+                    "belong to cluster (ID={1})".format(
+                        record.get('id'),
+                        cluster_id
+                    )
+                )
+
+        if error_messages:
+            raise errors.InvalidData("\n".join(error_messages))
+
+        return data_to_update

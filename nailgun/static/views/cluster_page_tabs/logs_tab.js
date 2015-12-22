2028f4ed45f76452370147ba11e9fc48ab2f314b
Kate Pimenova <kpimenova@mirantis.com>
2015-12-22 18:15:03 +0300

    Warning on Log Tab fixed
    Change-Id: I7eaa391e0af6db7bc841085021e887daeded5ff3

--- nailgun/static/views/cluster_page_tabs/logs_tab.js
+++ nailgun/static/views/cluster_page_tabs/logs_tab.js
@@ -313,7 +313,8 @@ function($, _, i18n, React, utils, models, componentMixins, controls) {
             </div>;
         },
         renderSourceSelect: function() {
-            var sourceOptions = this.state.type == 'local' ? this.getLocalSources() : this.getRemoteSources();
+            var createFragment = React.addons.createFragment,
+                sourceOptions = this.state.type == 'local' ? this.getLocalSources() : createFragment(this.getRemoteSources());
             return <div className='col-md-2 col-sm-3'>
                 <controls.Input
                     type='select'

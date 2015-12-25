443003c9a00d4ad8f0af805108d0bba5b9a113e1
Grigory Mikhailov <gmikhailov@mirantis.com>
2015-12-25 10:52:35 +0300

    Updated and new functional tests for "GUI support for Ironic" test group.
    Updated tests:
    1. T2199259: Enabling Ironic when create environment
    2. T2199263: Check that "Ironic" item is selected on "Settings" tab
    Added tests:
    1. T2199264: Baremetal Network "IP Ranges" correct changing
    2. T2199265: Baremetal Network "IP Ranges" adding and deletion additional fields
    3. T2199266: Baremetal and other networks intersections
    4. T2199267: Baremetal Network "Use the whole CIDR" option works
    5. T2199268: Baremetal Network "Use VLAN tagging" option works
    6. T2199269: Baremetal Network "Use VLAN tagging" option validation
    7. T2199271: Baremetal Network "CIDR" field validation
    
    Updated common.js library to prevent cluster deletion error via test failure.
    Updated networks.js library by new functions for new tests.
    Attention! Tets T2199266, T2199269 are failed due to bugs.
    
    Change-Id: Ic889639202f52d922975026d6dd9444d3126ffde

--- nailgun/static/tests/functional/pages/networks.js
+++ nailgun/static/tests/functional/pages/networks.js
@@ -29,6 +29,53 @@ define([
         switchNetworkManager: function() {
             return this.remote
                 .clickByCssSelector('input[name=net_provider]:not(:checked)');
+        },
+        checkBaremetalInitialState: function() {
+            return this.remote
+                // CIDR
+                .assertElementEnabled('div.baremetal div.cidr input[type="text"]', 'Baremetal "CIDR" textfield is enabled')
+                .assertElementPropertyEquals('div.baremetal div.cidr input[type="text"]', 'value', '192.168.3.0/24', 'Baremetal "CIDR" textfield has default value')
+                .assertElementEnabled('div.baremetal div.cidr input[type="checkbox"]', 'Baremetal "Use the whole CIDR" checkbox is enabled')
+                .assertElementNotSelected('div.baremetal div.cidr input[type="checkbox"]', 'Baremetal "Use the whole CIDR" checkbox is not selected')
+                // IP Ranges
+                .assertElementEnabled('div.baremetal div.ip_ranges input[name*="range-start"]', 'Baremetal "Start IP Range" textfield is enabled')
+                .assertElementEnabled('div.baremetal div.ip_ranges input[name*="range-end"]', 'Baremetal "End IP Range" textfield is enabled')
+                .assertElementEnabled('div.baremetal div.ip_ranges button.ip-ranges-add', 'Add new IP range button is enabled')
+                .assertElementNotExists('div.baremetal div.ip_ranges div.range-row[data-reactid$="$1"]', 'Only default IP range is observed')
+                .assertElementPropertyEquals('div.baremetal div.ip_ranges input[name*="range-start"]', 'value', '192.168.3.1', 'Baremetal "Start IP Range" textfield  has default value')
+                .assertElementPropertyEquals('div.baremetal div.ip_ranges input[name*="range-end"]', 'value', '192.168.3.50', 'Baremetal "End IP Range" textfield has default value')
+                // VLAN
+                .assertElementEnabled('div.baremetal div.vlan_start input[type="checkbox"]', 'Baremetal "Use VLAN tagging" checkbox is enabled')
+                .assertElementSelected('div.baremetal div.vlan_start input[type="checkbox"]', 'Baremetal "Use VLAN tagging" checkbox is selected')
+                .assertElementEnabled('div.baremetal div.vlan_start input[type="text"]', 'Baremetal "Use VLAN tagging" textfield is enabled')
+                .assertElementPropertyEquals('div.baremetal div.vlan_start input[type="text"]', 'value', '104', 'Baremetal "Use VLAN tagging" textfield textfield has default value')
+                // Generic
+                .assertElementDisabled('button.apply-btn', '"Save Settings" button is disabled')
+                .assertElementDisabled('button.btn-revert-changes', '"Cancel Changes" button is disabled')
+                .assertElementNotExists('div.baremetal div.has-error', 'No Baremetal errors are observed');
+        },
+        checkIncorrectValueInput: function() {
+            return this.remote
+                .assertElementDisabled('button.apply-btn', '"Save Settings" button is disabled')
+                .assertElementExists('a[class$="network_verification"]', '"Connectivity Check" link is existed')
+                .clickByCssSelector('a[class$="network_verification"]')
+                .assertElementDisabled('button.verify-networks-btn', '"Verify Networks" button is disabled')
+                .assertElementExists('a[class$="default"]', '"Default" link is existed')
+                .clickByCssSelector('a[class$="default"]');
+        },
+        saveSettings: function() {
+            return this.remote
+                .assertElementEnabled('button.apply-btn', '"Save Settings" button is enabled')
+                .clickByCssSelector('button.apply-btn')
+                .assertElementDisabled('button.apply-btn', '"Save Settings" button is disabled')
+                .assertElementNotExists('div.has-error', 'Settings saved successfully');
+        },
+        cancelChanges: function() {
+            return this.remote
+                .assertElementEnabled('button.btn-revert-changes', '"Cancel Changes" button is enabled')
+                .clickByCssSelector('button.btn-revert-changes')
+                .assertElementDisabled('button.btn-revert-changes', '"Cancel Changes" button is disabled')
+                .assertElementNotExists('div.has-error', 'Settings canceled successfully');
         }
     };
     return NetworksPage;

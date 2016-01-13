5f03c0e9fb9c93cd7b55e7442b6719d8a2eece1e
Artem Roma <aroma@mirantis.com>
2016-01-13 17:51:46 +0200

    Add logging setup for receiverd and assassind
    Since all services inside containers are now started via systemd we need
    add logging handling which in past was done by supervisord. Thus such
    settings are added for rpc receiver and assassin services. Loggers are
    set up independently from nailgun root logger as writing to files are
    required for mentioned components.
    
    Change-Id: I21136108d426531f572940dfc9f75fe153111d41
    Partial-Bug: #1523476

--- nailgun/tools/prepare_settings_yaml.sh
+++ nailgun/tools/prepare_settings_yaml.sh
@@ -13,4 +13,6 @@ DATABASE:
   passwd: "${NAILGUN_DB_PW}"
 API_LOG: ${NAILGUN_LOGS}/api.log
 APP_LOG: ${NAILGUN_LOGS}/app.log
+RPC_CONSUMER_LOG_PATH: "${NAILGUN_LOGS}/receiverd.log"
+ASSASSIN_LOG_PATH: "${NAILGUN_LOGS}/assassind.log"
 EOL

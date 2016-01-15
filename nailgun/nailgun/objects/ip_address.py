74fcd025f9e8ace25baba222c9340d670dc6e090
Ryan Moe <rmoe@mirantis.com>
2016-01-15 14:12:31 -0800

    Add functionality from NetworkManager to objects
    In preparation of removing direct database queries from NetworkManager,
    methods which are functionally equivalent have been added to the appropriate
    objects.
    
    Change-Id: Ic6d4984e4eb10e6cb897a929eee7a47e67798451
    Blueprint: network-config-refactoring

--- nailgun/nailgun/objects/ip_address.py
+++ nailgun/nailgun/objects/ip_address.py
@@ -14,6 +14,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+
 from sqlalchemy.orm import joinedload
 from sqlalchemy.sql import not_
 
@@ -25,13 +26,12 @@ from nailgun.objects import NailgunCollection
 from nailgun.objects import NailgunObject
 from nailgun.objects import NetworkGroup
 from nailgun.objects.serializers.base import BasicSerializer
-from nailgun.objects.serializers.ip_addr import IPAddrSerializer
 
 
 class IPAddr(NailgunObject):
 
     model = models.IPAddr
-    serializer = IPAddrSerializer
+    serializer = BasicSerializer
 
     @classmethod
     def get_ips_except_admin(cls, node_id=None,
@@ -117,7 +117,7 @@ class IPAddr(NailgunObject):
         node_group_id = Cluster.get_controllers_group_id(cluster)
         cluster_vips = db.query(models.IPAddr).join(
             models.IPAddr.network_data).filter(
-                models.IPAddr.vip_name.isnot(None) &
+                models.IPAddr.vip_type.isnot(None) &
                 (models.NetworkGroup.group_id == node_group_id))
         return cluster_vips
 
@@ -156,88 +156,3 @@ class IPAddrRange(NailgunObject):
 class IPAddrCollection(NailgunCollection):
 
     single = IPAddr
-
-    @classmethod
-    def get_by_cluster_id(cls, cluster_id):
-        """Get records filtered by cluster identifier.
-
-        Or returns all records if no cluster_id is provided.
-
-        :param cluster_id: cluster identifier or None to get all records
-        :type cluster_id: int|None
-        :return: vips query
-        :rtype: SQLAlchemy Query
-        """
-        query = cls.all()
-        if cluster_id is not None:
-            query = query.join(models.NetworkGroup)\
-                .join(models.NodeGroup)\
-                .filter(models.NodeGroup.cluster_id == cluster_id)
-        return query
-
-    @classmethod
-    def get_vips_by_cluster_id(cls, cluster_id,
-                               network_id=None, network_role=None):
-        """Get VIP filtered by cluster ID.
-
-        VIP is determined by not NULL vip_name field of IPAddr model.
-
-        :param cluster_id: cluster identifier or None to get all records
-        :type cluster_id: int|None
-        :param network_id: network identifier
-        :type network_id: int
-        :param network_role: network role
-        :type network_role: str
-        :return: vips query
-        :rtype: SQLAlchemy Query
-        """
-        query = cls.get_by_cluster_id(cluster_id)\
-            .filter(models.IPAddr.vip_name.isnot(None))
-
-        if network_id:
-            query = query.filter(models.IPAddr.network == network_id)
-
-        if network_role:
-            # Get all network_roles for cluster and gain vip names from it,
-            # then bound query to this names.
-            # See network_roles.yaml in plugin examples for the details of
-            # input structure.
-            cluster_obj = Cluster.get_by_uid(cluster_id)
-            vips = []
-
-            for cluster_network_role in Cluster.get_network_roles(cluster_obj):
-                if cluster_network_role.get('id') == network_role:
-                    vips.extend(
-                        cluster_network_role
-                        .get('properties', {})
-                        .get('vip', [])
-                    )
-
-            vip_names = (vip['name'] for vip in vips)
-            unique_vip_names = list(set(vip_names))
-            query = query.filter(models.IPAddr.vip_name.in_(unique_vip_names))
-
-        return query
-
-    @classmethod
-    def update_vips(cls, new_data_list):
-        """Perform batch update of VIP data.
-
-        :param new_data_list:
-        :type new_data_list: list(dict)
-        :return: vips query
-        :rtype: SQLAlchemy Query
-        """
-        # create dictionary where key is id
-        data_by_ids = {item['id']: item for item in new_data_list}
-
-        # get db instances
-        query = cls.filter_by_list(None, 'id', list(data_by_ids))
-
-        cls.lock_for_update(query).all()
-        for existing_instance in query:
-            cls.single.update(
-                existing_instance,
-                data_by_ids[existing_instance.id]
-            )
-        return query

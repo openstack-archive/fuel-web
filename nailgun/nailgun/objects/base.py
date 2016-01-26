8384398e574aa39460fcba067a41ff663349c15a
sslypushenko <sslypushenko@mirantis.com>
2016-01-27 00:05:36 +0200

    Fixed order_by method in case of multiple fields
    Fix works fine both for queries and iterables.
    Unit tests also reworked.
    
    Change-Id: I73313d1aeac60a7f27e4d718a0c07290d2dbb596
    Closes-Bug: #1538078

--- nailgun/nailgun/objects/base.py
+++ nailgun/nailgun/objects/base.py
@@ -177,6 +177,7 @@ class NailgunCollection(object):
         to SQLAlchemy query. If name starts with '-' desc ordering applies,
         else asc.
         """
+        order_args = []
         for field_name in order_by:
             if field_name.startswith('-'):
                 field_name = field_name.lstrip('-')
@@ -185,7 +186,8 @@ class NailgunCollection(object):
                 ordering = 'asc'
             field = getattr(cls.single.model, field_name)
             o_func = getattr(field, ordering)
-            query = query.order_by(o_func())
+            order_args.append(o_func())
+        query = query.order_by(*order_args)
         return query
 
     @classmethod
@@ -196,17 +198,46 @@ class NailgunCollection(object):
         :param order_by: tuple of model fields names for sorting.
         If name starts with '-' desc ordering applies, else asc.
         """
+        class SortWrapper(object):
+
+            def __init__(self, wrapped, order_by):
+                self.wrapped = wrapped
+                self.order_by = order_by
+
+            def __lt__(self, other):
+                for field in self.order_by:
+                    if (getattr(self.wrapped, field['name']) <
+                            getattr(other.wrapped, field['name'])):
+                        return field['asc']
+                    elif (getattr(self.wrapped, field['name']) >
+                            getattr(other.wrapped, field['name'])):
+                        return not field['asc']
+                return False
+
+            def __eq__(self, other):
+                for field in self.order_by:
+                    if (getattr(self.wrapped, field['name']) !=
+                            getattr(other.wrapped, field['name'])):
+                        return False
+                return True
+
+        order_by_fields = []
         for field_name in order_by:
             if field_name.startswith('-'):
-                field_name = field_name.lstrip('-')
-                reverse = True
+                order_by_fields.append({'name': field_name.lstrip('-'),
+                                        'asc': False})
             else:
-                reverse = False
+                order_by_fields.append({'name': field_name,
+                                        'asc': True})
+
+        if len(order_by_fields) == 1:
             iterable = sorted(
                 iterable,
-                key=lambda x: getattr(x, field_name),
-                reverse=reverse
-            )
+                key=lambda x: getattr(x, order_by_fields[0]['name']),
+                reverse=not order_by_fields[0]['asc'])
+        else:
+            iterable = [item.wrapped for item in sorted(
+                (SortWrapper(item, order_by_fields) for item in iterable))]
         return iterable
 
     @classmethod

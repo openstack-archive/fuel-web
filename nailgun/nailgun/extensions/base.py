277f5b4695b475d3352093ae2de5a464c3364f2c
Sylwester Brzeczkowski <sbrzeczkowski@mirantis.com>
2016-01-27 11:12:37 +0100

    Data pipelines in extensions
    Data pipelines in extensions for deployment and provisioning
    serialized data.
    
    Data pipeline is an attribute of Extension - a list of Classes
    which provide one of (or both) methods: process_provisioning,
    process_deployment. They can be used to change provisioning or
    deployment data just before running the action.
    
    Change-Id: Ie778c7ee6c9eb2abe2783b886ee6be481eccd2dc
    Implements: blueprint data-pipeline

--- nailgun/nailgun/extensions/base.py
+++ nailgun/nailgun/extensions/base.py
@@ -21,6 +21,23 @@ import abc
 import six
 
 
+class BasePipeline(object):
+
+    @classmethod
+    def process_deployment(cls, deployment_data, **kwargs):
+        """Change the deployment_data.
+
+        :param deployment_data: serialized data
+        """
+
+    @classmethod
+    def process_provisioning(cls, provisioning_data, **kwargs):
+        """Change the provisioning_data.
+
+        :param provisioning_data: serialized data
+        """
+
+
 @six.add_metaclass(abc.ABCMeta)
 class BaseExtension(object):
     """Base class for Nailgun extension
@@ -46,6 +63,17 @@ class BaseExtension(object):
 
     urls = []
     provides = []
+    data_pipelines = []
+
+    @classmethod
+    def process_deployment(cls, deployment_data, **kwargs):
+        for pipeline in cls.data_pipelines:
+            pipeline.process_deployment(deployment_data, **kwargs)
+
+    @classmethod
+    def process_provisioning(cls, provisioning_data, **kwargs):
+        for pipeline in cls.data_pipelines:
+            pipeline.process_provisioning(provisioning_data, **kwargs)
 
     @classmethod
     def alembic_migrations_path(cls):

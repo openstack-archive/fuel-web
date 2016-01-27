24b819a85a4b6d068b5a9b8a87913d7a67ce6416
Sylwester Brzeczkowski <sbrzeczkowski@mirantis.com>
2016-01-27 11:12:37 +0100

    Data pipelines in extensions
    Data pipelines in extensions for deployment and provisioning
    serialized data.
    
    Data pipeline is an attribute of Extension - a list of Classes
    which provide one of (or both) methods: process_provisioning,
    process_deployment. They can be used to change provisioning or
    deployment data just before running the action.
    
    Change-Id: Ie778c7ee6c9eb2abe2783b886ee6be481eccd2dc
    Implements: blueprint data-pipeline

--- nailgun/nailgun/test/unit/test_extensions.py
+++ nailgun/nailgun/test/unit/test_extensions.py
@@ -18,6 +18,7 @@ import mock
 
 from nailgun.errors import errors
 from nailgun.extensions import BaseExtension
+from nailgun.extensions import BaseExtensionPipeline
 from nailgun.extensions import fire_callback_on_cluster_delete
 from nailgun.extensions import fire_callback_on_node_collection_delete
 from nailgun.extensions import fire_callback_on_node_create
@@ -26,6 +27,9 @@ from nailgun.extensions import fire_callback_on_node_reset
 from nailgun.extensions import fire_callback_on_node_update
 from nailgun.extensions import get_extension
 from nailgun.extensions import node_extension_call
+from nailgun.orchestrator import deployment_graph
+from nailgun.orchestrator import deployment_serializers
+from nailgun.orchestrator import provisioning_serializers
 from nailgun.test.base import BaseTestCase
 
 
@@ -60,9 +64,9 @@ class TestBaseExtension(BaseTestCase):
             'ext_name-1.0.0')
 
 
-def make_mock_extensions():
+def make_mock_extensions(names=('ex1', 'ex2')):
     mocks = []
-    for name in ['ex1', 'ex2']:
+    for name in names:
         # NOTE(eli): since 'name' is reserved world
         # for mock constructor, we should assign
         # name explicitly
@@ -193,3 +197,245 @@ class TestExtensionUtils(BaseTestCase):
 
         for ext in get_m.return_value:
             ext.on_cluster_delete.assert_called_once_with(cluster)
+
+
+class TestPipeline(TestBaseExtension):
+
+    def _create_cluster_with_extensions(self):
+        self.env.create(
+            cluster_kwargs={'api': False},
+            nodes_kwargs=[{'roles': ['controller'], 'pending_addition': True}]
+        )
+        cluster = self.env.clusters[0]
+        cluster.extensions = [self.extension.name, 'volume_manager']
+        self.db.flush()
+
+        return cluster
+
+    @mock.patch('nailgun.orchestrator.deployment_serializers.'
+                'fire_callback_on_deployment_data_serialization')
+    def test_deployment_serialization(self, mfire_callback):
+        cluster = self._create_cluster_with_extensions()
+        graph = deployment_graph.AstuteGraph(cluster)
+        deployment_serializers.serialize(graph, cluster, cluster.nodes)
+
+        self.assertTrue(mfire_callback.called)
+        self.assertEqual(mfire_callback.call_args[1], {
+            'cluster': cluster,
+            'nodes': cluster.nodes,
+        })
+
+    @mock.patch.object(deployment_graph.AstuteGraph, 'deploy_task_serialize')
+    def test_deployment_serialization_ignore_customized(self, _):
+        cluster = self._create_cluster_with_extensions()
+        graph = deployment_graph.AstuteGraph(cluster)
+
+        # There are some changes in deployment data, and we don't ignore them
+        # so pipelines should not be called. Happens when user has done some
+        # changes to the data and run deployment.
+        data = [{"role": "ole"}]
+
+        with mock.patch('nailgun.orchestrator.deployment_serializers.'
+                        'fire_callback_on_deployment_data_serialization'
+                        ) as mfire_callback:
+            with mock.patch.object(
+                    cluster.nodes[0], 'replaced_deployment_info',
+                    new_callable=mock.Mock(return_value=data)):
+
+                deployment_serializers.serialize(graph, cluster, cluster.nodes)
+
+        self.assertFalse(mfire_callback.called)
+
+        # There are no changes in deployment data so pipelines can be called.
+        # We implicitly ignore any changes, but it doesn't matter here.
+        # Happens when user hasn't done any changes, and just run deploy.
+        with mock.patch('nailgun.orchestrator.deployment_serializers.'
+                        'fire_callback_on_deployment_data_serialization'
+                        ) as mfire_callback:
+            with mock.patch.object(
+                    cluster.nodes[0], 'replaced_deployment_info',
+                    new_callable=mock.Mock(return_value=[])):
+
+                deployment_serializers.serialize(graph, cluster, cluster.nodes)
+
+        self.assertTrue(mfire_callback.called)
+
+        # There are some changes in deployment data, but we ignore them,
+        # so pipelines can be called. Happens when user wants to download
+        # default deployment data after making some changes already.
+        with mock.patch('nailgun.orchestrator.deployment_serializers.'
+                        'fire_callback_on_deployment_data_serialization'
+                        ) as mfire_callback:
+            with mock.patch.object(
+                    cluster.nodes[0], 'replaced_deployment_info',
+                    new_callable=mock.Mock(return_value=data)):
+
+                deployment_serializers.serialize(
+                    graph, cluster, cluster.nodes, ignore_customized=True)
+
+        self.assertTrue(mfire_callback.called)
+
+        # There are no changes in deployment data. We implicitly don't ignore
+        # any changes, but because there is no changes pipelines can be called.
+        with mock.patch('nailgun.orchestrator.deployment_serializers.'
+                        'fire_callback_on_deployment_data_serialization'
+                        ) as mfire_callback:
+            with mock.patch.object(
+                    cluster.nodes[0], 'replaced_deployment_info',
+                    new_callable=mock.Mock(return_value=[])):
+
+                deployment_serializers.serialize(
+                    graph, cluster, cluster.nodes, ignore_customized=True)
+
+        self.assertTrue(mfire_callback.called)
+
+    def test_provisioning_serialization_ignore_customized(self):
+        cluster = self._create_cluster_with_extensions()
+
+        # There are some changes in provisioning data, and we don't ignore them
+        # so pipelines should not be called. Happens when user has done some
+        # changes to the data and run provisioning.
+        data = {"role": "ole"}
+
+        with mock.patch('nailgun.orchestrator.provisioning_serializers.'
+                        'fire_callback_on_provisioning_data_serialization'
+                        ) as mfire_callback:
+            with mock.patch.object(
+                    cluster.nodes[0], 'replaced_provisioning_info',
+                    new_callable=mock.Mock(return_value=data)):
+
+                provisioning_serializers.serialize(cluster, cluster.nodes)
+
+        self.assertFalse(mfire_callback.called)
+
+        # There are no changes in provisioning data so pipelines can be called.
+        # We implicitly ignore any changes, but it doesn't matter here.
+        # Happens when user hasn't done any changes, and just run deploy.
+        with mock.patch('nailgun.orchestrator.provisioning_serializers.'
+                        'fire_callback_on_provisioning_data_serialization'
+                        ) as mfire_callback:
+            with mock.patch.object(
+                    cluster.nodes[0], 'replaced_provisioning_info',
+                    new_callable=mock.Mock(return_value={})):
+
+                provisioning_serializers.serialize(cluster, cluster.nodes)
+
+        self.assertTrue(mfire_callback.called)
+
+        # There are some changes in provisioning data, but we ignore them,
+        # so pipelines can be called. Happens when user wants to download
+        # default provisioning data after making some changes already.
+        with mock.patch('nailgun.orchestrator.provisioning_serializers.'
+                        'fire_callback_on_provisioning_data_serialization'
+                        ) as mfire_callback:
+            with mock.patch.object(
+                    cluster.nodes[0], 'replaced_provisioning_info',
+                    new_callable=mock.Mock(return_value=data)):
+
+                provisioning_serializers.serialize(
+                    cluster, cluster.nodes, ignore_customized=True)
+
+        self.assertTrue(mfire_callback.called)
+
+        # There are no changes in provisioning data. We implicitly don't ignore
+        # any changes, but because there is no changes pipelines can be called.
+        with mock.patch('nailgun.orchestrator.provisioning_serializers.'
+                        'fire_callback_on_provisioning_data_serialization'
+                        ) as mfire_callback:
+            with mock.patch.object(
+                    cluster.nodes[0], 'replaced_provisioning_info',
+                    new_callable=mock.Mock(return_value={})):
+
+                provisioning_serializers.serialize(
+                    cluster, cluster.nodes, ignore_customized=True)
+
+        self.assertTrue(mfire_callback.called)
+
+    @mock.patch('nailgun.orchestrator.provisioning_serializers.'
+                'fire_callback_on_provisioning_data_serialization')
+    def test_provisioning_serialization(self, mfire_callback):
+        cluster = self._create_cluster_with_extensions()
+        provisioning_serializers.serialize(cluster, cluster.nodes)
+
+        self.assertTrue(mfire_callback.called)
+        self.assertEqual(mfire_callback.call_args[1], {
+            'cluster': cluster,
+            'nodes': cluster.nodes,
+        })
+
+    @mock.patch('nailgun.extensions.base.BaseExtensionPipeline.'
+                'process_provisioning')
+    def test_process_provisioning(self, mprocess):
+        cluster = self._create_cluster_with_extensions()
+        provisioning_serializers.serialize(cluster, cluster.nodes)
+        self.assertTrue(mprocess.called)
+
+    @mock.patch('nailgun.extensions.base.BaseExtensionPipeline.'
+                'process_deployment')
+    def test_process_deployment(self, mprocess):
+        cluster = self._create_cluster_with_extensions()
+        graph = deployment_graph.AstuteGraph(cluster)
+        deployment_serializers.serialize(graph, cluster, cluster.nodes)
+        self.assertTrue(mprocess.called)
+
+    def test_pipeline_change_data(self):
+        self.env.create(
+            cluster_kwargs={'api': False},
+            nodes_kwargs=[{'roles': ['controller'], 'pending_addition': True}]
+        )
+        cluster = self.env.clusters[0]
+        cluster.extensions = [self.extension.name]
+        self.db.flush()
+
+        class PipelinePlus1(BaseExtensionPipeline):
+
+            @classmethod
+            def process_provisioning(cls, data, **kwargs):
+                data['key'] += 1
+
+        class PipelinePlus2(BaseExtensionPipeline):
+
+            @classmethod
+            def process_provisioning(cls, data, **kwargs):
+                data['key'] += 2
+
+        class PipelinePlus3(BaseExtensionPipeline):
+
+            @classmethod
+            def process_deployment(cls, data, **kwargs):
+                data['key'] += 3
+
+        class Extension(BaseExtension):
+            name = 'ext_name'
+            version = '1.0.0'
+            description = 'ext description'
+            data_pipelines = (PipelinePlus1, PipelinePlus2, PipelinePlus3)
+
+        extension = Extension()
+
+        data = {'key': 0}
+
+        mserializer = mock.MagicMock()
+        mserializer.serialize.return_value = data
+
+        with mock.patch('nailgun.extensions.manager.get_all_extensions',
+                        return_value=[extension]):
+            with mock.patch('nailgun.orchestrator.provisioning_serializers.'
+                            'get_serializer_for_cluster',
+                            return_value=mserializer):
+                provisioning_serializers.serialize(cluster, cluster.nodes)
+
+        self.assertEqual(data['key'], 3)
+
+        temp_ser = mock.MagicMock()
+        temp_ser.serialize.return_value = data
+        mserializer.return_value = temp_ser
+        with mock.patch('nailgun.extensions.manager.get_all_extensions',
+                        return_value=[extension]):
+            with mock.patch('nailgun.orchestrator.deployment_serializers.'
+                            'get_serializer_for_cluster',
+                            return_value=mserializer):
+                graph = deployment_graph.AstuteGraph(cluster)
+                deployment_serializers.serialize(graph, cluster, cluster.nodes)
+
+        self.assertEqual(data['key'], 6)

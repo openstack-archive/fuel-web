ff9dcf167d7a923be4acbf8eff577c117e848a25
Ilya Kutukov <ikutukov@mirantis.com>
2016-01-28 19:49:06 +0300

    Plugin links handlers now returning 409 on existing url
    Previous behaviour allowed to create multiple links with the same url
    what was occurring during plugin tasks re-run.
    
    Now 409 error is returned when plugin- or cluster-level link creating
    or updating with the non-unique URL value.
    Plugin developers should handle such exception by themselves
    in plugin tasks.
    
    DocImpact
    Closes-Bug: #1538209
    
    Change-Id: I1de1e9f3957cbe11e91cd8cd4806ac1cd714d630

--- nailgun/nailgun/api/v1/validators/plugin_link.py
+++ nailgun/nailgun/api/v1/validators/plugin_link.py
@@ -13,32 +13,45 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-
 from nailgun.api.v1.validators.base import BasicValidator
 from nailgun.api.v1.validators.json_schema import plugin_link
+from nailgun.db import db
+from nailgun.errors import errors
 
 
 class PluginLinkValidator(BasicValidator):
     collection_schema = plugin_link.PLUGIN_LINKS_SCHEMA
 
     @classmethod
-    def validate(cls, data):
+    def _check_url_uniqueness(cls, data, model):
+        query = db().query(model).filter_by(
+            url=data["url"]
+        )
+        if db().query(query.exists()).scalar():
+            raise errors.AlreadyExists(
+                "Link with the same url {0} "
+                "already exists".format(data["url"]),
+                log_message=True
+            )
+
+    @classmethod
+    def validate(cls, data, model):
         parsed = super(PluginLinkValidator, cls).validate(data)
         cls.validate_schema(
             parsed,
             plugin_link.PLUGIN_LINK_SCHEMA
         )
+        cls._check_url_uniqueness(parsed, model)
         return parsed
 
     @classmethod
-    def validate_update(cls, data, instance):
+    def validate_update(cls, data, instance, model):
         parsed = super(PluginLinkValidator, cls).validate(data)
         cls.validate_schema(
             parsed,
             plugin_link.PLUGIN_LINK_UPDATE_SCHEMA
         )
+        new_url = parsed.get("url")
+        if new_url and new_url != instance.url:
+            cls._check_url_uniqueness(parsed, model)
         return parsed
-
-    @classmethod
-    def validate_create(cls, data):
-        return cls.validate(data)

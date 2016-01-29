00289bebd3a4a306738790e7f365a4dc8dc41dfb
Bogdan Dudko <bdudko@mirantis.com>
2016-01-29 17:57:31 +0200

    Updated navbar styles
    Change-Id: Iddc5c24bd67c8143f87992ff5358413b0363645c

--- nailgun/static/views/login_page.js
+++ nailgun/static/views/login_page.js
@@ -28,19 +28,20 @@ var LoginPage = React.createClass({
     hiddenLayout: true
   },
   render() {
+    var isMirantisIso = _.contains(app.version.get('feature_groups'), 'mirantis');
     return (
-      <div className='login-page'>
+      <div className={utils.classNames({'login-page': true, mirantis: isMirantisIso})}>
         <div className='container col-md-4 col-md-offset-4 col-xs-10 col-xs-offset-1'>
           <div className='box'>
-            <div className='logo-circle'></div>
-            <div className='logo'></div>
+            <div className='logo-circle' />
+            {!isMirantisIso && <div className='logo' />}
             <div className='fields-box'>
               <LoginForm />
             </div>
           </div>
         </div>
         <div className='footer col-xs-12'>
-          {_.contains(app.version.get('feature_groups'), 'mirantis') &&
+          {isMirantisIso &&
             <p className='text-center'>{i18n('common.copyright')}</p>
           }
           <p className='text-center'>{i18n('common.version')}: {app.version.get('release')}</p>

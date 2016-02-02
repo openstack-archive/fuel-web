8593ee24040d912b8455cd8f8e4c0585fe5cb18d
Denis Egorenko <degorenko@mirantis.com>
2016-02-02 15:48:30 +0300

    Remove Murano installation from Fuel from box
    Since Fuel 9.0 all functionality for Murano will be moved into
    plugin, hence we need remove it from Fuel.
    
    partially implements blueprint murano-fuel-plugin
    
    Change-Id: I553ac31db7609ee568b7a58ab84393722f2dba2d

---
+++ nailgun/nailgun/test/integration/test_orchestrator_serializer_90.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nailgun import consts
+from nailgun.db.sqlalchemy import models
+
+from nailgun.orchestrator.deployment_graph import AstuteGraph
+from nailgun.orchestrator.deployment_serializers import \
+    get_serializer_for_cluster
+from nailgun.test.integration.test_orchestrator_serializer_80 import \
+    TestDeploymentHASerializer80
+
+
+class TestDeploymentHASerializer90(TestDeploymentHASerializer80):
+
+    def setUp(self):
+        super(TestDeploymentHASerializer90, self).setUp()
+        self.cluster = self.env.create(
+            release_kwargs={'version': self.env_version},
+            cluster_kwargs={
+                'mode': consts.CLUSTER_MODES.ha_compact,
+                'net_provider': consts.CLUSTER_NET_PROVIDERS.neutron,
+                'net_segment_type': consts.NEUTRON_SEGMENT_TYPES.vlan})
+        self.cluster_db = self.db.query(models.Cluster).get(self.cluster['id'])
+        serializer_type = get_serializer_for_cluster(self.cluster_db)
+        self.serializer = serializer_type(AstuteGraph(self.cluster_db))
+
+    def test_block_device_disks(self):
+        glance_properties = self.serializer.generate_test_vm_image_data(
+            self.env.nodes[0])['test_vm_image']['glance_properties']
+        self.assertNotIn('murano_image_info', glance_properties)

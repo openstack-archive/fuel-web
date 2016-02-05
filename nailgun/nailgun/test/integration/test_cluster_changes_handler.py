a20f7753fa0cee1cee40c2bd8a8dee63f366ed54
Vitalii Myhal <vm198r@att.com>
2016-02-05 12:55:17 -0600

    Add --force flag for `fuel deploy-changes` command
    The flag allows to apply the changes to the cluster if the cluster
    is in the operational state. This allows you to deploy the changes
    without the reprovisioning procedure.
    
    Example: `fuel deploy-changes --env <env_id> --force`
    
    Partial-Bug: 1540558
    Change-Id: Ibc89fdbfbd0a36a890412cd8e861d35bcf930690

--- nailgun/nailgun/test/integration/test_cluster_changes_handler.py
+++ nailgun/nailgun/test/integration/test_cluster_changes_handler.py
@@ -1827,3 +1827,38 @@ class TestHandlers(BaseIntegrationTest):
 
         supertask = self.env.launch_deployment()
         self.env.wait_ready(supertask, timeout=60)
+
+    @fake_tasks()
+    def test_force_deploy(self):
+        self.env.create(
+            nodes_kwargs=[{'name': '', 'pending_addition': True}]
+        )
+
+        def _send_request():
+            return self.app.put(
+                reverse(
+                    'ClusterChangesHandler',
+                    kwargs={'cluster_id': self.env.clusters[0].id}
+                ),
+                headers=self.default_headers,
+                expect_errors=True
+            )
+
+        # Initial deployment
+        task = self.env.launch_deployment()
+        self.env.wait_ready(task, timeout=60)
+
+        # Try redeploy
+        resp = _send_request()
+        self.assertEqual(resp.status_code, 400)
+        self.assertEqual(resp.json_body.get('message'), 'No changes to deploy')
+
+        # Try force deploy
+        with patch('web.input') as mock_input:
+            mock_input(force=None).force = True
+            resp = _send_request()
+            self.assertEqual(resp.status_code, 202)
+            self.assertEqual(resp.json_body.get('name'),
+                             consts.TASK_NAMES.deploy)
+            self.assertEqual(resp.json_body.get('status'),
+                             consts.TASK_STATUSES.pending)

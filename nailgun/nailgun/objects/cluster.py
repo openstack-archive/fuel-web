9e32f611c56673f12093e5af968e8d4957b28c88
Artem Roma <aroma@mirantis.com>
2016-02-09 11:33:07 +0200

    Add allocation of VIPs at the creation of a cluster
    * VIP allocation added to objects.Cluster.create method;
    * new unit tests added;
    * existing tests updated.
    
    Change-Id: I8d502e8f3733d2e0b91069a4abeafc016005d944
    Blueprint: set-vip-address-via-api

--- nailgun/nailgun/objects/cluster.py
+++ nailgun/nailgun/objects/cluster.py
@@ -169,8 +169,9 @@ class Cluster(NailgunObject):
         cls.create_default_extensions(cluster)
 
         try:
-            cls.get_network_manager(cluster).\
-                create_network_groups_and_config(cluster, data)
+            net_manager = cls.get_network_manager(cluster)
+            net_manager.create_network_groups_and_config(cluster, data)
+
             cls.add_pending_changes(
                 cluster, consts.CLUSTER_CHANGES.attributes)
             cls.add_pending_changes(
@@ -180,10 +181,17 @@ class Cluster(NailgunObject):
 
             if assign_nodes:
                 cls.update_nodes(cluster, assign_nodes)
+
+            net_manager.assign_vips_for_net_groups(cluster)
+
         except (
             errors.OutOfVLANs,
             errors.OutOfIPs,
-            errors.NoSuitableCIDR
+            errors.NoSuitableCIDR,
+
+            # VIP assignment related errors
+            errors.CanNotFindCommonNodeGroup,
+            errors.CanNotFindNetworkForNodeGroup
         ) as exc:
             raise errors.CannotCreate(exc.message)
 

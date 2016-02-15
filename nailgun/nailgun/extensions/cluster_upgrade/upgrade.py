20f2ddbb506bebce1e55d7dfb56af25072d28121
Ilya Kharin <akscram@gmail.com>
2016-02-14 23:15:32 -0600

    Reassign nodes without reinstallation
    In some upgrade scenarios when shadow environments are used some of
    nodes should not be reprovisioned during this procedure. It is useful in
    combination when control plane nodes are reprovisioned and data plane
    nodes are updated in place.
    
    The update_cluster_assignment method of the Node class was changed to
    accept roles and pending_roles for node during the reassignment. It
    allows to specify proper roles by the upgrade extention. kernel_params
    is preserved because there is no possibility to change this parameter
    for already deployed nodes.
    
    The NodeReassignHandler handler accepts two additional parameters:
      - reprovision = True (default) - allows to skip the reprovision step
      - roles = [] (default) - allows to specify new roles
    
    Change-Id: Iedb20a904e58f5b9a86eb47de8d8d317dc3cc61b
    Blueprint: upgrade-major-openstack-environment

--- nailgun/nailgun/extensions/cluster_upgrade/upgrade.py
+++ nailgun/nailgun/extensions/cluster_upgrade/upgrade.py
@@ -169,7 +169,7 @@ class UpgradeHelper(object):
         new_net_manager.assign_vips_for_net_groups()
 
     @classmethod
-    def assign_node_to_cluster(cls, node, seed_cluster):
+    def assign_node_to_cluster(cls, node, seed_cluster, roles, pending_roles):
         orig_cluster = adapters.NailgunClusterAdapter.get_by_uid(
             node.cluster_id)
 
@@ -178,7 +178,7 @@ class UpgradeHelper(object):
         netgroups_id_mapping = cls.get_netgroups_id_mapping(
             orig_cluster, seed_cluster)
 
-        node.update_cluster_assignment(seed_cluster)
+        node.update_cluster_assignment(seed_cluster, roles, pending_roles)
         objects.Node.set_netgroups_ids(node, netgroups_id_mapping)
         orig_manager.set_nic_assignment_netgroups_ids(
             node, netgroups_id_mapping)

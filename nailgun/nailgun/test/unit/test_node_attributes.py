5ebb8e8b88af739d4b54e43e7f114888c17c0d97
Artur Svechnikov <asvechnikov@mirantis.com>
2016-02-16 14:43:42 +0300

    Extend kernel options with cpu pinning
    * Add isolcpus to kernel with list of isolated
    cpu ids.
    * Refactor and fix copyrights from previous patch
      https://review.openstack.org/#/c/281802/
    
    Change-Id: Ia1abf6c2c2bcd6d2d76ca8e2db36de5ac3812373
    Implements: blueprint support-numa-cpu-pinning

--- nailgun/nailgun/test/unit/test_node_attributes.py
+++ nailgun/nailgun/test/unit/test_node_attributes.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#    Copyright 2014 Mirantis, Inc.
+#    Copyright 2016 Mirantis, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain

029c721c686d45ea342bcfbf55e2ff137f482a0c
Aleksey Kasatkin <akasatkin@mirantis.com>
2016-02-17 13:15:39 +0200

    Accept SR-IOV data and save it in interface properties
    Now SR-IOV related data from nailgun-agent and API is stored in
    NodeNICInterface.interface_properties. It's also serialized back for API.
    
    Change-Id: I5bb57cf53a9f508233e9757b6d698dfb930c9d19
    Blueprint: support-sriov

--- nailgun/nailgun/test/integration/test_node_nic_handler.py
+++ nailgun/nailgun/test/integration/test_node_nic_handler.py
@@ -247,9 +247,17 @@ class TestHandlers(BaseIntegrationTest):
         node = self.env.create_node(api=True, meta=meta)
         new_meta = self.env.default_metadata()
         self.env.set_interfaces_in_meta(new_meta, [
-            {'name': 'new_nic', 'mac': '00:00:00:00:00:00',
-             'current_speed': 10, 'max_speed': 10, 'state': 'down'}])
-        node_data = {'mac': node['mac'], 'meta': new_meta}
+            {'name': 'new_nic',
+             'mac': '00:00:00:00:00:00',
+             'current_speed': 10,
+             'max_speed': 10,
+             'state': 'down',
+             'sriov': {
+                 'sriov_totalvfs': 8,
+                 'available': True,
+                 'pci_id': '1234:5678'
+             }}])
+        node_data = {'mac': node['mac'], 'meta': new_meta, 'is_agent': True}
         resp = self.app.put(
             reverse('NodeAgentHandler'),
             jsonutils.dumps(node_data),
@@ -267,6 +275,15 @@ class TestHandlers(BaseIntegrationTest):
         self.assertEqual(resp_nic['current_speed'], nic['current_speed'])
         self.assertEqual(resp_nic['max_speed'], nic['max_speed'])
         self.assertEqual(resp_nic['state'], nic['state'])
+        self.assertEqual(
+            resp_nic['interface_properties']['sriov'],
+            {
+                'enabled': False,
+                'sriov_numvfs': 0,
+                'sriov_totalvfs': 8,
+                'available': True,
+                'pci_id': '1234:5678'
+            })
         for conn in ('assigned_networks', ):
             self.assertEqual(resp_nic[conn], [])
 
@@ -462,9 +479,10 @@ class TestHandlers(BaseIntegrationTest):
             headers=self.default_headers)
         self.assertEqual(resp.status_code, 200)
         nic = resp.json_body[0]
-        self.assertEqual(nic['interface_properties'],
-                         {'disable_offloading': False,
-                          'mtu': None})
+        self.assertEqual(
+            nic['interface_properties'],
+            self.env.network_manager.get_default_interface_properties()
+        )
         # change mtu
         nic['interface_properties']['mtu'] = 1500
         nodes_list = [{'id': node['id'], 'interfaces': [nic]}]
@@ -486,9 +504,7 @@ class TestHandlers(BaseIntegrationTest):
             headers=self.default_headers)
         self.assertEqual(resp.status_code, 200)
         resp_nic = resp.json_body[0]
-        self.assertEqual(resp_nic['interface_properties'],
-                         {'disable_offloading': False,
-                          'mtu': 1500})
+        self.assertEqual(resp_nic['interface_properties']['mtu'], 1500)
 
     def test_nic_adds_by_agent(self):
         meta = self.env.default_metadata()

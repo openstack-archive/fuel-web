ade20916709e0485dafe4fe9ce49bf2fff50835f
Alexander Saprykin <asaprykin@mirantis.com>
2016-02-24 15:50:16 +0100

    Add cpu pinning to deployment info
    Change-Id: I245faef907ea366d91c8c4a166ef3678204025c1
    Implements: blueprint support-numa-cpu-pinning

--- nailgun/nailgun/test/integration/test_orchestrator_serializer_90.py
+++ nailgun/nailgun/test/integration/test_orchestrator_serializer_90.py
@@ -14,11 +14,17 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from nailgun import consts
+from nailgun import objects
+from nailgun.db.sqlalchemy import models
+
+from nailgun.orchestrator.deployment_graph import AstuteGraph
+from nailgun.orchestrator.deployment_serializers import \
+    get_serializer_for_cluster
 from nailgun.test.integration.test_orchestrator_serializer_80 import \
     TestDeploymentHASerializer80
-
-from nailgun.test.integration.test_orchestrator_serializer_80 import \
-    TestDeploymentAttributesSerialization80
+from nailgun.test.integration.test_orchestrator_serializer import \
+    BaseDeploymentSerializer
 
 
 class TestSerializer90Mixin(object):
@@ -27,9 +33,47 @@ class TestSerializer90Mixin(object):
 
 class TestDeploymentAttributesSerialization90(
     TestSerializer90Mixin,
-    TestDeploymentAttributesSerialization80
+    BaseDeploymentSerializer
 ):
-    pass
+
+    def setUp(self):
+        super(TestDeploymentAttributesSerialization90, self).setUp()
+        self.cluster = self.env.create(
+            release_kwargs={
+                'version': self.env_version,
+                'operating_system': consts.RELEASE_OS.ubuntu},
+            cluster_kwargs={
+                'mode': consts.CLUSTER_MODES.ha_compact,
+                'net_provider': consts.CLUSTER_NET_PROVIDERS.neutron,
+                'net_segment_type': consts.NEUTRON_SEGMENT_TYPES.vlan})
+        self.cluster_db = self.db.query(models.Cluster).get(self.cluster['id'])
+        serializer_type = get_serializer_for_cluster(self.cluster_db)
+        self.serializer = serializer_type(AstuteGraph(self.cluster_db))
+
+    def test_attributes_cpu_pinning(self):
+        meta = {'numa_topology': {
+            'numa_nodes': [{'id': 1, 'cpus': [1, 2, 3, 4]},
+                           {'id': 2, 'cpus': [5, 6, 7, 8]}]
+        }}
+        node = self.env.create_node(cluster_id=self.cluster_db.id,
+                                    roles=['compute'],
+                                    meta=meta)
+        node.attributes.update({
+            'cpu_pinning': {
+                'nova': {'value': 2},
+                'dpdk': {'value': 2},
+            }
+        })
+
+        objects.Cluster.prepare_for_deployment(self.cluster_db)
+        serialized_for_astute = self.serializer.serialize(
+            self.cluster_db, self.cluster_db.nodes)
+
+        serialized_node=  serialized_for_astute[0]
+        self.assertEqual(serialized_node['dpdk']['ovs_core_mask'], '0x2')
+        self.assertEqual(serialized_node['dpdk']['ovs_pmd_core_mask'], '0x4')
+        self.assertEqual(serialized_node['nova']['enable_cpu_pinning'], True)
+        self.assertEqual(serialized_node['nova']['cpu_pinning'], [3, 4])
 
 
 class TestDeploymentHASerializer90(

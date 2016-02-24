a95c8e192cf00ce7b3a03476c5eb68e5e6b783a4
Fedor Zhadaev <fzhadaev@mirantis.com>
2016-02-24 20:25:42 +0300

    Add DPDK support for node interfaces via API
     * Add list of supported DPDK drivers to openstack.yaml
     * Add correct transformations for enabling DPDK
     * Add handling of DPDK data received from nailgun-agent
    
    Implements blueprint support-dpdk
    
    Change-Id: I13c3b45fbc4fc43efbb9fca0ee994a8720138b38

--- nailgun/nailgun/test/integration/test_orchestrator_serializer_90.py
+++ nailgun/nailgun/test/integration/test_orchestrator_serializer_90.py
@@ -14,6 +14,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import mock
 import six
 
 from nailgun import consts
@@ -59,6 +60,131 @@ class TestDeploymentAttributesSerialization90(
     TestSerializer90Mixin,
     TestDeploymentAttributesSerialization80
 ):
+    @mock.patch('nailgun.objects.Node.get_supported_dpdk_drivers')
+    def test_serialization_with_dpdk(self, drivers_mock):
+        drivers_mock.return_value = {
+            'driver_1': ['test_id:1', 'test_id:2']
+        }
+        node = self.env.create_nodes_w_interfaces_count(
+            1, 3,
+            cluster_id=self.cluster_db.id,
+            roles=['compute'])[0]
+
+        nic_1 = node.interfaces[0]
+        nic_2 = node.interfaces[2]
+        nets_1 = nic_1.assigned_networks_list
+        nets_2 = nic_2.assigned_networks_list
+
+        dpdk_interface_name = nic_2.name
+
+        for i, net in enumerate(nets_1):
+            if net['name'] == 'private':
+                nets_2.append(nets_1.pop(i))
+                break
+        objects.NIC.assign_networks(nic_1, nets_1)
+        objects.NIC.assign_networks(nic_2, nets_2)
+
+        objects.NIC.update(nic_2,
+                           {'interface_properties':
+                               {
+                                   'dpdk': {'enabled': True,
+                                            'available': True},
+                                   'pci_id': 'test_id:2'
+                               }})
+        objects.Cluster.prepare_for_deployment(self.cluster_db)
+
+        serialised_for_astute = self.serializer.serialize(
+            self.cluster_db, self.cluster_db.nodes)
+        self.assertEqual(len(serialised_for_astute), 1)
+        node = serialised_for_astute[0]
+        dpdk = node.get('dpdk')
+        self.assertIsNotNone(dpdk)
+        self.assertTrue(dpdk.get('enabled'))
+
+        transformations = node['network_scheme']['transformations']
+        private_br = filter(lambda t: t.get('name') ==
+                            consts.DEFAULT_BRIDGES_NAMES.br_prv,
+                            transformations)[0]
+        dpdk_port = filter(lambda t: t.get('bridge') ==
+                           consts.DEFAULT_BRIDGES_NAMES.br_prv,
+                           transformations)[0]
+        self.assertEqual(private_br.get('vendor_specific'),
+                         {'datapath_type': 'netdev'})
+        self.assertEqual(dpdk_port.get('provider'), 'dpdkovs')
+
+        interfaces = node['network_scheme']['interfaces']
+        dpdk_interface = interfaces[dpdk_interface_name]
+        vendor_specific = dpdk_interface.get('vendor_specific', {})
+        self.assertEqual(vendor_specific.get('dpdk_driver'), 'driver_1')
+
+    @mock.patch('nailgun.objects.Node.get_supported_dpdk_drivers')
+    def test_serialization_with_dpdk_on_bond(self, drivers_mock):
+        drivers_mock.return_value = {
+            'driver_1': ['test_id:1', 'test_id:2']
+        }
+        node = self.env.create_nodes_w_interfaces_count(
+            1, 4,
+            cluster_id=self.cluster_db.id,
+            roles=['compute'])[0]
+
+        for iface in node.interfaces:
+            iface['interface_properties'].update({'pci_id': 'test_id:1'})
+
+        interfaces = self.env.node_nics_get(node.id).json_body
+
+        first_nic = interfaces[0]
+        nics_for_bond = [interfaces.pop(), interfaces.pop()]
+
+        networks_for_bond = []
+        bond_interface_name = 'bond0'
+
+        first_nic_networks = first_nic['assigned_networks']
+        for i, net in enumerate(first_nic_networks):
+            if net['name'] == 'private':
+                networks_for_bond.append(first_nic_networks.pop(i))
+                break
+
+        interfaces.append(
+            {
+                'name': bond_interface_name,
+                'type': consts.NETWORK_INTERFACE_TYPES.bond,
+                'mode': consts.BOND_MODES.balance_slb,
+                'slaves': nics_for_bond,
+                'assigned_networks': networks_for_bond,
+                'interface_properties':
+                    {
+                        'dpdk': {'enabled': True}
+                    }
+            }
+        )
+        self.env.node_nics_put(node.id, interfaces)
+        objects.Cluster.prepare_for_deployment(self.cluster_db)
+
+        serialised_for_astute = self.serializer.serialize(
+            self.cluster_db, self.cluster_db.nodes)
+        self.assertEqual(len(serialised_for_astute), 1)
+        node = serialised_for_astute[0]
+        dpdk = node.get('dpdk')
+        self.assertIsNotNone(dpdk)
+        self.assertTrue(dpdk.get('enabled'))
+
+        transformations = node['network_scheme']['transformations']
+        private_br = filter(lambda t: t.get('name') ==
+                            consts.DEFAULT_BRIDGES_NAMES.br_prv,
+                            transformations)[0]
+        dpdk_port = filter(lambda t: t.get('bridge') ==
+                           consts.DEFAULT_BRIDGES_NAMES.br_prv,
+                           transformations)[0]
+        self.assertEqual(private_br.get('vendor_specific'),
+                         {'datapath_type': 'netdev'})
+        self.assertEqual(dpdk_port.get('provider'), 'dpdkovs')
+
+        interfaces = node['network_scheme']['interfaces']
+        for iface in nics_for_bond:
+            dpdk_interface = interfaces[iface['name']]
+            vendor_specific = dpdk_interface.get('vendor_specific', {})
+            self.assertEqual(vendor_specific.get('dpdk_driver'), 'driver_1')
+
     def test_attributes_cpu_pinning(self):
         numa_nodes = [
             {'id': 0, 'memory': 2 ** 31, 'cpus': [1, 2, 3, 4]},

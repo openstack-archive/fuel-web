7ef56387039f8a47b61a42ffae2417d86bf3cf9a
Andriy Popovych <popovych.andrey@gmail.com>
2016-02-26 14:48:58 +0200

    DB model changes for Plugin NIC, BOND and Node attributes
    This patch:
      * extend current DB model with new entities and provide all
        required migrations.
      * extend plugin sync method to support storing new plugin
        attributes.
    
    Change-Id: I3edbde1d48461ce3fab7c93f17e2db5332b1f7fb
    Implements: blueprint nics-and-nodes-attributes-via-plugin

--- nailgun/nailgun/db/sqlalchemy/models/plugins.py
+++ nailgun/nailgun/db/sqlalchemy/models/plugins.py
@@ -52,6 +52,68 @@ class ClusterPlugins(Base):
                         server_default='{}')
 
 
+class NodeNICInterfaceClusterPlugin(Base):
+
+    __tablename__ = 'node_nic_interface_cluster_plugins'
+    id = Column(Integer, primary_key=True)
+    cluster_plugin_id = Column(
+        Integer,
+        ForeignKey('cluster_plugins.id', ondelete='CASCADE'),
+        nullable=False)
+    interface_id = Column(
+        Integer,
+        ForeignKey('node_nic_interfaces.id', ondelete='CASCADE'),
+        nullable=False)
+    node_id = Column(
+        Integer,
+        ForeignKey('nodes.id', ondelete='CASCADE'),
+        nullable=False)
+    attributes = Column(
+        MutableDict.as_mutable(JSON),
+        nullable=False,
+        server_default='{}')
+
+
+class NodeBondInterfaceClusterPlugin(Base):
+
+    __tablename__ = 'node_bond_interface_cluster_plugins'
+    id = Column(Integer, primary_key=True)
+    cluster_plugin_id = Column(
+        Integer,
+        ForeignKey('cluster_plugins.id', ondelete='CASCADE'),
+        nullable=False)
+    bond_id = Column(
+        Integer,
+        ForeignKey('node_bond_interfaces.id', ondelete='CASCADE'),
+        nullable=False)
+    node_id = Column(
+        Integer,
+        ForeignKey('nodes.id', ondelete='CASCADE'),
+        nullable=False)
+    attributes = Column(
+        MutableDict.as_mutable(JSON),
+        nullable=False,
+        server_default='{}')
+
+
+class NodeClusterPlugin(Base):
+
+    __tablename__ = 'node_cluster_plugins'
+    id = Column(Integer, primary_key=True)
+    cluster_plugin_id = Column(
+        Integer,
+        ForeignKey('cluster_plugins.id', ondelete='CASCADE'),
+        nullable=False)
+    node_id = Column(
+        Integer,
+        ForeignKey('nodes.id', ondelete='CASCADE'),
+        nullable=False)
+    attributes = Column(
+        MutableDict.as_mutable(JSON),
+        nullable=False,
+        server_default='{}')
+
+
 class Plugin(Base):
 
     __tablename__ = 'plugins'
@@ -83,6 +145,12 @@ class Plugin(Base):
         MutableDict.as_mutable(JSON), server_default='{}', nullable=False)
     network_roles_metadata = Column(
         MutableList.as_mutable(JSON), server_default='[]', nullable=False)
+    nic_attributes_metadata = Column(
+        MutableDict.as_mutable(JSON), server_default='{}', nullable=False)
+    bond_attributes_metadata = Column(
+        MutableDict.as_mutable(JSON), server_default='{}', nullable=False)
+    node_attributes_metadata = Column(
+        MutableDict.as_mutable(JSON), server_default='{}', nullable=False)
     components_metadata = Column(
         MutableList.as_mutable(JSON), server_default='[]')
     # TODO(apopovych): To support old plugins versions we need separate

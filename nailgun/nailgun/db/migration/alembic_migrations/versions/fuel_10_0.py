0f242a6bb1b80505152e58f5df82c44c86d1e727
Andriy Popovych <popovych.andrey@gmail.com>
2016-02-26 14:48:58 +0200

    Data model changes for Plugin NIC, BOND and Node attributes
    This patch:
      * extends current DB model with new entities and provides
        related migrations.
      * extends plugin sync method to support storing new plugins
        attributes.
      * providse cosmetic fix for ClusterPlugin model. Lets write table names
        in the plural but model names in the singular.
    
    Change-Id: I3edbde1d48461ce3fab7c93f17e2db5332b1f7fb
    Implements: blueprint nics-and-nodes-attributes-via-plugin

--- nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_10_0.py
+++ nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_10_0.py
@@ -19,10 +19,11 @@ Revises: 675105097a69
 Create Date: 2016-04-08 15:20:43.989472
 
 """
-from nailgun.db.sqlalchemy.models import fields
 
 from alembic import op
 import sqlalchemy as sa
+
+from nailgun.db.sqlalchemy.models import fields
 
 
 # revision identifiers, used by Alembic.
@@ -33,9 +34,11 @@ down_revision = '675105097a69'
 def upgrade():
     upgrade_tasks_snapshot()
     upgrade_plugin_links_constraints()
+    upgrade_plugin_with_nics_and_nodes_attributes()
 
 
 def downgrade():
+    downgrade_plugin_with_nics_and_nodes_attributes()
     downgrade_plugin_links_constraints()
     downgrade_tasks_snapshot()
 
@@ -98,3 +101,153 @@ def upgrade_tasks_snapshot():
 
 def downgrade_tasks_snapshot():
     op.drop_column('tasks', 'tasks_snapshot')
+
+
+def upgrade_plugin_with_nics_and_nodes_attributes():
+    op.add_column(
+        'plugins',
+        sa.Column(
+            'nic_attributes_metadata',
+            fields.JSON(),
+            nullable=False,
+            server_default='{}'
+        )
+    )
+
+    op.add_column(
+        'plugins',
+        sa.Column(
+            'bond_attributes_metadata',
+            fields.JSON(),
+            nullable=False,
+            server_default='{}'
+        )
+    )
+
+    op.add_column(
+        'plugins',
+        sa.Column(
+            'node_attributes_metadata',
+            fields.JSON(),
+            nullable=False,
+            server_default='{}'
+        )
+    )
+
+    op.add_column(
+        'node_nic_interfaces',
+        sa.Column(
+            'attributes',
+            fields.JSON(),
+            nullable=False,
+            server_default='{}'
+        )
+    )
+
+    op.add_column(
+        'node_nic_interfaces',
+        sa.Column(
+            'meta',
+            fields.JSON(),
+            nullable=False,
+            server_default='{}'
+        )
+    )
+
+    op.add_column(
+        'node_bond_interfaces',
+        sa.Column(
+            'attributes',
+            fields.JSON(),
+            nullable=False,
+            server_default='{}'
+        )
+    )
+
+    op.add_column(
+        'releases',
+        sa.Column(
+            'nic_attributes',
+            fields.JSON(),
+            nullable=False,
+            server_default='{}'
+        )
+    )
+
+    op.add_column(
+        'releases',
+        sa.Column(
+            'bond_attributes',
+            fields.JSON(),
+            nullable=False,
+            server_default='{}'
+        )
+    )
+
+    op.create_table(
+        'node_nic_interface_cluster_plugins',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column(
+            'attributes', fields.JSON(), nullable=False, server_default='{}'),
+        sa.Column('cluster_plugin_id', sa.Integer(), nullable=False),
+        sa.Column('interface_id', sa.Integer(), nullable=False),
+        sa.Column('node_id', sa.Integer(), nullable=False),
+        sa.PrimaryKeyConstraint('id'),
+        sa.ForeignKeyConstraint(
+            ['cluster_plugin_id'],
+            ['cluster_plugins.id'],
+            ondelete='CASCADE'),
+        sa.ForeignKeyConstraint(
+            ['interface_id'], ['node_nic_interfaces.id'], ondelete='CASCADE'),
+        sa.ForeignKeyConstraint(
+            ['node_id'], ['nodes.id'], ondelete='CASCADE')
+    )
+
+    op.create_table(
+        'node_bond_interface_cluster_plugins',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column(
+            'attributes', fields.JSON(), nullable=False, server_default='{}'),
+        sa.Column('cluster_plugin_id', sa.Integer(), nullable=False),
+        sa.Column('bond_id', sa.Integer(), nullable=False),
+        sa.Column('node_id', sa.Integer(), nullable=False),
+        sa.PrimaryKeyConstraint('id'),
+        sa.ForeignKeyConstraint(
+            ['cluster_plugin_id'],
+            ['cluster_plugins.id'],
+            ondelete='CASCADE'),
+        sa.ForeignKeyConstraint(
+            ['bond_id'], ['node_bond_interfaces.id'], ondelete='CASCADE'),
+        sa.ForeignKeyConstraint(
+            ['node_id'], ['nodes.id'], ondelete='CASCADE')
+    )
+
+    op.create_table(
+        'node_cluster_plugins',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column(
+            'attributes', fields.JSON(), nullable=False, server_default='{}'),
+        sa.Column('cluster_plugin_id', sa.Integer(), nullable=False),
+        sa.Column('node_id', sa.Integer(), nullable=False),
+        sa.PrimaryKeyConstraint('id'),
+        sa.ForeignKeyConstraint(
+            ['cluster_plugin_id'],
+            ['cluster_plugins.id'],
+            ondelete='CASCADE'),
+        sa.ForeignKeyConstraint(
+            ['node_id'], ['nodes.id'], ondelete='CASCADE')
+    )
+
+
+def downgrade_plugin_with_nics_and_nodes_attributes():
+    op.drop_table('node_cluster_plugins')
+    op.drop_table('node_bond_interface_cluster_plugins')
+    op.drop_table('node_nic_interface_cluster_plugins')
+    op.drop_column('releases', 'bond_attributes')
+    op.drop_column('releases', 'nic_attributes')
+    op.drop_column('node_bond_interfaces', 'attributes')
+    op.drop_column('node_nic_interfaces', 'meta')
+    op.drop_column('node_nic_interfaces', 'attributes')
+    op.drop_column('plugins', 'node_attributes_metadata')
+    op.drop_column('plugins', 'bond_attributes_metadata')
+    op.drop_column('plugins', 'nic_attributes_metadata')
--- nailgun/nailgun/test/unit/test_migration_fuel_10_0.py
+++ nailgun/nailgun/test/unit/test_migration_fuel_10_0.py
@@ -12,6 +12,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import datetime
+
 import alembic
 from oslo_serialization import jsonutils
 import sqlalchemy as sa
@@ -68,10 +70,95 @@ def prepare():
         meta.tables['releases'].insert(),
         [{
             'name': 'test_name',
-            'version': '2015.1-8.0',
+            'version': '2015.1-10.0',
             'operating_system': 'ubuntu',
             'state': 'available',
+            'deployment_tasks': jsonutils.dumps(JSON_TASKS),
+            'roles': jsonutils.dumps([
+                'controller',
+                'compute',
+                'virt',
+                'compute-vmware',
+                'ironic',
+                'cinder',
+                'cinder-block-device',
+                'cinder-vmware',
+                'ceph-osd',
+                'mongo',
+                'base-os',
+            ]),
+            'roles_metadata': jsonutils.dumps({
+                'controller': {
+                    'name': 'Controller',
+                },
+                'compute': {
+                    'name': 'Compute',
+                },
+                'virt': {
+                    'name': 'Virtual',
+                },
+                'compute-vmware': {
+                    'name': 'Compute VMware',
+                },
+                'ironic': {
+                    'name': 'Ironic',
+                },
+                'cinder': {
+                    'name': 'Cinder',
+                },
+                'cinder-block-device': {
+                    'name': 'Cinder Block Device',
+                },
+                'cinder-vmware': {
+                    'name': 'Cinder Proxy to VMware Datastore',
+                },
+                'ceph-osd': {
+                    'name': 'Ceph OSD',
+                },
+                'mongo': {
+                    'name': 'Telemetry - MongoDB',
+                },
+                'base-os': {
+                    'name': 'Operating System',
+                }
+            }),
+            'networks_metadata': jsonutils.dumps({
+                'neutron': {
+                    'networks': [
+                        {
+                            'assign_vip': True,
+                        },
+                    ]
+                },
+                'nova_network': {
+                    'networks': [
+                        {
+                            'assign_vip': False,
+                        },
+                    ]
+                },
+
+            }),
+            'network_roles_metadata': jsonutils.dumps([{
+                'id': 'admin/vip',
+                'default_mapping': 'fuelweb_admin',
+                'properties': {
+                    'subnet': True,
+                    'gateway': False,
+                    'vip': [
+                        {
+                            'name': 'release-vip1',
+                        },
+                        {
+                            'name': 'release-vip2',
+                            'namespace': 'release-vip2-namespace'
+                        }
+                    ]
+                }
+            }]),
+            'is_deployable': True,
         }])
+
     release_id = result.inserted_primary_key[0]
 
     cluster_ids = []
@@ -85,9 +172,24 @@ def prepare():
                 'status': 'new',
                 'net_provider': 'neutron',
                 'grouping': 'roles',
-                'fuel_version': '10.0'
+                'fuel_version': '10.0',
+                'deployment_tasks': jsonutils.dumps(JSON_TASKS)
             }])
         cluster_ids.append(result.inserted_primary_key[0])
+
+    result = db.execute(
+        meta.tables['nodes'].insert(),
+        [{
+            'uuid': '26b508d0-0d76-4159-bce9-f67ec2765480',
+            'cluster_id': None,
+            'group_id': None,
+            'status': 'discover',
+            'meta': '{}',
+            'mac': 'aa:aa:aa:aa:aa:aa',
+            'timestamp': datetime.datetime.utcnow(),
+        }]
+    )
+    node_id = result.inserted_primary_key[0]
 
     result = db.execute(
         meta.tables['plugins'].insert(),
@@ -97,7 +199,7 @@ def prepare():
             'version': '2.0.0',
             'description': 'Test plugin A for Fuel',
             'homepage': 'http://fuel_plugins.test_plugin.com',
-            'package_version': '4.0.0',
+            'package_version': '5.0.0',
             'groups': jsonutils.dumps(['tgroup']),
             'authors': jsonutils.dumps(['tauthor']),
             'licenses': jsonutils.dumps(['tlicense']),
@@ -105,7 +207,7 @@ def prepare():
                 {'repository_path': 'repositories/ubuntu'}
             ]),
             'deployment_tasks': jsonutils.dumps(JSON_TASKS),
-            'fuel_version': jsonutils.dumps(['8.0']),
+            'fuel_version': jsonutils.dumps(['10.0']),
             'network_roles_metadata': jsonutils.dumps([{
                 'id': 'admin/vip',
                 'default_mapping': 'fuelweb_admin',
@@ -136,14 +238,14 @@ def prepare():
             'version': '2.0.0',
             'description': 'Test plugin B for Fuel',
             'homepage': 'http://fuel_plugins.test_plugin.com',
-            'package_version': '4.0.0',
+            'package_version': '5.0.0',
             'groups': jsonutils.dumps(['tgroup']),
             'authors': jsonutils.dumps(['tauthor']),
             'licenses': jsonutils.dumps(['tlicense']),
             'releases': jsonutils.dumps([
                 {'repository_path': 'repositories/ubuntu'}
             ]),
-            'fuel_version': jsonutils.dumps(['8.0']),
+            'fuel_version': jsonutils.dumps(['10.0']),
             'network_roles_metadata': jsonutils.dumps([{
                 'id': 'admin/vip',
                 'default_mapping': 'fuelweb_admin',
@@ -197,6 +299,14 @@ def prepare():
     )
 
     db.execute(
+        meta.tables['cluster_plugins'].insert(),
+        [
+            {'cluster_id': cluster_ids[0], 'plugin_id': plugin_a_id},
+            {'cluster_id': cluster_ids[0], 'plugin_id': plugin_b_id}
+        ]
+    )
+
+    db.execute(
         meta.tables['plugin_links'].insert(),
         [
             {
@@ -215,6 +325,36 @@ def prepare():
                 'hidden': False
             }
         ]
+    )
+
+    db.execute(
+        meta.tables['node_nic_interfaces'].insert(),
+        [{
+            'id': 1,
+            'node_id': node_id,
+            'name': 'test_interface',
+            'mac': '00:00:00:00:00:01',
+            'max_speed': 200,
+            'current_speed': 100,
+            'ip_addr': '10.20.0.2',
+            'netmask': '255.255.255.0',
+            'state': 'test_state',
+            'interface_properties': jsonutils.dumps(
+                {'test_property': 'test_value'}),
+            'driver': 'test_driver',
+            'bus_info': 'some_test_info'
+        }]
+    )
+
+    db.execute(
+        meta.tables['node_bond_interfaces'].insert(),
+        [{
+            'node_id': node_id,
+            'name': 'test_bond_interface',
+            'mode': 'active-backup',
+            'bond_properties': jsonutils.dumps(
+                {'test_property': 'test_value'})
+        }]
     )
 
     db.commit()
@@ -254,3 +394,83 @@ class TestTasksSnapshotField(base.BaseAlembicMigrationTest):
             ])
         ).first()
         self.assertIsNotNone(result['tasks_snapshot'])
+
+
+class TestPluginAttributesMigration(base.BaseAlembicMigrationTest):
+
+    def test_new_attributes_fields_exist(self):
+        node_bond_interfaces_table = self.meta.tables['node_bond_interfaces']
+        node_nic_interfaces_table = self.meta.tables['node_nic_interfaces']
+        plugins_table = self.meta.tables['plugins']
+        releases_table = self.meta.tables['releases']
+        columns = [
+            plugins_table.c.nic_attributes_metadata,
+            plugins_table.c.bond_attributes_metadata,
+            plugins_table.c.node_attributes_metadata,
+            node_bond_interfaces_table.c.attributes,
+            node_nic_interfaces_table.c.attributes,
+            node_nic_interfaces_table.c.meta,
+            releases_table.c.nic_attributes,
+            releases_table.c.bond_attributes
+        ]
+
+        for column in columns:
+            db_values = db.execute(sa.select([column])).fetchone()
+            for db_value in db_values:
+                self.assertEqual(db_value, '{}')
+
+    def test_node_nic_interface_cluster_plugins_creation(self):
+        node_nic_interface_cluster_plugins = \
+            self.meta.tables['node_nic_interface_cluster_plugins']
+        cluster_plugins = self.meta.tables['cluster_plugins']
+        node_nic_interfaces = self.meta.tables['node_nic_interfaces']
+        nodes = self.meta.tables['nodes']
+
+        cluster_plugin_id = db.execute(sa.select([cluster_plugins])).scalar()
+        interface_id = db.execute(sa.select([node_nic_interfaces])).scalar()
+        node_id = db.execute(sa.select([nodes])).scalar()
+
+        db.execute(
+            node_nic_interface_cluster_plugins.insert(),
+            [{
+                'cluster_plugin_id': cluster_plugin_id,
+                'interface_id': interface_id,
+                'node_id': node_id,
+                'attributes': jsonutils.dumps({'test_attr': 'test'})
+            }])
+
+    def test_node_bond_interface_cluster_plugins_creation(self):
+        node_bond_interface_cluster_plugins = \
+            self.meta.tables['node_bond_interface_cluster_plugins']
+        cluster_plugins = self.meta.tables['cluster_plugins']
+        node_bond_interfaces = self.meta.tables['node_bond_interfaces']
+        nodes = self.meta.tables['nodes']
+
+        cluster_plugin_id = db.execute(sa.select([cluster_plugins])).scalar()
+        bond_id = db.execute(sa.select([node_bond_interfaces])).scalar()
+        node_id = db.execute(sa.select([nodes])).scalar()
+
+        db.execute(
+            node_bond_interface_cluster_plugins.insert(),
+            [{
+                'cluster_plugin_id': cluster_plugin_id,
+                'bond_id': bond_id,
+                'node_id': node_id,
+                'attributes': jsonutils.dumps({'test_attr': 'test'})
+            }])
+
+    def test_node_cluster_plugins_creation(self):
+        node_cluster_plugins = self.meta.tables['node_cluster_plugins']
+        cluster_plugins = self.meta.tables['cluster_plugins']
+        nodes = self.meta.tables['nodes']
+
+        cluster_plugin_id = db.execute(sa.select([cluster_plugins])).scalar()
+        node_id = db.execute(sa.select([nodes])).scalar()
+
+        db.execute(
+            node_cluster_plugins.insert(),
+            [{
+                'cluster_plugin_id': cluster_plugin_id,
+                'node_id': node_id,
+                'attributes': jsonutils.dumps({'test_attr': 'test'})
+            }])

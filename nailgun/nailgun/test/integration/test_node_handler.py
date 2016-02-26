31d3c0212c231d0a1296745709f7bdd784998124
Nikita Zubkov <nzubkov@mirantis.com>
2016-02-26 15:37:17 +0300

    Add new checks to NodeAttributesValidator
    Validator checks that node have enough cpus and at least one unassigned
    cpu for operating system.
    
    Change-Id: Ie4ee247de55882232e256ed8744edf997c170c40
    Implements: blueprint support-numa-cpu-pinning

--- nailgun/nailgun/test/integration/test_node_handler.py
+++ nailgun/nailgun/test/integration/test_node_handler.py
@@ -441,7 +441,8 @@ class TestHandlers(BaseIntegrationTest):
                     'type': 'text',
                     'value': 'value1'
                 }
-            }
+            },
+            'cpu_pinning': {},
         }
         node.attributes.update(fake_attributes)
         update_attributes = {

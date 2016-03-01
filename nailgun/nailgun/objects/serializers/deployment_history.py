8dd3372e17cf42dc0bb1c1bd79fd4ef97079028b
Vladimir Sharshov (warpc) <vsharshov@mirantis.com>
2016-03-01 22:13:56 +0300

    Store deployment tasks history
    Also fix error with wrong message:
    
        "(receiver) The following nodes is not found: <node ids>"
    
    Change-Id: I83aad50d3ae41fc56fecb8c141330903a8404848
    Implements: blueprint store-deployment-tasks-history

---
+++ nailgun/nailgun/objects/serializers/deployment_history.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nailgun.objects.serializers.base import BasicSerializer
+import time
+
+
+class DeploymentHistorySerializer(BasicSerializer):
+
+    fields = (
+        "deployment_graph_task_name",
+        "node_id",
+        "time_start",
+        "time_end",
+        "status",
+        "summary"
+    )
+
+    @classmethod
+    def serialize(cls, instance, fields=None):
+        data_dict = super(DeploymentHistorySerializer, cls).serialize(
+            instance,
+            fields)
+        if instance.time_start:
+            data_dict['time_start'] = int(time.mktime(
+                instance.time_start.timetuple()))
+        if instance.time_end:
+            data_dict['time_end'] = int(time.mktime(
+                instance.time_end.timetuple()))
+        return data_dict

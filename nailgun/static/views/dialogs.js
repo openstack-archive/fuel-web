fce0798b3615dcf5041f897876d4579aefed4998
Alexandra Morozova <astepanchuk@mirantis.com>
2016-03-01 09:33:03 +0100

    Optimizations for node attributes support
    Change-Id: I8f19d66fe7d8b067ef86daf756e3087c53bd42be

--- nailgun/static/views/dialogs.js
+++ nailgun/static/views/dialogs.js
@@ -1160,15 +1160,16 @@ export var ShowNodeInfoDialog = React.createClass({
           }
         );
         // processing hide restrictions
-        sortedAttributes = _.compact(_.map(sortedAttributes, (name) => {
-          if (!nodeAttributes.checkRestrictions(
-            this.state.configModels,
-            'hide',
-            nodeAttributes.get(name).metadata
-          ).result) {
-            return name;
-          }
-        }));
+        sortedAttributes = _.chain(_.keys(nodeAttributes.attributes))
+          .sortBy((name) => nodeAttributes.get(name + '.metadata.weight'))
+          .filter((name) => {
+            return (!nodeAttributes.checkRestrictions(
+              this.state.configModels,
+              'hide',
+              nodeAttributes.get(name).metadata
+            ).result);
+          })
+          .value();
         if (sortedAttributes.length) {
           groups.push('attributes');
           attributeFields = ['nova', 'dpdk'];
@@ -1320,7 +1321,8 @@ export var ShowNodeInfoDialog = React.createClass({
                     <div className='node-attributes'>
                       {_.map(sortedAttributes, (section) => {
                         return _.map(attributeFields, (field) => {
-                          var disabled = !isPendingAdditionNode ||
+                          var disabled = this.state.actionInProgress ||
+                            !isPendingAdditionNode ||
                             (nodeAttributes.checkRestrictions(
                               this.state.configModels,
                               'disabled',

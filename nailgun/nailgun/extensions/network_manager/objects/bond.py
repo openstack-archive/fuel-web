74e1ff313095cd038e5e9c0395349b09e4c1be87
Andriy Popovych <popovych.andrey@gmail.com>
2016-03-02 19:47:31 +0200

    Support for plugin NIC attributes
    Implements logic for merging NIC attributes from plugin
    
    Change-Id: I26106f1b55c704a9e79d01fadc48c88a92ccc414
    Implements: blueprint nics-and-nodes-attributes-via-plugin

--- nailgun/nailgun/extensions/network_manager/objects/bond.py
+++ nailgun/nailgun/extensions/network_manager/objects/bond.py
@@ -14,6 +14,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import copy
 
 from nailgun.db import db
 from nailgun.db.sqlalchemy import models
@@ -22,12 +23,42 @@ from nailgun.extensions.network_manager.objects.interface import NIC
 from nailgun.objects import NailgunCollection
 from nailgun.objects import NailgunObject
 from nailgun.objects.serializers.base import BasicSerializer
+from nailgun.plugins.manager import PluginManager
 
 
 class Bond(DPDKMixin, NailgunObject):
 
     model = models.NodeBondInterface
     serializer = BasicSerializer
+
+    #@classmethod
+    #def create(cls, data):
+    #    bond = super(Bond, cls).create(data)
+    #    cls.create_attributes(bond)
+    #
+    #    return bond
+
+    # FIXME: write tests
+    @classmethod
+    def create_attributes(cls, instance):
+        """Create attributes for bond with default values.
+
+        :param instance: NodeNICInterface instance
+        :type instance: NodeNICInterface model
+        :returns: None
+        """
+        attributes = copy.deepcopy(
+            instance.node.cluster.release.bond_attributes)
+        # set attributes for NICs with interface properties as default values
+        properties = instance.bond_properties
+        for prop in properties:
+            if prop in attributes:
+                attributes[prop]['value'] = properties[prop]
+
+        instance.attributes = attributes
+        PluginManager.add_plugin_attributes_for_bond(instance)
+
+        db().flush()
 
     @classmethod
     def assign_networks(cls, instance, networks):
@@ -72,6 +103,20 @@ class Bond(DPDKMixin, NailgunObject):
                 models.NodeBondInterface.assigned_networks_list,
                 aliased=True).filter(models.NetworkGroup.id.in_(networks))
         return bond_interfaces_query.all()
+
+    @classmethod
+    def get_attributes(cls, instance):
+        """Get native and plugin attributes for bond.
+
+        :param instance: NodeBondInterface instance
+        :type instance: NodeBondInterface model
+        :returns: dict -- Object of interface attributes
+        """
+        attributes = copy.deepcopy(instance.attributes)
+        attributes.update(
+            PluginManager.get_bond_attributes(instance))
+
+        return attributes
 
 
 class BondCollection(NailgunCollection):

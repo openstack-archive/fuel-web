a8426f1fa6496d60bd87e02f942c9cee8cbe265a
Andriy Popovych <popovych.andrey@gmail.com>
2016-03-02 19:47:31 +0200

    Support for plugin NIC attributes
    Implements logic for merging NIC attributes from plugin
    
    Change-Id: I26106f1b55c704a9e79d01fadc48c88a92ccc414
    Implements: blueprint nics-and-nodes-attributes-via-plugin

--- nailgun/nailgun/api/v1/urls.py
+++ nailgun/nailgun/api/v1/urls.py
@@ -75,6 +75,7 @@ from nailgun.api.v1.handlers.network_configuration \
     import TemplateNetworkConfigurationHandler
 
 from nailgun.api.v1.handlers.node import NodeAgentHandler
+from nailgun.api.v1.handlers.node import NodeAttributesDefaultsHandler
 from nailgun.api.v1.handlers.node import NodeAttributesHandler
 from nailgun.api.v1.handlers.node import NodeCollectionHandler
 from nailgun.api.v1.handlers.node import NodeHandler
@@ -83,9 +84,11 @@ from nailgun.api.v1.handlers.node import NodesAllocationStatsHandler
 from nailgun.api.v1.handlers.plugin import PluginCollectionHandler
 from nailgun.api.v1.handlers.plugin import PluginHandler
 from nailgun.api.v1.handlers.plugin import PluginSyncHandler
+
 from nailgun.api.v1.handlers.plugin_link import PluginLinkCollectionHandler
 from nailgun.api.v1.handlers.plugin_link import PluginLinkHandler
 
+from nailgun.api.v1.handlers.node import NodeBondAttributesDefaultsHandler
 from nailgun.api.v1.handlers.node import NodeCollectionNICsDefaultHandler
 from nailgun.api.v1.handlers.node import NodeCollectionNICsHandler
 from nailgun.api.v1.handlers.node import NodeNICsDefaultHandler
@@ -267,6 +270,10 @@ urls = (
     NodeHandler,
     r'/nodes/(?P<node_id>\d+)/attributes/?$',
     NodeAttributesHandler,
+    r'/nodes/(?P<node_id>\d+)/attributes/defaults/?$',
+    NodeAttributesDefaultsHandler,
+    r'/nodes/(?P<node_id>\d+)/bonds/attributes/defaults/?$',
+    NodeBondAttributesDefaultsHandler,
     r'/nodes/interfaces/?$',
     NodeCollectionNICsHandler,
     r'/nodes/interfaces/default_assignment/?$',
--- nailgun/nailgun/test/base.py
+++ nailgun/nailgun/test/base.py
@@ -1121,7 +1121,8 @@ class EnvironmentManager(object):
             )
 
     def make_bond_via_api(self, bond_name, bond_mode, nic_names, node_id=None,
-                          bond_properties=None, interface_properties=None):
+                          bond_properties=None, interface_properties=None,
+                          attrs=None):
         if not node_id:
             node_id = self.nodes[0]["id"]
         resp = self.app.get(
@@ -1149,7 +1150,8 @@ class EnvironmentManager(object):
             "type": NETWORK_INTERFACE_TYPES.bond,
             "mode": bond_mode,
             "slaves": slaves,
-            "assigned_networks": assigned_nets
+            "assigned_networks": assigned_nets,
+            "attributes": attrs or {}
         }
         if bond_properties:
             bond_dict["bond_properties"] = bond_properties

f86fc766b16f56a43d7475fd4456e54e23f9e522
Ilya Kutukov <ikutukov@mirantis.com>
2016-03-03 17:57:29 +0300

    SQLAlchemy usage cleanup for deployment graph related models
    PrimaryKeyConstraint shortcuts is added and imports is optimized.
    
    Change-Id: I346f2f35bb9f89b99ffffb0843edfeec2788fca4
    Implements: blueprint custom-graph-execution

--- nailgun/nailgun/db/sqlalchemy/models/deployment_graph.py
+++ nailgun/nailgun/db/sqlalchemy/models/deployment_graph.py
@@ -12,15 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from sqlalchemy import Column
-from sqlalchemy.dialects import postgresql as psql
-from sqlalchemy import Enum
-from sqlalchemy import ForeignKey
-from sqlalchemy import Integer
-from sqlalchemy.orm import relationship
-from sqlalchemy import String
-from sqlalchemy import Text
-from sqlalchemy import UniqueConstraint
+import sqlalchemy as sa
 
 from nailgun import consts
 from nailgun.db.sqlalchemy.models.base import Base
@@ -31,27 +23,27 @@ from nailgun.db.sqlalchemy.models.mutable import MutableList
 
 class DeploymentGraph(Base):
     __tablename__ = 'deployment_graphs'
-    id = Column(
-        Integer,
+    id = sa.Column(
+        sa.Integer,
         primary_key=True)
-    verbose_name = Column(
+    verbose_name = sa.Column(
         # not planned to used in business logic and
         # added to make work with custom graphs convenient
-        String(255),
+        sa.String(255),
         nullable=True)
-    tasks = relationship(
+    tasks = sa.orm.relationship(
         'DeploymentGraphTask',
         back_populates="deployment_graph")
 
-    plugins = relationship(
+    plugins = sa.orm.relationship(
         "PluginDeploymentGraph",
         back_populates="deployment_graph",
         lazy="dynamic")
-    clusters = relationship(
+    clusters = sa.orm.relationship(
         "ClusterDeploymentGraph",
         back_populates="deployment_graph",
         lazy="dynamic")
-    releases = relationship(
+    releases = sa.orm.relationship(
         "ReleaseDeploymentGraph",
         back_populates="deployment_graph",
         lazy="dynamic")
@@ -60,91 +52,93 @@ class DeploymentGraph(Base):
 class DeploymentGraphTask(Base):
     __tablename__ = 'deployment_graph_tasks'
     __table_args__ = (
-        UniqueConstraint(
+        sa.UniqueConstraint(
             'deployment_graph_id',
             'task_name',
             name='_task_name_deployment_graph_id_uc'),
     )
-    id = Column(
-        Integer,
+    id = sa.Column(
+        sa.Integer,
         primary_key=True)
-    deployment_graph_id = Column(
-        Integer,
-        ForeignKey('deployment_graphs.id', ondelete='CASCADE'),
+    deployment_graph_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey('deployment_graphs.id', ondelete='CASCADE'),
         nullable=False)
-    deployment_graph = relationship('DeploymentGraph', back_populates="tasks")
+    deployment_graph = sa.orm.relationship(
+        'DeploymentGraph',
+        back_populates='tasks')
 
     # not task_id because it could be perceived as fk
     # and not id because it is not unique inside table
-    task_name = Column(
-        String(255),
+    task_name = sa.Column(
+        sa.String(255),
         index=True,
         nullable=False)
-    version = Column(
-        String(255),
+    version = sa.Column(
+        sa.String(255),
         nullable=False,
         server_default='1.0.0',
         default='1.0.0')
-    condition = Column(
-        Text,
+    condition = sa.Column(
+        sa.Text,
         nullable=True)
-    type = Column(
-        Enum(
+    type = sa.Column(
+        sa.Enum(
             *consts.ORCHESTRATOR_TASK_TYPES,
             name='deployment_graph_tasks_type'),
         nullable=False)
-    groups = Column(
-        psql.ARRAY(String(255)),
+    groups = sa.Column(
+        sa.dialects.postgresql.ARRAY(sa.String(255)),
         default=[],
         server_default='{}',
         nullable=False)
-    tasks = Column(
-        psql.ARRAY(String(255)),
+    tasks = sa.Column(
+        sa.dialects.postgresql.ARRAY(sa.String(255)),
         default=[],
         server_default='{}',
         nullable=False)
-    roles = Column(    # node roles
-        psql.ARRAY(String(255)),
+    roles = sa.Column(    # node roles
+        sa.dialects.postgresql.ARRAY(sa.String(255)),
         default=[],
         server_default='{}',
         nullable=False)
     # list of Nailgun events on which this task should be re-executed
-    reexecute_on = Column(
-        psql.ARRAY(String(255)),
+    reexecute_on = sa.Column(
+        sa.dialects.postgresql.ARRAY(sa.String(255)),
         default=[],
         server_default='{}',
         nullable=False)
-    refresh_on = Column(    # new in 8.0
-        psql.ARRAY(String(255)),
+    refresh_on = sa.Column(    # new in 8.0
+        sa.dialects.postgresql.ARRAY(sa.String(255)),
         default=[],
         server_default='{}',
         nullable=False)
-    required_for = Column(
-        psql.ARRAY(String(255)),
+    required_for = sa.Column(
+        sa.dialects.postgresql.ARRAY(sa.String(255)),
         default=[],
         server_default='{}',
         nullable=False)
-    requires = Column(
-        psql.ARRAY(String(255)),
+    requires = sa.Column(
+        sa.dialects.postgresql.ARRAY(sa.String(255)),
         default=[],
         server_default='{}',
         nullable=False)
     # cross-depended-by with hypen is deprecated notation
-    cross_depended_by = Column(
+    cross_depended_by = sa.Column(
         MutableList.as_mutable(JSON),
         default=[],
         server_default='[]')
     # cross-depends with hypen is deprecated notation
-    cross_depends = Column(
+    cross_depends = sa.Column(
         MutableList.as_mutable(JSON),
         default=[],
         server_default='[]')
-    parameters = Column(
+    parameters = sa.Column(
         MutableDict.as_mutable(JSON),
         default={},
         server_default='{}')
     # custom field for all fields that does not fit into the schema
-    _custom = Column(
+    _custom = sa.Column(
         MutableDict.as_mutable(JSON),
         default={},
         server_default='{}')
@@ -153,32 +147,34 @@ class DeploymentGraphTask(Base):
 class ReleaseDeploymentGraph(Base):
     __tablename__ = 'release_deployment_graphs'
     __table_args__ = (
-        UniqueConstraint(
+        sa.UniqueConstraint(
             'release_id',
             'type',
             # deployment_graph_id is not under constraint
             name='_type_deployment_graph_id_uc'),
     )
-    id = Column(
-        Integer,
+    id = sa.Column(
+        sa.Integer,
         primary_key=True)
-    type = Column(
-        String(255),
+    type = sa.Column(
+        sa.String(255),
         nullable=False)
-    deployment_graph_id = Column(
-        Integer,
-        ForeignKey('deployment_graphs.id', ondelete="CASCADE"),
+    deployment_graph_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey('deployment_graphs.id', ondelete="CASCADE"),
         nullable=False,
         index=True)
-    deployment_graph = relationship(
+    deployment_graph = sa.orm.relationship(
         "DeploymentGraph",
         back_populates="releases")
-    release_id = Column(
-        Integer,
-        ForeignKey('releases.id', ondelete="CASCADE"),
+    release_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey('releases.id', ondelete="CASCADE"),
         nullable=False,
         index=True)
-    release = relationship("Release", back_populates="deployment_graphs")
+    release = sa.orm.relationship(
+        "Release",
+        back_populates="deployment_graphs")
 
 
 class PluginDeploymentGraph(Base):
@@ -188,60 +184,64 @@ class PluginDeploymentGraph(Base):
     # So far graphs is connected only to plugin record.
     __tablename__ = 'plugin_deployment_graphs'
     __table_args__ = (
-        UniqueConstraint(
+        sa.UniqueConstraint(
             'plugin_id',
             'type',
             # deployment_graph_id is not under constraint
             name='_type_deployment_graph_id_uc'),
     )
-    id = Column(
-        Integer,
+    id = sa.Column(
+        sa.Integer,
         primary_key=True)
-    type = Column(
-        String(255),
+    type = sa.Column(
+        sa.String(255),
         nullable=False)
-    deployment_graph_id = Column(
-        Integer,
-        ForeignKey('deployment_graphs.id', ondelete="CASCADE"),
+    deployment_graph_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey('deployment_graphs.id', ondelete="CASCADE"),
         nullable=False,
         index=True)
-    deployment_graph = relationship(
+    deployment_graph = sa.orm.relationship(
         "DeploymentGraph",
         back_populates="plugins")
-    plugin_id = Column(
-        Integer,
-        ForeignKey('plugins.id', ondelete="CASCADE"),
+    plugin_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey('plugins.id', ondelete="CASCADE"),
         nullable=False,
         index=True)
-    plugin = relationship("Plugin", back_populates="deployment_graphs")
+    plugin = sa.orm.relationship(
+        "Plugin",
+        back_populates="deployment_graphs")
 
 
 class ClusterDeploymentGraph(Base):
     __tablename__ = 'cluster_deployment_graphs'
     __table_args__ = (
-        UniqueConstraint(
+        sa.UniqueConstraint(
             'cluster_id',
             'type',
             # deployment_graph_id is not under constraint
             name='_type_deployment_graph_id_uc'),
     )
-    id = Column(
-        Integer,
+    id = sa.Column(
+        sa.Integer,
         primary_key=True)
-    type = Column(
-        String(255),
+    type = sa.Column(
+        sa.String(255),
         nullable=False)
-    deployment_graph_id = Column(
-        Integer,
-        ForeignKey('deployment_graphs.id', ondelete="CASCADE"),
+    deployment_graph_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey('deployment_graphs.id', ondelete="CASCADE"),
         nullable=False,
         index=True)
-    deployment_graph = relationship(
+    deployment_graph = sa.orm.relationship(
         "DeploymentGraph",
         back_populates="clusters")
-    cluster_id = Column(
-        Integer,
-        ForeignKey('clusters.id', ondelete="CASCADE"),
+    cluster_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey('clusters.id', ondelete="CASCADE"),
         nullable=False,
         index=True)
-    cluster = relationship("Cluster", back_populates="deployment_graphs")
+    cluster = sa.orm.relationship(
+        "Cluster",
+        back_populates="deployment_graphs")

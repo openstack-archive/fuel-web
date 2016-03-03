fedf4a1bce246753b14c35981c60d8c023b87c82
Ilya Kutukov <ikutukov@mirantis.com>
2016-03-03 16:17:14 +0300

    test_post and test_pre attributes is removed from DeploymentGraphTask model
    This fields is used only for testing and should not be in the core schema.
    With this change this filds will be processed with _custom task fields
    functionality.
    
    Change-Id: Ic9a0884bd5f5924ed85d9939594a14d4056fa9d8
    Implements: blueprint custom-graph-execution

--- nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_9_0.py
+++ nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_9_0.py
@@ -890,18 +890,6 @@ def upgrade_deployment_graph():
             nullable=True),
 
         sa.Column(
-            'test_post',
-            fields.JSON(),
-            default={},
-            server_default='{}'),
-
-        sa.Column(
-            'test_pre',
-            fields.JSON(),
-            default={},
-            server_default='{}'),
-
-        sa.Column(
             'type',
             sa.Enum(
                 'puppet',

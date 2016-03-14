71b42612de57b01977187c11ef703a5050d92c1a
Krzysztof Szukiełojć <kszukielojc@mirantis.com>
2016-03-14 13:30:45 +0100

    Don't add public network to all nodes
    In default setup we should not assign public ip to all nodes.
    Just to ones with controller node. So we filter out public ip
    network for nodes that should not have it.
    
    Change-Id: I2a9ea4d06cc1ba15bad20b817659b7539827472a
    Closes-Bug: 1415552

--- nailgun/nailgun/objects/cluster.py
+++ nailgun/nailgun/objects/cluster.py
@@ -389,6 +389,8 @@ class Cluster(NailgunObject):
         instance.attributes.editable = dict_merge(
             instance.attributes.editable, data['editable'])
         cls.add_pending_changes(instance, "attributes")
+        for node in instance.nodes:
+            cls.get_network_manager(instance).assign_networks_by_default(node)
         cls.get_network_manager(instance).update_restricted_networks(instance)
         db().flush()
 

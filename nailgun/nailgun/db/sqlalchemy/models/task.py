dabaa21f1d51f3fd9033bedf5576141c73521e66
Vladimir Sharshov (warpc) <vsharshov@mirantis.com>
2016-03-22 00:29:40 +0300

    Soft deletion for tasks
    Instead of real deletion of tasks Nailgun now
    mark it as deletion by recordring time deletion on
    special datetime field `deleted_at`.
    
    This changes required for spec below, because current
    UI workflow required deletion in some of scenarios which
    is conflict with idea of spec: save history of every
    deployment tasks. So we save tasks using soft deletion
    to show them using another API for tasks `/api/transactions`,
    which will introduce in future patches.
    
    Change-Id: I51a4b7c8871336ef14e79d229b3bccad7f6904aa
    Implements: blueprint store-deployment-tasks-history

--- nailgun/nailgun/db/sqlalchemy/models/task.py
+++ nailgun/nailgun/db/sqlalchemy/models/task.py
@@ -17,6 +17,7 @@
 import uuid
 
 from sqlalchemy import Column
+from sqlalchemy import DateTime
 from sqlalchemy import Enum
 from sqlalchemy import Float
 from sqlalchemy import ForeignKey
@@ -68,6 +69,7 @@ class Task(Base):
     # sum([t.progress * t.weight for t in supertask.subtasks]) /
     # sum([t.weight for t in supertask.subtasks])
     weight = Column(Float, default=1.0)
+    deleted_at = Column(DateTime)
 
     def __repr__(self):
         return "<Task '{0}' {1} ({2}) {3}>".format(

c668c875aeecae9ac98a185d9bd5a6c922d18564
Alexey Shtokolov <ashtokolov@mirantis.com>
2016-03-24 01:08:42 +0300

    Store settings and networks in deployment tasks
    Change-Id: I4f562ce8ac5c452d35d2e02c33163a4a7d997d86
    Implements: blueprint save-deployment-info-in-database

--- nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_9_0.py
+++ nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_9_0.py
@@ -1264,3 +1264,31 @@ def add_deleted_at_tasks_field():
 
 def delete_deleted_at_tasks_field():
     op.drop_column('tasks', 'deleted_at')
+
+def add_settings_tasks_filed():
+    op.add_column(
+            'tasks',
+            a.Column(
+                'settings',
+                fields.JSON(),
+                nullable=False,
+                server_default='{}'),
+        )
+    )
+
+def delete_settings_tasks_field():
+    op.drop_column('tasks', 'settings')
+
+def add_networks_tasks_filed():
+    op.add_column(
+            'tasks',
+            a.Column(
+                'networks',
+                fields.JSON(),
+                nullable=False,
+                server_default='{}'),
+        )
+    )
+
+def delete_settings_tasks_field():
+    op.drop_column('tasks', 'networks')

5cb38b34504a948b0d0f6d3828aa23e9717d60de
Bulat Gaifullin <bgaifullin@mirantis.com>
2016-03-23 15:22:41 +0300

    Introduced LCM serializers
    We need separate task serializers for LCM,
    because LCM uses context per node for serialize tasks.
    It also allows to isolate LCM related code for backward
    compatibility with existing environments, that is not ready for LCM.
    
    Change-Id: Ie95a58c8cf86eac1a5c3dbd956fafc401e40fed6
    Implements: blueprint computable-task-fields-yaql
    Depends-On: Ifb5ace0302dcf4e041d3962271faec669d494252
    Depends-On: I1dffada651b71d6e9dac4f02020eb4f900a5e2f4
    Depends-On: I64db3f0c78bccf40ab0b8548b5335bd87c668c68
    Depends-On: I65cb66e558b4107f66f43f2c091531377e86d34e

--- nailgun/nailgun/test/unit/test_utils.py
+++ nailgun/nailgun/test/unit/test_utils.py
@@ -14,7 +14,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from mock import patch
+import mock
 
 import requests
 
@@ -22,6 +22,7 @@ from nailgun.test import base
 from nailgun.utils import camel_to_snake_case
 from nailgun.utils import compact
 from nailgun.utils import dict_merge
+from nailgun.utils import dict_update
 from nailgun.utils import flatten
 from nailgun.utils import get_lines
 from nailgun.utils import grouper
@@ -54,6 +55,45 @@ class TestUtils(base.BaseIntegrationTest):
                                            "transparency": 100,
                                            "dict": {"stuff": "hz",
                                                     "another_stuff": "hz"}}})
+
+    def test_dict_update_first_level(self):
+        target = {
+            'a': {'b': 1},
+            'c': {'d': {'e': {'f': 2}}}
+        }
+        update1 = {
+            'a': {'b': 2},
+            'c': {'d': {'g': 2}}
+        }
+        dict_update(target, update1, level=1)
+        self.assertEqual({'b': 2}, target['a'])
+        self.assertEqual({'d': {'g': 2}}, target['c'])
+
+    def test_dict_update_2nd_levels(self):
+        target = {
+            'a': {'b': 1},
+            'c': {'d': {'e': {'f': {'k': 2}}, 'l': 1}}
+        }
+        update1 = {
+            'a': {'b': 2},
+            'c': {'d': {'e': {'g': 2}}}
+        }
+        dict_update(target, update1, level=2)
+        self.assertEqual({'b': 2}, target['a'])
+        self.assertEqual({'d': {'e': {'g': 2}, 'l': 1}}, target['c'])
+
+    def test_dict_update_all_levels(self):
+        target = {
+            'a': {'b': 1},
+            'c': {'d': {'e': {'f': 2}}}
+        }
+        update1 = {
+            'a': {'b': 2},
+            'c': {'d': {'g': 2}}
+        }
+        dict_update(target, update1, level=2)
+        self.assertEqual({'b': 2}, target['a'])
+        self.assertEqual({'d': {'g': 2, 'e': {'f': 2}}}, target['c'])
 
     def test_camel_case_to_snake_case(self):
         self.assertTrue(
@@ -106,6 +146,10 @@ class TestTraverse(base.BaseUnitTest):
         def test(cls, arg=None):
             return 'testvalue'
 
+        @classmethod
+        def evaluate(cls, func, arg=None):
+            return getattr(cls, func)(arg)
+
     data = {
         'foo': {
             'generator': 'test',
@@ -128,7 +172,10 @@ class TestTraverse(base.BaseUnitTest):
     }
 
     def test_wo_formatting_context(self):
-        result = traverse(self.data, self.TestGenerator)
+        result = traverse(
+            self.data,
+            keywords={'generator': self.TestGenerator.evaluate}
+        )
 
         self.assertEqual(result, {
             'foo': 'testvalue',
@@ -148,7 +195,11 @@ class TestTraverse(base.BaseUnitTest):
             ]})
 
     def test_w_formatting_context(self):
-        result = traverse(self.data, self.TestGenerator, {'a': 13})
+        result = traverse(
+            self.data,
+            formatter_context={'a': 13},
+            keywords={'generator': self.TestGenerator.evaluate}
+        )
 
         self.assertEqual(result, {
             'foo': 'testvalue',
@@ -169,14 +220,18 @@ class TestTraverse(base.BaseUnitTest):
 
     def test_formatter_returns_informative_error(self):
         with self.assertRaisesRegexp(ValueError, '{a}'):
-            traverse(self.data, self.TestGenerator, {'b': 13})
+            traverse(
+                self.data,
+                formatter_context={'b': 13},
+                keywords={'generator': self.TestGenerator.evaluate}
+            )
 
     def test_w_safe_formatting_context(self):
         data = self.data.copy()
         data['bar'] = 'test {b} value'
         result = traverse(
-            data, self.TestGenerator, {'a': 13},
-            text_format_safe
+            data, text_format_safe, {'a': 13},
+            keywords={'generator': self.TestGenerator.evaluate}
         )
 
         self.assertEqual(result, {
@@ -196,10 +251,25 @@ class TestTraverse(base.BaseUnitTest):
                 }
             ]})
 
+    def test_custom_keywords(self):
+        data = {
+            'key1': {'exp1': 'name1', 'exp1_arg': 'arg1'},
+            'key2': {'exp2': 'name2'},
+        }
+        generator1 = mock.MagicMock(return_value='val1')
+        generator2 = mock.MagicMock(return_value='val2')
+        result = traverse(data, keywords={
+            'exp1': generator1,
+            'exp2': generator2
+        })
+        self.assertEqual({'key1': 'val1', 'key2': 'val2'}, result)
+        generator1.assert_called_once_with('name1', 'arg1')
+        generator2.assert_called_once_with('name2')
+
 
 class TestGetDebianReleaseFile(base.BaseUnitTest):
 
-    @patch('nailgun.utils.debian.requests.get')
+    @mock.patch('nailgun.utils.debian.requests.get')
     def test_normal_ubuntu_repo(self, m_get):
         get_release_file({
             'name': 'myrepo',
@@ -210,7 +280,7 @@ class TestGetDebianReleaseFile(base.BaseUnitTest):
         m_get.assert_called_with(
             'http://some-uri.com/path/dists/mysuite/Release')
 
-    @patch('nailgun.utils.debian.requests.get')
+    @mock.patch('nailgun.utils.debian.requests.get')
     def test_flat_ubuntu_repo(self, m_get):
         testcases = [
             # (suite, uri)
@@ -228,7 +298,7 @@ class TestGetDebianReleaseFile(base.BaseUnitTest):
             })
             m_get.assert_called_with(uri)
 
-    @patch('nailgun.utils.debian.requests.get')
+    @mock.patch('nailgun.utils.debian.requests.get')
     def test_do_not_silence_http_errors(self, m_get):
         r = requests.Response()
         r.status_code = 404
@@ -241,7 +311,7 @@ class TestGetDebianReleaseFile(base.BaseUnitTest):
             'section': 'main university',
         })
 
-    @patch('nailgun.utils.debian.requests.get')
+    @mock.patch('nailgun.utils.debian.requests.get')
     def test_do_not_retry_on_404(self, m_get):
         r = requests.Response()
         r.status_code = 404
@@ -255,7 +325,7 @@ class TestGetDebianReleaseFile(base.BaseUnitTest):
         }, retries=3)
         self.assertEqual(m_get.call_count, 1)
 
-    @patch('nailgun.utils.debian.requests.get')
+    @mock.patch('nailgun.utils.debian.requests.get')
     def test_do_retry_on_error(self, m_get):
         r = requests.Response()
         r.status_code = 500
@@ -269,7 +339,7 @@ class TestGetDebianReleaseFile(base.BaseUnitTest):
         }, retries=3)
         self.assertEqual(m_get.call_count, 3)
 
-    @patch('nailgun.utils.debian.requests.get')
+    @mock.patch('nailgun.utils.debian.requests.get')
     def test_returns_content_if_http_ok(self, m_get):
         r = requests.Response()
         r._content = 'content'
@@ -539,8 +609,8 @@ class TestFakeNodeGenerator(base.BaseUnitTest):
             }
         ]
 
-        with patch('random.choice',
-                   side_effect=memory_random_side_effect) as mock_random:
+        with mock.patch('random.choice',
+                        side_effect=memory_random_side_effect) as mock_random:
             disks_meta = self.generator._generate_disks_meta(1)
             self.assertNotEqual(disks_meta[0]['size'], 0)
             self.assertEqual(mock_random.call_count, 3)
---
+++ nailgun/nailgun/utils/datadiff.py
@@ -0,0 +1,113 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from difflib import SequenceMatcher
+try:
+    from collections.abc import Hashable
+    from collections.abc import Iterable
+except ImportError:
+    from collections import Hashable
+    from collections import Iterable
+
+import six
+
+
+class DataDiff(object):
+    def __init__(self, added=None, deleted=None):
+        self.added = added
+        self.deleted = deleted
+
+
+def diff(a, b):
+    """Calculates plain diff(not recursively).
+
+    :param a: original data
+    :param b: modified data
+    :return: the DataDiff objects, that shows what was changed in a
+    """
+    if type(a) is not type(b):
+        raise ValueError(
+            "Cannot compare values of different types: {0} and {1}"
+            .format(type(a), type(b))
+        )
+
+    if isinstance(a, six.string_types):
+        return diff_array(a.splitlines(), b.splitlines())
+    if isinstance(a, dict):
+        return diff_dict(a, b)
+    if isinstance(a, (set, frozenset)):
+        return diff_set(a, b)
+    elif isinstance(a, list):
+        return diff_array(a, b)
+    if isinstance(a, Iterable):
+        return diff_array(list(a), list(b))
+    return diff_any(a, b)
+
+
+def diff_any(a, b):
+    return DataDiff() if a == b else DataDiff(added=b, deleted=a)
+
+
+def diff_array(a, b):
+    hashable_a = do_hashable(a)
+    hashable_b = do_hashable(b)
+
+    sm = SequenceMatcher(a=hashable_a, b=hashable_b)
+    added = []
+    deleted = []
+    for change, i1, i2, j1, j2 in sm.get_opcodes():
+        if change == 'insert':
+            added.extend(b[j1:j2])
+        elif change == 'delete':
+            deleted.extend(a[i1:i2])
+        elif change == 'replace':
+            added.extend(b[j1:j2])
+            deleted.extend(a[i1:i2])
+    return DataDiff(added=added, deleted=deleted)
+
+
+def diff_dict(a, b):
+    added = {}
+    deleted = {}
+    for key in a:
+        if key not in b:
+            deleted[key] = a[key]
+        elif a[key] != b[key]:
+            deleted[key] = a[key]
+            added[key] = b[key]
+    for key in b:
+        if key not in a:
+            added[key] = b[key]
+    return DataDiff(added=added, deleted=deleted)
+
+
+def diff_set(a, b):
+    return DataDiff(added=b - a, deleted=a - b)
+
+
+def do_hashable(data):
+    # convert top-level container
+    if isinstance(data, Hashable):
+        return data
+
+    if isinstance(data, dict):
+        return frozenset(do_hashable(x) for x in six.iteritems(data))
+    if isinstance(data, set):
+        return frozenset(do_hashable(x) for x in data)
+    if isinstance(data, Iterable):
+        return tuple(do_hashable(x) for x in data)
+
+    # this raises error if data is not hashable actually
+    hash(data)
+    return data

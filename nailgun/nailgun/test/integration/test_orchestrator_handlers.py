1bfc4986fc9e25ae922284ad2daee0f1544acab4
Ilya Kutukov <ikutukov@mirantis.com>
2016-03-23 04:00:41 +0300

    Custom graph run support added to handlers
    These handlers now support query string param `graph_type` that defining
    what graph to use during command execution:
    
    /clusters/<cluster_id>/deploy/
    /clusters/<cluster_id>/deploy_tasks/
    /clusters/<cluster_id>/deploy_tasks/graph.gv
    
    Change-Id: I893b5bed041a02197fcc4af5682766b1d64b02ed
    Implements: blueprint custom-graph-execution

--- nailgun/nailgun/test/integration/test_orchestrator_handlers.py
+++ nailgun/nailgun/test/integration/test_orchestrator_handlers.py
@@ -21,6 +21,7 @@ import six
 from nailgun import consts
 from nailgun.errors import errors
 from nailgun import objects
+from nailgun.objects import DeploymentGraph
 from nailgun.orchestrator.task_based_deployment import TaskProcessor
 
 from nailgun.db.sqlalchemy.models import Cluster
@@ -344,6 +345,86 @@ class TestSelectedNodesAction(BaseSelectedNodesTest):
         self.assertEqual(resp.status_code, 400)
         self.assertIn("Deployment tasks not found", resp_msg)
         self.assertIn(self.cluster.release.name, resp_msg)
+
+
+class TestCustomGraphAction(BaseSelectedNodesTest):
+
+    def setUp(self):
+        super(TestCustomGraphAction, self).setUp()
+        self.custom_tasks = [
+            {
+                'id': 'first-custom-task',
+                'type': 'stage',
+                'requires': ['pre_deployment_end']
+            }, {
+                'id': 'second-custom-task',
+                'type': 'stage',
+                'requires': ['deploy_start']
+            }
+        ]
+        self.custom_graph = DeploymentGraph.upsert_for_model(
+            {
+                'name': 'custom-graph-name',
+                'tasks': self.custom_tasks
+            },
+            self.cluster,
+            graph_type='custom-graph'
+        )
+        self.env.db().commit()
+
+    def make_action_url(self, handler_name, node_uids, graph_type=None):
+        if graph_type is None:
+            graph_type = 'default'
+        return reverse(
+            handler_name,
+            kwargs={'cluster_id': self.cluster.id}) + \
+            make_query(graph_type=graph_type, nodes=node_uids)
+
+    @patch('nailgun.task.task.rpc.cast')
+    def test_default_graph(self, mcast):
+        self.emulate_nodes_provisioning(self.nodes)
+
+        deploy_action_url = self.make_action_url(
+            "DeploySelectedNodes",
+            self.node_uids,
+            ["default"]
+        )
+
+        response = self.send_put(deploy_action_url)
+        self.assertEqual(response.status_code, 202)
+        self.assertEqual(
+            11,
+            len(mcast.call_args[0][1]['args']['tasks_directory']))
+
+    @patch('nailgun.task.task.rpc.cast')
+    def test_existing_custom_graph(self, mcast):
+        self.emulate_nodes_provisioning(self.nodes)
+
+        deploy_action_url = self.make_action_url(
+            "DeploySelectedNodes",
+            self.node_uids,
+            ["custom-graph"]
+        )
+
+        response = self.send_put(deploy_action_url)
+        self.assertEqual(response.status_code, 202)
+        self.assertEqual(
+            len(self.custom_tasks),
+            len(mcast.call_args[0][1]['args']['tasks_directory']))
+
+    @patch('nailgun.task.task.rpc.cast')
+    def test_not_existing_custom_graph(self, mcast):
+        self.emulate_nodes_provisioning(self.nodes)
+
+        deploy_action_url = self.make_action_url(
+            "DeploySelectedNodes",
+            self.node_uids,
+            ["not-existing-custom-graph"]
+        )
+
+        response = self.send_put(deploy_action_url)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(0, mcast.call_count)
 
 
 class TestDeploymentHandlerSkipTasks(BaseSelectedNodesTest):

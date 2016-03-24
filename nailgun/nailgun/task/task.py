6c833e06e18dad675b99f75d23185ff82609ada1
Vladimir Kuklin <vkuklin@mirantis.com>
2016-03-24 18:55:26 +0300

    Remove legacy graph checker
    Granular deployment has a checker for graph cycles which gets triggered
    even for task-based deployment. It is really hard to refactor things
    to make it run only for role-based deployment. As role-based deployment
    is going to be deprecated and there is already another networkx/dag.py
    checker in orchestrator_graph.py it seems ok to remove this checker from
    all the places.
    
    Change-Id: If081ce7a462bc5ced9459ba619d26aecf22f4916
    Closes-bug: #1560555

--- nailgun/nailgun/task/task.py
+++ nailgun/nailgun/task/task.py
@@ -1290,7 +1290,6 @@ class CheckBeforeDeploymentTask(object):
         cls._check_public_network(task)
         cls._check_vmware_consistency(task)
         cls._validate_network_template(task)
-        cls._check_deployment_graph_for_correctness(task)
 
         if objects.Release.is_external_mongo_enabled(task.cluster.release):
             cls._check_mongo_nodes(task)
@@ -1579,17 +1578,7 @@ class CheckBeforeDeploymentTask(object):
                          'network configuration template').format(error_roles)
             raise errors.NetworkTemplateMissingRoles(error_msg)
 
-    @classmethod
-    def _check_deployment_graph_for_correctness(self, task):
-        """Check that deployment graph doesn't have existing dependencies
-
-        example dependencies are: requires|required_for|tasks|groups
-        """
         # TODO(akostrikov) https://bugs.launchpad.net/fuel/+bug/1561485
-        deployment_tasks = objects.Cluster.get_deployment_tasks(task.cluster)
-        graph_validator = orchestrator_graph.GraphSolverValidator(
-            deployment_tasks)
-        graph_validator.check()
 
 
 class DumpTask(object):

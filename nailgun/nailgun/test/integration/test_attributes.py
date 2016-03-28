7d526b18bbf253cf66ac4da1acf5754f378d1d01
Alexey Shtokolov <ashtokolov@mirantis.com>
2016-03-28 22:21:16 +0300

    Add Deployment Info handlers
    /api/transactions/:transaction_id/deployment_info
    /api/transactions/:transaction_id/settings
    /api/transactions/:transaction_id/network_configuration
    
    Change-Id: Ifbb539757b1954c07eb12d01b82c57480930e1a3

--- nailgun/nailgun/test/integration/test_attributes.py
+++ nailgun/nailgun/test/integration/test_attributes.py
@@ -235,6 +235,32 @@ class TestClusterAttributes(BaseIntegrationTest):
         )
         self.assertEqual(404, resp.status_code)
 
+    def test_get_transaction_cluster_attributes(self):
+        self.env.create_cluster(api=True)
+        cluster = self.env.clusters[-1]
+        cluster_attrs = objects.Cluster.get_editable_attributes(
+            self.env.clusters[-1]
+        )
+        transaction = objects.Transaction.create({
+            'cluster_id': cluster.id,
+            'status': consts.TASK_STATUSES.ready,
+            'name': consts.TASK_NAMES.deployment
+        })
+        objects.Transaction.attach_cluster_settings(
+            transaction, {'editable': cluster_attrs}
+        )
+        self.assertIsNotNone(
+            objects.Transaction.get_cluster_settings(transaction)
+        )
+        resp = self.app.get(
+            reverse(
+                'TransactionClusterSettings',
+                kwargs={'transaction_id': transaction.id}),
+            headers=self.default_headers
+        )
+        self.assertEqual(200, resp.status_code)
+        self.datadiff(cluster_attrs, resp.json_body['editable'])
+
     def test_attributes_set_defaults(self):
         cluster = self.env.create_cluster(api=True)
         cluster_db = self.env.clusters[0]

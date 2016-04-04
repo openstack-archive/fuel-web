80fafcd4eb010bee394c775fabf19849b66a70df
Igor Zinovik <izinovik@mirantis.com>
2016-04-04 10:08:25 +0300

    Allow user upload CA bundle file for Glance with VMware backend
    Add field that allows user to upload CA certificate that emitted
    vCenters TLS/SSL certificate. It allow Glance with VMware backend verify
    connection to vCenter server (verification is turned on by default).
    If user did not uploaded certificate then, glance will be configured
    to skip verification step during connection to vCenter.
    
    Partial-bug: #1559067
    DocImpact: Document how to use 'CA file' field on VMware tab.
    Change-Id: I810bc000e54b941018a1190acb26d33150b74ce0
    (cherry picked from commit 59c8b8c7909de07bcbb97ec6c9ae509f98bdaff2)

--- nailgun/nailgun/test/integration/test_orchestrator_serializer.py
+++ nailgun/nailgun/test/integration/test_orchestrator_serializer.py
@@ -2580,6 +2580,7 @@ class BaseDeploymentSerializer(BaseSerializerTest):
         self.assertEqual(result['glance']['vc_password'], "secret")
         self.assertEqual(result['glance']['vc_datacenter'], "test_datacenter")
         self.assertEqual(result['glance']['vc_datastore'], "test_datastore")
+        self.assertEqual(result['glance']['vc_ca_file'], "file_blob")
 
     def check_no_murano_data(self):
         glance_properties = self.serializer.generate_test_vm_image_data(

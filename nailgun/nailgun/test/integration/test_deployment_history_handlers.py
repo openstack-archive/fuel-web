4bd59d58690df585419120da6b315139277e90b6
Ilya Kutukov <ikutukov@mirantis.com>
2016-04-07 21:59:16 +0300

    Deployment graph tasks parameters now saved in Transaction before deployment
    Deployment tasks snapshot is now saving during DeploymentTask message creation
    in Transaction.tasks_snapshot field.
    
    Methods:
    
        * attach_tasks_snapshot
        * get_tasks_snapshot
    
    are added to Transaction model.
    
    Handler /transactions/:transaction_id/deployment_history/
    now showing tasks parameters in output.
    
    Handler /transactions/:transaction_id/:deployment_task_name/
    is added and returning histore records for the specific
    deployment task.
    
    Change-Id: I59446ef456f7d3bd249c686b7a8e1a93a364daf2
    Partial-Bug: #1563317
    DocImpact

---
+++ nailgun/nailgun/test/integration/test_deployment_history_handlers.py
@@ -0,0 +1,206 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import mock
+
+from nailgun import consts
+from nailgun.test.base import BaseIntegrationTest
+from nailgun.utils import reverse
+
+
+class TestDeploymentHistoryHandlers(BaseIntegrationTest):
+
+    def setUp(self):
+        super(TestDeploymentHistoryHandlers, self).setUp()
+        self.test_tasks = [
+            {
+                'id': 'test',
+                'task_name': 'test',
+                'parameters': {'param1': 'value1'},
+                'type': 'puppet',
+                'roles': '*',
+                'version': '2.1.0',
+                'requires': ['pre_deployment_end']
+            }
+        ]
+        self.cluster_parameters = {
+            'nodes_kwargs': [
+                {
+                    'roles': ['controller'],
+                    'pending_addition': True
+                },
+            ],
+            'release_kwargs': {
+                'operating_system': consts.RELEASE_OS.ubuntu,
+                'version': 'mitaka-9.0'
+            }
+        }
+
+    @mock.patch('nailgun.task.task.rpc.cast')
+    @mock.patch('objects.Cluster.get_deployment_tasks')
+    def test_history_collection_handler(self, tasks_mock, rpc_mock):
+
+        tasks_mock.return_value = self.test_tasks
+
+        cluster = self.env.create(**self.cluster_parameters)
+
+        supertask = self.env.launch_deployment(cluster.id)
+        self.assertNotEqual(consts.TASK_STATUSES.error, supertask.status)
+        deployment_task = next(
+            t for t in supertask.subtasks
+            if t.name == consts.TASK_NAMES.deployment
+        )
+
+        response = self.app.get(
+            reverse(
+                'DeploymentHistoryCollectionHandler',
+                kwargs={
+                    'transaction_id': deployment_task.id,
+                    'deployment_task_name': ''
+                }
+            ),
+            headers=self.default_headers
+        )
+
+        self.assertItemsEqual(
+            [{
+                'id': 'test',
+                'task_name': 'test',
+                'parameters': {'param1': 'value1'},
+                'roles': '*',
+                'type': 'puppet',
+                'version': '2.1.0',
+                'requires': ['pre_deployment_end'],
+                'node_id': node.uid,
+                'status': 'pending',
+                'time_start': None,
+                'time_end': None,
+                'custom': {}
+            } for node in cluster.nodes],
+            response.json_body
+        )
+
+    @mock.patch('nailgun.task.task.rpc.cast')
+    @mock.patch('objects.Cluster.get_deployment_tasks')
+    def test_history_task_handler(self, tasks_mock, rpc_mock):
+
+        tasks_mock.return_value = self.test_tasks
+
+        cluster = self.env.create(**self.cluster_parameters)
+
+        supertask = self.env.launch_deployment(cluster.id)
+        self.assertNotEqual(consts.TASK_STATUSES.error, supertask.status)
+        deployment_task = next(
+            t for t in supertask.subtasks
+            if t.name == consts.TASK_NAMES.deployment
+        )
+
+        response = self.app.get(
+            reverse(
+                'DeploymentHistoryTaskCollectionHandler',
+                kwargs={
+                    'transaction_id': deployment_task.id,
+                    'deployment_task_name': 'test'
+                }
+            ),
+            headers=self.default_headers
+        )
+
+        self.assertItemsEqual(
+            [{
+                'id': 'test',
+                'task_name': 'test',
+                'parameters': {'param1': 'value1'},
+                'roles': '*',
+                'type': 'puppet',
+                'version': '2.1.0',
+                'requires': ['pre_deployment_end'],
+                'node_id': node.uid,
+                'status': 'pending',
+                'time_start': None,
+                'time_end': None,
+                'custom': {}
+            } for node in cluster.nodes],
+            response.json_body
+        )
+
+    @mock.patch('nailgun.task.task.rpc.cast')
+    @mock.patch('objects.Cluster.get_deployment_tasks')
+    def test_history_task_handler_not_found(self, tasks_mock, rpc_mock):
+        tasks_mock.return_value = self.test_tasks
+
+        cluster = self.env.create(**self.cluster_parameters)
+
+        supertask = self.env.launch_deployment(cluster.id)
+        self.assertNotEqual(consts.TASK_STATUSES.error, supertask.status)
+        deployment_task = next(
+            t for t in supertask.subtasks
+            if t.name == consts.TASK_NAMES.deployment
+        )
+
+        response = self.app.get(
+            reverse(
+                'DeploymentHistoryTaskCollectionHandler',
+                kwargs={
+                    'transaction_id': deployment_task.id,
+                    'deployment_task_name': 'NOSUCHTASK'
+                }
+            ),
+            headers=self.default_headers,
+            expect_errors=True
+        )
+        self.assertEqual(200, response.status_code)
+        self.assertEqual([], response.json_body)
+
+    @mock.patch('nailgun.task.task.rpc.cast')
+    @mock.patch('objects.Cluster.get_deployment_tasks')
+    def test_history_task_handler_no_snapshot_fall(self, tasks_mock, rpc_mock):
+        # check that if not valid graph snapshot is provided output will return
+        # to old history format without unwrapped tasks parameters
+        tasks_mock.return_value = self.test_tasks
+
+        cluster = self.env.create(**self.cluster_parameters)
+
+        supertask = self.env.launch_deployment(cluster.id)
+        self.assertNotEqual(consts.TASK_STATUSES.error, supertask.status)
+        deployment_task = next(
+            t for t in supertask.subtasks
+            if t.name == consts.TASK_NAMES.deployment
+        )
+        deployment_task.tasks_snapshot = None
+        self.db.commit()
+        response = self.app.get(
+            reverse(
+                'DeploymentHistoryTaskCollectionHandler',
+                kwargs={
+                    'transaction_id': deployment_task.id,
+                    'deployment_task_name': 'test'
+                }
+            ),
+            headers=self.default_headers,
+            expect_errors=True
+        )
+        self.assertEqual(200, response.status_code)
+        self.assertItemsEqual(
+            [{
+                'deployment_graph_task_name': 'test',
+                'node_id': node.uid,
+                'status': 'pending',
+                'time_start': None,
+                'time_end': None,
+                'custom': {}
+            } for node in cluster.nodes],
+            response.json_body
+        )

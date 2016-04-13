2df99a71ea09f6c24c280c9de436e297c252c844
Ryan Moe <rmoe@mirantis.com>
2016-04-13 11:27:28 -0700

    Move remaining nailgun.network files to extension
    Network checker, connectivity check, network template,
    and utils files were moved into the network manager extension.
    
    Change-Id: I2f7f192ebbc6e9b1902e03c2a65ffaf46df5348e
    Blueprint: network-manager-extension

--- nailgun/nailgun/extensions/network_manager/manager.py
+++ nailgun/nailgun/extensions/network_manager/manager.py
@@ -26,13 +26,13 @@ from netaddr import IPRange
 
 import six
 
+from . import utils
 from nailgun import consts
 
 from nailgun import errors
 from nailgun.extensions.network_manager.objects.serializers.nic import \
     NodeInterfacesSerializer
 from nailgun.logger import logger
-from nailgun.network import utils
 from nailgun import objects
 from nailgun.settings import settings
 from nailgun import utils as nailgun_utils

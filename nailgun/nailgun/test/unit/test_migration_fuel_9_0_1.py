6f5258f0479288dc75732cd7301821686484266a
Georgy Kibardin <gkibardin@mirantis.com>
2016-04-13 10:15:46 +0300

    Upgrade existing values for Cluster.replaced_deployment_info
    Expected value type for subj. has changed from dict to list
    in 9.0. From now migration takes into account not only 5.0
    databases.
    Original review is here https://review.openstack.org/249723
    
    Change-Id: Id1954d9607bc253c325e335e0dab0965d245c665
    Closes-Bug: #1568100

--- nailgun/nailgun/test/unit/test_migration_fuel_9_0_1.py
+++ nailgun/nailgun/test/unit/test_migration_fuel_9_0_1.py
@@ -14,9 +14,12 @@
 
 import alembic
 
+from nailgun.db import db
 from nailgun.db import dropdb
 from nailgun.db.migration import ALEMBIC_CONFIG
 from nailgun.test import base
+from oslo_serialization import jsonutils
+import sqlalchemy as sa
 
 _prepare_revision = '11a9adc6d36a'
 _test_revision = '675105097a69'
@@ -30,7 +33,40 @@ def setup_module():
 
 
 def prepare():
-    pass
+    meta = base.reflect_db_metadata()
+
+    result = db.execute(
+        meta.tables['releases'].insert(),
+        [{
+            'name': 'test_name',
+            'version': '2015.1-8.0',
+            'operating_system': 'ubuntu',
+            'state': 'available',
+            'networks_metadata': jsonutils.dumps({
+                'neutron': {
+                    'networks': [],
+                    'config': {}
+                }
+            })
+        }]
+    )
+    releaseid = result.inserted_primary_key[0]
+
+    db.execute(
+        meta.tables['clusters'].insert(),
+        [{
+            'name': 'test_env',
+            'release_id': releaseid,
+            'mode': 'ha_compact',
+            'status': 'new',
+            'net_provider': 'neutron',
+            'grouping': 'roles',
+            'fuel_version': '8.0',
+            'deployment_tasks': '[]',
+            'replaced_deployment_info': '{}'
+        }])
+
+    db.commit()
 
 
 class TestDeploymentHistoryMigration(base.BaseAlembicMigrationTest):
@@ -38,3 +74,12 @@ class TestDeploymentHistoryMigration(base.BaseAlembicMigrationTest):
         tbl = self.meta.tables['deployment_history']
         self.assertIn('deployment_history_task_name_status_idx',
                       [i.name for i in tbl.indexes])
+
+
+class TestClusterReplacedDeploymentInfo(base.BaseAlembicMigrationTest):
+
+    def test_cluster_replaced_deployment_info_corrected(self):
+        clusters_table = self.meta.tables['clusters']
+        columns = [clusters_table.c.replaced_deployment_info]
+        cluster = db.execute(sa.select(columns)).fetchone()
+        self.assertEqual(cluster.replaced_deployment_info, '[]')

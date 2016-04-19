322d165ce1cde316480e9c76762f30faeef0be60
Ilya Kutukov <ikutukov@mirantis.com>
2016-04-19 16:40:03 +0300

    Deployment and provision Nailgun tasks is now run asynchrously
    Deployment and provisioning preparations could last a long when Fuel is
    working with a hundreds of nodes. But operator should be able to
    observe cluster state and operate other clusters.
    
    Change-Id: I73802c91f93a46855b006cb05a8b5722109e9e6a
    Partial-Bug: #1569859

--- nailgun/nailgun/test/integration/test_task_managers.py
+++ nailgun/nailgun/test/integration/test_task_managers.py
@@ -60,6 +60,10 @@ class TestTaskManagers(BaseIntegrationTest):
         objects.DeploymentHistoryCollection.all().update(
             {'status': consts.HISTORY_TASK_STATUSES.ready})
 
+    def set_tasks_ready(self):
+        objects.TaskCollection.all().update(
+            {'status': consts.TASK_STATUSES.ready})
+
     @fake_tasks(override_state={"progress": 100, "status": "ready"})
     def test_deployment_task_managers(self):
         cluster = self.env.create(
@@ -1363,17 +1367,21 @@ class TestTaskManagers(BaseIntegrationTest):
         state_mock.return_value = []
 
         # deploy cluster at first time and create history
-        supertask = self.env.launch_deployment_selected([node.uid], cluster.id)
-        self.assertNotEqual(consts.TASK_STATUSES.error, supertask.status)
+        task1 = self.env.launch_deployment_selected([node.uid], cluster.id)
+        self.assertNotEqual(consts.TASK_STATUSES.error, task1.status)
+
+        # there should be no running tasks for this cluster to avoid conflict
+        # error while placing new deployment/provisioning task to execute
+        self.set_tasks_ready()
 
         self.set_history_ready()
 
-        state_mock.return_value = [(supertask, 'test1')]
-        task = self.env.launch_deployment_selected([node.uid], cluster.id)
-        self.assertNotEqual(consts.TASK_STATUSES.error, task.status)
+        state_mock.return_value = [(task1, 'test1')]
+        task2 = self.env.launch_deployment_selected([node.uid], cluster.id)
+        self.assertNotEqual(consts.TASK_STATUSES.error, task2.status)
         tasks_graph = rpc_mock.call_args[0][1]['args']['tasks_graph']
 
-        # chek that test1 task skipped by condition and test2 was not
+        # check that test1 task skipped by condition and test2 was not
         for task in tasks_graph['master']:
             if task['id'] == 'test1':
                 self.assertEqual(

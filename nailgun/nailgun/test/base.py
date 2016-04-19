028b3ecb4b17e0193f7e68fa910aa72a9389bc09
slava <vvalyavskiy@mirantis.com>
2016-04-19 22:51:10 +0300

    Do not allow to update network group when deploy is in progress
    Change-Id: Ia49e2524613171e14d69d8038db439f1f87bdf13
    Closes-Bug: #1559170

--- nailgun/nailgun/test/base.py
+++ nailgun/nailgun/test/base.py
@@ -1298,7 +1298,7 @@ class EnvironmentManager(object):
                 kwargs={'obj_id': ng_id}
             ),
             headers=self.default_headers,
-            expect_errors=False
+            expect_errors=expect_errors
         )
 
     def set_task_status_recursively(self, supertask, status):

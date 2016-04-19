e8247e86a32b5a2de783450792bfa78eafef6732
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2016-04-19 14:55:57 +0300

    Multiprocess tasks serialization
    Tasks serialization process take long time in case when environment
    contains many nodes. To reduce time of serialization, make
    serialization process work in parallel (reduces time of
    serialization mostly linear way with increasing workers pool)
    
    Change-Id: Id3753dbc6983256d410e69c98ab02b61ab6bfb7f
    Partial-Bug: #1572103
    Co-Authored-With: V. Kuklin <vkuklin@mirantis.com>
    Co-Authored-With: B. Gaifullin <bgaifullin@mirantis.com>

--- nailgun/nailgun/yaql_ext/serializers.py
+++ nailgun/nailgun/yaql_ext/serializers.py
@@ -12,7 +12,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-
 from oslo_serialization import jsonutils
 import yaml
 from yaql.language import specs

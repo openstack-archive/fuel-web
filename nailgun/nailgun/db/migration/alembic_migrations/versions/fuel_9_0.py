819b247e1617f1405e1a184281387d72715d09e5
slava <vvalyavskiy@mirantis.com>
2016-04-26 23:00:49 +0300

    Add migration for 'cgroups' field
    Change-Id: Iad5de47b6942cb1935e13cd36dddb9000ffd3f0a
    Closes-Bug: #1575263

--- nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_9_0.py
+++ nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_9_0.py
@@ -177,6 +177,16 @@ ceph_storage_attrs = {
     }
 }
 
+cgroups_attrs = {
+    'metadata': {
+        'label': 'Cgroups configuration for services',
+        'weight': 90,
+        'group': 'general',
+        'always_editable': True,
+        'restrictions': [{'condition': 'true', 'action': 'hide'}]
+    }
+}
+
 
 def upgrade():
     add_foreign_key_ondelete()
@@ -195,9 +205,11 @@ def upgrade():
     upgrade_task_attributes()
     upgrade_store_deployment_history()
     upgrade_ceph_cluster_attrs()
+    upgrade_cgroups_cluster_attrs()
 
 
 def downgrade():
+    downgrade_cgroups_cluster_attrs()
     downgrade_ceph_cluster_attrs()
     downgrade_store_deployment_history()
     downgrade_task_attributes()
@@ -1400,28 +1412,39 @@ def downgrade_store_deployment_history():
     drop_enum('history_task_statuses')
 
 
-def upgrade_ceph_cluster_attrs():
+def _update_cluster_editable_attrs(update_method):
     connection = op.get_bind()
 
     for cluster_id, editable in connection.execute(q_get_cluster_attrs):
         editable = jsonutils.loads(editable)
-        editable.get('storage', {}).update(ceph_storage_attrs)
+        update_method(editable)
         connection.execute(
             q_update_cluster_attrs,
             cluster_id=cluster_id,
             editable=jsonutils.dumps(editable)
         )
+
+
+def upgrade_ceph_cluster_attrs():
+    def _update(editable):
+        editable.get('storage', {}).update(ceph_storage_attrs)
+    _update_cluster_editable_attrs(_update)
 
 
 def downgrade_ceph_cluster_attrs():
-    connection = op.get_bind()
-
-    for cluster_id, editable in connection.execute(q_get_cluster_attrs):
-        editable = jsonutils.loads(editable)
+    def _update(editable):
         for ceph_attr in ceph_storage_attrs:
             editable.get('storage', {}).pop(ceph_attr, None)
-        connection.execute(
-            q_update_cluster_attrs,
-            cluster_id=cluster_id,
-            editable=jsonutils.dumps(editable)
-        )
+    _update_cluster_editable_attrs(_update)
+
+
+def upgrade_cgroups_cluster_attrs():
+    def _update(editable):
+        editable['cgroups'] = cgroups_attrs
+    _update_cluster_editable_attrs(_update)
+
+
+def downgrade_cgroups_cluster_attrs():
+    def _update(editable):
+        editable.pop('cgroups', None)
+    _update_cluster_editable_attrs(_update)

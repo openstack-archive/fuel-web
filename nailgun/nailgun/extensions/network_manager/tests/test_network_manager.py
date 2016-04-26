a04b6392740e79bd888c2b5ede0d2f32988d3a36
Ryan Moe <rmoe@mirantis.com>
2016-04-26 09:34:07 -0700

    Move network manager calls into callbacks
    Calls to network manager from object methods have been
    moved into callbacks implemented by the network manager
    extension.
    
    This commit makes the following new callbacks available
    to extensions:
    
     * on_cluster_create: called when a new cluster is created
     * on_cluster_patch_attributes: called when a cluster's attributes
        are updated
     * on_nodegroup_create: called when a node group is created
     * on_before_deployment_serialization: called before deployment
        serialization begins
     * on_before_provisioning_serialization: called before provisioning
        serialization begins
    
    Blueprint: network-manager-extension
    Change-Id: I9a3413f54c881edd098e623ea204d12a86695f87

--- nailgun/nailgun/extensions/network_manager/tests/test_network_manager.py
+++ nailgun/nailgun/extensions/network_manager/tests/test_network_manager.py
@@ -617,7 +617,7 @@ class TestNetworkManager(BaseIntegrationTest):
             (ip.network_data.name, ip.ip_addr) for ip in node.ip_addrs
         )
         self.assertEquals(node_net_ips,
-                          objects.Node.get_networks_ips_dict(node))
+                          objects.IPAddr.get_networks_ips_dict(node))
 
     def test_get_admin_ip_for_node(self):
         cluster = self.env.create(api=False)
@@ -649,9 +649,9 @@ class TestNetworkManager(BaseIntegrationTest):
         node_net_ips = \
             dict((net.name, self.env.network_manager.get_free_ips(net)[0])
                  for net in node.networks)
-        objects.Node.set_networks_ips(node, node_net_ips)
+        objects.IPAddr.set_networks_ips(node, node_net_ips)
         self.assertEquals(node_net_ips,
-                          objects.Node.get_networks_ips_dict(node))
+                          objects.IPAddr.get_networks_ips_dict(node))
 
     def test_set_netgroups_ids(self):
         cluster = self.env.create_cluster(api=False)

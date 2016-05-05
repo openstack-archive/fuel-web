e43b2bb5228ed27742e6b398aa561d427434c5f4
Vladimir Kuklin <vkuklin@mirantis.com>
2016-05-05 12:51:46 +0300

    Allow a user to run dry-run and noop deployment
    Now following handlers:
    
         /clusters/:cluster_id/changes/
         /clusters/:cluster_id/changes/redeploy/
         /clusters/:cluster_id/deploy/
         /clusters/:cluster_id/deploy_tasks/
    
    are supporting ?noop=1 argument that is telling to
    run puppet with --noop switch and
    ?dry_run=1 that is telling not to run puppet at all.
    
    Dry run and noop assume that they do not actually affect
    cluster status regardless of their result.
    
    Also, remove redundant update of nodes statuses to 'deploying'
    within OpenStackConfigManager and DeploymentTaskManager as it should be
    done by receiever.
    
    Change-Id: I374fc86b63af64411d4a5ca45ff6c3680cb44897
    Partial-bug: #1569839

--- nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_9_0_1.py
+++ nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_9_0_1.py
@@ -23,6 +23,7 @@ Create Date: 2016-04-28 22:23:40.895589
 from alembic import op
 import sqlalchemy as sa
 
+from nailgun.utils.migration import upgrade_enum
 
 # revision identifiers, used by Alembic.
 revision = '675105097a69'
@@ -31,11 +32,13 @@ down_revision = '11a9adc6d36a'
 
 def upgrade():
     upgrade_deployment_history()
+    upgrade_transaction_names()
     upgrade_clusters_replaced_info_wrong_default()
 
 
 def downgrade():
     downgrade_clusters_replaced_info_wrong_default()
+    downgrade_transaction_names()
     downgrade_deployment_history()
 
 
@@ -64,3 +67,69 @@ def downgrade_clusters_replaced_info_wrong_default():
         "UPDATE clusters SET replaced_deployment_info = '{}' "
         "WHERE replaced_deployment_info = '[]'")
     connection.execute(update_query)
+
+transaction_names_old = (
+    'super',
+
+    # Cluster changes
+    # For deployment supertask, it contains
+    # two subtasks deployment and provision
+    'deploy',
+    'deployment',
+    'provision',
+    'stop_deployment',
+    'reset_environment',
+    'update',
+    'spawn_vms',
+
+    'node_deletion',
+    'cluster_deletion',
+    'remove_images',
+    'check_before_deployment',
+
+    # network
+    'check_networks',
+    'verify_networks',
+    'check_dhcp',
+    'verify_network_connectivity',
+    'multicast_verification',
+    'check_repo_availability',
+    'check_repo_availability_with_setup',
+
+    # dump
+    'dump',
+
+    'capacity_log',
+
+    # statistics
+    'create_stats_user',
+    'remove_stats_user',
+
+    # setup dhcp via dnsmasq for multi-node-groups
+    'update_dnsmasq'
+)
+
+transaction_names_new = transaction_names_old + (
+    'dry_run_deployment',
+    'noop_deployment',
+)
+
+
+def upgrade_transaction_names():
+    upgrade_enum(
+        'tasks',
+        'name',
+        'task_name',
+        transaction_names_old,
+        transaction_names_new
+    )
+
+
+def downgrade_transaction_names():
+    upgrade_enum(
+        'tasks',
+        'name',
+        'task_name',
+        transaction_names_new,
+        transaction_names_old
+    )

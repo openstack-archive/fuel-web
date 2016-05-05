310f7e5eb2aeec8933639781069019d52131d9a6
Vladimir Kuklin <vkuklin@mirantis.com>
2016-05-05 12:51:46 +0300

    Allow a user to run dry-run deployment
    Now following handlers:
    
         /clusters/:cluster_id/changes/
         /clusters/:cluster_id/changes/redeploy/
         /clusters/:cluster_id/deploy/
         /clusters/:cluster_id/deploy_tasks/
    
    ?dry_run=1 that is telling Astute not to run cluster executionat all.
    
    Dry run assumes that it does not actually affect
    cluster status regardless of their result.
    
    Also, remove redundant update of nodes statuses to 'deploying'
    within OpenStackConfigManager and DeploymentTaskManager as it should be
    done by receiever.
    
    Do not set cluster status to 'deployment' for these nodes in order to
    retain its real status
    
    Modify stop deployment tests to move failing stop deployment for already
    deployed clusters to another test class. Since 9.0 we can run stop
    deployment for new clusters.
    
    Change-Id: I374fc86b63af64411d4a5ca45ff6c3680cb44897
    Partial-bug: #1569839

--- nailgun/nailgun/task/task.py
+++ nailgun/nailgun/task/task.py
@@ -143,7 +143,7 @@ class BaseDeploymentTask(object):
             try:
                 args = getattr(cls, method)(transaction, **kwargs)
                 # save tasks history
-                if 'tasks_graph' in args:
+                if 'tasks_graph' in args and not args.get('dry_run', False):
                     logger.info("tasks history saving is started.")
                     objects.DeploymentHistoryCollection.create(
                         transaction, args['tasks_graph']
@@ -210,7 +210,8 @@ class DeploymentTask(BaseDeploymentTask):
 
     @classmethod
     def message(cls, task, nodes, affected_nodes=None, deployment_tasks=None,
-                reexecutable_filter=None, graph_type=None, force=False):
+                reexecutable_filter=None, graph_type=None,
+                force=False, dry_run=False, **kwargs):
         """Builds RPC message for deployment task.
 
         :param task: the database task object instance
@@ -219,9 +220,12 @@ class DeploymentTask(BaseDeploymentTask):
         :param deployment_tasks: the list of tasks_ids to execute,
                                  if None, all tasks will be executed
         :param reexecutable_filter: the list of events to find subscribed tasks
+        :param force: force
+        :param dry_run: dry run
         :param graph_type: deployment graph type
         """
         logger.debug("DeploymentTask.message(task=%s)" % task.uuid)
+
         task_ids = deployment_tasks or []
 
         objects.NodeCollection.lock_nodes(nodes)
@@ -260,7 +264,8 @@ class DeploymentTask(BaseDeploymentTask):
         deployment_mode, message = cls.call_deployment_method(
             task, tasks=deployment_tasks, nodes=nodes,
             affected_nodes=affected_nodes, selected_task_ids=task_ids,
-            events=reexecutable_filter, force=force
+            events=reexecutable_filter, force=force,
+            dry_run=dry_run, **kwargs
         )
 
         # After serialization set pending_addition to False
@@ -290,7 +295,8 @@ class DeploymentTask(BaseDeploymentTask):
 
     @classmethod
     def granular_deploy(cls, transaction, tasks, nodes,
-                        affected_nodes, selected_task_ids, events, **kwargs):
+                        affected_nodes, selected_task_ids, events,
+                        dry_run=False, **kwargs):
         """Builds parameters for granular deployment.
 
         :param transaction: the transaction object
@@ -300,8 +306,12 @@ class DeploymentTask(BaseDeploymentTask):
         :param selected_task_ids: the list of tasks_ids to execute,
                                    if None, all tasks will be executed
         :param events: the list of events to find subscribed tasks
+        :param dry_run: dry run
         :return: the arguments for RPC message
         """
+        if dry_run:
+            raise errors.DryRunSupportedOnlyByLCM()
+
         graph = orchestrator_graph.AstuteGraph(transaction.cluster, tasks)
         graph.check()
         graph.only_tasks(selected_task_ids)
@@ -338,7 +348,8 @@ class DeploymentTask(BaseDeploymentTask):
 
     @classmethod
     def task_deploy(cls, transaction, tasks, nodes, affected_nodes,
-                    selected_task_ids, events, **kwargs):
+                    selected_task_ids, events, dry_run=False,
+                    **kwargs):
         """Builds parameters for task based deployment.
 
         :param transaction: the transaction object
@@ -348,8 +359,12 @@ class DeploymentTask(BaseDeploymentTask):
         :param selected_task_ids: the list of tasks_ids to execute,
                                   if None, all tasks will be executed
         :param events: the list of events to find subscribed tasks
+        :param dry_run: dry run
         :return:  RPC method name, the arguments for RPC message
         """
+
+        if dry_run:
+            raise errors.DryRunSupportedOnlyByLCM()
 
         task_processor = task_based_deployment.TaskProcessor
         for task in tasks:
@@ -501,11 +516,12 @@ class ClusterTransaction(DeploymentTask):
 
     @classmethod
     def task_deploy(cls, transaction, tasks, nodes, force=False,
-                    selected_task_ids=None, **kwargs):
+                    selected_task_ids=None, dry_run=False, **kwargs):
         logger.info("The cluster transaction is initiated.")
         logger.info("cluster serialization is started.")
         # we should update information for all nodes except deleted
         # TODO(bgaifullin) pass role resolver to serializers
+
         deployment_info = deployment_serializers.serialize_for_lcm(
             transaction.cluster, nodes
         )
@@ -548,7 +564,8 @@ class ClusterTransaction(DeploymentTask):
         logger.info("tasks serialization is finished.")
         return {
             "tasks_directory": directory,
-            "tasks_graph": graph
+            "tasks_graph": graph,
+            "dry_run": dry_run,
         }
 
 
@@ -1800,9 +1817,11 @@ class CheckBeforeDeploymentTask(object):
 
     @classmethod
     def _check_dpdk_network_scheme(cls, network_scheme, node_group):
-        """Check that endpoint with dpdk provider mapped only to neutron/private
+        """DPDK endpoint provider check
 
+        Check that endpoint with dpdk provider mapped only to neutron/private
         """
+
         for net_template in network_scheme.values():
             roles = net_template['roles']
 

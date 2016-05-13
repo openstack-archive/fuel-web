d4b3fa0a32da1bf3a140a1a45c1430cbf0ec3787
Sergey Abramov <sabramov@mirantis.com>
2016-05-13 17:53:43 +0300

    Fix cluster attributes dns_list and ntp_list
    Settings in release change. Cloned cluster should have values valid for
    its release.
    
    dns_list and net_list were text values but in version mitaka-9.0 it
    changed to text_list
    
    Change-Id: Iac0aa42b7c36333e6d9c40b8a27a19df9efe36f5
    Closes-Bug: 1572179

--- nailgun/nailgun/extensions/cluster_upgrade/upgrade.py
+++ nailgun/nailgun/extensions/cluster_upgrade/upgrade.py
@@ -42,6 +42,13 @@ def merge_attributes(a, b):
         for key, values in six.iteritems(pairs):
             if key != "metadata" and key in a_values:
                 values["value"] = a_values[key]["value"]
+                # in mitaka-9.0 type of values dns_list and ntp_list
+                # changed from 'text' (string of values separated by comma)
+                # into 'text_list' (list of text values)
+                # bug: 1572179
+                if a_values[key]['type'] == 'text' and \
+                        values['type']== 'text_list':
+                    values["value"] = values['value'].split(',')
     return attrs
 
 

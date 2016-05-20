e56d69586314b1d8421f6de23c9c2b93f01de921
Georgy Kibardin <gkibardin@mirantis.com>
2016-05-20 16:31:20 +0300

    Using text/plain for error responses
    This prevents XSS attacks injecting scripts to error messages. Currently
    http error constructor sets content-type to text/html for errors with
    the code 500.
    
    Change-Id: I21b5855ebe102060f4046706b5c6ab2e3e532162
    Closes-Bug: #1583602
    (cherry picked from commit 25fca4bb847bf6e16b0a0c7c35437b7a478e81b1)

--- nailgun/nailgun/api/v1/handlers/base.py
+++ nailgun/nailgun/api/v1/handlers/base.py
@@ -306,6 +306,7 @@ def content_json(func, cls, *args, **kwargs):
             )
         )
         http_error.data = json_resp(http_error.data)
+        web.header('Content-Type', 'text/plain')
         raise http_error
 
     if all([

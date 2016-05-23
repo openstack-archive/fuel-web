781266ff0e10f2915480750c71ab313524260129
Dmitry Guryanov <dguryanov@mirantis.com>
2016-05-23 17:32:54 +0300

    Fix deployment_tasks api, when graph_type is an empty string
    Nailgun should treat empty string, given to
    
    /clusters/(?P<obj_id>\d+)/deployment_tasks/?$
    /clusters/(?P<obj_id>\d+)/deployment_tasks/plugins/?$
    /clusters/(?P<obj_id>\d+)/deployment_tasks/release/?$
    
    as default graph type. So let's convert some 'false'
    value to None in handlers.
    
    Change-Id: I238764d24fc14b2a41baaca37dabb6264e7e91d8
    Closes-Bug: #1576261

--- nailgun/nailgun/api/v1/handlers/cluster.py
+++ nailgun/nailgun/api/v1/handlers/cluster.py
@@ -338,7 +338,7 @@ class ClusterPluginsDeploymentTasksHandler(BaseHandler):
                * 404 (object not found)
         """
         obj = self.get_object_or_404(self.single, obj_id)
-        graph_type = web.input(graph_type=None).graph_type
+        graph_type = web.input(graph_type=None).graph_type or None
         tasks = self.single.get_plugins_deployment_tasks(
             obj, graph_type=graph_type)
         return tasks
@@ -356,7 +356,7 @@ class ClusterReleaseDeploymentTasksHandler(BaseHandler):
                * 404 (object not found)
         """
         obj = self.get_object_or_404(self.single, obj_id)
-        graph_type = web.input(graph_type=None).graph_type
+        graph_type = web.input(graph_type=None).graph_type or None
         tasks = self.single.get_release_deployment_tasks(
             obj, graph_type=graph_type)
         return tasks

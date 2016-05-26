8e12c153a5cde4c5e560df8a5c6f8ea4a19d7735
Ilya Kutukov <ikutukov@mirantis.com>
2016-05-26 17:22:16 +0300

    Move tasks snapshot field migration to 9.0.1
    Curently tasks snapshot field migration is included in 10.0.0
    migration. But in stable/mitaka branch it is in 9.0.1. This migration
    action should be located consistently in 9.0.1 in all branches.
    
    Change-Id: I235a6ac1ce551370a032b54f350ce2f3f339cb11
    Partial-Bug: #1563317

--- nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_10_0.py
+++ nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_10_0.py
@@ -19,7 +19,6 @@ Revises: 675105097a69
 Create Date: 2016-04-08 15:20:43.989472
 
 """
-from nailgun.db.sqlalchemy.models import fields
 
 from alembic import op
 import sqlalchemy as sa
@@ -31,13 +30,11 @@ down_revision = '675105097a69'
 
 
 def upgrade():
-    upgrade_tasks_snapshot()
     upgrade_plugin_links_constraints()
 
 
 def downgrade():
     downgrade_plugin_links_constraints()
-    downgrade_tasks_snapshot()
 
 
 def upgrade_plugin_links_constraints():
@@ -83,18 +80,3 @@ def downgrade_plugin_links_constraints():
                        'cluster_plugin_links')
 
     op.drop_constraint('plugin_links_url_uc', 'plugin_links')
-
-
-def upgrade_tasks_snapshot():
-    op.add_column(
-        'tasks',
-        sa.Column(
-            'tasks_snapshot',
-            fields.JSON(),
-            nullable=True
-        )
-    )
-
-
-def downgrade_tasks_snapshot():
-    op.drop_column('tasks', 'tasks_snapshot')

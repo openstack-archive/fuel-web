dd078fd3d1475ff12709d92d9197bddefcb3b80a
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2016-06-07 16:53:53 +0300

    Add fuyaql to allow debug expressions on master node
    Sometimes there is needed to test new YAQL expression.
    To allow do it easier, include new YAQL shell as a part
    of nailgun.
    
    Change-Id: Ibf211adf4b9b111e143b4dc5512fe9c1998c3927
    Closes-Bug: #1590007

---
+++ nailgun/nailgun/fuyaql/f_consts.py
@@ -0,0 +1,10 @@
+reserved_commands = {
+    ':show cluster': 'show_cluster',
+    ':use cluster': 'use_cluster',
+    ':show tasks': 'show_tasks',
+    ':oldcontext task': 'use_old_context_from_task',
+    ':newcontext task': 'use_new_context_from_task',
+    ':show nodes': 'show_nodes',
+    ':show node': 'show_node',
+    ':use node': 'use_node',
+}

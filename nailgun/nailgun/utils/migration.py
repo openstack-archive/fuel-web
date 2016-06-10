57c111a76a31eeefdf39d8aa72c2a4c1cff99dc3
Dmitry Teselkin <dteselkin@mirantis.com>
2016-06-10 19:33:45 +0300

    [WIP] s/trusty/xenial/g
    Change-Id: Ic9d3062db06f6e6fe11c4cbb1de3ef36b34aa22e
    Blueprint: mos-xenial

--- nailgun/nailgun/utils/migration.py
+++ nailgun/nailgun/utils/migration.py
@@ -255,9 +255,9 @@ def upgrade_release_fill_orchestrator_data(connection, versions):
             repo_path = 'http://{MASTER_IP}:8080/{OS}/fuelweb/x86_64'.format(
                 MASTER_IP=settings.MASTER_IP, OS=release[1].lower())
 
-            # for ubuntu we need to add 'trusty main'
+            # for ubuntu we need to add 'xenial main'
             if release[1].lower() == 'ubuntu':
-                repo_path += ' trusty main'
+                repo_path += ' xenial main'
 
             connection.execute(
                 insert_query,

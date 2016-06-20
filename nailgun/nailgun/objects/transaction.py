a61af4fade72717c8845a61681aa47eae1a24d9a
Dmitry Guryanov <dguryanov@mirantis.com>
2016-06-20 19:42:02 +0300

    Add filters for GET /api/transactions/
    Add add ability to filter transactions by task name
    and status.
    
    Closes-Bug: #1593751
    
    Change-Id: If4443045992bfc3816ae219e7607f6897767ba5b

--- nailgun/nailgun/objects/transaction.py
+++ nailgun/nailgun/objects/transaction.py
@@ -85,8 +85,15 @@ class TransactionCollection(NailgunCollection):
     single = Transaction
 
     @classmethod
-    def get_by_cluster_id(cls, cluster_id):
-        return cls.filter_by(None, cluster_id=cluster_id)
+    def get_transactions(cls, cluster_id, tasks_names=None, statuses=None):
+        query = cls.all()
+        if cluster_id:
+            query = cls.filter_by(query, cluster_id=cluster_id)
+        if tasks_names:
+            query = cls.filter_by_list(query, 'name', tasks_names)
+        if statuses:
+            query = cls.filter_by_list(query, 'status', statuses)
+        return query
 
     @classmethod
     def get_last_succeed_run(cls, cluster):

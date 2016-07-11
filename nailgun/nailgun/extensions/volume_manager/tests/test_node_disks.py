b2f27a8f2f84b16f84bd6dd27e0037cc3ead7477
Grzegorz Szafrański <gszafranski@mirantis.com>
2016-07-11 09:42:10 +0200

    Changing filesystems structure.
    Separating number of directories:
    /home
    /var/log/audit
    /var
    /tmp
    into standalone filesystems for security reason.
    
    Closes-Bug: #1526329
    Change-Id: I614d79e83420d496daa647bd3cdefc6aab7bdebf
    
    Changing filesystems structure.
    
    Separating number of directories:
    /home
    /var/log/audit
    /var
    /tmp
    into standalone filesystems for security reason.
    
    Closes-Bug: #1526329
    
    Change-Id: I18f25208616b52a019dd9959c040d3d8685a4aed

--- nailgun/nailgun/extensions/volume_manager/tests/test_node_disks.py
+++ nailgun/nailgun/extensions/volume_manager/tests/test_node_disks.py
@@ -638,7 +638,7 @@ class TestNodeVolumesInformationHandler(BaseIntegrationTest):
         node_db = self.create_node('controller')
         response = self.get(node_db.id)
         self.check_volumes(
-            response, ['os', 'image', 'mysql', 'logs', 'horizon'])
+            response, ['os', 'image', 'mysql', 'logs', 'horizon', 'home', 'audit', 'var', 'tmp'])
 
     def test_volumes_information_for_ceph_role(self):
         node_db = self.create_node('ceph-osd')
@@ -766,6 +766,18 @@ class TestVolumeManager(BaseIntegrationTest):
     def horizon_size(self, disks):
         return self.volumes_sum_size(disks, 'horizon')
 
+    def horizon_size(self, disks):
+        return self.volumes_sum_size(disks, 'home')
+
+    def horizon_size(self, disks):
+        return self.volumes_sum_size(disks, 'audit')
+
+    def horizon_size(self, disks):
+        return self.volumes_sum_size(disks, 'var')
+
+    def horizon_size(self, disks):
+        return self.volumes_sum_size(disks, 'tmp')
+
     def mysql_size(self, disks):
         return self.volumes_sum_size(disks, 'mysql')
 
@@ -879,13 +891,19 @@ class TestVolumeManager(BaseIntegrationTest):
         mysql_sum_size = self.mysql_size(disks)
         glance_sum_size = self.glance_size(disks)
         horizon_sum_size = self.horizon_size(disks)
+        home_sum_size = self.home_size(disks)
+        audit_sum_size = self.audit_size(disks)
+        var_sum_size = self.var_size(disks)
+        tmp_sum_size = self.tmp_size(disks)
         logs_sum_size = self.logs_size(disks)
         reserved_size = self.reserved_size(disks)
 
         self.assertEqual(disks_size_sum - reserved_size,
                          os_sum_size + glance_sum_size +
                          mysql_sum_size + logs_sum_size +
-                         horizon_sum_size)
+                         horizon_sum_size + home_sum_size +
+                         audit_sum_size + var_sum_size + 
+                         tmp_sum_size)
         self.logical_volume_sizes_should_equal_all_phisical_volumes(
             VolumeManagerExtension.get_node_volumes(node))
         self.check_disk_size_equal_sum_of_all_volumes(

63e77cb10ea15b376ed3063bc773e434481db671
Elena Kosareva <ekosareva@mirantis.com>
2016-07-13 11:23:33 +0300

    Add migrations for NICs and Bonds 'interface_properties'
    * Add default bond_attributes and nic_attributes for upgrade 'releases' table
    * Add upgrade and downgrade migrations for NICs from 'interface_properties'
      into 'attributes' and 'meta' fields and contrary.
    * Add upgrade and downgrade migrations for Bonds from 'interface_properties'
      and 'bond_properties' fields into 'attributes' field and contrary.
    
    Change-Id: Ib1b1ad55372fa1e7e67a51273ce237acb61a7201
    Implements: blueprint nics-and-nodes-attributes-via-plugin

--- nailgun/nailgun/extensions/network_manager/models/network.py
+++ nailgun/nailgun/extensions/network_manager/models/network.py
@@ -139,9 +139,6 @@ class NodeNICInterface(Base):
     ip_addr = Column(psql.INET)
     netmask = Column(psql.INET)
     state = Column(String(25))
-    interface_properties = Column(
-        MutableDict.as_mutable(JSON), default={}, nullable=False,
-        server_default='{}')
     parent_id = Column(
         Integer,
         ForeignKey('node_bond_interfaces.id', ondelete='SET NULL')
@@ -186,9 +183,6 @@ class NodeBondInterface(Base):
         secondary=NetworkBondAssignment.__table__,
         order_by="NetworkGroup.id")
     state = Column(String(25))
-    interface_properties = Column(
-        MutableDict.as_mutable(JSON), default={}, nullable=False,
-        server_default='{}')
     mode = Column(
         Enum(
             *consts.BOND_MODES,

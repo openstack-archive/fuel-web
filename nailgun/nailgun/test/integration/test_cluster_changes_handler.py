b433ee89f0ae17e0b1d2dbda40636cb19e7c3155
Oleksiy Molchanov <omolchanov@mirantis.com>
2016-07-15 13:16:37 +0300

    Restrict usage of controller and osd role on single host
    Update tests
    
    Change-Id: I64119fa3eb640c23e1f6e3d59e496b6f226be4e4
    Closes-Bug: 1603142

--- nailgun/nailgun/test/integration/test_cluster_changes_handler.py
+++ nailgun/nailgun/test/integration/test_cluster_changes_handler.py
@@ -1616,8 +1616,9 @@ class TestHandlers(BaseIntegrationTest):
     def test_occurs_error_not_enough_osds_for_ceph(self):
         cluster = self.env.create(
             nodes_kwargs=[
-                {'roles': ['controller', 'ceph-osd'],
-                 'pending_addition': True}])
+                {'roles': ['controller'], 'pending_addition': True},
+                {'roles': ['ceph-osd'], 'pending_addition': True}
+            ])
 
         self.app.patch(
             reverse(
@@ -1684,8 +1685,10 @@ class TestHandlers(BaseIntegrationTest):
     def test_enough_osds_for_ceph(self):
         cluster = self.env.create(
             nodes_kwargs=[
-                {'roles': ['controller', 'ceph-osd'],
-                 'pending_addition': True}])
+                {'roles': ['controller'], 'pending_addition': True},
+                {'roles': ['ceph-osd'], 'pending_addition': True},
+            ])
+
         self.app.patch(
             reverse(
                 'ClusterAttributesHandler',

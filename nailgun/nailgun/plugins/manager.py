70374807aa1cc0a29bb7d77e1ee56e67bf482013
Andriy Popovych <popovych.andrey@gmail.com>
2016-07-15 12:53:55 +0300

    Bond attributes in interface API
    Remove restrictions on set of BOND properties. Now BOND can have
    any attributes.
    Remove redundant `bond_properties`.
    
    Change-Id: I60d1a0628d84c5ba49bb5b45824d660297dacccc
    Implements: blueprint nics-and-nodes-attributes-via-plugin

--- nailgun/nailgun/plugins/manager.py
+++ nailgun/nailgun/plugins/manager.py
@@ -398,7 +398,6 @@ class PluginManager(object):
 
         return components
 
-    # ENTRY POINT
     @classmethod
     def get_bond_default_attributes(cls, cluster):
         """Get plugin bond attributes metadata for cluster.
@@ -448,7 +447,7 @@ class PluginManager(object):
 
         for plugin in plugins:
             metadata = plugin.pop('metadata')
-            NodeNICInterfaceClusterPlugin.\
+            NodeBondInterfaceClusterPlugin.\
                 set_attributes(
                     metadata['bond_plugin_id'],
                     plugin

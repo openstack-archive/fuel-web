8ea3cfaa41541567bd5def127161d222a53ae4b6
Dmitry Guryanov <dguryanov@mirantis.com>
2016-07-21 18:50:44 +0300

    don't merge common_attrs with node data in deployment_info
    The size of deployment_info grows as n^2 depending on
    nodes number. That's because common_attrs, which is
    merged into each node's contains info about all nodes.
    
    For example for 600 nodes we store about 1Gb of data in
    the database. So as first step let's store common_attrs
    separately in deployment_info structure inside python
    code and in the database.
    
    Change-Id: I431062b3f9c8dedd407570729166072b780dc59a
    Partial-Bug: #1596987

--- nailgun/nailgun/lcm/context.py
+++ nailgun/nailgun/lcm/context.py
@@ -12,6 +12,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from nailgun import utils
+
 
 class TransactionContext(object):
     def __init__(self, new_state, old_state=None, **kwargs):
@@ -27,7 +29,13 @@ class TransactionContext(object):
         self.options = kwargs
 
     def get_new_data(self, node_id):
-        return self.new[node_id]
+        return utils.dict_merge(self.new['common_attrs'],
+                                self.new['nodes'][node_id])
 
     def get_old_data(self, node_id, task_id):
-        return self.old.get(task_id, {}).get(node_id, {})
+        dinfo = self.old.get(task_id, {})
+        if not dinfo:
+            return {}
+        else:
+            return utils.dict_merge(dinfo['common_attrs'],
+                                    dinfo['nodes'].get(node_id, {}))

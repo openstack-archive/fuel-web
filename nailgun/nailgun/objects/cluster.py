074d06f6bd7b2823f93603fbc5eb06ff7cb628e6
Bulat Gaifullin <bgaifullin@mirantis.com>
2016-07-25 21:47:43 +0300

    Added virtual role 'deleted' for RoleResolver
    This role can be used to link tasks to nodes
    which were marked as deleted
    The role 'deleted' does not match pattern '*' or '/.*/',
    so it should be specified explicitly, for example:
    "roles: ['deleted']".
    Also it can be combined with other roles. for example:
    "roles: ['/.*/', 'deleted']
    
    Change-Id: I56cdf944aee594d68434c6fd8bb4d49461cb7909
    Implements: blueprint graph-concept-extension

--- nailgun/nailgun/objects/cluster.py
+++ nailgun/nailgun/objects/cluster.py
@@ -1292,9 +1292,14 @@ class Cluster(NailgunObject):
             Node.apply_network_template(node, template)
 
     @classmethod
+    def get_all_nodes(cls, instance):
+        return db().query(models.Node).filter(
+            models.Node.cluster_id == instance.id
+        ).all()
+
+    @classmethod
     def get_nodes_ids(cls, instance):
-        return [x[0] for x in db().query(models.Node.id).filter(
-            models.Node.cluster_id == instance.id).all()]
+        return [x['id'] for x in cls.get_all_nodes(instance)]
 
     @classmethod
     def get_vips(cls, instance):

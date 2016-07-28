0c0a666c5448176134262f66c1f6cc2b1c4f729d
Georgy Kibardin <gkibardin@mirantis.com>
2016-07-28 14:11:46 +0300

    Limit cookie usage to download routes
    In order to fix CSRF issues we gradually getting rid of cookies. The
    first step is to limit their usage to download routes only.
    
    Change-Id: I0ec451e8a536dc37af08f2cd2efe58500737b56e
    Partial-Bug: #1592732

--- nailgun/nailgun/middleware/keystone.py
+++ nailgun/nailgun/middleware/keystone.py
@@ -43,18 +43,25 @@ class CookieTokenMixin(object):
     Token is taken from X-Auth-Token header or
     if that doesn't exist, from the cookie.
     """
+
+    cookie_routes = [
+        r'/dump/(?P<snapshot_name>[A-Za-z0-9-_.]+)$',
+        r'/capacity/csv/?$'
+    ]
+
+    cookie_routes_regexs = [re.compile(r) for r in cookie_routes]
+
     def get_auth_token(self, env):
         token = env.get('HTTP_X_AUTH_TOKEN', '')
-
         if token:
             return token
 
-        c = Cookie.SimpleCookie(env.get('HTTP_COOKIE', ''))
-
-        token = c.get('token')
-
-        if token:
-            return token.value
+        path = env.get('PATH_INFO', '/')
+        if any(r.match(path) for r in self.cookie_routes_regexs):
+            c = Cookie.SimpleCookie(env.get('HTTP_COOKIE', ''))
+            token = c.get('token')
+            if token:
+                return token.value
 
         return ''
 

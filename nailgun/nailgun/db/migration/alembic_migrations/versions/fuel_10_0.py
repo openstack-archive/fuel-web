8e2220b789982c6f48535b120b23a45a80801c5f
Bulat Gaifullin <bgaifullin@mirantis.com>
2016-08-15 16:54:44 +0300

    Introduce new orchestration tasks
    There are 3 new orchestration tasks:
    
    * master_shell
    
      Run task on master node with a context of other node. If 'roles'
      selects N nodes, the task will be executed N times.
    
    * erase_node
    
      Erase node. It's necessary task if we want to remove nodes by means
      of graphs and not pre-hardcoded actions in Astute.
    
    * move_to_bootstrap
    
      Change node's PXE config to boot via LAN (into bootstrap). As a
      previous one, it's necessary task for deletion graph.
    
    Change-Id: Ie8f852762b837a68e0e0b49e11653a8f2e56a014
    Blueprint: graph-concept-extension

--- nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_10_0.py
+++ nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_10_0.py
@@ -26,6 +26,7 @@ import sqlalchemy as sa
 from oslo_serialization import jsonutils
 
 from nailgun.db.sqlalchemy.models import fields
+from nailgun.utils.migration import upgrade_enum
 
 
 # revision identifiers, used by Alembic.
@@ -40,9 +41,11 @@ def upgrade():
     upgrade_release_required_component_types()
     upgrade_task_model()
     upgrade_deployment_graphs_attributes()
+    upgrade_orchestrator_task_types()
 
 
 def downgrade():
+    downgrade_orchestrator_task_types()
     downgrade_deployment_graphs_attributes()
     downgrade_task_model()
     downgrade_release_required_component_types()
@@ -369,3 +372,44 @@ def downgrade_deployment_graphs_attributes():
     op.drop_column('deployment_graphs', 'on_success')
     op.drop_column('deployment_graphs', 'on_error')
     op.drop_column('deployment_graphs', 'on_stop')
+
+
+orchestrator_task_types_old = (
+    'puppet',
+    'shell',
+    'sync',
+    'upload_file',
+    'group',
+    'stage',
+    'skipped',
+    'reboot',
+    'copy_files',
+    'role'
+)
+
+
+orchestrator_task_types_new = orchestrator_task_types_old + (
+    'master_shell',
+    'move_to_bootstrap',
+    'erase_node'
+)
+
+
+def upgrade_orchestrator_task_types():
+    upgrade_enum(
+        'deployment_graph_tasks',
+        'type',
+        'deployment_graph_tasks_type',
+        orchestrator_task_types_old,
+        orchestrator_task_types_new
+    )
+
+
+def downgrade_orchestrator_task_types():
+    upgrade_enum(
+        'deployment_graph_tasks',
+        'type',
+        'deployment_graph_tasks_type',
+        orchestrator_task_types_new,
+        orchestrator_task_types_old
+    )

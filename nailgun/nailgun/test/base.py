e53c6b1332f262c4dc3cbbd62d52f28d5b2cbcc6
Andriy Popovych <popovych.andrey@gmail.com>
2016-08-15 11:03:35 +0300

    Support for plugin NIC attributes
    Refactor interface logic:
      * remove interface_properties
      * CRUD operations for NIC attributes
      * default values for NIC meta
    
    Change-Id: I26106f1b55c704a9e79d01fadc48c88a92ccc414
    Implements: blueprint nics-and-nodes-attributes-via-plugin

--- nailgun/nailgun/test/base.py
+++ nailgun/nailgun/test/base.py
@@ -543,9 +543,9 @@ class EnvironmentManager(object):
         deployment_tasks = plugin_data.pop('deployment_tasks', None)
         tasks = plugin_data.pop('tasks', None)
         components = plugin_data.pop('components', None)
-        nic_config = plugin_data.pop('nic_config', None)
-        bond_config = plugin_data.pop('bond_config', None)
-        node_config = plugin_data.pop('node_config', None)
+        nic_config = plugin_data.pop('nic_attributes_metadata', None)
+        bond_config = plugin_data.pop('bond_attributes_metadata', None)
+        node_config = plugin_data.pop('node_attributes_metadata', None)
 
         mocked_metadata = {
             'metadata.*': plugin_data,
@@ -1215,7 +1215,8 @@ class EnvironmentManager(object):
             )
 
     def make_bond_via_api(self, bond_name, bond_mode, nic_names, node_id=None,
-                          bond_properties=None, interface_properties=None):
+                          bond_properties=None, interface_properties=None,
+                          attrs=None):
         if not node_id:
             node_id = self.nodes[0]["id"]
         resp = self.app.get(
@@ -1243,7 +1244,8 @@ class EnvironmentManager(object):
             "type": NETWORK_INTERFACE_TYPES.bond,
             "mode": bond_mode,
             "slaves": slaves,
-            "assigned_networks": assigned_nets
+            "assigned_networks": assigned_nets,
+            "attributes": attrs or {}
         }
         if bond_properties:
             bond_dict["bond_properties"] = bond_properties

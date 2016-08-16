beffa37f7d4ec148eedbda4e4b0ce3049c7192a2
Andriy Popovych <popovych.andrey@gmail.com>
2016-08-16 14:31:15 +0300

    Bond attributes in interface API
    Remove restrictions on set of BOND properties. Now BOND can have
    any attributes.
    Remove redundant `bond_properties`.
    
    Change-Id: I60d1a0628d84c5ba49bb5b45824d660297dacccc
    Implements: blueprint nics-and-nodes-attributes-via-plugin

--- nailgun/nailgun/test/integration/test_orchestrator_serializer.py
+++ nailgun/nailgun/test/integration/test_orchestrator_serializer.py
@@ -628,8 +628,9 @@ class TestNovaNetworkOrchestratorSerializer61(OrchestratorSerializerTestBase):
             self.move_network(node.id, 'management', 'eth0', 'eth1')
             self.env.make_bond_via_api(
                 'lnx_bond', '', ['eth1', 'eth2'], node.id,
-                bond_properties={
-                    'mode': consts.BOND_MODES.balance_rr})
+                attrs={
+                    'mode': {
+                        'value': {'value': consts.BOND_MODES.balance_rr}}})
         serializer = self.create_serializer(cluster)
         facts = serializer.serialize(cluster, cluster.nodes)['nodes']
         for node in facts:
@@ -675,8 +676,9 @@ class TestNovaNetworkOrchestratorSerializer61(OrchestratorSerializerTestBase):
             self.move_network(node.id, 'fixed', 'eth0', 'eth1')
             self.env.make_bond_via_api(
                 'lnx_bond', '', ['eth1', 'eth2'], node.id,
-                bond_properties={
-                    'mode': consts.BOND_MODES.balance_rr})
+                attrs={
+                    'mode': {
+                        'value': {'value': consts.BOND_MODES.balance_rr}}})
         serializer = self.create_serializer(cluster)
         facts = serializer.serialize(cluster, cluster.nodes)['nodes']
         for node in facts:
@@ -949,11 +951,10 @@ class TestNeutronOrchestratorSerializer61(OrchestratorSerializerTestBase):
             self.move_network(node.id, 'storage', 'eth0', 'eth1')
             self.env.make_bond_via_api(
                 'lnx_bond', '', ['eth1', 'eth2'], node.id,
-                bond_properties={
-                    'mode': consts.BOND_MODES.balance_rr
-                },
-                attrs={'mtu': {'value': {'value': 9000}}},
-                interface_properties={'mtu': 9000}
+                attrs={
+                    'mtu': {'value': {'value': 9000}},
+                    'mode': {'value': {'value': consts.BOND_MODES.balance_rr}}
+                }
             )
         serializer = self.create_serializer(cluster)
         facts = serializer.serialize(cluster, cluster.nodes)['nodes']
@@ -1094,14 +1095,22 @@ class TestNeutronOrchestratorSerializer61(OrchestratorSerializerTestBase):
             self.move_network(node.id, 'storage', 'eth0', 'eth1')
             self.env.make_bond_via_api(
                 'lnx_bond', '', ['eth1', 'eth2'], node.id,
-                bond_properties={
-                    'mode': consts.BOND_MODES.l_802_3ad,
-                    'xmit_hash_policy': consts.BOND_XMIT_HASH_POLICY.layer2,
-                    'lacp_rate': consts.BOND_LACP_RATES.slow,
-                    'type__': consts.BOND_TYPES.linux
-                },
-                attrs={'mtu': {'value': {'value': 9000}}},
-                interface_properties={'mtu': 9000})
+                attrs={
+                    'mtu': {
+                        'value': {
+                            'value': 9000}},
+                    'mode': {
+                        'value': {
+                            'value': consts.BOND_MODES.l_802_3ad}},
+                    'xmit_hash_policy': {
+                        'value': {
+                            'value': consts.BOND_XMIT_HASH_POLICY.layer2}},
+                    'lacp_rate': {
+                        'value': {
+                            'value': consts.BOND_LACP_RATES.slow}},
+                    'type__': {
+                        'value': consts.BOND_TYPES.linux}
+                })
         serializer = self.create_serializer(cluster)
         facts = serializer.serialize(cluster, cluster.nodes)['nodes']
         for node in facts:

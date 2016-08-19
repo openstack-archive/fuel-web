openstack-archive/fuel-web

commit c179e5d0b4b8a5b4566120eceb81a1d0ed9577c8

add a function get_param_as_set for handler
It's a common task to get list of something from URL,
so let's add this get_param_as_set function and use
it where possible.

Change-Id: I4ae28553117f5ae4de35350270a40e3ac6beaf41
Partial-Bug: #1593751

nailgun/nailgun/api/v1/handlers/deployment_history.py
@@ -13,7 +13,6 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-import web
 
 from nailgun.api.v1.handlers import base
 from nailgun.api.v1.handlers.base import content
@@ -41,23 +40,15 @@ class DeploymentHistoryCollectionHandler(base.CollectionHandler):
             objects.Transaction, transaction_id)
 
         # process input parameters
-        nodes_ids = web.input(nodes=None).nodes
-        statuses = web.input(statuses=None).statuses
-        tasks_names = web.input(tasks_names=None).tasks_names
-
+        nodes_ids = self.get_param_as_set('nodes_ids')
+        statuses = self.get_param_as_set('statuses')
+        tasks_names = self.get_param_as_set('tasks_names')
         try:
             self.validator.validate_query(nodes_ids=nodes_ids,
                                           statuses=statuses,
                                           tasks_names=tasks_names)
         except errors.ValidationException as exc:
             raise self.http(400, exc.message)
-
-        if nodes_ids:
-            nodes_ids = set(nodes_ids.strip().split(','))
-        if statuses:
-            statuses = set(statuses.strip().split(','))
-        if tasks_names:
-            tasks_names = set(tasks_names.strip().split(','))
 
         # fetch and serialize history
         return self.collection.get_history(transaction=transaction,

nailgun/nailgun/api/v1/validators/deployment_history.py
@@ -21,10 +21,11 @@ class DeploymentHistoryValidator(BasicValidator):
 
     @classmethod
     def validate_query(cls, nodes_ids, statuses, tasks_names):
-        if statuses:
-            statuses = set(statuses.strip().split(','))
-            if not statuses.issubset(set(consts.HISTORY_TASK_STATUSES)):
-                raise errors.ValidationException(
-                    "Statuses parameter could be only: {}".format(
-                        ", ".join(consts.HISTORY_TASK_STATUSES))
-                )
+        if not statuses:
+            return
+
+        if not statuses.issubset(set(consts.HISTORY_TASK_STATUSES)):
+            raise errors.ValidationException(
+                "Statuses parameter could be only: {}".format(
+                    ", ".join(consts.HISTORY_TASK_STATUSES))
+            )

nailgun/nailgun/api/v1/handlers/base.py
@@ -252,6 +252,25 @@ class BaseHandler(object):
 
         raise self.http(status, objects.Task.to_json(task))
 
+    @staticmethod
+    def get_param_as_set(param_name, delimiter=',', default=None):
+        """Parse array param from web.input()
+
+        :param param_name: parameter name in web.input()
+        :type param_name: str
+        :param delimiter: delimiter
+        :type delimiter: str
+        :returns: list of items
+        :rtype: set of str or None
+        """
+        if param_name in web.input():
+            return set(six.moves.map(
+                six.text_type.strip,
+                getattr(web.input(), param_name).split(delimiter))
+            )
+        else:
+            return default
+
 
 def content_json(func, cls, *args, **kwargs):
     json_resp = lambda data: (

nailgun/nailgun/api/v1/handlers/node.py
@@ -164,9 +164,10 @@ class NodeCollectionHandler(CollectionHandler):
         """
         # TODO(pkaminski): web.py does not support parsing of array arguments
         # in the queryset so we specify the input as comma-separated list
+        node_ids = self.get_param_as_set('ids', default=[])
         node_ids = self.checked_data(
-            validate_method=self.validator.validate_collection_delete,
-            data=web.input().get('ids', '')
+            validate_method=self.validator.validate_ids_list,
+            data=node_ids
         )
 
         nodes = self.get_objects_list_or_404(self.collection, node_ids)

nailgun/nailgun/api/v1/handlers/orchestrator.py
@@ -59,18 +59,18 @@ class NodesFilterMixin(object):
 
         else return default nodes
         """
-        nodes = web.input(nodes=None).nodes
-        if nodes:
-            node_ids = self.checked_data(data=nodes)
-            nodes_obj = self.get_objects_list_or_404(
-                objects.NodeCollection,
-                node_ids
-            )
-            self.checked_data(self.validator.validate_placement,
-                              data=nodes_obj, cluster=cluster)
-            return nodes_obj
+        nodes = self.get_param_as_set('nodes', default=[])
+        if not nodes:
+            return self.get_default_nodes(cluster) or []
 
-        return self.get_default_nodes(cluster) or []
+        node_ids = self.checked_data(data=nodes)
+        nodes_obj = self.get_objects_list_or_404(
+            objects.NodeCollection,
+            node_ids
+        )
+        self.checked_data(self.validator.validate_placement,
+                          data=nodes_obj, cluster=cluster)
+        return nodes_obj
 
 
 class DefaultOrchestratorInfo(NodesFilterMixin, BaseHandler):
@@ -367,14 +367,14 @@ class TaskDeployGraph(BaseHandler):
         tasks = objects.Cluster.get_deployment_tasks(cluster, graph_type)
         graph = orchestrator_graph.GraphSolver(tasks)
 
-        tasks = web.input(tasks=None).tasks
+        tasks = self.get_param_as_set('tasks', default=[])
         parents_for = web.input(parents_for=None).parents_for
-        remove = web.input(remove=None).remove
+        remove = self.get_param_as_set('remove')
 
         if tasks:
             tasks = self.checked_data(
                 self.validator.validate,
-                data=tasks,
+                data=list(tasks),
                 cluster=cluster,
                 graph_type=graph_type)
             logger.debug('Tasks used in dot graph %s', tasks)
@@ -387,7 +387,7 @@ class TaskDeployGraph(BaseHandler):
             logger.debug('Graph with predecessors for %s', parents_for)
 
         if remove:
-            remove = list(set(remove.split(',')))
+            remove = list(remove)
             remove = self.checked_data(
                 self.validator.validate_tasks_types,
                 data=remove)
@@ -419,9 +419,8 @@ class SerializedTasksHandler(NodesFilterMixin, BaseHandler):
         """
         cluster = self.get_object_or_404(objects.Cluster, cluster_id)
         nodes = self.get_nodes(cluster)
-        tasks = web.input(tasks=None).tasks
         graph_type = web.input(graph_type=None).graph_type or None
-        task_ids = [t.strip() for t in tasks.split(',')] if tasks else None
+        task_ids = self.get_param_as_set('tasks')
 
         try:
             if objects.Release.is_lcm_supported(cluster.release):

nailgun/nailgun/api/v1/validators/base.py
@@ -122,6 +122,25 @@ class BasicValidator(object):
                 "from Fuel Master node, please check '/etc/puppet' "
                 "directory.".format(cluster.release.name))
 
+    @classmethod
+    def validate_ids_list(cls, data):
+        """Validate list of integer identifiers.
+
+        :param data: ids list to be validated and converted
+        :type data: iterable of strings
+        :returns: converted and verified data
+        :rtype: list of integers
+        """
+        try:
+            ret = [int(d) for d in data]
+        except ValueError:
+            raise errors.InvalidData('Comma-separated numbers list expected',
+                                     log_message=True)
+
+        cls.validate_schema(ret, base_types.IDS_ARRAY)
+
+        return ret
+
 
 class BaseDefferedTaskValidator(BasicValidator):
 

nailgun/nailgun/api/v1/validators/node.py
@@ -331,36 +331,12 @@ class NodeValidator(base.BasicValidator):
             cls.validate_update(nd)
         return d
 
-    @classmethod
-    def validate_collection_delete(cls, data):
-        try:
-            d = [int(d) for d in data.split(',')]
-        except ValueError:
-            raise errors.InvalidData('Comma-separated number list expected',
-                                     log_message=True)
-
-        cls.validate_schema(d, base_types.IDS_ARRAY)
-
-        return d
-
 
 class NodesFilterValidator(base.BasicValidator):
 
     @classmethod
     def validate(cls, nodes):
-        """Used for filtering nodes
-
-        :param nodes: list of ids in string representation.
-                      Example: "1,99,3,4"
-
-        :returns: list of integers
-        """
-        try:
-            node_ids = set(map(int, nodes.split(',')))
-        except ValueError:
-            raise errors.InvalidData('Provided id is not integer')
-
-        return node_ids
+        return super(NodesFilterValidator, cls).validate_ids_list(nodes)
 
     @classmethod
     def validate_placement(cls, nodes, cluster):

nailgun/nailgun/api/v1/validators/orchestrator_graph.py
@@ -92,8 +92,7 @@ class GraphSolverVisualizationValidator(TaskDeploymentValidator):
         :returns: list of tasks
         :rtype: list[dict]
         """
-        tasks = list(set(data.split(',')))
-        return cls.validate_tasks(tasks, cluster, graph_type)
+        return cls.validate_tasks(data, cluster, graph_type)
 
     @classmethod
     def validate_task_presence(cls, task, graph):

commit 49427c54ec2c666d00892feed5211f2290f3b8b0

Fix deployment tasks history API breakage
This is a partial revert of a regression caused by
c179e5d0b4b8a5b4566120eceb81a1d0ed9577c8
and
ecef951437fa694d59fff418c68d45e78dc30aa8

Change-Id: I3b294def77ef98289ec32196c3b7676ddfb782f0
Closes-bug: #1615083

nailgun/nailgun/api/v1/handlers/deployment_history.py
@@ -40,7 +40,7 @@ class DeploymentHistoryCollectionHandler(base.CollectionHandler):
             objects.Transaction, transaction_id)
 
         # process input parameters
-        nodes_ids = self.get_param_as_set('nodes_ids')
+        nodes_ids = self.get_param_as_set('nodes')
         statuses = self.get_param_as_set('statuses')
         tasks_names = self.get_param_as_set('tasks_names')
         try:
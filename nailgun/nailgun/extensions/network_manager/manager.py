e36d3e4eddc3f9f78e9e7c0bca7b2b5e477d3dcc
Andriy Popovych <popovych.andrey@gmail.com>
2016-08-23 13:59:17 +0300

    Serialization for NIC and Bond attributes
    Add plugin's NIC and Bond attributes in serialized data
    for orchestrator.
    
    Change-Id: I9b6543cfc443929cd9d64d8578e2fec3be43bf19
    Implements: blueprint nics-and-nodes-attributes-via-plugin

--- nailgun/nailgun/extensions/network_manager/manager.py
+++ nailgun/nailgun/extensions/network_manager/manager.py
@@ -1395,9 +1395,7 @@ class NetworkManager(object):
                 'disable_offloading':
                 iface.attributes['offloading']['disable']['value']
             }
-
-        if iface.attributes.get('offloading', {}).get(
-                'modes', {}).get('value'):
+        if get_in(iface.attributes, 'offloading', 'modes', 'value'):
             properties['ethtool'] = {
                 'offload': iface.attributes['offloading']['modes']['value']
             }

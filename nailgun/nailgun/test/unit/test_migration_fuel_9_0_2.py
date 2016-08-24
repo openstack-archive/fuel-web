e83ef51232a8f830aefec84404bf0a2ddd9e83a8
Anastasiia Guzikova <aguzikova@mirantis.com>
2016-08-25 02:38:06 +0300

    Fuel Nailgun: Noop run support
    Task model is extended with noop_run boolean column;
    Introdicing and passing down the noop_run param from API to
    execution manager;
    Execution manager supports noop_run argument,
    and uses it for creating astute message;
    DeploymentHistory model is extended with summary JSON column;
    The summary column should be returned only if some
    include_summary=1 query string is passed to API.
    
    Implements blueprint: puppet-noop-run
    
    Change-Id: I80090d96f818cef7c6f88208bdacf5849f0f5d0f

--- nailgun/nailgun/test/unit/test_migration_fuel_9_0_2.py
+++ nailgun/nailgun/test/unit/test_migration_fuel_9_0_2.py
@@ -295,6 +295,32 @@ def prepare():
         }]
     )
 
+    db.execute(
+        meta.tables['tasks'].insert(),
+        [
+            {
+                'id': 55,
+                'uuid': '219eaafe-01a1-4f26-8edc-b9d9b0df06b3',
+                'name': 'deployment',
+                'status': 'running',
+                'deployment_info': jsonutils.dumps({}),
+            },
+        ]
+    )
+    db.execute(
+        meta.tables['deployment_history'].insert(),
+        [
+            {
+                'uuid': 'fake_uuid_0',
+                'deployment_graph_task_name': 'fake',
+                'node_id': 'fake_node_id',
+                'task_id': 55,
+                'status': 'pending',
+                'summary': jsonutils.dumps({'fake': 'fake'}),
+            }
+        ]
+    )
+
     db.commit()
 
 
@@ -487,3 +513,11 @@ class TestOrchestratorTaskTypesMigration(base.BaseAlembicMigrationTest):
             'select unnest(enum_range(NULL::deployment_graph_tasks_type))'
         )).fetchall()
         self.assertTrue(expected_values.issubset((x[0] for x in result)))
+
+
+class TestDeploymentHistoryMigration(base.BaseAlembicMigrationTest):
+
+    def test_deployment_history_summary_field_exist(self):
+        result = db.execute(sa.select([
+            self.meta.tables['deployment_history']])).first()
+        self.assertIn('summary', result)

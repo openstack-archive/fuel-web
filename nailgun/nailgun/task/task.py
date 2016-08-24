cb36f4a7b7b2e4806c3952f5b367f75cd8bdd6fa
Bulat Gaifullin <bgaifullin@mirantis.com>
2016-08-24 23:11:09 +0300

    Do not allow to run 2 or more tasks simultaneously
    Simultaneous run of 2 or tasks may cause
    side-effects and the simplest way is prevent this action.
    Also fixed all places where rpc.cast called without commit
    
    Change-Id: I029768900d345540c3b501f1fa3649b063d3a55d
    Partial-Bug: 1615557

--- nailgun/nailgun/task/task.py
+++ nailgun/nailgun/task/task.py
@@ -836,6 +836,7 @@ class DeletionTask(object):
 
         logger.debug("Calling rpc remove_nodes method with nodes %s",
                      nodes_to_delete)
+        db().commit()
         rpc.cast('naily', msg_delete)
 
     @classmethod
@@ -881,7 +882,7 @@ class DeleteIBPImagesTask(object):
     def execute(cls, cluster, image_data):
         task = Task(name=consts.TASK_NAMES.remove_images, cluster=cluster)
         db().add(task)
-        db().flush()
+        db().commit()
         rpc.cast('naily', cls.message(task, image_data))
 
 
@@ -923,6 +924,7 @@ class StopDeploymentTask(object):
 
     @classmethod
     def execute(cls, task, deploy_task=None, provision_task=None):
+        db().commit()
         if provision_task:
             rpc.cast(
                 'naily',
@@ -1984,7 +1986,7 @@ class DumpTask(object):
                 'settings': conf or cls.conf()
             }
         )
-        db().flush()
+        db().commit()
         rpc.cast('naily', message)
 
 
@@ -2050,6 +2052,7 @@ class CheckRepoAvailability(BaseNetworkVerification):
         return rpc_message
 
     def execute(self):
+        db().commit()
         rpc.cast('naily', self.get_message())
 
     def _get_nodes_to_check(self):
@@ -2178,6 +2181,7 @@ class CreateStatsUserTask(object):
 
     @classmethod
     def execute(cls, task, primary_controller):
+        db().commit()
         rpc.cast(
             'naily',
             cls.message(task, primary_controller)
@@ -2210,6 +2214,7 @@ class RemoveStatsUserTask(object):
 
     @classmethod
     def execute(cls, task, primary_controller):
+        db().commit()
         rpc.cast(
             'naily',
             cls.message(task, primary_controller)

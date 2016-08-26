a48867dc73568522fbebcff1f0ce3073acffa377
tivaliy <vitaliy@kulanov.org.ua>
2016-08-26 12:29:33 +0300

    Add check 'role' existence in release
    Now Nailgun doesn't check 'role' existance in release
    that leads to 'Internal Server Error 500' while trying
    to get/update/delete non-existing roles. This patch adds
    check role procedure and returns 404 not found responce
    instead Internal Server Error.
    
    Change-Id: I9a9e0fc59a86b414dce1fc3b036b90d00db94325
    Closes-Bug: 1617229

--- nailgun/nailgun/test/integration/test_role_api.py
+++ nailgun/nailgun/test/integration/test_role_api.py
@@ -91,6 +91,7 @@ class TestRoleApi(BaseRoleTest):
         self.assertIn('volumes_roles_mapping', resp.body)
 
     def test_update_role_w_invalid_volumes_id(self):
+        self.env.create_role(self.release.id, self.role_data)
         self.role_data['volumes_roles_mapping'][0]['id'] = 'some_string'
         resp = self.env.update_role(
             self.release.id,
@@ -100,6 +101,17 @@ class TestRoleApi(BaseRoleTest):
         self.assertEqual(400, resp.status_code)
         self.assertIn('Wrong data in volumes_roles_mapping', resp.body)
 
+    def test_update_role_not_present(self):
+        self.env.create_role(self.release.id, self.role_data)
+        role_name = 'blah_role'
+        resp = self.env.update_role(
+            self.release.id,
+            role_name,
+            self.role_data,
+            expect_errors=True)
+        self.assertEqual(404, resp.status_code)
+        self.assertIn('is not found for the release', resp.body)
+
     def test_delete_role(self):
 
         self.env.create_role(self.release.id, self.role_data)
@@ -107,6 +119,16 @@ class TestRoleApi(BaseRoleTest):
             self.release.id, self.role_data['name'])
 
         self.assertEqual(delete_resp.status_code, 204)
+
+    def test_delete_role_not_present(self):
+        self.env.create_role(self.release.id, self.role_data)
+        role_name = 'blah_role'
+        delete_resp = self.env.delete_role(
+            self.release.id,
+            role_name,
+            expect_errors=True)
+        self.assertEqual(delete_resp.status_code, 404)
+        self.assertIn('is not found for the release', delete_resp.body)
 
     def test_delete_assigned_role(self):
         role = self.env.create_role(self.release.id, self.role_data).json
@@ -155,6 +177,16 @@ class TestRoleApi(BaseRoleTest):
 
         self.assertEqual(role.status_code, 200)
         self.assertEqual(role.json['name'], self.role_data['name'])
+
+    def test_get_role_not_present(self):
+        self.env.create_role(self.release.id, self.role_data)
+        role_name = 'blah_role'
+        resp = self.env.get_role(
+            self.release.id,
+            role_name,
+            expect_errors=True)
+        self.assertEqual(resp.status_code, 404)
+        self.assertIn('is not found for the release', resp.body)
 
     def test_create_role_with_numbers(self):
         self.role_data['name'] = '1234'

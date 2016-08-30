eb3195728e31abbaa703025d86070193d842af2b
Ilya Kutukov <ikutukov@mirantis.com>
2016-08-30 17:27:28 +0300

    Now empty parameters is returned via get_param_as_set as empty set
    Previous behaviour with returning not empty set('') vas invalid
    
    Change-Id: I0a3c6aa41150d7557d29d2ca58f03f768112abce
    Closes-Bug: #1618476

--- nailgun/nailgun/test/integration/test_deployment_history_handlers.py
+++ nailgun/nailgun/test/integration/test_deployment_history_handlers.py
@@ -241,6 +241,33 @@ class TestDeploymentHistoryHandlers(BaseIntegrationTest):
 
     @mock.patch('nailgun.task.task.rpc.cast')
     @mock.patch('objects.Cluster.get_deployment_tasks')
+    def test_history_task_with_empty_statuses(self, tasks_mock, _):
+        tasks_mock.return_value = self.test_tasks
+
+        cluster = self.env.create(**self.cluster_parameters)
+
+        supertask = self.env.launch_deployment(cluster.id)
+        self.assertNotEqual(consts.TASK_STATUSES.error, supertask.status)
+        deployment_task = next(
+            t for t in supertask.subtasks
+            if t.name == consts.TASK_NAMES.deployment
+        )
+
+        response = self.app.get(
+            reverse(
+                'DeploymentHistoryCollectionHandler',
+                kwargs={
+                    'transaction_id': deployment_task.id
+                }
+            ) + '?statuses=',
+            headers=self.default_headers,
+            expect_errors=True
+        )
+
+        self.assertEqual(200, response.status_code)
+
+    @mock.patch('nailgun.task.task.rpc.cast')
+    @mock.patch('objects.Cluster.get_deployment_tasks')
     def test_history_collection_handler_csv(self, tasks_mock, _):
         self.maxDiff = None
         tasks_mock.return_value = self.test_tasks

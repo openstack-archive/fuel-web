fe87cfa835ab7cff264c45879eb720b8755e571b
Bulat Gaifullin <bgaifullin@mirantis.com>
2016-08-31 12:14:34 +0300

    Added legacy plugin tasks to graph only if graph is default
    Also save graph_type in nailgun.task(AKA transaction)
    
    Change-Id: I946a092df4d7c1bb143a47915cbc1455cc8c53c5
    Closes-Bug: 1618758
    Closes-Bug: 1618914

--- nailgun/nailgun/task/task.py
+++ nailgun/nailgun/task/task.py
@@ -248,6 +248,7 @@ class DeploymentTask(BaseDeploymentTask):
         deployment_tasks = objects.Cluster.get_deployment_tasks(
             task.cluster, graph_type
         )
+        task.graph_type = graph_type or consts.DEFAULT_DEPLOYMENT_GRAPH_TYPE
         objects.Transaction.attach_tasks_snapshot(
             task,
             deployment_tasks
@@ -525,11 +526,13 @@ class ClusterTransaction(DeploymentTask):
 
         if objects.Cluster.is_propagate_task_deploy_enabled(cluster):
             logger.info("The legacy tasks adaptation is used.")
-            tasks = adapt_legacy_tasks(
-                tasks,
-                objects.Cluster.get_legacy_plugin_tasks(cluster),
-                role_resolver,
-            )
+            if transaction.graph_type == consts.DEFAULT_DEPLOYMENT_GRAPH_TYPE:
+                plugin_tasks = objects.Cluster.get_legacy_plugin_tasks(
+                    cluster
+                )
+            else:
+                plugin_tasks = None
+            tasks = adapt_legacy_tasks(tasks, plugin_tasks, role_resolver)
 
         directory, graph, metadata = lcm.TransactionSerializer.serialize(
             context,

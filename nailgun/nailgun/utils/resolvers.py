cb656b034183b10011f4bb9525d650acdf266af7
Ryan Moe <rmoe@mirantis.com>
2016-09-06 10:44:10 -0700

    Resolve tasks by node tags
    A 'tags' attribute has been added to each role in 'roles_metadata'.
    Initially all non-controller roles will only have a tag of their own
    role name. This will allow existing tasks which do not have tags
    associated with them to work correctly. In the abscence of tags a
    task's roles will be used to determine which nodes it will run on.
    
    Implements: blueprint role-decomposition
    
    Change-Id: I390580146048b6e00ec5c42d0adf995a4cff9167

--- nailgun/nailgun/utils/resolvers.py
+++ nailgun/nailgun/utils/resolvers.py
@@ -67,7 +67,7 @@ class NullResolver(BaseRoleResolver):
         return []
 
 
-class RoleResolver(BaseRoleResolver):
+class TagResolver(BaseRoleResolver):
     """The general role resolver.
 
     Allows to use patterns in name of role
@@ -86,7 +86,7 @@ class RoleResolver(BaseRoleResolver):
         """
         self.__mapping = defaultdict(set)
         for node in nodes:
-            for r in objects.Node.all_roles(node):
+            for r in objects.Node.all_tags(node):
                 self.__mapping[r].add(node.uid)
 
     def resolve(self, roles, policy=None):

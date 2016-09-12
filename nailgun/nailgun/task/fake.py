2e768d743b00dbec63e2dc432b1f1800bff78800
Vladimir Sharshov (warpc) <vsharshov@mirantis.com>
2016-09-12 14:07:46 +0300

    Do not move task status to running after ack
    Astute will send 'running' status for task
    when start to process it, so we do not need
    task in orchestrator mechanism.
    
    Change-Id: I5e2deccdbd27daff3e3b131ef9c8c7ffbbb40dd4
    Closes-Bug: #1621003

--- nailgun/nailgun/task/fake.py
+++ nailgun/nailgun/task/fake.py
@@ -757,12 +757,6 @@ class FakeCheckRepositories(FakeAmpqThread):
         }]
 
 
-class FakeTaskInOrchestrator(FakeAmpqThread):
-    def message_gen(self):
-        self.sleep(self.tick_interval)
-        return [{'task_uuid': self.task_uuid}]
-
-
 FAKE_THREADS = {
     'native_provision': FakeProvisionThread,
     'image_provision': FakeProvisionThread,
@@ -780,5 +774,4 @@ FAKE_THREADS = {
     'execute_tasks': FakeExecuteTasksThread,
     'check_repositories': FakeCheckRepositories,
     'check_repositories_with_setup': FakeCheckRepositories,
-    'task_in_orchestrator': FakeTaskInOrchestrator
 }

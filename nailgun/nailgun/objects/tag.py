55ac025bc0647016736eb14e92bf80ced27f3a1c
Viacheslav Valyavskiy <vvalyavskiy@mirantis.com>
2016-10-13 20:50:33 +0300

    Fix minor issues in tag API
    Change-Id: I33277e04159b1c07c7d508611eea689fd6935393
    Implements: blueprint role-decomposition
    (cherry picked from commit 319cb284c56d816613b6b74a492f0c31cd2fc4fa)

--- nailgun/nailgun/objects/tag.py
+++ nailgun/nailgun/objects/tag.py
@@ -35,7 +35,7 @@ class Tag(NailgunObject):
 class TagCollection(NailgunCollection):
 
     @classmethod
-    def get_cluster_tags_query(cls, cluster):
+    def get_cluster_tags(cls, cluster, **kwargs):
         plugins_ids = (ClusterPlugin.get_enabled(cluster.id)
                        .with_entities(models.Plugin.id).subquery())
         return db().query(models.Tag).filter(
@@ -45,18 +45,34 @@ class TagCollection(NailgunCollection):
              (models.Tag.owner_type == 'cluster')) |
             ((models.Tag.owner_id.in_(plugins_ids)) &
              (models.Tag.owner_type == 'plugin'))
-        )
+        ).filter_by(**kwargs)
 
     @classmethod
-    def get_node_tags_query(cls, node_id):
+    def get_node_tags(cls, node):
         return db().query(models.Tag).join(
             models.NodeTag
         ).filter(
-            models.NodeTag.node_id == node_id
+            models.NodeTag.node_id == node.id
         )
 
     @classmethod
-    def get_cluster_tags(cls, cluster, **kwargs):
-        return cls.get_cluster_tags_query(cluster).filter_by(**kwargs)
+    def get_node_tags_ids(cls, node):
+        return cls.get_node_tags(node).with_entities(
+            models.NodeTag.tag_id
+        )
+
+    @classmethod
+    def get_node_tags_ids_in_range(cls, node, tag_ids):
+        return cls.get_node_tags_ids(node).filter(
+            models.NodeTag.tag_id.in_(tag_ids)
+        )
+
+    @classmethod
+    def get_tag_nodes(cls, tag):
+        return db().query(models.Node).join(
+            models.NodeTag
+        ).filter(
+            models.NodeTag.tag_id == tag.id
+        )
 
     single = Tag

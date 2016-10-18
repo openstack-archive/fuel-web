23338d04d4c97bff9a9cd1c7d8226ae808304a50
Mikhail <mzhnichkov@mirantis.com>
2016-10-18 15:07:43 +0300

    Add default tags
    Tags for rabbitmq, database, keystone and neutron.
    
    Change-Id: I5cbaf58f57845c02550e2b422b269f41eec98322
    Implements: blueprint role-decomposition

--- nailgun/nailgun/orchestrator/deployment_serializers.py
+++ nailgun/nailgun/orchestrator/deployment_serializers.py
@@ -46,7 +46,9 @@ class DeploymentMultinodeSerializer(object):
     neutron_network_serializer = \
         neutron_serializers.NeutronNetworkDeploymentSerializer
 
-    critical_roles = frozenset(('controller', 'ceph-osd', 'primary-mongo'))
+    critical_roles = frozenset(('controller', 'ceph-osd', 'primary-mongo',
+                                'primary-database', 'primary-rabbitmq',
+                                'primary-keystone', 'primary-neutron'))
 
     def __init__(self, tasks_graph=None):
         self.task_graph = tasks_graph
@@ -334,7 +336,9 @@ class DeploymentHASerializer(DeploymentMultinodeSerializer):
         'primary-mongo',
         'primary-swift-proxy',
         'ceph-osd',
-        'controller'
+        'controller',
+        'primary-database', 'primary-rabbitmq',
+        'primary-keystone', 'primary-neutron'
     ))
 
     def get_last_controller(self, nodes):

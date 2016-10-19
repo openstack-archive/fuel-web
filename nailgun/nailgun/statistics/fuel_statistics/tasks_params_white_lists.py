df095259ef87e79f92e350df4ab586b3db207902
Anastasiya <atolochkova@mirantis.com>
2016-10-19 18:09:34 +0300

    Add radio "Security Groups" to Security section
    It is required for firewall driver selecting
    
    Implements: blueprint security-groups-support-for-ovs
    
    Change-Id: Id9307c5e72e35de9090804813122ea44045a371b
    (cherry picked from commit 4478efb3c4d1bb4843d6758e4713da5f723c322a)

--- nailgun/nailgun/statistics/fuel_statistics/tasks_params_white_lists.py
+++ nailgun/nailgun/statistics/fuel_statistics/tasks_params_white_lists.py
@@ -213,7 +213,8 @@ _task_output_white_list_template = {
                 },
                 "debug": "",
                 "deployment_id": "",
-                "openstack_version_prev": ""
+                "openstack_version_prev": "",
+                "security_groups": ""
             },
             "pre_deployment": {
                 "*": {}

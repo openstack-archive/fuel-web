f1550ad2a20b489c6af7d971c3c6742c1e5c1433
Viacheslav Valyavskiy <vvalyavskiy@mirantis.com>
2016-11-08 21:57:39 +0300

    Add ability to create role for cluster
    Change-Id: Idcde0a582507293490379d146c4e24e5e58790d8
    Implements: blueprint role-decomposition

---
+++ nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_11_0.py
@@ -0,0 +1,62 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Fuel 11.0
+
+Revision ID: dc8bc8751c42
+Revises: c6edea552f1e
+Create Date: 2016-10-22 02:11:47.708895
+
+"""
+
+from alembic import op
+
+import sqlalchemy as sa
+
+from nailgun.db.sqlalchemy.models import fields
+
+
+# revision identifiers, used by Alembic.
+revision = 'dc8bc8751c42'
+down_revision = 'c6edea552f1e'
+
+
+def upgrade():
+    upgrade_cluster_roles()
+
+
+def downgrade():
+    downgrade_cluster_roles()
+
+
+def upgrade_cluster_roles():
+    op.add_column(
+        'clusters',
+        sa.Column('roles_metadata',
+                  fields.JSON(),
+                  default={},
+                  server_default='{}'),
+    )
+    op.add_column(
+        'clusters',
+        sa.Column('volumes_metadata',
+                  fields.JSON(),
+                  default={},
+                  server_default='{}'),
+    )
+
+
+def downgrade_cluster_roles():
+    op.drop_column('clusters', 'roles_metadata')
+    op.drop_column('clusters', 'volumes_metadata')

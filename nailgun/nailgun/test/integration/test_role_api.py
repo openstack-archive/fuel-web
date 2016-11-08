4d99088622e92832e44f21418ba0b55210c951b1
Viacheslav Valyavskiy <vvalyavskiy@mirantis.com>
2016-11-08 21:57:39 +0300

    Add ability to create role for cluster
    Change-Id: Idcde0a582507293490379d146c4e24e5e58790d8
    Implements: blueprint role-decomposition

--- nailgun/nailgun/test/integration/test_role_api.py
+++ nailgun/nailgun/test/integration/test_role_api.py
@@ -23,6 +23,8 @@ from nailgun.test import base
 class BaseRoleTest(base.BaseIntegrationTest):
 
     ROLE = ""
+    OWNER_MAP = {'cluster': 'clusters',
+                 'release': 'releases'}
 
     def setUp(self):
         super(BaseRoleTest, self).setUp()
@@ -43,102 +45,106 @@ class TestRoleApi(BaseRoleTest):
     """
 
     def test_get_all_roles(self):
-        self.env.create_role(self.release.id, self.role_data)
+        owner_type, owner_id = self.OWNER_MAP['release'], self.release.id
+        self.env.create_role(owner_type, owner_id, self.role_data)
 
-        resp = self.app.get(
-            base.reverse(
-                'RoleCollectionHandler',
-                {'release_id': self.release.id}),
-            headers=self.default_headers,
-        )
+        resp = self.env.get_all_roles(owner_type, owner_id)
 
         self.assertEqual(
             len(self.release.roles_metadata.keys()),
             len(resp.json))
 
         created_role = next((
-            role for role in resp.json if role['name'] == 'my_role'))
+            role
+            for role in resp.json if role['name'] == self.role_data['name']))
         self.assertEqual(created_role, self.role_data)
 
     def test_create_role(self):
-        resp = self.env.create_role(self.release.id, self.role_data)
+        owner_type, owner_id = self.OWNER_MAP['release'], self.release.id
+        resp = self.env.create_role(owner_type, owner_id, self.role_data)
         self.assertEqual(resp.json['meta'], self.role_data['meta'])
 
     def test_update_role(self):
         changed_name = 'Another name'
+        owner_type, owner_id = self.OWNER_MAP['release'], self.release.id
 
-        resp = self.env.create_role(self.release.id, self.role_data)
+        resp = self.env.create_role(owner_type, owner_id, self.role_data)
 
         data = resp.json
         data['meta']['name'] = changed_name
 
-        resp = self.env.update_role(self.release.id, data['name'], data)
+        resp = self.env.update_role(owner_type, owner_id, data['name'], data)
         self.assertEqual(resp.json['meta']['name'], changed_name)
 
     def test_create_role_wo_volumes(self):
+        owner_type, owner_id = self.OWNER_MAP['release'], self.release.id
         self.role_data['volumes_roles_mapping'] = []
         resp = self.env.create_role(
-            self.release.id, self.role_data, expect_errors=True)
+            owner_type, owner_id, self.role_data, expect_errors=True)
         self.assertEqual(resp.status_code, 400)
 
     def test_create_role_w_invalid_volumes_allocate_size(self):
+        owner_type, owner_id = self.OWNER_MAP['release'], self.release.id
         self.role_data['volumes_roles_mapping'][0]['allocate_size'] = \
             'some_string'
         resp = self.env.create_role(
-            self.release.id, self.role_data, expect_errors=True)
+            owner_type, owner_id, self.role_data, expect_errors=True)
         self.assertEqual(400, resp.status_code)
         self.assertIn('Failed validating', resp.body)
         self.assertIn('volumes_roles_mapping', resp.body)
 
     def test_create_role_w_invalid_id(self):
+        owner_type, owner_id = self.OWNER_MAP['release'], self.release.id
         self.role_data['volumes_roles_mapping'][0]['id'] = 'invalid_id'
         resp = self.env.create_role(
-            self.release.id, self.role_data, expect_errors=True)
+            owner_type, owner_id, self.role_data, expect_errors=True)
         self.assertEqual(400, resp.status_code)
         self.assertIn('Wrong data in volumes_roles_mapping', resp.body)
 
     def test_update_role_w_invalid_volumes_id(self):
-        self.env.create_role(self.release.id, self.role_data)
+        owner_type, owner_id = self.OWNER_MAP['release'], self.release.id
+        self.env.create_role(owner_type, owner_id, self.role_data)
         self.role_data['volumes_roles_mapping'][0]['id'] = 'some_string'
-        resp = self.env.update_role(
-            self.release.id,
-            self.role_data['name'],
-            self.role_data,
-            expect_errors=True)
+        resp = self.env.update_role(owner_type,
+                                    owner_id,
+                                    self.role_data['name'],
+                                    self.role_data,
+                                    expect_errors=True)
         self.assertEqual(400, resp.status_code)
         self.assertIn('Wrong data in volumes_roles_mapping', resp.body)
 
     def test_update_role_not_present(self):
-        self.env.create_role(self.release.id, self.role_data)
+        owner_type, owner_id = self.OWNER_MAP['release'], self.release.id
+        self.env.create_role(owner_type, owner_id, self.role_data)
         role_name = 'blah_role'
-        resp = self.env.update_role(
-            self.release.id,
-            role_name,
-            self.role_data,
-            expect_errors=True)
+        resp = self.env.update_role(owner_type,
+                                    owner_id,
+                                    role_name,
+                                    self.role_data,
+                                    expect_errors=True)
         self.assertEqual(404, resp.status_code)
         self.assertIn('is not found for the release', resp.body)
 
     def test_delete_role(self):
-
-        self.env.create_role(self.release.id, self.role_data)
+        owner_type, owner_id = self.OWNER_MAP['release'], self.release.id
+        self.env.create_role(owner_type, owner_id, self.role_data)
         delete_resp = self.env.delete_role(
-            self.release.id, self.role_data['name'])
+            owner_type, owner_id, self.role_data['name'])
 
         self.assertEqual(delete_resp.status_code, 204)
 
     def test_delete_role_not_present(self):
-        self.env.create_role(self.release.id, self.role_data)
+        owner_type, owner_id = self.OWNER_MAP['release'], self.release.id
+        self.env.create_role(owner_type, owner_id, self.role_data)
         role_name = 'blah_role'
         delete_resp = self.env.delete_role(
-            self.release.id,
-            role_name,
-            expect_errors=True)
+            owner_type, owner_id, role_name, expect_errors=True)
         self.assertEqual(delete_resp.status_code, 404)
         self.assertIn('is not found for the release', delete_resp.body)
 
     def test_delete_assigned_role(self):
-        role = self.env.create_role(self.release.id, self.role_data).json
+        owner_type, owner_id = self.OWNER_MAP['release'], self.release.id
+        role = self.env.create_role(owner_type, owner_id, self.role_data).json
         self.env.create(
             nodes_kwargs=[
                 {'roles': [role['name']], 'pending_addition': True},
@@ -147,13 +153,14 @@ class TestRoleApi(BaseRoleTest):
         )
 
         delete_resp = self.env.delete_role(
-            self.release.id, role['name'], expect_errors=True)
+            owner_type, owner_id, role['name'], expect_errors=True)
         self.assertEqual(delete_resp.status_code, 400)
 
     def test_delete_role_when_assigned_another_role(self):
         # There was bug with such validation
         # https://bugs.launchpad.net/fuel/+bug/1488091
-        role = self.env.create_role(self.release.id, self.role_data).json
+        owner_type, owner_id = self.OWNER_MAP['release'], self.release.id
+        role = self.env.create_role(owner_type, owner_id, self.role_data).json
         self.env.create(
             nodes_kwargs=[
                 {'roles': ['compute'], 'pending_addition': True},
@@ -161,12 +168,12 @@ class TestRoleApi(BaseRoleTest):
             cluster_kwargs={'release_id': self.release.id},
         )
 
-        delete_resp = self.env.delete_role(
-            self.release.id, role['name'])
+        delete_resp = self.env.delete_role(owner_type, owner_id, role['name'])
         self.assertEqual(delete_resp.status_code, 204)
 
     def test_delete_pending_assigned_role(self):
-        role = self.env.create_role(self.release.id, self.role_data).json
+        owner_type, owner_id = self.OWNER_MAP['release'], self.release.id
+        role = self.env.create_role(owner_type, owner_id, self.role_data).json
         self.env.create(
             nodes_kwargs=[
                 {'pending_roles': [role['name']], 'pending_addition': True},
@@ -175,30 +182,31 @@ class TestRoleApi(BaseRoleTest):
         )
 
         delete_resp = self.env.delete_role(
-            self.release.id, role['name'], expect_errors=True)
+            owner_type, owner_id, role['name'], expect_errors=True)
         self.assertEqual(delete_resp.status_code, 400)
 
     def test_get_role(self):
-        self.env.create_role(self.release.id, self.role_data)
-        role = self.env.get_role(self.release.id, self.role_data['name'])
+        owner_type, owner_id = self.OWNER_MAP['release'], self.release.id
+        self.env.create_role(owner_type, owner_id, self.role_data)
+        role = self.env.get_role(owner_type, owner_id, self.role_data['name'])
 
         self.assertEqual(role.status_code, 200)
         self.assertEqual(role.json['name'], self.role_data['name'])
 
     def test_get_role_not_present(self):
-        self.env.create_role(self.release.id, self.role_data)
+        owner_type, owner_id = self.OWNER_MAP['release'], self.release.id
+        self.env.create_role(owner_type, owner_id, self.role_data)
         role_name = 'blah_role'
         resp = self.env.get_role(
-            self.release.id,
-            role_name,
-            expect_errors=True)
+            owner_type, owner_id, role_name, expect_errors=True)
         self.assertEqual(resp.status_code, 404)
         self.assertIn('is not found for the release', resp.body)
 
     def test_create_role_with_special_symbols(self):
+        owner_type, owner_id = self.OWNER_MAP['release'], self.release.id
         self.role_data['name'] = '@#$%^&*()'
         resp = self.env.create_role(
-            self.release.id, self.role_data, expect_errors=True)
+            owner_type, owner_id, self.role_data, expect_errors=True)
 
         self.assertEqual(resp.status_code, 400)
 
@@ -235,5 +243,6 @@ volumes_roles_mapping:
 """
 
     def test_create_role(self):
-        resp = self.env.create_role(self.release.id, self.role_data)
+        owner_type, owner_id = self.OWNER_MAP['release'], self.release.id
+        resp = self.env.create_role(owner_type, owner_id, self.role_data)
         self.assertEqual(resp.json['meta'], self.role_data['meta'])

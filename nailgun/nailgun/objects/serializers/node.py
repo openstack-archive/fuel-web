1750da471f2587dcefc1fd17eb0b74f08511d065
Mikhail Zhnichkov <mzhnichkov@mirantis.com>
2016-11-08 09:53:42 +0000

    Revert "Add tag API"
    This reverts commit 6f86ab6047af993df24af676686f5ce49975ad7e.
    
    Change-Id: I30f23703954fcd41b7e5c4c79bed381f73d95727

--- nailgun/nailgun/objects/serializers/node.py
+++ nailgun/nailgun/objects/serializers/node.py
@@ -55,7 +55,6 @@ class NodeSerializer(BasicSerializer):
         data_dict = super(NodeSerializer, cls).serialize(instance, fields)
         data_dict['fqdn'] = Node.get_node_fqdn(instance)
         data_dict['status'] = Node.get_status(instance)
-        data_dict['tags'] = instance.tag_names
         return data_dict
 
 

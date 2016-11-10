520e2306905f9afa084b4530e3f2f4e21ced8af0
Viacheslav Valyavskiy <vvalyavskiy@mirantis.com>
2016-11-10 21:53:10 +0300

    Add tag API
    Change-Id: I4e8325866ef63e424d5c6a93138e731001731568
    Implements: blueprint role-decomposition

---
+++ nailgun/nailgun/objects/serializers/tag.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nailgun.objects.serializers.base import BasicSerializer
+
+
+class TagSerializer(BasicSerializer):
+
+    @classmethod
+    def serialize_from_obj(cls, obj_cls, obj, tag_name):
+        tag_meta = obj_cls.get_tags_metadata(obj)[tag_name]
+
+        return {'name': tag_name, 'meta': tag_meta}

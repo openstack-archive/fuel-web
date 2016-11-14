0b8029b8f83c34d06d409bb8999bd6e56fded4fb
Viacheslav Valyavskiy <vvalyavskiy@mirantis.com>
2016-11-14 21:20:49 +0300

    Introduce tags_metadata file for plugins
    'tags_metadata.yaml' file contains set of tags for tasks
    serialization. These tags can be used in roles introduced
    by the plugin.
    
    Change-Id: I1e68a240b2fe535188404eb47c23b875a4616f2a
    Implements: blueprint role-decomposition
    (cherry picked from commit e8ed5cba839beb9c93b6259332848db75e9d4efc)

---
+++ nailgun/nailgun/plugins/loaders/loader_v6.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from loader_v5 import PluginLoaderV5
+
+
+class PluginLoaderV6(PluginLoaderV5):
+    @property
+    def paths_to_fields(self):
+        path_to_fields = dict(super(PluginLoaderV6, self).paths_to_fields)
+        path_to_fields['tags_metadata'] = 'node_tags.yaml'
+        return path_to_fields

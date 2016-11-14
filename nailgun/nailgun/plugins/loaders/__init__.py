2c18dd1fb847e1f9750f3878c04aaac3780ec94d
Viacheslav Valyavskiy <vvalyavskiy@mirantis.com>
2016-11-14 21:20:49 +0300

    Introduce tags_metadata file for plugins
    'tags_metadata.yaml' file contains set of tags for tasks
    serialization. These tags can be used in roles introduced
    by the plugin.
    
    (cherry picked from commit e8ed5cba839beb9c93b6259332848db75e9d4efc)
    Implements: blueprint role-decomposition
    Change-Id: I1e68a240b2fe535188404eb47c23b875a4616f2a

--- nailgun/nailgun/plugins/loaders/__init__.py
+++ nailgun/nailgun/plugins/loaders/__init__.py
@@ -3,3 +3,4 @@ from loader_v1 import PluginLoaderV1
 from loader_v3 import PluginLoaderV3
 from loader_v4 import PluginLoaderV4
 from loader_v5 import PluginLoaderV5
+from loader_v6 import PluginLoaderV6

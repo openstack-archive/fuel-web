46a670a7905797fed0f6b2123b7b7177ebfc4935
Anastasiya <atolochkova@mirantis.com>
2016-11-15 11:32:55 +0300

    Data migration for the firewall driver switch
    * data migration was prepared
    * unit tests for migration was added
    
    Change-Id: Iafe9267cc1c64c0c966e0be8fc9181f31f029b75
    Implements: blueprint security-groups-support-for-ovs
    (cherry picked from commit ec9ccd02d4a468e41f59eee4ed60c1a28cad98f8)

--- nailgun/nailgun/utils/__init__.py
+++ nailgun/nailgun/utils/__init__.py
@@ -347,3 +347,26 @@ def parse_bool(value):
         return False
     raise ValueError('Invalid value: {0}'.format(value))
 
+
+def get_environment_version(version):
+    """Returns environment version based on release version.
+
+    A release version consists of 'OSt' and 'MOS' versions:
+        '2014.1.1-5.0.2'
+
+    so we need to extract 'MOS' version and returns it as result.
+
+    :param: version: release version
+    :returns: an environment version
+    """
+    # unfortunately, Fuel 5.0 didn't have an env version in release_version
+    # so we need to handle that special case
+    if version == '2014.1':
+        version = '5.0'
+    else:
+        try:
+            version = version.split('-')[1]
+        except IndexError:
+            version = ''
+    return version
+

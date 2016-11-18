3912f51ead128a798aff4070e7f946768ef8b7c7
Mikhail <mzhnichkov@mirantis.com>
2016-11-18 17:46:08 +0300

    Add tags and roles validate
    Change-Id: Ia45c81a46e9323faa6471438acbfa6d65d01b7f4
    Implements: blueprint role-decomposition

--- nailgun/nailgun/objects/cluster.py
+++ nailgun/nailgun/objects/cluster.py
@@ -415,19 +415,27 @@ class Cluster(NailgunObject):
         Previous ones are deleted.
 
         :param instance: a Cluster instance
-        :param role: a tag dict
+        :param tag: a tag dict
         :returns: None
         """
         instance.tags_metadata[tag['name']] = tag['meta']
 
     @classmethod
     def remove_tag(cls, instance, tag_name):
+        res = instance.tags_metadata.pop(tag_name, None)
+        if tag_name not in instance.release.tags_metadata:
+            cls.remove_tag_from_roles(instance, tag_name)
+            cls.remove_primary_tag(instance, tag_name)
+        db().flush()
+        return bool(res)
+
+    @classmethod
+    def remove_tag_from_roles(cls, instance, tag_name):
         for role, meta in six.iteritems(cls.get_own_roles(instance)):
             tags = meta.get('tags', [])
             if tag_name in tags:
                 tags.remove(tag_name)
                 instance.roles_metadata.changed()
-        return bool(instance.tags_metadata.pop(tag_name, None))
 
     @classmethod
     def _create_public_map(cls, instance, roles_metadata=None):
@@ -842,12 +850,26 @@ class Cluster(NailgunObject):
         return available_roles
 
     @classmethod
+    def get_roles_by_tag(cls, tag_name, instance):
+        roles = []
+        for role, meta in six.iteritems(cls.get_own_roles(instance)):
+            if tag_name in meta.get('tags', {}):
+                roles.append(role)
+        return set(roles)
+
+    @classmethod
     def get_own_roles(cls, instance):
         return instance.roles_metadata
 
     @classmethod
     def get_own_tags(cls, instance):
         return instance.tags_metadata
+
+    @classmethod
+    def remove_primary_tag(cls, instance, tag):
+        node = cls.get_primary_node(instance, tag)
+        if node:
+            node.primary_tags.remove(tag)
 
     @classmethod
     def set_primary_tag(cls, instance, nodes, tag):

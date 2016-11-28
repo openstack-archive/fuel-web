9a452f12647e2abd65e8a93bd7a1001da25b5c5d
Mikhail <mzhnichkov@mirantis.com>
2016-11-28 15:03:15 +0300

    Fix 9.2 migration
    Downgrade field "tags" from roles_metadata
    
    Change-Id: Ic1a2b23a7abcdd2686e5911778ea2c57118da501
    Implements: blueprint role-decomposition
    (cherry picked from commit 56f8ecbb166294fa4df1505dde3396193dbac9ee)

--- nailgun/nailgun/test/unit/test_downgrade_fuel_9_2.py
+++ nailgun/nailgun/test/unit/test_downgrade_fuel_9_2.py
@@ -30,6 +30,47 @@ from nailgun.test import base
 _prepare_revision = '3763c404ca48'
 _test_revision = 'f2314e5d63c9'
 
+ROLES_META = {
+    'controller': {
+        'tags': [
+            'controller',
+            'rabbitmq',
+            'database',
+            'keystone',
+            'neutron'
+        ]
+    }
+}
+
+PLUGIN_ROLE_META = {
+    'test_plugin_role': {
+        'tags': ['test_plugin_tag']
+    }
+}
+
+PLUGIN_TAGS_META = {
+    'test_plugin_tag':
+        {'has_primary': False}
+}
+
+TAGS_META = {
+    'controller': {
+        'has_primary': True,
+    },
+    'rabbitmq': {
+        'has_primary': True
+    },
+    'database': {
+        'has_primary': True
+    },
+    'keystone': {
+        'has_primary': True
+    },
+    'neutron': {
+        'has_primary': True
+    }
+}
+
 
 def setup_module():
     dropdb()
@@ -40,6 +81,15 @@ def setup_module():
 
 def prepare():
     meta = base.reflect_db_metadata()
+    plugin = {
+        'name': 'Test_P',
+        'version': '3.0.0',
+        'title': 'Test Plugin',
+        'package_version': '5.0.0',
+        'roles_metadata': jsonutils.dumps(PLUGIN_ROLE_META),
+        'tags_metadata': jsonutils.dumps(PLUGIN_TAGS_META)
+    }
+    result = db.execute(meta.tables['plugins'].insert(), [plugin])
 
     result = db.execute(
         meta.tables['releases'].insert(),
@@ -48,15 +98,11 @@ def prepare():
             'version': '2016.1-10.0',
             'operating_system': 'ubuntu',
             'state': 'available',
-            'roles': jsonutils.dumps([
-                'controller',
-            ]),
-            'roles_metadata': jsonutils.dumps({
-                'controller': {
-                    'name': 'Controller',
-                },
-            }),
-            'is_deployable': True
+            'deployment_tasks': '[]',
+            'roles_metadata': jsonutils.dumps(ROLES_META),
+            'tags_matadata': jsonutils.dumps(TAGS_META),
+            'is_deployable': True,
+            'networks_metadata': '{}',
         }])
 
     release_id = result.inserted_primary_key[0]
@@ -71,6 +117,8 @@ def prepare():
             'net_provider': 'neutron',
             'grouping': 'roles',
             'fuel_version': '10.0',
+            'roles_metadata': jsonutils.dumps(ROLES_META),
+            'tags_metadata': '{}',
         }])
     cluster_id = result.inserted_primary_key[0]
 
@@ -92,10 +140,32 @@ def prepare():
     db.commit()
 
 
-class TestPluginTags(base.BaseAlembicMigrationTest):
+class TestTags(base.BaseAlembicMigrationTest):
     def test_primary_tags_downgrade(self):
         nodes = self.meta.tables['nodes']
         query = sa.select([nodes.c.primary_roles]).where(
             nodes.c.uuid == 'fcd49872-3917-4a18-98f9-3f5acfe3fdec')
         primary_roles = db.execute(query).fetchone()[0]
         self.assertItemsEqual(primary_roles, ['role_y'])
+
+    def test_downgrade_tags_metadata(self):
+        releases = self.meta.tables['releases']
+        self.assertNotIn('tags_metadata', releases.c._all_columns)
+
+        clusters = self.meta.tables['clusters']
+        self.assertNotIn('tags_metadata', clusters.c._all_columns)
+        self.assertNotIn('roles_metadata', clusters.c._all_columns)
+
+        plugins = self.meta.tables['plugins']
+        self.assertNotIn('tags_metadata', plugins.c._all_columns)
+
+    def test_downgrade_field_tags_from_roles(self):
+        releases = self.meta.tables['releases']
+        query = sa.select([releases.c.roles_metadata])
+        for role_meta in db.execute(query).fetchall():
+            self.assertNotIn('tags', role_meta)
+
+        plugins = self.meta.tables['plugins']
+        query = sa.select([plugins.c.roles_metadata])
+        for role_meta in db.execute(query):
+            self.assertNotIn('tags', role_meta)

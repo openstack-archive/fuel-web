a8a787d5f6d35ae5d5556b4ad05211df13e5dfa4
Vladimir Kuklin <vkuklin@mirantis.com>
2016-12-12 18:34:12 +0300

    Fix DPDK settings get for upgrades
    Change-Id: I226cb5d1168b1b02c9779b5fd48d30fd52f83728
    Closes-bug: #1645471
    Co-Authored-By: Fedor Zhadaev <fzhadaev@mirantis.com>

--- nailgun/nailgun/task/task.py
+++ nailgun/nailgun/task/task.py
@@ -448,7 +448,7 @@ class ClusterTransaction(DeploymentTask):
 
     @classmethod
     def mark_skipped(cls, tasks, ids_not_to_skip):
-        """Change tasks type which ids not present in ids_not_to_skip to skipped
+        """Change tasks type which ids are not ids_not_to_skip to skipped
 
         :param tasks: the list of deployment tasks to execute
         :param ids_not_to_skip: the list of task ids that will be not skipped
@@ -1526,7 +1526,8 @@ class CheckBeforeDeploymentTask(object):
         # TODO(asvechnikov): Make an appropriate versioning of tasks
         if objects.Release.is_nfv_supported(task.cluster.release):
             cls._check_sriov_properties(task)
-            cls._check_dpdk_properties(task)
+            if objects.Cluster.dpdk_enabled(task.cluster):
+                cls._check_dpdk_properties(task)
 
         if objects.Release.is_external_mongo_enabled(task.cluster.release):
             cls._check_mongo_nodes(task)
@@ -1868,8 +1869,10 @@ class CheckBeforeDeploymentTask(object):
     @classmethod
     def _check_dpdk_properties(self, task):
         dpdk_enabled = False
-        for node in task.cluster.nodes:
-            if node.pending_deletion:
+        for node in objects.Cluster.get_nodes_not_for_deletion(task.cluster):
+
+            if (not objects.NodeAttributes.is_cpu_pinning_enabled(node) and
+                    not objects.Node.dpdk_enabled):
                 continue
 
             try:

54ff86c2802745ee79a1c056329214d674131947
f3flight <f3flight@gmail.com>
2016-12-23 16:05:42 -0800

    Change node search and validation methods
    - Remove searches by any MAC
    - Add search by system uuid
    - Allow duplicate MACs (except "mac" property)
    
    The previous implementation used a fallback matching rule which
    would match a node if any of its MACs are found in the database.
    This is not reliable because MACs can sometimes be identical due to
    hardware specifics or other reasons.
    
    This change removes matching by any MAC with matching by system uuid,
    which is reliably unique for both hardware and virtual systems,
    and also constant for a given system unless it's motherboard is
    replaced.
    
    This change allows nodes to have identical MACs on iterfaces.
    However, "mac" property still has to be unique.
    
    User is responsible for ensuring that interfaces with identical MACs
    (if any) are not assigned to the same networks, which will obviously
    lead to deployment & connectivity issues.
    
    This change adds a randomly generated system uuid to the default_metadata
    in tests.
    
    Closes-Bug: 1650646
    Change-Id: I49855e2b5ff30559cb2385a03abb8cfdd6a5f1f2

--- nailgun/nailgun/api/v1/validators/node.py
+++ nailgun/nailgun/api/v1/validators/node.py
@@ -24,7 +24,6 @@ from nailgun.api.v1.validators.orchestrator_graph import \
 from nailgun import consts
 from nailgun.db import db
 from nailgun.db.sqlalchemy.models import Node
-from nailgun.db.sqlalchemy.models import NodeNICInterface
 from nailgun import errors
 from nailgun import objects
 from nailgun import utils
@@ -116,65 +115,22 @@ class NodeValidator(base.BasicValidator):
                 "Only bootstrap nodes are allowed to be registered."
             )
 
-        if 'mac' not in data:
-            raise errors.InvalidData(
-                "No mac address specified",
-                log_message=True
-            )
+        if not data.get('mac'):
+            raise errors.InvalidData("No mac address specified",
+                                     log_message=True)
 
-        if cls.does_node_exist_in_db(data):
+        node = objects.Node.get_by_meta(data)
+        if node:
+            sys_uuid = node.meta.get('system', {}).get('uuid')
             raise errors.AlreadyExists(
-                "Node with mac {0} already "
-                "exists - doing nothing".format(data["mac"]),
-                log_level="info"
-            )
-
-        if cls.validate_existent_node_mac_create(data):
-            raise errors.AlreadyExists(
-                "Node with mac {0} already "
-                "exists - doing nothing".format(data["mac"]),
-                log_level="info"
-            )
+                "A node with one of the following attributes (mac: %s,"
+                " id: %s, system uuid: %s) already exists - doing nothing"
+                % (node.mac, node.id, sys_uuid), log_level="info")
 
         if 'meta' in data:
             MetaValidator.validate_create(data['meta'])
 
         return data
-
-    @classmethod
-    def does_node_exist_in_db(cls, data):
-        mac = data['mac'].lower()
-        q = db().query(Node)
-
-        if q.filter(Node.mac == mac).first() or \
-            q.join(NodeNICInterface, Node.nic_interfaces).filter(
-                NodeNICInterface.mac == mac).first():
-            return True
-        return False
-
-    @classmethod
-    def _validate_existent_node(cls, data, validate_method):
-        if 'meta' in data:
-            data['meta'] = validate_method(data['meta'])
-            if 'interfaces' in data['meta']:
-                existent_node = db().query(Node).\
-                    join(NodeNICInterface, Node.nic_interfaces).\
-                    filter(NodeNICInterface.mac.in_(
-                        [n['mac'].lower() for n in data['meta']['interfaces']]
-                    )).first()
-                return existent_node
-
-    @classmethod
-    def validate_existent_node_mac_create(cls, data):
-        return cls._validate_existent_node(
-            data,
-            MetaValidator.validate_create)
-
-    @classmethod
-    def validate_existent_node_mac_update(cls, data):
-        return cls._validate_existent_node(
-            data,
-            MetaValidator.validate_update)
 
     @classmethod
     def validate_roles(cls, data, node, roles):
@@ -249,33 +205,31 @@ class NodeValidator(base.BasicValidator):
             d = data
         cls.validate_schema(d, node_schema.single_schema)
 
-        if not d.get("mac") and not d.get("id") and not instance:
-            raise errors.InvalidData(
-                "Neither MAC nor ID is specified",
-                log_message=True
-            )
-
-        existent_node = None
-        q = db().query(Node)
-        if "mac" in d:
-            existent_node = q.filter_by(mac=d["mac"].lower()).first() \
-                or cls.validate_existent_node_mac_update(d)
-            if not existent_node:
-                raise errors.InvalidData(
-                    "Invalid MAC is specified",
-                    log_message=True
-                )
-
-        if "id" in d and d["id"]:
-            existent_node = q.get(d["id"])
-            if not existent_node:
-                raise errors.InvalidData(
-                    "Invalid ID specified",
-                    log_message=True
-                )
-
         if not instance:
-            instance = existent_node
+            system_uuid = d.get('meta', {}).get('system', {}).get('uuid')
+            if not d.get('mac') and not d.get('id') and not system_uuid:
+                raise errors.InvalidData(
+                    'Cannot find node to update - no identification params '
+                    "specified, mac or id or meta['system']['uuid'] required.",
+                    log_message=True)
+            q = db().query(Node)
+            n_by_mac = objects.Node.get_by_mac_or_uid(mac=d.get('mac'),
+                                                      query=q)
+            n_by_id = objects.Node.get_by_mac_or_uid(node_uid=d.get('id'),
+                                                     query=q)
+            n_by_s_uuid = objects.Node.get_by_system_uuid(d, query=q)
+            instance = n_by_mac or n_by_id or n_by_s_uuid
+            ids = set([n.id for n in [n_by_mac, n_by_id, n_by_s_uuid] if n])
+            if not instance:
+                raise errors.InvalidData(
+                    'Cannot find node to update - no nodes found with mac: %s '
+                    'or id: %s or system_uuid: %s' %
+                    (d.get('mac'), d.get('id'), system_uuid))
+            if len(ids) > 1:
+                raise errors.InvalidData(
+                    'Cannot choose a node to update - ambiguous data provided.'
+                    ' Mac: %s, id: %s, system uuid: %s belong to more than one'
+                    'node.' % (d.get('mac'), d.get('id'), system_uuid))
 
         if d.get("hostname") is not None:
             cls.validate_hostname(d["hostname"], instance)

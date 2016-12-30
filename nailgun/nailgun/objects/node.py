2ea67248647a34f3098796639d4eb0265c0fa10f
Anastasiya <atolochkova@mirantis.com>
2016-12-30 11:31:21 +0400

    Limit the minimal RAM amount for OVS+DPDK to 1024MB
    * min value was set in openstack.yaml
    * appropriate validator was added
    * tests for validator were changed
    * migration node and release attributes was added
    * tests for migration were added
    
    Change-Id: Iec723c7f02e874bf8567665a30af30c61aff7f34
    Closes-Bug: #1653081

--- nailgun/nailgun/objects/node.py
+++ nailgun/nailgun/objects/node.py
@@ -1563,7 +1563,8 @@ class NodeAttributes(object):
     @classmethod
     def is_dpdk_hugepages_enabled(cls, node, attributes=None):
         hugepages = cls._safe_get_hugepages(node, attributes=attributes)
-        return 'dpdk' in hugepages and bool(hugepages['dpdk']['value'])
+        return ('dpdk' in hugepages and bool(hugepages['dpdk']['value']) and
+                Node.dpdk_enabled(node))
 
     @classmethod
     def dpdk_hugepages_attrs(cls, node):
@@ -1577,7 +1578,8 @@ class NodeAttributes(object):
         """
         hugepages = cls._safe_get_hugepages(node)
 
-        if 'dpdk' not in hugepages or not hugepages['dpdk']['value']:
+        if Node.dpdk_enabled(node) and 'dpdk' not in hugepages \
+                or not hugepages['dpdk']['value']:
             return {}
 
         dpdk_memory = hugepages['dpdk']['value']

1878679dcf4ed42e83af92e6bdddf986da5b8e19
Mikhail <mzhnichkov@mirantis.com>
2017-01-16 23:40:10 +0400

    Serialize mtu for dpdk interface with 'i40e' driver
    On NIC: XL710 with driver 'i40e'  MTU does not take into
    account 4 bytes VLAN tag, so we should increase it manually
    
    DocImpact
    Change-Id: I3d95db9ec6fae4d8cd397c429d785dbdf1502b21
    Partial-Bug: #1587310
    Co-Authored-By: Fedor Zhadaev <fzhadaev@mirantis.com>

--- nailgun/nailgun/extensions/network_manager/managers/neutron.py
+++ nailgun/nailgun/extensions/network_manager/managers/neutron.py
@@ -32,6 +32,7 @@ from nailgun.extensions.network_manager.manager import AssignIPs70Mixin
 from nailgun.extensions.network_manager.manager import AssignIPsLegacyMixin
 from nailgun.extensions.network_manager.manager import NetworkManager
 from nailgun import objects
+from nailgun import utils
 
 from nailgun.extensions.network_manager.serializers.neutron_serializers \
     import NeutronNetworkTemplateSerializer70
@@ -553,4 +554,28 @@ class NeutronManager70(
 
 
 class NeutronManager80(AllocateVIPs80Mixin, NeutronManager70):
-    pass
+    @classmethod
+    def get_iface_properties(cls, iface):
+        result = super(NeutronManager80, cls).get_iface_properties(iface)
+
+        if (iface.type == consts.NETWORK_INTERFACE_TYPES.ether and
+                iface.driver == 'i40e' and
+                objects.NIC.dpdk_enabled(iface)):
+            # On NIC with i40e driver MTU should be increased manually
+            # because driver automatically sets value 4 bytes less
+            # LP 1587310
+            mtu = utils.get_in(result, 'mtu', 'value', 'value')
+            if mtu:
+                result.update({
+                    'mtu': {
+                        'value': {
+                            'value': mtu + consts.SIZE_OF_VLAN_TAG}
+                    }})
+            else:
+                result.update({
+                    'mtu': {
+                        'value': {
+                            'value':
+                                consts.DEFAULT_MTU + consts.SIZE_OF_VLAN_TAG}
+                    }})
+        return result

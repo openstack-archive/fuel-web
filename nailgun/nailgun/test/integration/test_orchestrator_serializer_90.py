bdf64299e0364a5f8a3d1980eb06ba0deafac1c4
Anastasia Balobashina <atolochkova@mirantis.com>
2017-01-17 17:37:40 +0000

    Revert "Limit the minimal RAM amount for OVS+DPDK to 1024MB"
    This reverts commit d70d0ada26ee9a43f87766bd8e11be393186ef0f.
    
    Change-Id: Ia34ad8b7ab4b6747bb259e1a9eccd862b7c2fe3b

--- nailgun/nailgun/test/integration/test_orchestrator_serializer_90.py
+++ nailgun/nailgun/test/integration/test_orchestrator_serializer_90.py
@@ -427,8 +427,9 @@ class TestDeploymentAttributesSerialization90(
             numa_nodes.append({
                 'id': i,
                 'cpus': [i],
-                'memory': 2 * 1024 ** 3
+                'memory': 1024 ** 3
             })
+
         meta = {
             'numa_topology': {
                 'supported_hugepages': [2048],
@@ -439,14 +440,10 @@ class TestDeploymentAttributesSerialization90(
             cluster_id=self.cluster_db.id,
             roles=['compute'],
             meta=meta)
-
-        node.interfaces[0].attributes.get('dpdk', {}).get(
-            'enabled', {})['value'] = True
-
         node.attributes.update({
             'hugepages': {
                 'dpdk': {
-                    'value': 1024},
+                    'value': 128},
                 'nova': {
                     'value': {'2048': 1}}}}
         )
@@ -455,7 +452,7 @@ class TestDeploymentAttributesSerialization90(
         serialized_node = serialized_for_astute['nodes'][0]
 
         self.assertEquals(
-            [1024, 1024, 1024],
+            [128, 128, 128],
             serialized_node['dpdk']['ovs_socket_mem'])
         self.assertTrue(serialized_node['nova']['enable_hugepages'])
 

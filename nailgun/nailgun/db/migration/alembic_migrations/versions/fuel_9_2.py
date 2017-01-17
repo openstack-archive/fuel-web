16bbc8c0866cd31586d50c6761750f76cfd787af
Anastasia Balobashina <atolochkova@mirantis.com>
2017-01-17 18:33:10 +0000

    Revert "Fixes for commit 6e4ef67269b849edb20e7ea9e73685a5cee0e6fc"
    This reverts commit 7f53502d3f09646a29d622cf0f826ae653db7f40.
    
    Change-Id: Id63fa55f076bdd7dcd27593217f86d576058be89

--- nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_9_2.py
+++ nailgun/nailgun/db/migration/alembic_migrations/versions/fuel_9_2.py
@@ -242,14 +242,10 @@ DEFAULT_RELEASE_BOND_ATTRIBUTES = {
     }
 }
 
-# minimal RAM amount for OVS+DPDK in MB
 MIN_DPDK_HUGEPAGES_MEMORY = 1024
 
 # version of Fuel when security group switch was added
 FUEL_SECURITY_GROUPS_VERSION = '9.0'
-
-# version of Fuel when DPDK hugepages was introduced
-FUEL_DPDK_HUGEPAGES_VERSION = '9.0'
 
 
 def update_vmware_attributes_metadata(upgrade):
@@ -406,16 +402,14 @@ def upgrade_cluster_attributes(connection):
 
 def upgrade_release_node_attributes(connection):
     select_query = sa.sql.text(
-        'SELECT id, node_attributes, version FROM releases '
+        'SELECT id, node_attributes FROM releases '
         'WHERE node_attributes IS NOT NULL')
 
     update_query = sa.sql.text(
         'UPDATE releases SET node_attributes = :node_attributes '
         'WHERE id = :release_id')
 
-    for release_id, node_attrs, version in connection.execute(select_query):
-        if not is_feature_supported(version, FUEL_DPDK_HUGEPAGES_VERSION):
-            continue
+    for release_id, node_attrs in connection.execute(select_query):
         node_attrs = jsonutils.loads(node_attrs)
         dpdk = node_attrs.setdefault('hugepages', {}).setdefault('dpdk', {})
         dpdk['min'] = MIN_DPDK_HUGEPAGES_MEMORY
@@ -428,18 +422,14 @@ def upgrade_release_node_attributes(connection):
 
 def upgrade_node_attributes(connection):
     select_query = sa.sql.text(
-        'SELECT nodes.id, attributes, version FROM nodes INNER JOIN clusters '
-        'ON clusters.id = nodes.cluster_id INNER JOIN releases '
-        'ON releases.id = clusters.release_id '
-        'WHERE nodes.attributes IS NOT NULL')
+        'SELECT id, attributes FROM nodes '
+        'WHERE attributes IS NOT NULL')
 
     update_query = sa.sql.text(
         'UPDATE nodes SET attributes = :attributes '
         'WHERE id = :node_id')
 
-    for node_id, attrs, version in connection.execute(select_query):
-        if not is_feature_supported(version, FUEL_DPDK_HUGEPAGES_VERSION):
-            continue
+    for node_id, attrs in connection.execute(select_query):
         attrs = jsonutils.loads(attrs)
         dpdk = attrs.setdefault('hugepages', {}).setdefault('dpdk', {})
         dpdk['min'] = MIN_DPDK_HUGEPAGES_MEMORY

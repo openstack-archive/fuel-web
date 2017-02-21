076e539472fe66de0a7925c0762ed70b2efa1f7f
Vladimir Kozhukalov <kozhukalov@gmail.com>
2017-02-21 13:28:07 +0300

    Add dhcp params to network_metadata
    Provision as a graph feature assumes we run puppet task
    on the master to configure dhcpd, bind and tftp.
    This task among other things binds node MACs to IP and
    we need to pass some dhcp params as a part of network_metadata.
    
    Change-Id: I221d100a092fd52802c8833ac162a67deeef1906
    Implements: blueprint get-rid-cobbler-dnsmasq

--- nailgun/nailgun/test/integration/test_orchestrator_serializer_70.py
+++ nailgun/nailgun/test/integration/test_orchestrator_serializer_70.py
@@ -471,7 +471,7 @@ class TestDeploymentAttributesSerialization70(
                 self.assertItemsEqual(
                     v,
                     ['uid', 'fqdn', 'name', 'user_node_name',
-                     'swift_zone', 'node_roles', 'network_roles']
+                     'swift_zone', 'node_roles', 'network_roles', 'dhcp']
                 )
                 node = objects.Node.get_by_uid(v['uid'])
                 ip_by_net = neutron_serializer.get_network_to_ip_mapping(node)
@@ -505,6 +505,17 @@ class TestDeploymentAttributesSerialization70(
                         [ip_by_net['private']] * len(self.private))
 
                 self.assertEqual(v['network_roles'], dict(network_roles))
+                net_manager = objects.Cluster.get_network_manager(node.cluster)
+                admin_ip = net_manager.get_admin_ip_for_node(node)
+                dhcp_params = {
+                    'name': objects.Node.get_node_fqdn(node),
+                    'ip_address': admin_ip,
+                    'mac': [],
+                }
+                for interface in node.nic_interfaces:
+                    params['mac'].append(interface.mac)
+
+                self.assertEqual(v['dhcp'], dict(dhcp_params))
             self.check_vips_serialized(node_data)
 
     def test_generate_vmware_attributes_data(self):
@@ -619,7 +630,7 @@ class TestDeploymentSerializationForNovaNetwork70(
                 self.assertItemsEqual(
                     node_attrs,
                     ['uid', 'fqdn', 'name', 'user_node_name',
-                     'swift_zone', 'node_roles', 'network_roles']
+                     'swift_zone', 'node_roles', 'network_roles', 'dhcp']
                 )
                 self.assertEqual(objects.Node.get_slave_name(node), node_name)
                 self.assertEqual(node_attrs['uid'], node.uid)
@@ -668,6 +679,19 @@ class TestDeploymentSerializationForNovaNetwork70(
                     node_attrs['network_roles'],
                     network_roles
                 )
+
+                net_manager = objects.Cluster.get_network_manager(node.cluster)
+                admin_ip = net_manager.get_admin_ip_for_node(node)
+                dhcp_params = {
+                    'name': objects.Node.get_node_fqdn(node),
+                    'ip_address': admin_ip,
+                    'mac': [],
+                }
+                for interface in node.nic_interfaces:
+                    params['mac'].append(interface.mac)
+
+                self.assertEqual(v['dhcp'], dict(dhcp_params))
+
             self.check_vips_serialized(node_data)
 
     def test_generate_vmware_attributes_data(self):

e61cc055ab4a274344659a88df941b75353a53d0
Andreas Jaeger <aj@suse.com>
2019-12-18 09:52:23 +0100

    Retire repository
    Fuel (from openstack namespace) and fuel-ccp (in x namespace)
    repositories are unused and ready to retire.
    
    This change removes all content from the repository and adds the usual
    README file to point out that the repository is retired following the
    process from
    https://docs.openstack.org/infra/manual/drivers.html#retiring-a-project
    
    See also
    http://lists.openstack.org/pipermail/openstack-discuss/2019-December/011647.html
    
    Depends-On: https://review.opendev.org/699362
    Change-Id: I10917e42829b459c41ca46514faa72fc46abf7be

--- nailgun/nailgun/plugins/loaders/__init__.py
+++
@@ -1,6 +0,0 @@
-from loader_base import PluginLoaderBase
-from loader_v1 import PluginLoaderV1
-from loader_v3 import PluginLoaderV3
-from loader_v4 import PluginLoaderV4
-from loader_v5 import PluginLoaderV5
-from loader_v6 import PluginLoaderV6
